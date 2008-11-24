54f47331fded8be3b909e2f0f0baad3bd9e90173
Jukka Zitting <jukka@apache.org>
2008-11-24 11:25:24 +0000

    PDFBOX-380: Limited support for SC and SCN operator
    Patch by Andreas Lehmkühler.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pdfbox/trunk@720167 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/pdfbox/util/operator/SetNonStrokingICCBasedColor.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pdfbox.util.operator;
+
+import java.util.List;
+
+import org.apache.pdfbox.cos.COSNumber;
+import org.apache.pdfbox.pdmodel.graphics.color.*;
+import org.apache.pdfbox.util.PDFOperator;
+import org.apache.pdfbox.pdfviewer.PageDrawer;
+
+import java.io.IOException;
+
+/**
+ * 
+ * @author <a href="mailto:andreas@lehmi.de">Andreas Lehmkühler</a>
+ * @version $Revision: 1.0 $
+ */
+public class SetNonStrokingICCBasedColor extends OperatorProcessor 
+{
+    /**
+     * scn Set color space for non stroking operations.
+     * @param operator The operator that is being executed.
+     * @param arguments List
+     * @throws IOException If an error occurs while processing the font.
+     */
+    public void process(PDFOperator operator, List arguments) throws IOException
+    {
+        PDColorSpaceInstance colorInstance = context.getGraphicsState().getNonStrokingColorSpace();
+        PDColorSpace cs = colorInstance.getColorSpace();
+        int numberOfComponents = cs.getNumberOfComponents();
+        float[] values = new float[numberOfComponents];
+        for( int i=0; i<numberOfComponents; i++ )
+        {
+            values[i] = ((COSNumber)arguments.get( i )).floatValue();
+        }
+        colorInstance.setColorSpaceValue( values );
+        
+        if (context instanceof PageDrawer){
+            PageDrawer drawer = (PageDrawer)context;
+            drawer.ColorChanged (true);
+        }
+    }
+}
