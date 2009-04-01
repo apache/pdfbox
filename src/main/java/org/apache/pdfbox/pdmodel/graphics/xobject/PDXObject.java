015a4c73299c934d19f06d67dee52112f61e1d3b
Daniel Wilson <danielwilson@apache.org>
2009-04-01 02:22:11 +0000

    Fix an NPE in PDPixelMap. Make PDXObject inherit LoggingObject so it can participate in logging. Add a source test file. Submit modified control PNG files for changes that are changes at the binary level, but are not visible.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pdfbox/trunk@760769 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/pdfbox/pdmodel/graphics/xobject/PDXObject.java
+++ src/main/java/org/apache/pdfbox/pdmodel/graphics/xobject/PDXObject.java
@@ -28,6 +28,8 @@ import org.apache.pdfbox.pdmodel.common.COSObjectable;
 import org.apache.pdfbox.pdmodel.common.PDMetadata;
 import org.apache.pdfbox.pdmodel.common.PDStream;
 
+import org.apache.pdfbox.exceptions.LoggingObject;
+
 /**
  * The base class for all XObjects in the PDF document.
  *
@@ -36,7 +38,7 @@ import org.apache.pdfbox.pdmodel.common.PDStream;
  * @author Marcel Kammer
  * @version $Revision: 1.14 $
  */
-public abstract class PDXObject implements COSObjectable
+public abstract class PDXObject extends LoggingObject implements COSObjectable
 {
     private PDStream xobject;
 
