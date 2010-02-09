bbfdfb7104a4468bfad89f3adb38b8a78c6efed9
Jukka Zitting <jukka@apache.org>
2010-02-09 00:14:28 +0000

    PDFBOX-558: PDPageDestination getPageNumber only returns page number when underlying COSArray has a page number
    Add a PDPageDestionation.findPageNumber() to complement the existing getPageNumber() method. Based on the patch contributed by Wulf Berschin.
    
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@907852 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/pdfbox/pdmodel/interactive/documentnavigation/destination/PDPageDestination.java
+++ src/main/java/org/apache/pdfbox/pdmodel/interactive/documentnavigation/destination/PDPageDestination.java
@@ -16,12 +16,16 @@
  */
 package org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.pdfbox.cos.COSArray;
 import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSNumber;
 
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageNode;
 
 /**
  * This represents a destination to a page, see subclasses for specific parameters.
@@ -104,6 +108,39 @@ public abstract class PDPageDestination extends PDDestination
             if( page instanceof COSNumber )
             {
                 retval = ((COSNumber)page).intValue();
+            }
+        }
+        return retval;
+    }
+
+    /**
+     * Returns the page number for this destination, regardless of whether
+     * this is a page number or a reference to a page.
+     *
+     * @since Apache PDFBox 1.0.0
+     * @see PDOutlineItem
+     * @return page number, or -1 if the destination type is unknown
+     */
+    public int findPageNumber() {
+        int retval = -1;
+        if( array.size() > 0 )
+        {
+            COSBase page = array.getObject( 0 );
+            if( page instanceof COSNumber )
+            {
+                retval = ((COSNumber)page).intValue();
+            }
+            else if (page instanceof COSDictionary)
+            {
+                COSBase parent = page;
+                while (((COSDictionary) parent).getDictionaryObject("Parent", "P") != null) {
+                    parent = ((COSDictionary) parent).getDictionaryObject( "Parent", "P" );
+                }
+                // now parent is the pages node
+                PDPageNode pages = new PDPageNode((COSDictionary) parent);
+                List<PDPage> allPages = new ArrayList<PDPage>();
+                pages.getAllKids(allPages);
+                retval = allPages.indexOf(new PDPage((COSDictionary) page)) + 1;
             }
         }
         return retval;
