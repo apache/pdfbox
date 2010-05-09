99028e3f240b7febc7c5ac056298f4c9a7ecef36
Andreas Lehmkühler <lehmi@apache.org>
2010-05-09 17:44:32 +0000

    PDFBOX-406: removed the unused implementation of the cloneable interface. Patch by Peter Lenahan (Peter_Lenahan at ibi dot com)
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@942569 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/PDMatrix.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/PDMatrix.java
@@ -27,7 +27,7 @@ import org.apache.pdfbox.cos.COSNumber;
  * @author <a href="mailto:ben@benlitchfield.com">Ben Litchfield</a>
  * @version $Revision: 1.3 $
  */
-public class PDMatrix implements Cloneable, COSObjectable
+public class PDMatrix implements COSObjectable
 {
     private COSArray matrix;
 
