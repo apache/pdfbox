7f601ee87811862a04e52f3e8a53d0e75abc02d2
Andreas Lehmkuhler <lehmi@apache.org>
2013-11-24 14:30:38 +0000

    PDFBOX-1782: added getter/setter for the MaxLen value of a textfield
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1544977 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDTextbox.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDTextbox.java
@@ -17,12 +17,13 @@
 package org.apache.pdfbox.pdmodel.interactive.form;
 
 import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.cos.COSName;
 
 /**
  * A class for handling the PDF field as a textbox.
  *
  * @author sug
- * @version $Revision: 1.9 $
+ * 
  */
 public class PDTextbox extends PDVariableText
 {
@@ -47,4 +48,25 @@ public class PDTextbox extends PDVariableText
     {
         super( theAcroForm, field);
     }
+    
+    /**
+     * Returns the maximum number of characters of the text field.
+     * 
+     * @return the maximum number of characters, returns -1 if the value isn't present
+     */
+    public int getMaxLen()
+    {
+        return getDictionary().getInt(COSName.MAX_LEN);
+    }
+
+    /**
+     * Sets the maximum number of characters of the text field.
+     * 
+     * @param maxLen the maximum number of characters
+     */
+    public void setMaxLen(int maxLen)
+    {
+        getDictionary().setInt(COSName.MAX_LEN, maxLen);
+    }
+
 }
