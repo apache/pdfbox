a8bb7cf3a829eeb08c9710204f1792c8f8322d88
Andreas Lehmkühler <lehmi@apache.org>
2013-11-24 13:55:24 +0000

    PDFBOX-1213: added some style imformation to the PDF2HTML converter as proposed by Axel Dörfler
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/1.8@1544975 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/util/PDFText2HTML.java
+++ pdfbox/src/main/java/org/apache/pdfbox/util/PDFText2HTML.java
@@ -17,25 +17,28 @@
 package org.apache.pdfbox.util;
 
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.font.PDFontDescriptor;
 
 /**
  * Wrap stripped text in simple HTML, trying to form HTML paragraphs. Paragraphs
  * broken by pages, columns, or figures are not mended.
  *
- *
  * @author jjb - http://www.johnjbarton.com
- * @version $Revision: 1.3 $
+ * 
  */
 public class PDFText2HTML extends PDFTextStripper
 {
     private static final int INITIAL_PDF_TO_HTML_BYTES = 8192;
 
     private boolean onFirstPage = true;
+    private FontState fontState = new FontState();
 
     /**
      * Constructor.
@@ -180,6 +183,19 @@ public class PDFText2HTML extends PDFTextStripper
     }
 
     /**
+     * Write a string to the output stream, maintain font state, and escape some HTML characters.
+     * The font state is only preserved per word.
+     *
+     * @param text The text to write to the stream.
+     * @param textPositions the corresponding text positions
+     * @throws IOException If there is an error writing to the stream.
+     */
+    protected void writeString(String text, List<TextPosition> textPositions) throws IOException
+    {
+        super.writeString(fontState.push(text, textPositions));
+    }
+
+    /**
      * Write a string to the output stream and escape some HTML characters.
      *
      * @param chars String to be written to the stream
@@ -192,44 +208,215 @@ public class PDFText2HTML extends PDFTextStripper
     }
 
     /**
+     * Writes the paragraph end "</p>" to the output. Furthermore, it will also clear the font state.
+     * 
+     * {@inheritDoc}
+     */
+    @Override
+    protected void writeParagraphEnd() throws IOException
+    {
+        writeString(fontState.clear());
+        super.writeParagraphEnd();
+    }
+
+    /**
      * Escape some HTML characters.
      *
      * @param chars String to be escaped
      * @return returns escaped String.
      */
-    private String escape(String chars)
+    private static String escape(String chars)
     {
         StringBuilder builder = new StringBuilder(chars.length());
         for (int i = 0; i < chars.length(); i++)
         {
-            char c = chars.charAt(i);
-            // write non-ASCII as named entities
-            if ((c < 32) || (c > 126))
-            {
-                int charAsInt = c;
-                builder.append("&#").append(charAsInt).append(";");
-            }
-            else
-            {
-                switch (c)
-                {
-                case 34:
-                    builder.append("&quot;");
-                    break;
-                case 38:
-                    builder.append("&amp;");
-                    break;
-                case 60:
-                    builder.append("&lt;");
-                    break;
-                case 62:
-                    builder.append("&gt;");
-                    break;
-                default:
-                    builder.append(String.valueOf(c));
-                }
-            }
+            appendEscaped(builder, chars.charAt(i));
         }
         return builder.toString();
+    }
+
+    private static void appendEscaped(StringBuilder builder, char character)
+    {
+        // write non-ASCII as named entities
+        if ((character < 32) || (character > 126))
+        {
+            int charAsInt = character;
+            builder.append("&#").append(charAsInt).append(";");
+        }
+        else
+        {
+            switch (character)
+            {
+            case 34:
+                builder.append("&quot;");
+                break;
+            case 38:
+                builder.append("&amp;");
+                break;
+            case 60:
+                builder.append("&lt;");
+                break;
+            case 62:
+                builder.append("&gt;");
+                break;
+            default:
+                builder.append(String.valueOf(character));
+            }
+        }
+    }
+
+    /**
+     * A helper class to maintain the current font state. It's public methods will emit opening and
+     * closing tags as needed, and in the correct order.
+     *
+     * @author Axel Dörfler
+     */
+    private static class FontState
+    {
+        protected List<String> stateList = new ArrayList<String>();
+        protected Set<String> stateSet = new HashSet<String>();
+
+        /**
+         * Pushes new {@link TextPosition TextPositions} into the font state. The state is only
+         * preserved correctly for each letter if the number of letters in <code>text</code> matches
+         * the number of {@link TextPosition} objects. Otherwise, it's done once for the complete
+         * array (just by looking at its first entry).
+         *
+         * @return A string that contains the text including tag changes caused by its font state.
+         */
+        public String push(String text, List<TextPosition> textPositions)
+        {
+            StringBuilder buffer = new StringBuilder();
+
+            if (text.length() == textPositions.size())
+            {
+                // There is a 1:1 mapping, and we can use the TextPositions directly
+                for (int i = 0; i < text.length(); i++)
+                {
+                    push(buffer, text.charAt(i), textPositions.get(i));
+                }
+            }
+            else if (!text.isEmpty())
+            {
+                // The normalized text does not match the number of TextPositions, so we'll just
+                // have a look at its first entry.
+                // TODO change PDFTextStripper.normalize() such that it maintains the 1:1 relation
+                if (textPositions.isEmpty())
+                {
+                    return text;
+                }
+                push(buffer, text.charAt(0), textPositions.get(0));
+                buffer.append(escape(text.substring(1)));
+            }
+            return buffer.toString();
+        }
+
+        /**
+         * Closes all open states.
+         * @return A string that contains the closing tags of all currently open states.
+         */
+        public String clear()
+        {
+            StringBuilder buffer = new StringBuilder();
+            closeUntil(buffer, null);
+            stateList.clear();
+            stateSet.clear();
+            return buffer.toString();
+        }
+
+        protected String push(StringBuilder buffer, char character, TextPosition textPosition)
+        {
+            boolean bold = false;
+            boolean italics = false;
+
+            PDFontDescriptor descriptor = textPosition.getFont().getFontDescriptor();
+            if (descriptor != null)
+            {
+                bold = isBold(descriptor);
+                italics = isItalic(descriptor);
+            }
+            
+            buffer.append(bold ? open("b") : close("b"));
+            buffer.append(italics ? open("i") : close("i"));
+            appendEscaped(buffer, character);
+
+            return buffer.toString();
+        }
+
+        private String open(String tag)
+        {
+            if (stateSet.contains(tag))
+            {
+                return "";
+            }
+            stateList.add(tag);
+            stateSet.add(tag);
+
+            return openTag(tag);
+        }
+
+        private String close(String tag)
+        {
+            if (!stateSet.contains(tag))
+            {
+                return "";
+            }
+            // Close all tags until (but including) the one we should close
+            StringBuilder tagsBuilder = new StringBuilder();
+            int index = closeUntil(tagsBuilder, tag);
+
+            // Remove from state
+            stateList.remove(index);
+            stateSet.remove(tag);
+
+            // Now open the states that were closed but should remain open again
+            for (; index < stateList.size(); index++)
+            {
+                tagsBuilder.append(openTag(stateList.get(index)));
+            }
+            return tagsBuilder.toString();
+        }
+
+        private int closeUntil(StringBuilder tagsBuilder, String endTag)
+        {
+            for (int i = stateList.size(); i-- > 0;)
+            {
+                String tag = stateList.get(i);
+                tagsBuilder.append(closeTag(tag));
+                if (endTag != null && tag.equals(endTag))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private String openTag(String tag)
+        {
+            return "<" + tag + ">";
+        }
+
+        private String closeTag(String tag)
+        {
+            return "</" + tag + ">";
+        }
+
+        private boolean isBold(PDFontDescriptor descriptor)
+        {
+            if (descriptor.isForceBold())
+            {
+                return true;
+            }
+            return descriptor.getFontName().contains("Bold");
+        }
+
+        private boolean isItalic(PDFontDescriptor descriptor)
+        {
+            if (descriptor.isItalic())
+            {
+                return true;
+            }
+            return descriptor.getFontName().contains("Italic");
+        }
     }
 }
