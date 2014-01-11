5c39d6126922a219187a5e4e4c71c3ffa98a3c9b
Andreas Lehmkühler <lehmi@apache.org>
2014-01-11 13:42:12 +0000

    PDFBOX-161: avoid EmptyStackException if the number of q/Q-operators isn't well balanced
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1557389 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/util/operator/GRestore.java
+++ pdfbox/src/main/java/org/apache/pdfbox/util/operator/GRestore.java
@@ -18,24 +18,40 @@ package org.apache.pdfbox.util.operator;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.pdmodel.graphics.PDGraphicsState;
 import org.apache.pdfbox.util.PDFOperator;
 
 /**
- *
+ * Process the Q operator.
+ * 
  * @author Huault : huault@free.fr
- * @version $Revision: 1.4 $
+ * 
  */
 public class GRestore extends OperatorProcessor
 {
+	
     /**
-     * process : Q : Restore graphics state.
-     * @param operator The operator that is being executed.
-     * @param arguments List
+     * Log instance.
+     */
+    private static final Log LOG = LogFactory.getLog(GRestore.class);
+
+    /**
+     * {@inheritDoc}
      */
     public void process(PDFOperator operator, List<COSBase> arguments)
     {
-        context.setGraphicsState( (PDGraphicsState)context.getGraphicsStack().pop() );
+    	if (context.getGraphicsStack().size() > 0)
+    	{
+    		context.setGraphicsState( (PDGraphicsState)context.getGraphicsStack().pop() );
+    	}
+    	else
+    	{
+    		// this shouldn't happen but it does, see PDFBOX-161
+    		// TODO make this self healing mechanism optional for preflight??
+    		LOG.debug("GRestore: no graphics state left to be restored.");
+    	}
     }
 }
