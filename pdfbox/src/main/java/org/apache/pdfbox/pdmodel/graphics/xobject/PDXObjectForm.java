0ba8d7d8f81de6aee3735ebbc921161db9c918c9
Andreas Lehmkühler <lehmi@apache.org>
2014-01-27 18:39:41 +0000

    PDFBOX-1865: take the length of the stream into account when comparing xobjects
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1561782 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/xobject/PDXObjectForm.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/xobject/PDXObjectForm.java
@@ -126,8 +126,18 @@ public class PDXObjectForm extends PDXObject
             	Map<String, PDXObject> xobjects = retval.getXObjects();
             	if (xobjects != null && xobjects.containsKey(name))
             	{
-            		retval.removeXObject(name);
-            		LOG.debug("Removed XObjectForm "+name+" to avoid a recursion");
+            	    PDXObject xobject = xobjects.get(name);
+            	    if (xobject instanceof PDXObjectForm)
+            	    {
+            	        int length1 = getCOSStream().getInt(COSName.LENGTH);
+                        int length2 = xobject.getCOSStream().getInt(COSName.LENGTH);
+                        // seems to be the same object
+            	        if (length1 == length2)
+            	        {
+            	            retval.removeXObject(name);
+                            LOG.debug("Removed XObjectForm "+name+" to avoid a recursion");
+            	        }
+            	    }
             	}
             }
         }
