6237f8de9f6eca5c8f749bfb33d15ad49faf9df5
Andreas Lehmkuhler <lehmi@apache.org>
2014-02-01 15:23:24 +0000

    PDFBOX-1869: added ShadingType1 support as proposed by Tilman Hausherr
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1563429 13f79535-47bb-0310-9956-ffa450edef68

---
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/shading/Type1ShadingContext.java
@@ -0,0 +1,239 @@
+/*
+ * Copyright 2014 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pdfbox.pdmodel.graphics.shading;
+
+import java.awt.PaintContext;
+import java.awt.Transparency;
+import java.awt.color.ColorSpace;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.NoninvertibleTransformException;
+import java.awt.image.ColorModel;
+import java.awt.image.ComponentColorModel;
+import java.awt.image.DataBuffer;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
+import java.io.IOException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.pdfbox.cos.COSArray;
+import org.apache.pdfbox.pdmodel.common.function.PDFunction;
+import org.apache.pdfbox.pdmodel.graphics.color.PDColorSpace;
+import org.apache.pdfbox.pdmodel.graphics.color.PDDeviceN;
+import org.apache.pdfbox.pdmodel.graphics.color.PDDeviceRGB;
+import org.apache.pdfbox.pdmodel.graphics.color.PDSeparation;
+import org.apache.pdfbox.util.Matrix;
+
+/**
+ * This represents the Paint of a type 1 (Function based) shading context.
+ *
+ * @author lehmi
+ * @author Tilman Hausherr <tilman@snafu.de>
+ */
+class Type1ShadingContext implements PaintContext
+{
+    private static final Log LOG = LogFactory.getLog(Type1ShadingContext.class);
+
+    private ColorModel outputColorModel;
+    private ColorSpace shadingColorSpace;
+    private PDFunction shadingTinttransform;
+    private PDShadingType1 shadingType;
+    private AffineTransform rat;
+    private float[] domain;
+    private Matrix matrix;
+    private float[] background;
+
+    /**
+     * Constructor creates an instance to be used for fill operations.
+     *
+     * @param shadingType1 the shading type to be used
+     * @param colorModelValue the color model to be used
+     * @param xform transformation for user to device space
+     * @param ctm current transformation matrix
+     * @param pageHeight height of the current page
+     *
+     */
+    public Type1ShadingContext(PDShadingType1 shadingType1, ColorModel colorModelValue, 
+            AffineTransform xform, Matrix currentTransformationMatrix, int pageHeight)
+    {
+        shadingType = shadingType1;
+
+        // colorSpace 
+        try
+        {
+            PDColorSpace cs = shadingType.getColorSpace();
+            if (!(cs instanceof PDDeviceRGB))
+            {
+                // we have to create an instance of the shading colorspace if it isn't RGB
+                shadingColorSpace = cs.getJavaColorSpace();
+                if (cs instanceof PDDeviceN)
+                {
+                    shadingTinttransform = ((PDDeviceN) cs).getTintTransform();
+                }
+                else if (cs instanceof PDSeparation)
+                {
+                    shadingTinttransform = ((PDSeparation) cs).getTintTransform();
+                }
+            }
+        }
+        catch (IOException exception)
+        {
+            LOG.error("error while creating colorSpace", exception);
+        }
+        // create the output colormodel using RGB+alpha as colorspace
+        ColorSpace outputCS = ColorSpace.getInstance(ColorSpace.CS_sRGB);
+        outputColorModel = new ComponentColorModel(outputCS, true, false, Transparency.TRANSLUCENT,
+                DataBuffer.TYPE_BYTE);
+        
+        // spec p.308
+        // (Optional) An array of four numbers [ xmin xmax ymin ymax ] 
+        // specifying the rectangular domain of coordinates over which the 
+        // color function(s) are defined. Default value: [ 0.0 1.0 0.0 1.0 ].
+        if (shadingType.getDomain() != null)
+        {
+            domain = shadingType.getDomain().toFloatArray();
+        }
+        else
+        {
+            domain = new float[]
+            {
+                0, 1, 0, 1
+            };
+        }
+
+        matrix = shadingType.getMatrix();
+        if (matrix == null)
+        {
+            matrix = new Matrix();
+        }
+
+        try
+        {
+            // get inverse transform to be independent of current user / device space 
+            // when handling actual pixels in getRaster()
+            rat = matrix.createAffineTransform().createInverse();
+            rat.concatenate(xform.createInverse());
+        }
+        catch (NoninvertibleTransformException ex)
+        {
+            LOG.error(ex,ex);
+        }
+
+        // get background values if available
+        COSArray bg = shadingType1.getBackground();
+        if (bg != null)
+        {
+            background = bg.toFloatArray();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void dispose()
+    {
+        outputColorModel = null;
+        shadingColorSpace = null;
+        shadingTinttransform = null;
+        shadingType = null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ColorModel getColorModel()
+    {
+        return outputColorModel;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Raster getRaster(int x, int y, int w, int h)
+    {
+        // create writable raster
+        
+        WritableRaster raster = getColorModel().createCompatibleWritableRaster(w, h);
+        int[] data = new int[w * h * 4];
+        for (int j = 0; j < h; j++)
+        {
+            for (int i = 0; i < w; i++)
+            {
+                int index = (j * w + i) * 4;
+                boolean useBackground = false;
+                float[] values = new float[]
+                {
+                    x + i, y + j
+                };
+                rat.transform(values, 0, values, 0, 1);
+                if (values[0] < domain[0] || values[0] > domain[1] || values[1] < domain[2] || values[1] > domain[3])
+                {
+                    if (background != null)
+                    {
+                        useBackground = true;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                try
+                {
+                    if (useBackground)
+                    {
+                        values = background;
+                    }
+                    else
+                    {
+                        values = shadingType.evalFunction(values);
+                    }
+                    // convert color values from shading colorspace to RGB 
+                    if (shadingColorSpace != null)
+                    {
+                        if (shadingTinttransform != null)
+                        {
+                            values = shadingTinttransform.eval(values);
+                        }
+                        values = shadingColorSpace.toRGB(values);
+                    }
+                }
+                catch (IOException exception)
+                {
+                    LOG.error("error while processing a function", exception);
+                }
+                data[index] = (int) (values[0] * 255);
+                data[index + 1] = (int) (values[1] * 255);
+                data[index + 2] = (int) (values[2] * 255);
+                data[index + 3] = 255;
+            }
+        }
+        raster.setPixels(0, 0, w, h, data);
+        return raster;
+    }
+
+    public float[] getDomain()
+    {
+        return domain;
+    }
+
+    public PDFunction getShadingTintTransform()
+    {
+        return shadingTinttransform;
+    }
+}
