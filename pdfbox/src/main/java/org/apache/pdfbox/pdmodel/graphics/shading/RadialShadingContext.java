e3a2aa64b4a840ff9d28fd2bc904df922f1b694d
Andreas Lehmkühler <lehmi@apache.org>
2014-02-02 15:20:53 +0000

    PDFBOX-615: take the shading matrix into account based on a proposal by Luis Bernado, add background support
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/1.8@1563633 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/shading/RadialShadingContext.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/shading/RadialShadingContext.java
@@ -17,9 +17,12 @@
 package org.apache.pdfbox.pdmodel.graphics.shading;
 
 import java.awt.PaintContext;
+import java.awt.Transparency;
 import java.awt.color.ColorSpace;
 import java.awt.geom.AffineTransform;
 import java.awt.image.ColorModel;
+import java.awt.image.ComponentColorModel;
+import java.awt.image.DataBuffer;
 import java.awt.image.Raster;
 import java.awt.image.WritableRaster;
 import java.io.IOException;
@@ -44,16 +47,16 @@ import org.apache.pdfbox.util.Matrix;
 public class RadialShadingContext implements PaintContext 
 {
 
-    private ColorModel colorModel;
+    private ColorModel outputColorModel;
     private PDFunction function;
     private ColorSpace shadingColorSpace;
     private PDFunction shadingTinttransform;
+    private PDShadingType3 shadingType;
 
     private float[] coords;
     private float[] domain;
-    private int[] extend0Values;
-    private int[] extend1Values;
     private boolean[] extend;
+    private float[] background;
     private double x1x0; 
     private double y1y0;
     private double r1r0;
@@ -82,6 +85,7 @@ public class RadialShadingContext implements PaintContext
     public RadialShadingContext(PDShadingType3 shadingType3, ColorModel colorModelValue, 
             AffineTransform xform, Matrix ctm, int pageHeight) 
     {
+        shadingType = shadingType3;
         coords = shadingType3.getCoords().toFloatArray();
         if (ctm != null)
         {
@@ -90,12 +94,18 @@ public class RadialShadingContext implements PaintContext
             // transform the coords from shading to user space
             ctm.createAffineTransform().transform(coords, 0, coordsTemp, 0, 1);
             ctm.createAffineTransform().transform(coords, 3, coordsTemp, 3, 1);
+            // scale radius to user space
+            coords[2] *= ctm.getXScale();
+            coords[5] *= ctm.getXScale();
             // move the 0,0-reference
             coordsTemp[1] = pageHeight - coordsTemp[1];
             coordsTemp[4] = pageHeight - coordsTemp[4];
             // transform the coords from user to device space
             xform.transform(coordsTemp, 0, coords, 0, 1);
             xform.transform(coordsTemp, 3, coords, 3, 1);
+            // scale radius to device space
+            coords[2] *= xform.getScaleX();
+            coords[5] *= xform.getScaleX();
         }
         else
         {
@@ -106,98 +116,82 @@ public class RadialShadingContext implements PaintContext
             coords[1] = pageHeight + translateY - coords[1];
             coords[4] = pageHeight + translateY - coords[4];
         }
-        // colorSpace 
-        try 
+        // get the shading colorSpace
+        try
         {
-            PDColorSpace cs = shadingType3.getColorSpace();
+            PDColorSpace cs = shadingType.getColorSpace();
             if (!(cs instanceof PDDeviceRGB))
             {
                 // we have to create an instance of the shading colorspace if it isn't RGB
                 shadingColorSpace = cs.getJavaColorSpace();
                 if (cs instanceof PDDeviceN)
                 {
-                    shadingTinttransform = ((PDDeviceN)cs).getTintTransform();
+                    shadingTinttransform = ((PDDeviceN) cs).getTintTransform();
                 }
                 else if (cs instanceof PDSeparation)
                 {
-                    shadingTinttransform = ((PDSeparation)cs).getTintTransform();
+                    shadingTinttransform = ((PDSeparation) cs).getTintTransform();
                 }
             }
-        } 
-        catch (IOException exception) 
+        }
+        catch (IOException exception)
         {
             LOG.error("error while creating colorSpace", exception);
         }
-        // colorModel
-        if (colorModelValue != null)
+        // create the output colormodel using RGB+alpha as colorspace
+        ColorSpace outputCS = ColorSpace.getInstance(ColorSpace.CS_sRGB);
+        outputColorModel = new ComponentColorModel(outputCS, true, false, Transparency.TRANSLUCENT,
+                DataBuffer.TYPE_BYTE);
+        // domain values
+        if (shadingType.getDomain() != null)
         {
-            colorModel = colorModelValue;
+            domain = shadingType.getDomain().toFloatArray();
         }
         else
         {
-            try
-            {
-                // TODO bpc != 8 ??  
-                colorModel = shadingType3.getColorSpace().createColorModel(8);
-            }
-            catch(IOException exception)
-            {
-                LOG.error("error while creating colorModel", exception);
-            }
-        }
-        // shading function
-        try
-        {
-            function = shadingType3.getFunction();
-        }
-        catch(IOException exception)
-        {
-            LOG.error("error while creating a function", exception);
-        }
-        // domain values
-        if (shadingType3.getDomain() != null)
-        {
-            domain = shadingType3.getDomain().toFloatArray();
-        }
-        else 
-        {
             // set default values
-            domain = new float[]{0,1};
+            domain = new float[] { 0, 1 };
         }
         // extend values
-        COSArray extendValues = shadingType3.getExtend();
-        if (shadingType3.getExtend() != null)
+        COSArray extendValues = shadingType.getExtend();
+        if (shadingType.getExtend() != null)
         {
             extend = new boolean[2];
-            extend[0] = ((COSBoolean)extendValues.get(0)).getValue();
-            extend[1] = ((COSBoolean)extendValues.get(1)).getValue();
+            extend[0] = ((COSBoolean) extendValues.get(0)).getValue();
+            extend[1] = ((COSBoolean) extendValues.get(1)).getValue();
         }
         else
         {
             // set default values
-            extend = new boolean[]{false,false};
+            extend = new boolean[] { false, false };
         }
         // calculate some constants to be used in getRaster
-        x1x0 = coords[3] - coords[0]; 
+        x1x0 = coords[3] - coords[0];
         y1y0 = coords[4] - coords[1];
         r1r0 = coords[5] - coords[2];
-        x1x0pow2 = Math.pow(x1x0,2);
-        y1y0pow2 = Math.pow(y1y0,2);
-        r0pow2 = Math.pow(coords[2],2);
+        x1x0pow2 = Math.pow(x1x0, 2);
+        y1y0pow2 = Math.pow(y1y0, 2);
+        r0pow2 = Math.pow(coords[2], 2);
         denom = x1x0pow2 + y1y0pow2 - Math.pow(r1r0, 2);
-        d1d0 = domain[1]-domain[0];
-        // TODO take a possible Background value into account
-        
+        d1d0 = domain[1] - domain[0];
+
+        // get background values if available
+        COSArray bg = shadingType3.getBackground();
+        if (bg != null)
+        {
+            background = bg.toFloatArray();
+        }
     }
     /**
      * {@inheritDoc}
      */
     public void dispose() 
     {
-        colorModel = null;
+    	outputColorModel = null;
         function = null;
         shadingColorSpace = null;
         shadingTinttransform = null;
+        shadingType = null;
     }
 
     /**
@@ -205,7 +199,7 @@ public class RadialShadingContext implements PaintContext
      */
     public ColorModel getColorModel() 
     {
-        return colorModel;
+        return outputColorModel;
     }
 
     /**
@@ -215,125 +209,146 @@ public class RadialShadingContext implements PaintContext
     {
         // create writable raster
         WritableRaster raster = getColorModel().createCompatibleWritableRaster(w, h);
-        float[] input = new float[1];
-        float inputValue;
-        boolean saveExtend0 = false;
-        boolean saveExtend1 = false;
-        int[] data = new int[w * h * 3];
-        for (int j = 0; j < h; j++) 
+        float inputValue = -1;
+        boolean useBackground;
+        int[] data = new int[w * h * 4];
+        for (int j = 0; j < h; j++)
         {
-            for (int i = 0; i < w; i++) 
+            for (int i = 0; i < w; i++)
             {
-                int index = (j * w + i) * 3;
+                useBackground = false;
                 float[] inputValues = calculateInputValues(x + i, y + j);
-                // choose 1 of the 2 values
-                if (inputValues[0] >= domain[0] && inputValues[0] <= domain[1])
+                if (Float.isNaN(inputValues[0]) && Float.isNaN(inputValues[1]))
                 {
-                    // both values are in the domain -> choose the larger one 
-                    if(inputValues[1] >= domain[0] && inputValues[1] <= domain[1])
+                    if (background != null)
                     {
-                        inputValue = Math.max(inputValues[0], inputValues[1]);
-                    } 
-                    // first value is in the domain, the second not -> choose first value
+                        useBackground = true;
+                    }
                     else
                     {
-                        inputValue = inputValues[0];
+                        continue;
                     }
                 }
                 else
                 {
-                    // first value is not in the domain, but the second -> choose second value
-                    if(inputValues[1] >= domain[0] && inputValues[1] <= domain[1])
+                    // choose 1 of the 2 values
+                    if (inputValues[0] >= domain[0] && inputValues[0] <= domain[1])
                     {
-                        inputValue = inputValues[1];
+                        // both values are in the domain -> choose the larger one
+                        if (inputValues[1] >= domain[0] && inputValues[1] <= domain[1])
+                        {
+                            inputValue = Math.max(inputValues[0], inputValues[1]);
+                        }
+                        // first value is in the domain, the second not -> choose first value
+                        else
+                        {
+                            inputValue = inputValues[0];
+                        }
                     }
-                    // TODO
-                    // both are not in the domain -> choose the first as I don't know it better
                     else
                     {
-                        inputValue = inputValues[0];
-                    }
-                }
-                // input value is out of range
-                if (inputValue < domain[0])
-                {
-                    // the shading has to be extended if extend[0] == true
-                    if (extend[0])
-                    {
-                        if (extend0Values == null)
+                        // first value is not in the domain, but the second -> choose second value
+                        if (inputValues[1] >= domain[0] && inputValues[1] <= domain[1])
                         {
-                            inputValue = domain[0];
-                            saveExtend0 = true;
+                            inputValue = inputValues[1];
                         }
+                        // both are not in the domain (is this correct, or is there some miscalculation??)
                         else
                         {
-                            // use the chached values
-                            System.arraycopy(extend0Values, 0, data, index, 3);
-                            continue;
+                            // TODO don't know what to do in such cases. The following works
+                            // with the given examples
+                            // choose the smaller if extend[0] is set to true
+                            if (extend[0])
+                            {
+                                inputValue = Math.min(inputValues[0], inputValues[1]);
+                            }
+                            // choose the bigger one in all other cases
+                            else
+                            {
+                                inputValue = Math.max(inputValues[0], inputValues[1]);
+                            }
                         }
                     }
-                    else 
+                    // input value is out of range
+                    if (inputValue > domain[1])
                     {
-                        continue;
-                    }
-                }
-                // input value is out of range
-                else if (inputValue > domain[1])
-                {
-                    // the shading has to be extended if extend[1] == true
-                    if (extend[1])
-                    {
-                        if (extend1Values == null)
+                        // the shading has to be extended if extend[1] == true
+                        if (extend[1])
                         {
                             inputValue = domain[1];
-                            saveExtend1 = true;
                         }
                         else
                         {
-                            // use the chached values 
-                            System.arraycopy(extend1Values, 0, data, index, 3);
-                            continue;
+                            if (background != null)
+                            {
+                                useBackground = true;
+                            }
+                            else
+                            {
+                                continue;
+                            }
                         }
                     }
-                    else 
+                    // input value is out of range
+                    else if (inputValue < domain[0])
                     {
-                        continue;
+                        // the shading has to be extended if extend[0] == true
+                        if (extend[0])
+                        {
+                            inputValue = domain[0];
+                        }
+                        else
+                        {
+                            if (background != null)
+                            {
+                                useBackground = true;
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
                     }
                 }
-                input[0] = (float)(domain[0] + (d1d0*inputValue));
                 float[] values = null;
-                try 
+                int index = (j * w + i) * 4;
+                if (useBackground)
                 {
-                    values = function.eval(input);
-                    // convert color values from shading colorspace to RGB 
-                    if (shadingColorSpace != null)
+                    // use the given backgound color values
+                    values = background;
+                }
+                else
+                {
+                    try
                     {
-                        if (shadingTinttransform != null)
+                        float input = (float) (domain[0] + (d1d0 * inputValue));
+                        values = shadingType.evalFunction(input);
+                    }
+                    catch (IOException exception)
+                    {
+                        LOG.error("error while processing a function", exception);
+                    }
+                }
+                // convert color values from shading colorspace to RGB
+                if (shadingColorSpace != null)
+                {
+                    if (shadingTinttransform != null)
+                    {
+                        try
                         {
                             values = shadingTinttransform.eval(values);
                         }
-                        values = shadingColorSpace.toRGB(values);
+                        catch (IOException exception)
+                        {
+                            LOG.error("error while processing a function", exception);
+                        }
                     }
+                    values = shadingColorSpace.toRGB(values);
                 }
-                catch (IOException exception) 
-                {
-                    LOG.error("error while processing a function", exception);
-                }
-                data[index] = (int)(values[0]*255);
-                data[index+1] = (int)(values[1]*255);
-                data[index+2] = (int)(values[2]*255);
-                if (saveExtend0)
-                {
-                    // chache values
-                    extend0Values = new int[3];
-                    System.arraycopy(data, index, extend0Values, 0, 3);
-                }
-                if (saveExtend1)
-                {
-                    // chache values
-                    extend1Values = new int[3];
-                    System.arraycopy(data, index, extend1Values, 0, 3);
-                }
+                data[index] = (int) (values[0] * 255);
+                data[index + 1] = (int) (values[1] * 255);
+                data[index + 2] = (int) (values[2] * 255);
+                data[index + 3] = 255;
             }
         }
         raster.setPixels(0, 0, w, h, data);
@@ -365,13 +380,59 @@ public class RadialShadingContext implements PaintContext
          *  
          *  The following code calculates the 2 possible values of s
          */
-        
-        float[] values = new float[2];
-        double p = (-0.25)*((x - coords[0])*x1x0 + (y - coords[1])*y1y0 - r1r0) / denom;
-        double q = (Math.pow(x - coords[0],2) + Math.pow(y - coords[1],2) - r0pow2) / denom;
-        double root = Math.sqrt(Math.pow(p , 2) - q);
-        values[0] = (float)((-1)*p + root);
-        values[1] = (float)((-1)*p - root);
-        return values;
+        double p = -(x - coords[0]) * x1x0 - (y - coords[1]) * y1y0 - coords[2] * r1r0;
+        double q = (Math.pow(x - coords[0], 2) + Math.pow(y - coords[1], 2) - r0pow2);
+        double root = Math.sqrt(p * p - denom * q);
+        float root1 = (float) ((-p + root) / denom);
+        float root2 = (float) ((-p - root) / denom);
+        if (denom < 0)
+        {
+            return new float[] { root1, root2 };
+        }
+        else
+        {
+            return new float[] { root2, root1 };
+        }
     }
+    
+    /**
+     * Returns the coords values.
+     * 
+     * @return the coords values as array
+     */
+    public float[] getCoords() 
+    {
+        return coords;
+    }
+        
+    /**
+     * Returns the domain values.
+     * 
+     * @return the domain values as array
+     */
+    public float[] getDomain() 
+    {
+        return domain;
+    }
+        
+    /**
+     * Returns the extend values.
+     * 
+     * @return the extend values as array
+     */
+    public boolean[] getExtend() 
+    {
+        return extend;
+    }
+    
+    /**
+     * Returns the function used for the shading tint transformation.
+     * 
+     * @return the shading tint transformation function
+     */
+    public PDFunction getShadingTintTransform() 
+    {
+        return shadingTinttransform;
+    }
+
 }
