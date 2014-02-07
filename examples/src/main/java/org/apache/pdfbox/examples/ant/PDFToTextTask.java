ff99defccbc6feb252748b2e7dcff07ba9430404
Andreas Lehmkuhler <lehmi@apache.org>
2014-02-07 15:53:23 +0000

    PDFBOX-1891: (re)moved the ant module
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1565695 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/ant/PDFToTextTask.java
+++ examples/src/main/java/org/apache/pdfbox/examples/ant/PDFToTextTask.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.pdfbox.ant;
+package org.apache.pdfbox.examples.ant;
 
 import java.io.File;
 
