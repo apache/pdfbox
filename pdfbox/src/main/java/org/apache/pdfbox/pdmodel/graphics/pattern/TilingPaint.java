295fb8f57aeb09d55e9e96b97693123b84f7c1d6
John Hewson <jahewson@apache.org>
2014-02-26 06:52:35 +0000

    Housekeeping for Pattern classes
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1571934 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/pattern/TilingPaint.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/pattern/TilingPaint.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.pdfbox.pdmodel.graphics.pattern.tiling;
+package org.apache.pdfbox.pdmodel.graphics.pattern;
 
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
