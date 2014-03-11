60356f7a50598c76d08a1b2dd401cd544713006b
Tilman Hausherr <tilman@apache.org>
2014-03-11 22:30:00 +0000

    PDFBOX-1975: add the helper methods for the unit tests (not used yet)
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1576531 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/test/java/org/apache/pdfbox/util/TestImageIOUtils.java
+++ pdfbox/src/test/java/org/apache/pdfbox/util/TestImageIOUtils.java
@@ -17,17 +17,32 @@
 package org.apache.pdfbox.util;
 
 import java.awt.image.BufferedImage;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.Iterator;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.stream.ImageInputStream;
 
 import junit.framework.TestCase;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.fail;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * Test suite for ImageIOUtil.
@@ -109,4 +124,107 @@ public class TestImageIOUtils extends TestCase
             doTestFile(file, outDir);
         }
     }
+
+    private static final String STANDARD_METADATA_FORMAT = "javax_imageio_1.0";
+    
+    /**
+     * checks whether the resolution of an image file is as expected.
+     *
+     * @param filename the name of the file
+     * @param expectedResolution the expected resolution
+     *
+     * @throws IOException if something goes wrong
+     */
+    private void checkResolution(String filename, int expectedResolution)
+            throws IOException
+    {
+        String suffix = filename.substring(filename.lastIndexOf('.') + 1);
+        if ("BMP".equals(suffix.toUpperCase()))
+        {
+            // BMP reader doesn't work
+            checkBmpResolution(filename, expectedResolution);
+            return;
+        }
+        Iterator readers = ImageIO.getImageReadersBySuffix(suffix);
+        assertTrue("No image reader found for suffix " + suffix, readers.hasNext());
+        ImageReader reader = (ImageReader) readers.next();
+        ImageInputStream iis = ImageIO.createImageInputStream(new File(filename));
+        assertNotNull("No ImageInputStream created for file " + filename, iis);
+        reader.setInput(iis);
+        IIOMetadata imageMetadata = reader.getImageMetadata(0);
+        Element root = (Element) imageMetadata.getAsTree(STANDARD_METADATA_FORMAT);
+        NodeList dimensionNodes = root.getElementsByTagName("Dimension");
+        assertTrue("No resolution found in image file " + filename, dimensionNodes.getLength() > 0);
+        Element dimensionElement = (Element) dimensionNodes.item(0);
+
+        NodeList pixelSizeNodes = dimensionElement.getElementsByTagName("HorizontalPixelSize");
+        assertTrue("No X resolution found in image file " + filename, pixelSizeNodes.getLength() > 0);
+        Node pixelSizeNode = pixelSizeNodes.item(0);
+        String val = pixelSizeNode.getAttributes().getNamedItem("value").getNodeValue();
+        int actualResolution = (int) Math.round(25.4 / Double.parseDouble(val));
+        assertEquals("X resolution doesn't match in image file " + filename, expectedResolution, actualResolution);
+
+        pixelSizeNodes = dimensionElement.getElementsByTagName("VerticalPixelSize");
+        assertTrue("No Y resolution found in image file " + filename, pixelSizeNodes.getLength() > 0);
+        pixelSizeNode = pixelSizeNodes.item(0);
+        val = pixelSizeNode.getAttributes().getNamedItem("value").getNodeValue();
+        actualResolution = (int) Math.round(25.4 / Double.parseDouble(val));
+        assertEquals("Y resolution doesn't match", expectedResolution, actualResolution);
+
+        iis.close();
+        reader.dispose();
+    }
+
+    /**
+     * checks whether the resolution of a BMP image file is as expected.
+     *
+     * @param filename the name of the BMP file
+     * @param expectedResolution the expected resolution
+     *
+     * @throws IOException if something goes wrong
+     */
+    private void checkBmpResolution(String filename, int expectedResolution)
+            throws FileNotFoundException, IOException
+    {
+        // BMP format explained here:
+        // http://www.javaworld.com/article/2077561/learn-java/java-tip-60--saving-bitmap-files-in-java.html
+        // we skip 38 bytes and then read two 4 byte-integers and reverse the bytes
+        DataInputStream dis = new DataInputStream(new FileInputStream(new File(filename)));
+        int skipped = dis.skipBytes(38);
+        assertEquals("Can't skip 38 bytes in image file " + filename, 38, skipped);
+        int pixelsPerMeter = Integer.reverseBytes(dis.readInt());
+        int actualResolution = (int) Math.round(pixelsPerMeter / 100.0 * 2.54);
+        assertEquals("X resolution doesn't match in image file " + filename,
+                expectedResolution, actualResolution);
+        pixelsPerMeter = Integer.reverseBytes(dis.readInt());
+        actualResolution = (int) Math.round(pixelsPerMeter / 100.0 * 2.54);
+        assertEquals("Y resolution doesn't match in image file " + filename,
+                expectedResolution, actualResolution);
+        dis.close();
+    }
+
+    /**
+     * Get the compression of a TIFF file
+     *
+     * @param filename Filename
+     * @return the TIFF compression
+     *
+     * @throws IOException
+     */
+    String getTiffCompression(String filename) throws IOException
+    {
+        Iterator readers = ImageIO.getImageReadersBySuffix("tiff");
+        ImageReader reader = (ImageReader) readers.next();
+        ImageInputStream iis = ImageIO.createImageInputStream(new File(filename));
+        reader.setInput(iis);
+        IIOMetadata imageMetadata = reader.getImageMetadata(0);
+        Element root = (Element) imageMetadata.getAsTree(STANDARD_METADATA_FORMAT);
+        Element comprElement = (Element) root.getElementsByTagName("Compression").item(0);
+        Node comprTypeNode = comprElement.getElementsByTagName("CompressionTypeName").item(0);
+        String compression = comprTypeNode.getAttributes().getNamedItem("value").getNodeValue();
+        iis.close();
+        reader.dispose();
+        return compression;
+    }
+    
 }
