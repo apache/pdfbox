3a3c49d0250aec90bf0048aa81522216a7f8868d
Tilman Hausherr <tilman@apache.org>
2014-03-17 18:12:39 +0000

    PDFBOX-1990: forgot one import
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1578492 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/test/java/org/apache/pdfbox/pdmodel/graphics/image/PDInlineImageTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/graphics/image/PDInlineImageTest.java
@@ -22,6 +22,7 @@ import java.io.IOException;
 import junit.framework.TestCase;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.util.ImageIOUtil;
 
 /**
  * Unit tests for PDInlineImage
