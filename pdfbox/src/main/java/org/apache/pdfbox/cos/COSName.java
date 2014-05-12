b69c46433f1ecfca7acf5e7952db8e2061ed0836
Tilman Hausherr <tilman@apache.org>
2014-05-12 20:07:49 +0000

    PDFBOX-2073: add '{' and '}' to the hex conversion list
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1594079 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/cos/COSName.java
+++ pdfbox/src/main/java/org/apache/pdfbox/cos/COSName.java
@@ -589,6 +589,7 @@ public final class COSName extends COSBase implements Comparable<COSName>
 
             if (current <= 32 || current >= 127 || current == '(' || current == ')' || current == '[' || current == ']'
                     || current == '/' || current == '%' || current == '<' || current == '>'
+                    || current == '{' || current == '}' // PDFBOX-2073                    
                     || current == NAME_ESCAPE[0])
             {
                 output.write(NAME_ESCAPE);
