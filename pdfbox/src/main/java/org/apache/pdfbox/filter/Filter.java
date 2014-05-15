492a0555165648edd77893fd4582606f4a726598
Tilman Hausherr <tilman@apache.org>
2014-05-15 16:31:08 +0000

    PDFBOX-2070: DRY refactoring
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1594969 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/filter/Filter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/filter/Filter.java
@@ -19,6 +19,9 @@ package org.apache.pdfbox.filter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Iterator;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -46,6 +49,7 @@ public abstract class Filter
      * @param encoded the encoded byte stream
      * @param decoded the stream where decoded data will be written
      * @param parameters the parameters used for decoding
+     * @param index the index to the filter being decoded
      * @return repaired parameters dictionary, or the original parameters dictionary
      * @throws IOException if the stream cannot be decoded
      */
@@ -57,6 +61,7 @@ public abstract class Filter
      * @param input the byte stream to encode
      * @param encoded the stream where encoded data will be written
      * @param parameters the parameters used for encoding
+     * @param index the index to the filter being encoded
      * @throws IOException if the stream cannot be encoded
      */
     public final void encode(InputStream input, OutputStream encoded, COSDictionary parameters,
@@ -93,4 +98,32 @@ public abstract class Filter
         }
         return new COSDictionary();
     }
+
+    /**
+     * Finds a suitable image reader for a format.
+     *
+     * @param formatName The format to search for.
+     * @param errorCause The probably cause if something goes wrong.
+     * @return The image reader for the format.
+     * @throws MissingImageReaderException if no image reader is found.
+     */
+    protected ImageReader findImageReader(String formatName, String errorCause) throws MissingImageReaderException
+    {
+        Iterator<ImageReader> readers = ImageIO.getImageReadersByFormatName(formatName);
+        ImageReader reader = null;
+        while (readers.hasNext())
+        {
+            reader = readers.next();
+            if (reader.canReadRaster())
+            {
+                break;
+            }
+        }
+        if (reader == null)
+        {
+            throw new MissingImageReaderException("Cannot read " + formatName + " image: " + errorCause);
+        }
+        return reader;
+    }
+
 }
