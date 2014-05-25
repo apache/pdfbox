b22c6c8ddaeee97ed6835621a0ff4ba3bd3635d6
Tilman Hausherr <tilman@apache.org>
2014-05-25 14:58:51 +0000

    PDFBOX-2079: remove final CR LF or LF from output of readUntilEndStream()
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/1.8@1597435 13f79535-47bb-0310-9956-ffa450edef68

---
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfparser/EndstreamOutputStream.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2014 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.pdfbox.pdfparser;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * This class is only for the readUntilEndStream methods, to prevent a
+ * final CR LF or LF (but not a final CR!) from being written to the output.
+ * Because of that, only the 3-param write() method is implemented. This solves
+ * PDFBOX-2079 and avoids making readUntilEndStream() even more complex than it
+ * already is.
+ *
+ * @author Tilman Hausherr
+ */
+class EndstreamOutputStream extends BufferedOutputStream
+{
+    //TODO: replace this class with a PullBackOutputStream class if there ever is one
+    
+    private boolean hasCR = false;
+    private boolean hasLF = false;
+    
+    public EndstreamOutputStream(OutputStream out)
+    {
+        super(out);
+    }
+
+    /**
+     * Write CR and/or LF that were kept, then writes len bytes from the 
+     * specified byte array starting at offset off to this output stream,
+     * except trailing CR, CR LF, or LF.
+     * @param b byte array.
+     * @param off offset.
+     * @param len length of segment to write.
+     * @throws IOException 
+     */
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException
+    {
+        // first write what we kept last time
+        if (hasCR)
+        {
+            if (!hasLF && len == 1 && b[off] == '\n')
+            {
+                // previous buffer ended with CR
+                // actual buffer contains only LF so it will be the last one
+                // => we're done
+                hasCR = false; // to avoid this getting written in the flush
+                return;                    
+            }
+            super.write('\r');
+            hasCR = false;
+        }
+        if (hasLF)
+        {
+            super.write('\n');
+            hasLF = false;
+        } 
+        // don't write CR, LF, or CR LF if at the end of the buffer
+        if (len > 0)
+        {
+            if (b[off + len - 1] == '\r')
+            {
+                hasCR = true;
+                --len;
+            }
+            else if (b[off + len - 1] == '\n')
+            {
+                hasLF = true;
+                --len;
+                if (len > 0 && b[off + len - 1] == '\r')
+                {
+                    hasCR = true;
+                    --len;
+                }
+            }
+        }        
+        super.write(b, off, len);
+    } 
+
+    /**
+     * write out a single CR if one was kept. Don't write kept CR LF or LF, 
+     * and then call the base method to flush.
+     * 
+     * @throws IOException 
+     */
+    @Override
+    public void flush() throws IOException
+    {
+        // if there is only a CR and no LF, write it
+        if (hasCR && !hasLF)
+        {
+            super.write('\r');
+        }
+        hasCR = false;
+        hasLF = false;
+        super.flush();
+    }
+}
