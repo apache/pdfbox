24aca5aeb853caa1fd875af5c1be6bc71116e4fb
Andreas Lehmkuhler <lehmi@apache.org>
2014-06-28 14:38:29 +0000

    PDFBOX-2154: print some log information instead of throwing a NPE when rendering unsupported type3 fonts
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/1.8@1606362 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType3Font.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType3Font.java
@@ -16,30 +16,28 @@
  */
 package org.apache.pdfbox.pdmodel.font;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
-import org.apache.pdfbox.cos.COSStream;
 import org.apache.pdfbox.pdmodel.common.PDMatrix;
 
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.geom.AffineTransform;
-
 import java.io.IOException;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * This is implementation of the Type3 Font.
  *
- * @author <a href="mailto:ben@benlitchfield.com">Ben Litchfield</a>
- * @version $Revision: 1.8 $
+ * @author Ben Litchfield
+ * 
  */
 public class PDType3Font extends PDSimpleFont
 {
-    //A map of character code to java.awt.Image for the glyph
-    private Map<Character, Image> images = new HashMap<Character, Image>();
+    /**
+     * Log instance.
+     */
+    private static final Log LOG = LogFactory.getLog(PDType3Font.class);
 
     /**
      * Constructor.
@@ -61,57 +59,12 @@ public class PDType3Font extends PDSimpleFont
     }
 
     /**
-     * Type3 fonts have their glyphs defined as a content stream.  This
-     * will create the image that represents that character
-     *
-     * @throws IOException If there is an error creating the image.
-     */
-    private Image createImageIfNecessary( char character ) throws IOException
-    {
-        Character c = new Character( character );
-        Image retval = (Image)images.get( c );
-        if( retval == null )
-        {
-            COSDictionary charProcs = (COSDictionary)font.getDictionaryObject( COSName.CHAR_PROCS );
-            COSStream stream = (COSStream)charProcs.getDictionaryObject( COSName.getPDFName( "" + character ) );
-            if( stream != null )
-            {
-                Type3StreamParser parser = new Type3StreamParser();
-                retval = parser.createImage( stream );
-                images.put( c, retval );
-            }
-            else
-            {
-                //stream should not be null!!
-            }
-        }
-        return retval;
-
-    }
-
-    /**
      * {@inheritDoc}
      */
     public void drawString( String string, int[] codePoints, Graphics g, float fontSize, AffineTransform at, float x, float y ) 
         throws IOException
     {
-        for(int i=0; i<string.length(); i++)
-        {
-            //todo need to use image observers and such
-            char c = string.charAt( i );
-            Image image = createImageIfNecessary( c );
-            if( image != null )
-            {
-                int newWidth = (int)(.12*image.getWidth(null));
-                int newHeight = (int)(.12*image.getHeight(null));
-                if( newWidth > 0 && newHeight > 0 )
-                {
-                    image = image.getScaledInstance( newWidth, newHeight, Image.SCALE_SMOOTH );
-                    g.drawImage( image, (int)x, (int)y, null );
-                    x+=newWidth;
-                }
-            }
-        }
+        LOG.info("Rendering of type3 fonts isn't supported in PDFBox 1.8.x. It will be available in the 2.0 version!");
     }
 
     /**
