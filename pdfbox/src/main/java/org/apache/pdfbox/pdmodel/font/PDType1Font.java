0e6daf374f47256a0b5f19ca5d7571167e61a47b
John Hewson <jahewson@apache.org>
2014-09-21 20:51:09 +0000

    PDFBOX-2372: Standard 14 simple fonts should have empty .notdef glyph
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1626630 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType1Font.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType1Font.java
@@ -440,6 +440,15 @@ public class PDType1Font extends PDSimpleFont implements PDType1Equivalent
     @Override
     public GeneralPath getPath(String name) throws IOException
     {
-        return type1Equivalent.getPath(name);
+        // Adobe's Standard 14 fonts have an empty .notdef glyph, but Microsoft's don't
+        // so we need to fake this glyph otherwise we get unwanted rectangles, see PDFBOX-2372
+        if (".notdef".equals(name) && isStandard14())
+        {
+            return new GeneralPath();
+        }
+        else
+        {
+            return type1Equivalent.getPath(name);
+        }
     }
 }
