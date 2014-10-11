5431c8e11e91b17392971b72ec21567705ce9b9b
Tilman Hausherr <tilman@apache.org>
2014-10-11 13:22:59 +0000

    PDFBOX-2402: ignore stuff after the object is parsed if the parser is lenient, as suggested by Michele Balistreri
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/1.8@1631043 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdfparser/NonSequentialPDFParser.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfparser/NonSequentialPDFParser.java
@@ -1380,7 +1380,7 @@ public class NonSequentialPDFParser extends PDFParser
 
                 if (!endObjectKey.startsWith("endobj"))
                 {
-                    if (endObjectKey.endsWith(" obj") && isLenient)
+                    if (isLenient)
                     {
                         LOG.warn("Object (" + readObjNr + ":" + readObjGen + ") at offset "
                                 + offsetOrObjstmObNr + " does not end with 'endobj' but with '" + endObjectKey + "'");
