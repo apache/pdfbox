c4c10a654a5c523803f7f74e661b00061d151990
John Hewson <jahewson@apache.org>
2014-10-31 07:44:04 +0000

    PDFBOX-1292: Blend modes were always null!
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1635714 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/blend/BlendMode.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/blend/BlendMode.java
@@ -30,6 +30,149 @@ import java.util.Map;
  */
 public abstract class BlendMode
 {
+    /**
+     * Determines the blend mode from the BM entry in the COS ExtGState.
+     *
+     * @param cosBlendMode name or array
+     * @return blending mode
+     */
+    public static BlendMode getInstance(COSBase cosBlendMode)
+    {
+        BlendMode result = null;
+        if (cosBlendMode instanceof COSName)
+        {
+            result = BLEND_MODES.get((COSName)cosBlendMode);
+        }
+        else if (cosBlendMode instanceof COSArray)
+        {
+            COSArray cosBlendModeArray = (COSArray) cosBlendMode;
+            for (int i = 0; i < cosBlendModeArray.size(); i++)
+            {
+                result = BLEND_MODES.get(cosBlendModeArray.get(i));
+                if (result != null)
+                {
+                    break;
+                }
+            }
+        }
+
+        if (result != null)
+        {
+            return result;
+        }
+        return BlendMode.COMPATIBLE;
+    }
+
+    public static final SeparableBlendMode NORMAL = new SeparableBlendMode()
+    {
+        public float blendChannel(float srcValue, float dstValue)
+        {
+            return srcValue;
+        }
+    };
+
+    public static final SeparableBlendMode COMPATIBLE = NORMAL;
+
+    public static final SeparableBlendMode MULTIPLY = new SeparableBlendMode()
+    {
+        public float blendChannel(float srcValue, float dstValue)
+        {
+            return srcValue * dstValue;
+        }
+    };
+
+    public static final SeparableBlendMode SCREEN = new SeparableBlendMode()
+    {
+        public float blendChannel(float srcValue, float dstValue)
+        {
+            return srcValue + dstValue - srcValue * dstValue;
+        }
+    };
+
+    public static final SeparableBlendMode OVERLAY = new SeparableBlendMode()
+    {
+        public float blendChannel(float srcValue, float dstValue)
+        {
+            return (dstValue <= 0.5) ? 2 * dstValue * srcValue : 2 * (srcValue + dstValue - srcValue
+                    * dstValue) - 1;
+        }
+    };
+
+    public static final SeparableBlendMode DARKEN = new SeparableBlendMode()
+    {
+        public float blendChannel(float srcValue, float dstValue)
+        {
+            return Math.min(srcValue, dstValue);
+        }
+    };
+
+    public static final SeparableBlendMode LIGHTEN = new SeparableBlendMode()
+    {
+        public float blendChannel(float srcValue, float dstValue)
+        {
+            return Math.max(srcValue, dstValue);
+        }
+    };
+
+    public static final SeparableBlendMode COLOR_DODGE = new SeparableBlendMode()
+    {
+        public float blendChannel(float srcValue, float dstValue)
+        {
+            return (srcValue < 1) ? Math.min(1, dstValue / (1 - srcValue)) : 1;
+        }
+    };
+
+    public static final SeparableBlendMode COLOR_BURN = new SeparableBlendMode()
+    {
+        public float blendChannel(float srcValue, float dstValue)
+        {
+            return (srcValue > 0) ? 1 - Math.min(1, (1 - dstValue) / srcValue) : 0;
+        }
+    };
+
+    public static final SeparableBlendMode HARD_LIGHT = new SeparableBlendMode()
+    {
+        public float blendChannel(float srcValue, float dstValue)
+        {
+            return (srcValue <= 0.5) ? 2 * dstValue * srcValue :
+                    2 * (srcValue + dstValue - srcValue * dstValue) - 1;
+        }
+    };
+
+    public static final SeparableBlendMode SOFT_LIGHT = new SeparableBlendMode()
+    {
+        public float blendChannel(float srcValue, float dstValue)
+        {
+            if (srcValue <= 0.5)
+            {
+                return dstValue - (1 - 2 * srcValue) * dstValue * (1 - dstValue);
+            }
+            else
+            {
+                float D = (dstValue <= 0.25) ? ((16 * dstValue - 12) * dstValue + 4) * dstValue
+                        : (float) Math .sqrt(dstValue);
+                return dstValue + (2 * srcValue - 1) * (D - dstValue);
+            }
+        }
+    };
+
+    public static final SeparableBlendMode DIFFERENCE = new SeparableBlendMode()
+    {
+        public float blendChannel(float srcValue, float dstValue)
+        {
+            return Math.abs(dstValue - srcValue);
+        }
+    };
+
+    public static final SeparableBlendMode EXCLUSION = new SeparableBlendMode()
+    {
+        public float blendChannel(float srcValue, float dstValue)
+        {
+            return dstValue + srcValue - 2 * dstValue * srcValue;
+        }
+    };
+
+    // this map *must* come after the declarations above, otherwise its values will be null
     private static final Map<COSName, BlendMode> BLEND_MODES = createBlendModeMap();
 
     private static Map<COSName, BlendMode> createBlendModeMap()
@@ -51,147 +194,6 @@ public abstract class BlendMode
         // TODO - non-separable blending modes
         return map;
     }
-
-    /**
-     * Determines the blend mode from the BM entry in the COS ExtGState.
-     *
-     * @param cosBlendMode name or array
-     * @return blending mode
-     */
-    public static BlendMode getInstance(COSBase cosBlendMode)
-    {
-        BlendMode result = null;
-        if (cosBlendMode instanceof COSName)
-        {
-            result = BLEND_MODES.get(cosBlendMode);
-        }
-        else if (cosBlendMode instanceof COSArray)
-        {
-            COSArray cosBlendModeArray = (COSArray) cosBlendMode;
-            for (int i = 0; i < cosBlendModeArray.size(); i++)
-            {
-                result = BLEND_MODES.get(cosBlendModeArray.get(i));
-                if (result != null)
-                {
-                    break;
-                }
-            }
-        }
-        if (result != null)
-        {
-            return result;
-        }
-        return BlendMode.COMPATIBLE;
-    }
-
-    public static SeparableBlendMode NORMAL = new SeparableBlendMode()
-    {
-        public float blendChannel(float srcValue, float dstValue)
-        {
-            return srcValue;
-        }
-    };
-
-    public static SeparableBlendMode COMPATIBLE = NORMAL;
-
-    public static SeparableBlendMode MULTIPLY = new SeparableBlendMode()
-    {
-        public float blendChannel(float srcValue, float dstValue)
-        {
-            return srcValue * dstValue;
-        }
-    };
-
-    public static SeparableBlendMode SCREEN = new SeparableBlendMode()
-    {
-        public float blendChannel(float srcValue, float dstValue)
-        {
-            return srcValue + dstValue - srcValue * dstValue;
-        }
-    };
-
-    public static SeparableBlendMode OVERLAY = new SeparableBlendMode()
-    {
-        public float blendChannel(float srcValue, float dstValue)
-        {
-            return (dstValue <= 0.5) ? 2 * dstValue * srcValue : 2 * (srcValue + dstValue - srcValue
-                    * dstValue) - 1;
-        }
-    };
-
-    public static SeparableBlendMode DARKEN = new SeparableBlendMode()
-    {
-        public float blendChannel(float srcValue, float dstValue)
-        {
-            return Math.min(srcValue, dstValue);
-        }
-    };
-
-    public static SeparableBlendMode LIGHTEN = new SeparableBlendMode()
-    {
-        public float blendChannel(float srcValue, float dstValue)
-        {
-            return Math.max(srcValue, dstValue);
-        }
-    };
-
-    public static SeparableBlendMode COLOR_DODGE = new SeparableBlendMode()
-    {
-        public float blendChannel(float srcValue, float dstValue)
-        {
-            return (srcValue < 1) ? Math.min(1, dstValue / (1 - srcValue)) : 1;
-        }
-    };
-
-    public static SeparableBlendMode COLOR_BURN = new SeparableBlendMode()
-    {
-        public float blendChannel(float srcValue, float dstValue)
-        {
-            return (srcValue > 0) ? 1 - Math.min(1, (1 - dstValue) / srcValue) : 0;
-        }
-    };
-
-    public static SeparableBlendMode HARD_LIGHT = new SeparableBlendMode()
-    {
-        public float blendChannel(float srcValue, float dstValue)
-        {
-            return (srcValue <= 0.5) ? 2 * dstValue * srcValue :
-                    2 * (srcValue + dstValue - srcValue * dstValue) - 1;
-        }
-    };
-
-    public static SeparableBlendMode SOFT_LIGHT = new SeparableBlendMode()
-    {
-        public float blendChannel(float srcValue, float dstValue)
-        {
-            if (srcValue <= 0.5)
-            {
-                return dstValue - (1 - 2 * srcValue) * dstValue * (1 - dstValue);
-            }
-            else
-            {
-                float D = (dstValue <= 0.25) ? ((16 * dstValue - 12) * dstValue + 4) * dstValue
-                        : (float) Math .sqrt(dstValue);
-                return dstValue + (2 * srcValue - 1) * (D - dstValue);
-            }
-        }
-    };
-
-    public static SeparableBlendMode DIFFERENCE = new SeparableBlendMode()
-    {
-        public float blendChannel(float srcValue, float dstValue)
-        {
-            return Math.abs(dstValue - srcValue);
-        }
-    };
-
-    public static SeparableBlendMode EXCLUSION = new SeparableBlendMode()
-    {
-        public float blendChannel(float srcValue, float dstValue)
-        {
-            return dstValue + srcValue - 2 * dstValue * srcValue;
-        }
-    };
 
     BlendMode()
     {
