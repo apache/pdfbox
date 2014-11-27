db63a1c7847e52505337fd68da0cb5a58f80ff52
John Hewson <jahewson@apache.org>
2014-11-27 20:10:13 +0000

    PDFBOX-2526: Allow the FontProvider to perform reverse substitutions.
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1642226 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/FontProvider.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/FontProvider.java
@@ -16,6 +16,9 @@
  */
 package org.apache.pdfbox.pdmodel.font;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 import org.apache.fontbox.cff.CFFFont;
 import org.apache.fontbox.ttf.TrueTypeFont;
 import org.apache.fontbox.type1.Type1Font;
@@ -25,7 +28,7 @@ import org.apache.fontbox.type1.Type1Font;
  *
  * @author John Hewson
  */
-public interface FontProvider
+public abstract class FontProvider
 {
     /**
      * Returns a TrueType which corresponds to the given PostScript name. If there is no
@@ -33,7 +36,7 @@ public interface FontProvider
      *
      * @param postScriptName PostScript font name
      */
-    public TrueTypeFont getTrueTypeFont(String postScriptName);
+    public abstract TrueTypeFont getTrueTypeFont(String postScriptName);
 
     /**
      * Returns a CFF font which corresponds to the given PostScript name. If there is no
@@ -41,7 +44,7 @@ public interface FontProvider
      *
      * @param postScriptName PostScript font name
      */
-    public CFFFont getCFFFont(String postScriptName);
+    public abstract CFFFont getCFFFont(String postScriptName);
 
     /**
      * Returns a Type 1 which corresponds to the given PostScript name. If there is no
@@ -49,11 +52,57 @@ public interface FontProvider
      *
      * @param postScriptName PostScript font name
      */
-    public Type1Font getType1Font(String postScriptName);
+    public abstract Type1Font getType1Font(String postScriptName);
 
     /**
      * Returns a string containing debugging information. This will be written to the log if no
      * suitable fonts are found and no fallback fonts are available. May be null.
      */
-    public String toDebugString();
+    public abstract String toDebugString();
+
+    /**
+     * Returns the font names for a given font. This allows substitution based on the PostScript
+     * name of the external font, instead of just the BaseName in the PDF.
+     */
+    protected final Set<String> getNames(TrueTypeFont font) throws IOException
+    {
+        return getPostScriptNames(font.getName());
+        // could add format-specific names here if needed
+    }
+
+    /**
+     * Returns the font names for a given font. This allows substitution based on the PostScript
+     * name of the external font, instead of just the BaseName in the PDF.
+     */
+    protected final Set<String> getNames(Type1Font font) throws IOException
+    {
+        return getPostScriptNames(font.getName());
+        // could add format-specific names here if needed
+    }
+
+    /**
+     * Returns the font names for a given font. This allows substitution based on the PostScript
+     * name of the external font, instead of just the BaseName in the PDF.
+     */
+    protected final Set<String> getNames(CFFFont font) throws IOException
+    {
+        return getPostScriptNames(font.getName());
+        // could add format-specific names here if needed
+    }
+
+    /**
+     * Returns a list of alternative names for the given PostScript name.
+     */
+    private Set<String> getPostScriptNames(String postScriptName) throws IOException
+    {
+        Set<String> names = new HashSet<String>();
+
+        // built-in PostScript name
+        names.add(postScriptName);
+
+        // remove hyphens (e.g. Arial-Black -> ArialBlack)
+        names.add(postScriptName.replaceAll("-", ""));
+
+        return names;
+    }
 }
