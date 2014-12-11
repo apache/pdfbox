82e6cde5f5c860c4ba4617f7fc0b0637475c61ae
Tilman Hausherr <tilman@apache.org>
2014-12-11 07:28:11 +0000

    PDFBOX-2552: normalize the rotation angle for all values
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1644567 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/util/LayerUtility.java
+++ pdfbox/src/main/java/org/apache/pdfbox/util/LayerUtility.java
@@ -292,24 +292,17 @@ public class LayerUtility
     private static int getNormalizedRotation(PDPage page)
     {
         //Handle the /Rotation entry on the page dict
-        int rotation = page.getRotation();
-        while (rotation >= 360)
+        int rotationAngle = page.getRotation();
+        // normalize the rotation angle
+        while (rotationAngle < 0)
         {
-            rotation -= 360;
+            rotationAngle += 360;
         }
-        if (rotation < 0)
+        while (rotationAngle >= 360)
         {
-            rotation = 0;
+            rotationAngle -= 360;
         }
-        switch (rotation)
-        {
-        case 90:
-        case 180:
-        case 270:
-            return rotation;
-        default:
-            return 0;
-        }
+        return rotationAngle;
     }
 
 
