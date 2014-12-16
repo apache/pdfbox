ecc510cd88f6438c32b51f192e512e443c5f79f5
Tilman Hausherr <tilman@apache.org>
2014-12-16 17:55:36 +0000

    PDFBOX-1675: remove non utf8 char
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1646029 13f79535-47bb-0310-9956-ffa450edef68

--- preflight/src/main/java/org/apache/pdfbox/preflight/action/GoToRemoteAction.java
+++ preflight/src/main/java/org/apache/pdfbox/preflight/action/GoToRemoteAction.java
@@ -39,7 +39,7 @@ public class GoToRemoteAction extends GoToAction
      * 
      * @param amFact Instance of ActionManagerFactory used to create ActionManager to check Next actions.
      * @param adict the COSDictionary of the action wrapped by this class.
-     * @param ctx the preflight context´.
+     * @param ctx the preflight context.
      * @param aaKey the name of the key which identify the action in a additional action dictionary.
      */
     public GoToRemoteAction(ActionManagerFactory amFact, COSDictionary adict, PreflightContext ctx, String aaKey)
