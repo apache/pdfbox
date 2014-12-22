f57a75cd9d9c81b10a52db9ba4ecd581a2818a47
Maruan Sahyoun <msahyoun@apache.org>
2014-12-22 21:52:29 +0000

    PDFBOX-2576 provide cause of exception; StringBuilder for concatenation
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1647420 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fdf/XMLUtil.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fdf/XMLUtil.java
@@ -21,12 +21,15 @@ import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
+import org.xml.sax.SAXException;
 
 /**
  * This class with handle some simple XML operations.
@@ -58,10 +61,17 @@ final class XMLUtil
             DocumentBuilder builder = builderFactory.newDocumentBuilder();
             return builder.parse( is );
         }
-        catch( Exception e )
+        catch (FactoryConfigurationError  e)
         {
-            IOException thrown = new IOException( e.getMessage() );
-            throw thrown;
+            throw new IOException( e.getMessage(), e );
+        }
+        catch (ParserConfigurationException e)
+        {
+            throw new IOException( e.getMessage(), e );
+        }
+        catch (SAXException e)
+        {
+            throw new IOException( e.getMessage(), e );
         }
     }
 
@@ -73,16 +83,17 @@ final class XMLUtil
      */
     public static String getNodeValue( Element node )
     {
-        String retval = "";
+        StringBuilder sb = new StringBuilder();
         NodeList children = node.getChildNodes();
-        for( int i=0; i<children.getLength(); i++ )
+        int numNodes = children.getLength();
+        for( int i=0; i<numNodes; i++ )
         {
             Node next = children.item( i );
             if( next instanceof Text )
             {
-                retval += next.getNodeValue();
+                sb.append(next.getNodeValue());
             }
         }
-        return retval;
+        return sb.toString();
     }
 }
