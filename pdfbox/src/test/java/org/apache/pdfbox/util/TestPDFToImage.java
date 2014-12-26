3531055c3a0d71fbd65b7d3509861076d032fe35
Tilman Hausherr <tilman@apache.org>
2014-12-26 10:46:40 +0000

    PDFBOX-2440: avoid exception in test when saving encrypted file
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1647948 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/test/java/org/apache/pdfbox/util/TestPDFToImage.java
+++ pdfbox/src/test/java/org/apache/pdfbox/util/TestPDFToImage.java
@@ -243,6 +243,7 @@ public class TestPDFToImage extends TestCase
             // test to see whether file is destroyed in pdfbox
             new FileOutputStream(new File(outDir + file.getName() + ".saveerror")).close();
             File tmpFile = File.createTempFile("pdfbox", ".pdf");
+            document.setAllSecurityToBeRemoved(true);
             document.save(tmpFile);
             new File(outDir + file.getName() + ".saveerror").delete();
             new FileOutputStream(new File(outDir + file.getName() + ".reloaderror")).close();
