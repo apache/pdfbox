2dd5275f12a1839c8cee9a8d7bc901a10f5cd591
Tilman Hausherr <tilman@apache.org>
2015-01-13 16:53:20 +0000

    PDFBOX-2576: remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1651403 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDDocument.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDDocument.java
@@ -16,8 +16,6 @@
  */
 package org.apache.pdfbox.pdmodel;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
