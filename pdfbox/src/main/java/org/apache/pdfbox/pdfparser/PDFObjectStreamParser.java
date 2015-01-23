0e6bd0db6cc74b9c565b7180b3fe16f9d7e8eb68
Andreas Lehmkühler <lehmi@apache.org>
2015-01-23 18:42:30 +0000

    PDFBOX-2615: skip endobject marker when reading an object stream
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/1.8@1654303 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdfparser/PDFObjectStreamParser.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfparser/PDFObjectStreamParser.java
@@ -120,6 +120,13 @@ public class PDFObjectStreamParser extends BaseParser
                 {
                     LOG.debug( "parsed=" + object );
                 }
+                // According to the spec objects within an object stream shall not be enclosed 
+                // by obj/endobj tags, but there are some pdfs in the wild using those tags 
+                // skip endobject marker if present
+                if (!pdfSource.isEOF() && pdfSource.peek() == 'e')
+                {
+                    readLine();
+                }
                 objectCounter++;
             }
         }
