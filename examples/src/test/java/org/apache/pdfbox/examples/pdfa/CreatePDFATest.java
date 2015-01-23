2dbdb2bc9ab963e63c021e7afd14b9438cfa43de
Tilman Hausherr <tilman@apache.org>
2015-01-23 08:05:08 +0000

    PDFBOX-2621: created test for CreatePDFA that uses preflight to check for PDF/A-1b validity
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/1.8@1654133 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/src/test/java/org/apache/pdfbox/examples/pdfa/CreatePDFATest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2015 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pdfbox.examples.pdfa;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+import org.apache.pdfbox.preflight.PreflightDocument;
+import org.apache.pdfbox.preflight.ValidationResult;
+import org.apache.pdfbox.preflight.ValidationResult.ValidationError;
+import org.apache.pdfbox.preflight.parser.PreflightParser;
+
+/**
+ *
+ * @author Tilman Hausherr
+ */
+public class CreatePDFATest extends TestCase
+{
+    private final String outDir = "target/test-output/";
+
+    @Override
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        new File(outDir).mkdirs();
+    }
+
+    /**
+     * Test of doIt method of class CreatePDFA.
+     */
+    public void testCreatePDFA() throws Exception
+    {
+        System.out.println("testCreatePDFA");
+        String pdfaFilename = outDir + "/PDFA.pdf";
+        String message = "The quick brown fox jumps over the lazy dog ‰ˆ¸ƒ÷‹ﬂ @∞^≤≥ {[]}";
+        CreatePDFA instance = new CreatePDFA();
+        instance.doIt(pdfaFilename, message);
+        
+        PreflightParser preflightParser = new PreflightParser(new File(pdfaFilename));
+        preflightParser.parse();
+        PreflightDocument preflightDocument = preflightParser.getPreflightDocument();
+        preflightDocument.validate();
+        ValidationResult result = preflightDocument.getResult();
+        for (ValidationError ve : result.getErrorsList())
+        {
+            System.err.println(ve.getErrorCode() + ": " + ve.getDetails());
+        }
+        assertTrue("PDF file created with CreatePDFA is not valid PDF/A-1b", result.isValid());
+        preflightDocument.close();
+        preflightParser.clearResources();
+    }
+    
+}
