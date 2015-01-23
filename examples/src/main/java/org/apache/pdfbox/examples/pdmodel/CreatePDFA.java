51edc865e11c7c967b3a1807e13761c3b8f135c5
John Hewson <jahewson@apache.org>
2015-01-23 18:41:41 +0000

    PDFBOX-2589: Moved CreatePDFA into the same directory as the other pdmodel examples
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1654302 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/pdmodel/CreatePDFA.java
+++ examples/src/main/java/org/apache/pdfbox/examples/pdmodel/CreatePDFA.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.pdfbox.examples.pdfa;
+package org.apache.pdfbox.examples.pdmodel;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
