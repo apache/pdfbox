6e7b599ebda3d0b59fadf4cc4c5eb7118741b151
John Hewson <jahewson@apache.org>
2015-01-23 18:32:02 +0000

    PDFBOX-2589: Simplify HelloWorldType1AfmPfb example
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1654295 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/src/main/java/org/apache/pdfbox/examples/pdmodel/HelloWorldType1.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.pdfbox.examples.pdmodel;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
+/**
+ * Creates a simple document with a Type 1 font (.afm + .pfb).
+ */
+public class HelloWorldType1
+{
+    public static void main(String[] args) throws IOException
+    {
+        if (args.length != 3)
+        {
+            System.err.println("usage: " + HelloWorldType1.class.getName() +
+                    " <output-file> <Message> <afm-file>");
+            System.exit(1);
+        }
+
+        String file = args[0];
+        String message = args[1];
+        String afmPath = args[2];
+        
+        PDDocument doc = new PDDocument();
+        try
+        {
+            PDPage page = new PDPage();
+            doc.addPage(page);
+
+            PDFont font = new PDType1Font(doc, new FileInputStream(afmPath),
+                    new FileInputStream(afmPath.replace(".afm", ".pfb")));
+
+            PDPageContentStream contents = new PDPageContentStream(doc, page);
+            contents.beginText();
+            contents.setFont(font, 12);
+            contents.newLineAtOffset(100, 700);
+            contents.showText(message);
+            contents.endText();
+            contents.close();
+
+            doc.save(file);
+            System.out.println(file + " created!");    
+        }
+        finally
+        {
+            doc.close();
+        }
+    }
+}
