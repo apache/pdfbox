8c6a72fbca692a7a6c2c28dfb8cc280d179726cc
Tilman Hausherr <tilman@apache.org>
2015-01-24 06:54:42 +0000

    PDFBOX-2576: make field final
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1654476 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/digitalsignature/PDSignature.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/digitalsignature/PDSignature.java
@@ -38,7 +38,7 @@ import org.apache.pdfbox.pdmodel.common.COSObjectable;
  */
 public class PDSignature implements COSObjectable
 {
-    private COSDictionary dictionary;
+    private final COSDictionary dictionary;
 
     /**
      * A signature filter value.
@@ -104,6 +104,7 @@ public class PDSignature implements COSObjectable
      *
      * @return The cos object that matches this Java object.
      */
+    @Override
     public COSBase getCOSObject()
     {
         return getDictionary();
