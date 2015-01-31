75fd5e9bc7310800e442067c7c18057946a7c788
Maruan Sahyoun <msahyoun@apache.org>
2015-01-31 15:24:18 +0000

    PDFBOX-2619 allow to retrieve the properties raw value
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/1.8@1656204 13f79535-47bb-0310-9956-ffa450edef68

--- xmpbox/src/main/java/org/apache/xmpbox/type/AbstractSimpleProperty.java
+++ xmpbox/src/main/java/org/apache/xmpbox/type/AbstractSimpleProperty.java
@@ -35,6 +35,8 @@ public abstract class AbstractSimpleProperty extends AbstractField
     private String namespace;
 
     private String prefix;
+    
+    private Object rawValue;
 
     /**
      * Property specific type constructor (namespaceURI is given)
@@ -57,7 +59,7 @@ public abstract class AbstractSimpleProperty extends AbstractField
         setValue(value);
         this.namespace = namespaceURI;
         this.prefix = prefix;
-
+        this.rawValue = value;
     }
 
     /**
@@ -76,6 +78,21 @@ public abstract class AbstractSimpleProperty extends AbstractField
     public abstract String getStringValue();
 
     public abstract Object getValue();
+    
+    /**
+     * Return the properties raw value.
+     * <p>
+     * The properties raw value is how it has been
+     * serialized into the XML. Allows to retrieve the
+     * low level date for validation purposes.
+     * </p> 
+     * 
+     * @return the raw value.
+     */
+    public Object getRawValue()
+    {
+        return rawValue;
+    }
 
     public String toString()
     {
