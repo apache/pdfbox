35ad6a97a08d6a2a24649da6657e62f187a04f56
John Hewson <jahewson@apache.org>
2015-02-06 01:16:35 +0000

    PDFBOX-2580: Move PDAppearanceString and helpers back into the pdmodel.interactive.form package
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1657733 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/AppearanceGenerator.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/AppearanceGenerator.java
@@ -14,26 +14,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.pdfbox.service.interactive.form;
+package org.apache.pdfbox.pdmodel.interactive.form;
 
 import java.io.IOException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.pdfbox.pdmodel.common.PDTextStream;
-import org.apache.pdfbox.pdmodel.interactive.form.PDField;
-import org.apache.pdfbox.pdmodel.interactive.form.PDVariableText;
 
 /**
  * (Re-) Generate the appearance for a field.
  * 
  * The fields appearance defines the 'look' the field has when it's rendered for display or printing.
- * 
  */
-
-public final class AppearanceGenerator
+class AppearanceGenerator
 {
-
     private static final Log LOG = LogFactory.getLog(AppearanceGenerator.class);
 
     private AppearanceGenerator()
