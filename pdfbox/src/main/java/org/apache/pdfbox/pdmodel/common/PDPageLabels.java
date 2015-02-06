28795c3e8d07186ad642222eb7a8e9eaa4ef3b3f
Tilman Hausherr <tilman@apache.org>
2015-02-06 21:52:43 +0000

    PDFBOX-2576: remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1657975 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/PDPageLabels.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/PDPageLabels.java
@@ -22,7 +22,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.Map.Entry;
 
