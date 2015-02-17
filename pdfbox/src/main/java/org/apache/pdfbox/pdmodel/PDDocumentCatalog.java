406325af2a7262888e250b9068f71eeb6908f087
Maruan Sahyoun <msahyoun@apache.org>
2015-02-17 19:40:34 +0000

    PDFBOX-2687 avoid ClassCastException when adding an OutputIntent; rename getter for OutputIntents
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1660475 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDDocumentCatalog.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDDocumentCatalog.java
@@ -328,7 +328,7 @@ public class PDDocumentCatalog implements COSObjectable
      *
      * @return The list of PDOutputIntent
      */
-    public List<PDOutputIntent> getOutputIntent ()
+    public List<PDOutputIntent> getOutputIntents()
     {
         List<PDOutputIntent> retval = new ArrayList<PDOutputIntent>();
         COSArray array = (COSArray)root.getDictionaryObject(COSName.OUTPUT_INTENTS);
@@ -349,9 +349,9 @@ public class PDDocumentCatalog implements COSObjectable
      *
      * @param outputIntent the OutputIntent to add.
      */
-    public void addOutputIntent (PDOutputIntent outputIntent)
+    public void addOutputIntent(PDOutputIntent outputIntent)
     {
-        COSArray array = (COSArray)root.getItem(COSName.OUTPUT_INTENTS);
+        COSArray array = (COSArray)root.getDictionaryObject(COSName.OUTPUT_INTENTS);
         if (array == null)
         {
             array = new COSArray();
@@ -366,7 +366,8 @@ public class PDDocumentCatalog implements COSObjectable
      * @param outputIntents the list of OutputIntents, if the list is empty all OutputIntents are
      * removed.
      */
-    public void setOutputIntents (List<PDOutputIntent> outputIntents) {
+    public void setOutputIntents(List<PDOutputIntent> outputIntents) 
+    {
         COSArray array = new COSArray();
         for (PDOutputIntent intent : outputIntents)
         {
