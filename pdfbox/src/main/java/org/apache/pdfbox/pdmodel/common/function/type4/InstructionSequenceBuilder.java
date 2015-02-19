52e169271fa55eb27963d633bb15b0b778bf7875
Tilman Hausherr <tilman@apache.org>
2015-02-19 19:28:10 +0000

    PDFBOX-2576: make class final; make fields final
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1660981 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/function/type4/InstructionSequenceBuilder.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/function/type4/InstructionSequenceBuilder.java
@@ -25,11 +25,11 @@ import java.util.regex.Pattern;
  *
  * @version $Revision$
  */
-public class InstructionSequenceBuilder extends Parser.AbstractSyntaxHandler
+public final class InstructionSequenceBuilder extends Parser.AbstractSyntaxHandler
 {
 
-    private InstructionSequence mainSequence = new InstructionSequence();
-    private Stack<InstructionSequence> seqStack = new Stack<InstructionSequence>();
+    private final InstructionSequence mainSequence = new InstructionSequence();
+    private final Stack<InstructionSequence> seqStack = new Stack<InstructionSequence>();
 
     private InstructionSequenceBuilder()
     {
@@ -67,6 +67,7 @@ public class InstructionSequenceBuilder extends Parser.AbstractSyntaxHandler
     private static final Pattern REAL_PATTERN = Pattern.compile("[\\-]?\\d*\\.\\d*([Ee]\\-?\\d+)?");
 
     /** {@inheritDoc} */
+    @Override
     public void token(CharSequence text)
     {
         String token = text.toString();
