6922ab7590dbdee852eb2e21942753f8a2d04eea
Tilman Hausherr <tilman@apache.org>
2015-02-25 17:04:09 +0000

    PDFBOX-2576: add private constructor; make field final
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1662266 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/ttf/WGL4Names.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/WGL4Names.java
@@ -81,7 +81,7 @@ public class WGL4Names
     /**
      * The indices of the standard mac glyph names.
      */
-    public static Map<String,Integer> MAC_GLYPH_NAMES_INDICES;
+    public static final Map<String,Integer> MAC_GLYPH_NAMES_INDICES;
 
     static
     {
@@ -90,5 +90,9 @@ public class WGL4Names
         {
             MAC_GLYPH_NAMES_INDICES.put(MAC_GLYPH_NAMES[i],i);
         }
+    }
+    
+    private WGL4Names ()
+    {
     }
 }
