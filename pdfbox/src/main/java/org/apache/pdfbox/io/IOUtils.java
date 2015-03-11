717aa29350bd69a0f13efc3e1bc4c3124cdfc1f1
Tilman Hausherr <tilman@apache.org>
2015-03-11 21:08:39 +0000

    PDFBOX-2707: replace four closeQuietly methods with one, as suggested by Andrea Vacondio
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1666014 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/io/IOUtils.java
+++ pdfbox/src/main/java/org/apache/pdfbox/io/IOUtils.java
@@ -20,11 +20,10 @@
 package org.apache.pdfbox.io;
 
 import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.Reader;
-import java.io.Writer;
 
 /**
  * This class contains various I/O-related methods.
@@ -97,80 +96,23 @@ public final class IOUtils
         return buffer.length - remaining;
     }
 
-
     /**
-     * Unconditionally close an <code>InputStream</code>.
-     * <p>
-     * Equivalent to {@link InputStream#close()}, except any exceptions will be ignored.
-     * This is typically used in finally blocks.
+     * Null safe close of the given {@link Closeable} suppressing any exception.
      *
-     * @param input  the InputStream to close, may be null or already closed
+     * @param closeable to be closed
      */
-    public static void closeQuietly(InputStream input) {
-        try {
-            if (input != null) {
-                input.close();
+    public static void closeQuietly(Closeable closeable)
+    {
+        try
+        {
+            if (closeable != null)
+            {
+                closeable.close();
             }
-        } catch (IOException ioe) {
+        }
+        catch (IOException ioe)
+        {
             // ignore
         }
     }
-    
-    /**
-     * Unconditionally close an <code>Reader</code>.
-     * <p>
-     * Equivalent to {@link Reader#close()}, except any exceptions will be ignored.
-     * This is typically used in finally blocks.
-     *
-     * @param input  the Reader to close, may be null or already closed
-     */
-    public static void closeQuietly(Reader input) {
-        try {
-            if (input != null) {
-                input.close();
-            }
-        } catch (IOException ioe) {
-            // ignore
-        }
-    }
-
-    /**
-     * Unconditionally close a <code>Writer</code>.
-     * <p>
-     * Equivalent to {@link Writer#close()}, except any exceptions will be ignored.
-     * This is typically used in finally blocks.
-     *
-     * @param output  the Writer to close, may be null or already closed
-     */
-    public static void closeQuietly(Writer output) {
-        try {
-            if (output != null) {
-                output.close();
-            }
-        } catch (IOException ioe) {
-            // ignore
-        }
-    }
-
-    /**
-     * Unconditionally close an <code>OutputStream</code>.
-     * <p>
-     * Equivalent to {@link OutputStream#close()}, except any exceptions will be ignored.
-     * This is typically used in finally blocks.
-     *
-     * @param output  the OutputStream to close, may be null or already closed
-     */
-    public static void closeQuietly(OutputStream output) {
-        try {
-            if (output != null) {
-                output.close();
-            }
-        } catch (IOException ioe) {
-            // ignore
-        }
-    }
-
-
-    
-    
 }
