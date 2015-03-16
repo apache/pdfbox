1c73c94fb5ee1bb839d9b45fa1b76f5c7388d1e9
Maruan Sahyoun <msahyoun@apache.org>
2015-03-16 13:34:37 +0000

    PDFBOX-2710 don't look up field type in kids
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/1.8@1666991 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDFieldFactory.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDFieldFactory.java
@@ -17,11 +17,8 @@
 package org.apache.pdfbox.pdmodel.interactive.form;
 
 import org.apache.pdfbox.cos.COSDictionary;
-import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationWidget;
 
 import java.io.IOException;
-
-import java.util.List;
 
 /**
  * This is the Factory for creating and returning the correct
@@ -34,7 +31,6 @@ public class PDFieldFactory
 {
     private static final int RADIO_BITMASK = 32768;
     private static final int PUSHBUTTON_BITMASK = 65536;
-    private static final int RADIOS_IN_UNISON_BITMASK = 33554432;
 
     private static final String FIELD_TYPE_BTN = "Btn";
     private static final String FIELD_TYPE_TX = "Tx";
@@ -148,29 +144,7 @@ public class PDFieldFactory
     private static boolean isButton(PDField field) throws IOException
     {
         String ft = field.findFieldType();
-        boolean retval = FIELD_TYPE_BTN.equals( ft );
-        List kids = field.getKids();
-        if( ft == null && kids != null && kids.size() > 0)
-        {
-            //sometimes if it is a button the type is only defined by one
-            //of the kids entries
-            Object obj = kids.get( 0 );
-            COSDictionary kidDict = null;
-            if( obj instanceof PDField )
-            {
-                kidDict = ((PDField)obj).getDictionary();
-            }
-            else if( obj instanceof PDAnnotationWidget )
-            {
-                kidDict = ((PDAnnotationWidget)obj).getDictionary();
-            }
-            else
-            {
-                throw new IOException( "Error:Unexpected type of kids field:" + obj );
-            }
-            retval = isButton( new PDUnknownField( field.getAcroForm(), kidDict ) );
-        }
-        return retval;
+        return FIELD_TYPE_BTN.equals( ft );
     }
 
    /**
