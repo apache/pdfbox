58b561d9638f22a667064ff1d4914958e6b4963f
Tilman Hausherr <tilman@apache.org>
2015-04-02 21:33:18 +0000

    PDFBOX-2576: make fields final
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1670979 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/function/type4/Operators.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/function/type4/Operators.java
@@ -74,7 +74,7 @@ public class Operators
     private static final Operator POP = new StackOperators.Pop();
     private static final Operator ROLL = new StackOperators.Roll();
 
-    private Map<String, Operator> operators = new java.util.HashMap<String, Operator>();
+    private final Map<String, Operator> operators = new java.util.HashMap<String, Operator>();
 
     /**
      * Creates a new Operators object with the default set of operators.
