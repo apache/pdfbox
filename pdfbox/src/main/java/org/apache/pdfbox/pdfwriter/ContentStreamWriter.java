1268ad0de225a19c8e23f69e8f3dce019ec89c8b
Tilman Hausherr <tilman@apache.org>
2015-04-22 17:12:07 +0000

    PDFBOX-2772: apppend EI after inline image
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/1.8@1675423 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdfwriter/ContentStreamWriter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfwriter/ContentStreamWriter.java
@@ -154,6 +154,9 @@ public class ContentStreamWriter
                 output.write( "ID".getBytes("ISO-8859-1") );
                 output.write( EOL );
                 output.write( op.getImageData() );
+                output.write( EOL );
+                output.write( "EI".getBytes("ISO-8859-1") );
+                output.write( EOL );
             }
             else
             {
