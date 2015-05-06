e92a7c68122ecf811202c4d808f1cbbadfa47912
Tilman Hausherr <tilman@apache.org>
2015-05-06 16:40:24 +0000

    PDFBOX-2576: remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1678039 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDDocumentInformation.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDDocumentInformation.java
@@ -20,7 +20,6 @@ import java.util.Calendar;
 import java.util.Set;
 import java.util.TreeSet;
 
-import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
 
