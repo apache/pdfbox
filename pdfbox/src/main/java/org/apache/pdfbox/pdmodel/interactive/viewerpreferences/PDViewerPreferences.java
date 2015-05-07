8448e6ca9e7d0a57017138adf3da71294cf0efae
Tilman Hausherr <tilman@apache.org>
2015-05-07 17:50:51 +0000

    PDFBOX-2783: Use getCOSObject() instead of getDictionary()
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1678243 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/viewerpreferences/PDViewerPreferences.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/viewerpreferences/PDViewerPreferences.java
@@ -16,7 +16,6 @@
  */
 package org.apache.pdfbox.pdmodel.interactive.viewerpreferences;
 
-import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
 
@@ -196,7 +195,7 @@ public class PDViewerPreferences implements COSObjectable
         AppDefault
     }
 
-    private COSDictionary prefs;
+    private final COSDictionary prefs;
 
     /**
      * Constructor that is used for a preexisting dictionary.
@@ -213,17 +212,8 @@ public class PDViewerPreferences implements COSObjectable
      *
      * @return The underlying info dictionary.
      */
-    public COSDictionary getDictionary()
-    {
-        return prefs;
-    }
-
-    /**
-     * Convert this standard java object to a COS object.
-     *
-     * @return The cos object that matches this Java object.
-     */
-    public COSBase getCOSObject()
+    @Override
+    public COSDictionary getCOSObject()
     {
         return prefs;
     }
