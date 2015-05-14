b64cc4250267c13fe65a0f0c4bf86454a31ca2c7
kbashar <khyrulbashar.0.1@gmail.com>
2015-05-14 15:30:40 +0600

    all tree string showing functionalites without pretty ui

--- tools/src/main/java/org/apache/pdfbox/tools/pdfdebugger/ui/TreeStatusPane.java
+++ tools/src/main/java/org/apache/pdfbox/tools/pdfdebugger/ui/TreeStatusPane.java
@@ -1,26 +1,48 @@
 package org.apache.pdfbox.tools.pdfdebugger.ui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JTree;
+import javax.swing.border.Border;
+import javax.swing.tree.TreePath;
+import org.apache.pdfbox.cos.COSArray;
+import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.cos.COSObject;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.tools.gui.ArrayEntry;
+import org.apache.pdfbox.tools.gui.MapEntry;
 
 /**
  * Created by kbashar on 5/5/15.
  */
 public class TreeStatusPane extends JPanel implements MouseListener
 {
+    private Object rootNode;
+    private JTree tree;
+
     private String statusString;
     private JTextField statusTextEditField;
     private JLabel statusLabel;
+    private Border textFieldDefaultBorder;
+    private Border textFieldErrorBorder;
+    private Action textInputAction;
 
-    public TreeStatusPane()
+    public TreeStatusPane(JTree _tree)
     {
+        tree = _tree;
         init();
     }
 
@@ -32,6 +54,31 @@ public class TreeStatusPane extends JPanel implements MouseListener
         statusLabel = new JLabel();
         this.add(statusLabel);
         this.addMouseListener(this);
+        textFieldDefaultBorder = BorderFactory.createLineBorder(Color.black);
+        textFieldErrorBorder = BorderFactory.createLineBorder(Color.RED);
+        textInputAction = new AbstractAction()
+        {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent)
+            {
+                try
+                {
+
+                    TreePath path = getPath(statusTextEditField.getText());
+                    tree.setSelectionPath(path);
+                }
+                catch (NullPointerException e)
+                {
+                    statusTextEditField.setBorder(textFieldErrorBorder);
+                }
+            }
+        };
+        statusTextEditField.setAction(textInputAction);
+    }
+
+    public void updateRootNode(Object obj)
+    {
+        rootNode = obj;
     }
 
     public String getStatusString()
@@ -64,6 +111,118 @@ public class TreeStatusPane extends JPanel implements MouseListener
         }
     }
 
+    private TreePath getPath(String statusString) throws NullPointerException
+    {
+        ArrayList<String> nodes = parsePathString(statusString);
+        Object obj = rootNode;
+        TreePath path = new TreePath(obj);
+        for (String node : nodes)
+        {
+            obj = searchNode(obj, node);
+            path = path.pathByAddingChild(obj);
+        }
+        return path;
+    }
+
+    private ArrayList<String> parsePathString(String path)
+    {
+        ArrayList<String> nodes = new ArrayList<String>();
+        for (String node : path.split(">"))
+        {
+            if (node.startsWith("["))
+            {
+                ;
+            }
+            {
+                node = node.replace("]", "").replace("[", "");
+            }
+            node = node.trim();
+            System.out.println(node);
+            nodes.add(node);
+        }
+        return nodes;
+    }
+
+    private Object searchNode(Object obj, String searchStr)
+    {
+        if (obj instanceof MapEntry)
+        {
+            obj = ((MapEntry) obj).getValue();
+        }
+        else if (obj instanceof ArrayEntry)
+        {
+            obj = ((ArrayEntry) obj).getValue();
+        }
+        if (obj instanceof COSObject)
+        {
+            obj = ((COSObject) obj).getObject();
+        }
+        if (obj instanceof COSDictionary)
+        {
+            COSDictionary dic = (COSDictionary) obj;
+            if (dic.containsKey(searchStr))
+            {
+                MapEntry entry = new MapEntry();
+                entry.setKey(COSName.getPDFName(searchStr));
+                entry.setValue(dic.getItem(searchStr));
+                return entry;
+            }
+        }
+        else if (obj instanceof COSArray)
+        {
+            int index = Integer.parseInt(searchStr);
+            COSArray array = (COSArray) obj;
+            if (index <= array.size() - 1)
+            {
+                ArrayEntry entry = new ArrayEntry();
+                entry.setIndex(index);
+                entry.setValue(array.getObject(index));
+                return entry;
+            }
+        }
+        else
+        {
+            System.out.println(obj.toString());
+            return null;
+        }
+        return null;
+    }
+
+    public void showPath(TreePath path)
+    {
+        StringBuilder pathStringBuilder = new StringBuilder();
+        while (path.getParentPath() != null)
+        {
+            Object object = path.getLastPathComponent();
+            pathStringBuilder.insert(0, " > " + getObjectName(object));
+            path = path.getParentPath();
+        }
+        pathStringBuilder.delete(0, 2);
+        System.out.println(pathStringBuilder.toString());
+        this.setStatusString(pathStringBuilder.toString());
+    }
+
+    private String getObjectName(Object object)
+    {
+        String name = "";
+        if (object instanceof MapEntry)
+        {
+            MapEntry entry = (MapEntry) object;
+            COSName key = (COSName) entry.getKey();
+            name = key.getName();
+        }
+        else if (object instanceof ArrayEntry)
+        {
+            ArrayEntry entry = (ArrayEntry) object;
+            name = "[" + entry.getIndex() + "]";
+        }
+        else
+        {
+            throw new RuntimeException("Unknown COS type " + object.getClass().getName());
+        }
+        return name;
+    }
+
     @Override
     public void mouseClicked(MouseEvent mouseEvent)
     {
@@ -80,7 +239,8 @@ public class TreeStatusPane extends JPanel implements MouseListener
             this.remove(statusLabel);
             this.validate();
             statusTextEditField.setText(statusString);
-            this.add(statusTextEditField);
+            statusTextEditField.setBorder(textFieldDefaultBorder);
+            this.add(statusTextEditField, BorderLayout.PAGE_END);
         }
     }
 
