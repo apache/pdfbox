3e7448f055f9903f1a9d6b9562d859d842761f29
Tilman Hausherr <tilman@apache.org>
2015-05-22 21:41:24 +0000

    PDFBOX-2576: make fields final; remove unneeded casts
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1681241 13f79535-47bb-0310-9956-ffa450edef68

--- tools/src/test/java/org/apache/pdfbox/tools/TestExtractText.java
+++ tools/src/test/java/org/apache/pdfbox/tools/TestExtractText.java
@@ -49,7 +49,7 @@ public class TestExtractText extends TestCase
         }
 
         String result = outBytes.toString("UTF-8");
-        assertTrue(result.indexOf("PDF1") != -1);
-        assertTrue(result.indexOf("PDF2") != -1);
+        assertTrue(result.contains("PDF1"));
+        assertTrue(result.contains("PDF2"));
     }
 }
