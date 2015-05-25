e9d5119b47968a89884057300a73a97bdf0d7f06
Tilman Hausherr <tilman@apache.org>
2015-05-25 19:34:06 +0000

    PDFBOX-2812: avoid NPE in PDColorSpaceFactory.createColorSpace()
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/1.8@1681643 13f79535-47bb-0310-9956-ffa450edef68

---
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/graphics/color/PDICCBasedTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2015 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pdfbox.pdmodel.graphics.color;
+
+import java.awt.color.ColorSpace;
+import java.io.IOException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.junit.Assert;
+import static org.junit.Assert.assertFalse;
+import org.junit.Test;
+
+/**
+ *
+ * @author Tilman Hausherr
+ */
+public class PDICCBasedTest
+{
+
+    public PDICCBasedTest()
+    {
+    }
+
+    /**
+     * Test of Constructor for PDFBOX-2812.
+     */
+    @Test
+    public void testConstructor() throws IOException
+    {
+        PDDocument doc = new PDDocument();
+        ColorSpace cs = ColorSpace.getInstance(ColorSpace.CS_GRAY);
+        assertFalse(cs.isCS_sRGB()); // this test doesn't work with CS_sRGB
+        PDICCBased iccBased = (PDICCBased) PDColorSpaceFactory.createColorSpace(doc, cs);
+        Assert.assertEquals("ICCBased", iccBased.getName());
+    }
+
+}
