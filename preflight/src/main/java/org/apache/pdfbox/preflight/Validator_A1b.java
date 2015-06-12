5c82b023d3a21888605a8115212b736ba6b70ec4
Tilman Hausherr <tilman@apache.org>
2015-06-12 20:49:55 +0000

    PDFBOX-2576: remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1685162 13f79535-47bb-0310-9956-ffa450edef68

--- preflight/src/main/java/org/apache/pdfbox/preflight/Validator_A1b.java
+++ preflight/src/main/java/org/apache/pdfbox/preflight/Validator_A1b.java
@@ -34,7 +34,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
