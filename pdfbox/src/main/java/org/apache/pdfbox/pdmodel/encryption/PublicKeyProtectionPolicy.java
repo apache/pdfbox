91bef191119db388a608c8d3d95bd783d1dfad3e
Tilman Hausherr <tilman@apache.org>
2015-06-13 14:59:04 +0000

    PDFBOX-2576: make field private, as a public iterator is available
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1685266 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/PublicKeyProtectionPolicy.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/PublicKeyProtectionPolicy.java
@@ -59,7 +59,7 @@ import java.util.List;
  */
 public final class PublicKeyProtectionPolicy extends ProtectionPolicy
 {
-    public final List<PublicKeyRecipient> recipients = new ArrayList<PublicKeyRecipient>();
+    private final List<PublicKeyRecipient> recipients = new ArrayList<PublicKeyRecipient>();
     private X509Certificate decryptionCertificate;
 
     /**
