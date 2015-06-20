a5ed0ad555f367efc221ca8c6674e065c56aba92
Tilman Hausherr <tilman@apache.org>
2015-06-20 10:55:00 +0000

    PDFBOX-2837: avoid output being written in EBCDIC in non ASCII systems
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1686558 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdfwriter/COSWriter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfwriter/COSWriter.java
@@ -702,7 +702,7 @@ public class COSWriter implements ICOSVisitor, Closeable
         byte[] buffer = byteOut.toByteArray();
 
         // overwrite the ByteRange in the buffer
-        byte[] byteRangeBytes = byteRange.getBytes();
+        byte[] byteRangeBytes = byteRange.getBytes(Charsets.ISO_8859_1);
         for (int i = 0; i < byteRangeLength; i++)
         {
             if (i >= byteRangeBytes.length)
@@ -738,7 +738,7 @@ public class COSWriter implements ICOSVisitor, Closeable
         }
 
         // overwrite the signature Contents in the buffer
-        byte[] signatureBytes = signature.getBytes();
+        byte[] signatureBytes = signature.getBytes(Charsets.ISO_8859_1);
         System.arraycopy(signatureBytes, 0, buffer, bufSignatureOffset + 1, signatureBytes.length);
 
         // write the data to the incremental output stream
