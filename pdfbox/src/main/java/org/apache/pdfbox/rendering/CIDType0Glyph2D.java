8df670b6d978ebd643ba9588f71870f8b7c4d286
Tilman Hausherr <tilman@apache.org>
2015-07-06 20:56:26 +0000

    PDFBOX-2852: remove public modifier from constructor in non public class
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1689497 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/CIDType0Glyph2D.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/CIDType0Glyph2D.java
@@ -42,7 +42,7 @@ final class CIDType0Glyph2D implements Glyph2D
      *
      * @param font Type 0 CIDFont
      */
-    public CIDType0Glyph2D(PDCIDFontType0 font) // todo: what about PDCIDFontType2?
+    CIDType0Glyph2D(PDCIDFontType0 font) // todo: what about PDCIDFontType2?
     {
         this.font = font;
         fontName = font.getBaseFont();
