1809bc720bf507d1ee8bee2760bcbfef0893bfa4
Tilman Hausherr <tilman@apache.org>
2015-07-14 15:52:51 +0000

    PDFBOX-2852: remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1691003 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDAnnotationAdditionalActions.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDAnnotationAdditionalActions.java
@@ -16,7 +16,6 @@
  */
 package org.apache.pdfbox.pdmodel.interactive.action;
 
-import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.pdmodel.common.COSObjectable;
 
