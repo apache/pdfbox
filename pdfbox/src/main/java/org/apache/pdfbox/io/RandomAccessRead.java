e466c728e5d6ac6c14273b1afa0997610268a78b
John Hewson <jahewson@apache.org>
2015-07-17 22:35:24 +0000

    PDFBOX-2301: Split RandomAccessWrite into its own interface
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1691656 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/io/RandomAccessRead.java
+++ pdfbox/src/main/java/org/apache/pdfbox/io/RandomAccessRead.java
@@ -16,14 +16,43 @@
  */
 package org.apache.pdfbox.io;
 
+import java.io.Closeable;
 import java.io.IOException;
 
 /**
  * An interface allowing random access read operations.
  */
-public interface RandomAccessRead extends SequentialRead
+public interface RandomAccessRead extends Closeable
 {
+    /**
+     * Read a single byte of data.
+     *
+     * @return The byte of data that is being read.
+     *
+     * @throws IOException If there is an error while reading the data.
+     */
+    int read() throws IOException;
 
+    /**
+     * Read a buffer of data.
+     *
+     * @param b The buffer to write the data to.
+     * @return The number of bytes that were actually read.
+     * @throws IOException If there was an error while reading the data.
+     */
+    int read(byte[] b) throws IOException;
+
+    /**
+     * Read a buffer of data.
+     *
+     * @param b The buffer to write the data to.
+     * @param offset Offset into the buffer to start writing.
+     * @param length The amount of data to attempt to read.
+     * @return The number of bytes that were actually read.
+     * @throws IOException If there was an error while reading the data.
+     */
+    int read(byte[] b, int offset, int length) throws IOException;
+    
     /**
      * Returns offset of next byte to be returned by a read method.
      * 
