55447fe5a008ba4c7b8ff9de83cf545cabada398
Tilman Hausherr <tilman@apache.org>
2015-07-25 14:44:54 +0000

    PDFBOX-2530: zoom feature for images + refactor zoom menu as singleton, as done by Khyrul Bashar in GSoC2015
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1692625 13f79535-47bb-0310-9956-ffa450edef68

--- tools/src/main/java/org/apache/pdfbox/tools/PDFDebugger.java
+++ tools/src/main/java/org/apache/pdfbox/tools/PDFDebugger.java
@@ -39,16 +39,13 @@ import java.util.List;
 import java.util.Set;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
-import javax.swing.ButtonGroup;
 import javax.swing.JComponent;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.KeyStroke;
-import javax.swing.SwingUtilities;
 import javax.swing.TransferHandler;
 import javax.swing.UIManager;
 import javax.swing.border.BevelBorder;
@@ -85,6 +82,7 @@ import org.apache.pdfbox.tools.pdfdebugger.streampane.StreamPane;
 import org.apache.pdfbox.tools.pdfdebugger.treestatus.TreeStatus;
 import org.apache.pdfbox.tools.pdfdebugger.treestatus.TreeStatusPane;
 import org.apache.pdfbox.tools.pdfdebugger.ui.Tree;
+import org.apache.pdfbox.tools.pdfdebugger.ui.ZoomMenu;
 import org.apache.pdfbox.tools.util.FileOpenSaveDialog;
 import org.apache.pdfbox.tools.util.RecentFiles;
 
@@ -98,7 +96,6 @@ public class PDFDebugger extends javax.swing.JFrame
     private TreeStatusPane statusPane;
     private RecentFiles recentFiles;
     private boolean isPageMode;
-    private float scale = 1;
 
     private PDDocument document;
     private String currentFilePath;
@@ -155,15 +152,6 @@ public class PDFDebugger extends javax.swing.JFrame
         helpMenu = new JMenu();
         contentsMenuItem = new JMenuItem();
         aboutMenuItem = new JMenuItem();
-        zoomMenu = new JMenu();
-        zoom50Item = new JRadioButtonMenuItem();
-        zoom100Item = new JRadioButtonMenuItem();
-        zoom200Item = new JRadioButtonMenuItem();
-        zoom100Item.setSelected(true);
-        ButtonGroup bg = new ButtonGroup();
-        bg.add(zoom50Item);
-        bg.add(zoom100Item);
-        bg.add(zoom200Item);
 
         tree.setCellRenderer( new PDFTreeCellRenderer() );
         tree.setModel( null );
@@ -313,38 +301,9 @@ public class PDFDebugger extends javax.swing.JFrame
         aboutMenuItem.setText("About");
         helpMenu.add(aboutMenuItem);
         
-        zoomMenu.setText("Zoom");
-        zoom50Item.setText("50%");
-        zoom100Item.setText("100%");
-        zoom200Item.setText("200%");
-        Action zoomAction = new AbstractAction()
-        {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent)
-            {
-                Object source = actionEvent.getSource();
-                if (zoom50Item.equals(source))
-                {
-                    scale = 0.5f;
-                }
-                if (zoom100Item.equals(source))
-                {
-                    scale = 1;
-                }
-                if (zoom200Item.equals(source))
-                {
-                    scale = 2;
-                }
-                jTree1ValueChanged(null);
-            }
-        };
-        zoom50Item.addActionListener(zoomAction);
-        zoom100Item.addActionListener(zoomAction);
-        zoom200Item.addActionListener(zoomAction);        
-        zoomMenu.add(zoom50Item);
-        zoomMenu.add(zoom100Item);
-        zoomMenu.add(zoom200Item);
-        viewMenu.add(zoomMenu);
+        ZoomMenu zoomMenu = ZoomMenu.getInstance();
+        zoomMenu.setEnableMenu(false);
+        viewMenu.add(zoomMenu.getMenu());
 
         setJMenuBar(menuBar);
 
@@ -477,13 +436,9 @@ public class PDFDebugger extends javax.swing.JFrame
                 
                 if (isPage(selectedNode))
                 {
-                    zoomMenu.setEnabled(true);
-                    SwingUtilities.updateComponentTreeUI(menuBar);
                     showPage(selectedNode);
                     return;
                 }
-                zoomMenu.setEnabled(false);
-                SwingUtilities.updateComponentTreeUI(menuBar);
                 
                 if (isSpecialColorSpace(selectedNode) || isOtherColorSpace(selectedNode))
                 {
@@ -601,26 +556,6 @@ public class PDFDebugger extends javax.swing.JFrame
         return getUnderneathObject(selectedNode) instanceof COSStream;
     }
 
-    private boolean isFont(Object selectedNode)
-    {
-        selectedNode = getUnderneathObject(selectedNode);
-        if (selectedNode instanceof COSDictionary)
-        {
-            COSDictionary dic = (COSDictionary)selectedNode;
-            return  dic.containsKey(COSName.TYPE) &&
-                    dic.getCOSName(COSName.TYPE).equals(COSName.FONT) &&
-                    !isCIDFont(dic);
-        }
-        return false;
-    }
-
-    private boolean isCIDFont(COSDictionary dic)
-    {
-        return dic.containsKey(COSName.SUBTYPE) &&
-                (dic.getCOSName(COSName.SUBTYPE).equals(COSName.CID_FONT_TYPE0)
-                || dic.getCOSName(COSName.SUBTYPE).equals(COSName.CID_FONT_TYPE2));
-    }
-
     /**
      * Show a Panel describing color spaces in more detail and interactive way.
      * @param csNode the special color space containing node.
@@ -673,7 +608,7 @@ public class PDFDebugger extends javax.swing.JFrame
         COSBase typeItem = page.getItem(COSName.TYPE);
         if (COSName.PAGE.equals(typeItem))
         {
-            PagePane pagePane = new PagePane(document, page, scale);
+            PagePane pagePane = new PagePane(document, page);
             jSplitPane1.setRightComponent(new JScrollPane(pagePane.getPanel()));
         }
     }
@@ -1057,10 +992,6 @@ public class PDFDebugger extends javax.swing.JFrame
     private JMenu recentFilesMenu;
     private JMenu viewMenu;
     private JMenuItem viewModeItem;
-    private JMenu zoomMenu;
-    private JRadioButtonMenuItem zoom50Item;
-    private JRadioButtonMenuItem zoom100Item;
-    private JRadioButtonMenuItem zoom200Item;
     private JScrollPane jScrollPane1;
     private JScrollPane jScrollPane2;
     private javax.swing.JSplitPane jSplitPane1;
---
+++ tools/src/main/java/org/apache/pdfbox/tools/pdfdebugger/ui/ZoomMenu.java
@@ -0,0 +1,130 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.pdfbox.tools.pdfdebugger.ui;
+
+import java.awt.Component;
+import java.awt.event.ActionListener;
+import javax.swing.ButtonGroup;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+
+/**
+ * @author Khyrul Bashar
+ */
+public class ZoomMenu
+{
+    public static final String ZOOM_50_PERCENT = "50%";
+    public static final String ZOOM_100_PERCENT = "100%";
+    public static final String ZOOM_200_PERCENT = "200%";
+
+    private static ZoomMenu instance;
+    private JMenu menu = null;
+    private JRadioButtonMenuItem zoom50Item;
+    private JRadioButtonMenuItem zoom100Item;
+    private JRadioButtonMenuItem zoom200Item;
+
+    private ZoomMenu()
+    {
+        menu = createZoomMenu();
+    }
+
+    public static ZoomMenu getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new ZoomMenu();
+        }
+        return instance;
+    }
+
+    public JMenu getMenu()
+    {
+        return this.menu;
+    }
+
+    public void setEnableMenu(boolean isEnable)
+    {
+        menu.setEnabled(isEnable);
+    }
+
+    public ZoomMenu menuListeners(ActionListener listener)
+    {
+        for (Component comp: menu.getMenuComponents())
+        {
+            JMenuItem menuItem = (JMenuItem) comp;
+            removeActionListeners(menuItem);
+            menuItem.addActionListener(listener);
+        }
+        return instance;
+    }
+
+    public void setZoomSelection(String selection)
+    {
+        if (ZOOM_50_PERCENT.equals(selection))
+        {
+            zoom50Item.setSelected(true);
+        }
+        else if (ZOOM_100_PERCENT.equals(selection))
+        {
+            zoom100Item.setSelected(true);
+        }
+        else if (ZOOM_200_PERCENT.equals(selection))
+        {
+            zoom200Item.setSelected(true);
+        }
+        else
+        {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void removeActionListeners(JMenuItem menuItem)
+    {
+        for (ActionListener listener: menuItem.getActionListeners())
+        {
+            menuItem.removeActionListener(listener);
+        }
+    }
+
+    private JMenu createZoomMenu()
+    {
+        menu = new JMenu();
+        menu.setText("Zoom");
+
+        zoom50Item = new JRadioButtonMenuItem();
+        zoom100Item = new JRadioButtonMenuItem();
+        zoom200Item = new JRadioButtonMenuItem();
+        zoom100Item.setSelected(true);
+
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(zoom50Item);
+        bg.add(zoom100Item);
+        bg.add(zoom200Item);
+
+        zoom50Item.setText(ZOOM_50_PERCENT);
+        zoom100Item.setText(ZOOM_100_PERCENT);
+        zoom200Item.setText(ZOOM_200_PERCENT);
+
+        menu.add(zoom50Item);
+        menu.add(zoom100Item);
+        menu.add(zoom200Item);
+
+        return menu;
+    }
+}
