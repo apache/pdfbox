0923ad8e15322a18bfafc184455c69df4db68f66
Tilman Hausherr <tilman@apache.org>
2015-07-29 16:48:45 +0000

    PDFBOX-2909: throw IOException if /Function element not dictionary or array
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1693316 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/shading/PDShading.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/shading/PDShading.java
@@ -335,8 +335,8 @@ public abstract class PDShading implements COSObjectable
     /**
      * Provide the function(s) of the shading dictionary as array.
      *
-     * @return an array containing the function(s)
-     * @throws IOException if something went wrong
+     * @return an array containing the function(s).
+     * @throws IOException if we were unable to create a function.
      */
     private PDFunction[] getFunctionsArray() throws IOException
     {
@@ -348,7 +348,7 @@ public abstract class PDShading implements COSObjectable
                 functionArray = new PDFunction[1];
                 functionArray[0] = PDFunction.create(functionObject);
             }
-            else
+            else if (functionObject instanceof COSArray)
             {
                 COSArray functionCOSArray = (COSArray) functionObject;
                 int numberOfFunctions = functionCOSArray.size();
@@ -357,6 +357,10 @@ public abstract class PDShading implements COSObjectable
                 {
                     functionArray[i] = PDFunction.create(functionCOSArray.get(i));
                 }
+            }
+            else
+            {
+                throw new IOException("mandatory /Function element must be a dictionary or an array");
             }
         }
         return functionArray;
