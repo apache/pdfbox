c93a3ccb53d74d2bdbd08337a6f34ddf89bb9590
John Hewson <jahewson@apache.org>
2015-08-26 00:47:20 +0000

    PDFBOX-2941: lower quality text anti-aliasing for Windows
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1697792 13f79535-47bb-0310-9956-ffa450edef68

--- tools/src/main/java/org/apache/pdfbox/tools/pdfdebugger/hexviewer/ASCIIPane.java
+++ tools/src/main/java/org/apache/pdfbox/tools/pdfdebugger/hexviewer/ASCIIPane.java
@@ -56,10 +56,7 @@ class ASCIIPane extends JComponent implements HexModelChangeListener
         super.paintComponent(g);
 
         Graphics2D g2d = (Graphics2D)g;
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHints(HexView.RENDERING_HINTS);
         
         Rectangle bound = getVisibleRect();
 
--- tools/src/main/java/org/apache/pdfbox/tools/pdfdebugger/hexviewer/UpperPane.java
+++ tools/src/main/java/org/apache/pdfbox/tools/pdfdebugger/hexviewer/UpperPane.java
@@ -21,7 +21,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
@@ -43,12 +42,9 @@ class UpperPane extends JPanel
     protected void paintComponent(Graphics g)
     {
         super.paintComponent(g);
-
+        
         Graphics2D g2d = (Graphics2D)g;
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHints(HexView.RENDERING_HINTS);
         
         int x = HexView.LINE_INSET-2;
         int y = 16;
