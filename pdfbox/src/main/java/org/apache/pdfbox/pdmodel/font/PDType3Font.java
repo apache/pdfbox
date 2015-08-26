cf24997c969d643bbe900de07a28a21c855ef740
Tilman Hausherr <tilman@apache.org>
2015-08-26 15:58:32 +0000

    PDFBOX-2943: get width from type3 fonts from the charproc content stream, by Timur Kamalov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1697961 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType3Font.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType3Font.java
@@ -19,14 +19,22 @@ package org.apache.pdfbox.pdmodel.font;
 import java.awt.geom.GeneralPath;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.fontbox.FontBoxFont;
 import org.apache.fontbox.util.BoundingBox;
+import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.cos.COSArray;
+import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.cos.COSNumber;
+import org.apache.pdfbox.cos.COSObject;
 import org.apache.pdfbox.cos.COSStream;
+import org.apache.pdfbox.pdfparser.PDFStreamParser;
 import org.apache.pdfbox.pdmodel.PDResources;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.encoding.DictionaryEncoding;
@@ -43,6 +51,9 @@ import org.apache.pdfbox.util.Vector;
 public class PDType3Font extends PDSimpleFont
 {
     private static final Log LOG = LogFactory.getLog(PDType3Font.class);
+
+    private static final String D0_OPERATOR = "d0";
+    private static final String D1_OPERATOR = "d1";
 
     private PDResources resources;
     private COSDictionary charProcs;
@@ -139,10 +150,66 @@ public class PDType3Font extends PDSimpleFont
     }
 
     @Override
-    public float getWidthFromFont(int code)
+    public float getWidthFromFont(int code) throws IOException
     {
-       // todo: could these be extracted from the font's stream?
-       throw new UnsupportedOperationException("not suppported");
+        try
+        {
+            PDType3CharProc charProc = getCharProc(code);
+            if (charProc == null)
+            {
+                throw new IOException("No CharProc for glyph " + code + " found");
+            }
+            List<COSBase> arguments = new ArrayList<COSBase>();
+            PDFStreamParser parser = new PDFStreamParser(charProc);
+            Object token = parser.parseNextToken();
+            while (token != null)
+            {
+                if (token instanceof COSObject)
+                {
+                    arguments.add(((COSObject) token).getObject());
+                }
+                else if (token instanceof Operator)
+                {
+                    return parseType3WidthOperator((Operator) token, arguments);
+                }
+                else
+                {
+                    arguments.add((COSBase) token);
+                }
+                token = parser.parseNextToken();
+            }
+        }
+        catch (IOException e)
+        {
+            LOG.error("Error processing CharProc for glyph " + code);
+            LOG.error(e);
+        }
+        return -1;
+    }
+
+    private float parseType3WidthOperator(Operator operator, List arguments) throws IOException
+    {
+        if (operator.getName().equals(D0_OPERATOR) || operator.getName().equals(D1_OPERATOR))
+        {
+            Object obj = arguments.get(0);
+            if (obj instanceof Number)
+            {
+                return ((Number) obj).floatValue();
+            }
+            else if (obj instanceof COSNumber)
+            {
+                return ((COSNumber) obj).floatValue();
+            }
+            else
+            {
+                throw new IOException("Unexpected argument type. Expected : COSInteger or Number / Received : "
+                        + obj.getClass().getName());
+            }
+        }
+        else
+        {
+            throw new IOException("Type3 CharProc : First operator must be d0 or d1");
+        }
     }
 
     @Override
