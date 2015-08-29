a1480d55fcb8f738131bd660f939d18b395d6747
Tilman Hausherr <tilman@apache.org>
2015-08-29 11:01:40 +0000

    PDFBOX-2941: separate filter choice from hex + text view, as done by Khyrul Bashar
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1698458 13f79535-47bb-0310-9956-ffa450edef68

--- tools/src/main/java/org/apache/pdfbox/tools/pdfdebugger/streampane/StreamPaneView.java
+++ tools/src/main/java/org/apache/pdfbox/tools/pdfdebugger/streampane/StreamPaneView.java
@@ -17,13 +17,7 @@
 package org.apache.pdfbox.tools.pdfdebugger.streampane;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.util.List;
-import javax.swing.BoxLayout;
-import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.text.StyledDocument;
 import org.apache.pdfbox.tools.pdfdebugger.streampane.tooltip.ToolTipController;
@@ -31,22 +25,16 @@ import org.apache.pdfbox.tools.pdfdebugger.streampane.tooltip.ToolTipController;
 /**
  * @author Khyrul Bashar
  */
-class StreamPaneView extends JPanel
+class StreamPaneView
 {
-    private final JPanel headerPanel;
     private final JPanel contentPanel;
 
     /**
      * Constructor.
-     * @param filterTypes String array that provides a list of key for available stream versions.
-     * @param initialChoice String instance. Initial choice for showing in the pane.
-     * @param listener ActionListener that listens for stream version choice changing.
      */
-    StreamPaneView(List<String> filterTypes, String initialChoice, ActionListener listener)
+    StreamPaneView()
     {
-        headerPanel = createHeaderPanel(filterTypes, initialChoice, listener);
         contentPanel = new JPanel(new BorderLayout());
-        initUI();
     }
 
     /**
@@ -57,10 +45,9 @@ class StreamPaneView extends JPanel
     void showStreamText(StyledDocument document, ToolTipController toolTipController)
     {
         contentPanel.removeAll();
-
         StreamTextView textView = new StreamTextView(document, toolTipController);
         contentPanel.add(textView.getView(), BorderLayout.CENTER);
-        this.validate();
+        contentPanel.validate();
     }
 
 
@@ -72,31 +59,11 @@ class StreamPaneView extends JPanel
     {
         contentPanel.removeAll();
         contentPanel.add(new StreamImageView(image).getView(), BorderLayout.CENTER);
-        this.validate();
-    }
-
-    private JPanel createHeaderPanel(List<String> availableFilters, String i, ActionListener actionListener)
-    {
-        JComboBox filters = new JComboBox(availableFilters.toArray());
-        filters.setSelectedItem(i);
-        filters.addActionListener(actionListener);
-
-        JPanel panel = new JPanel(new FlowLayout());
-        panel.add(filters);
-
-        return panel;
+        contentPanel.validate();
     }
 
     public JPanel getStreamPanel()
     {
-        return this;
-    }
-
-    private void initUI()
-    {
-        this.setPreferredSize(new Dimension(300, 500));
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.add(headerPanel);
-        this.add(contentPanel);
+        return contentPanel;
     }
 }
