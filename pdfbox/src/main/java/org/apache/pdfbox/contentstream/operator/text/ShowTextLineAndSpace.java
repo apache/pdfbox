913791a42cd47c63e0491fda8ec5fd21664ea5c6
Tilman Hausherr <tilman@apache.org>
2015-09-21 19:42:26 +0000

    PDFBOX-2982: avoid ClassCastException
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1704409 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/ShowTextLineAndSpace.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/ShowTextLineAndSpace.java
@@ -23,6 +23,7 @@ import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
 
 import java.io.IOException;
+import org.apache.pdfbox.contentstream.operator.MissingOperandException;
 
 /**
  * ": Set word and character spacing, move to next line, and show text.
@@ -34,6 +35,10 @@ public class ShowTextLineAndSpace extends OperatorProcessor
     @Override
     public void process(Operator operator, List<COSBase> arguments) throws IOException
     {
+        if (arguments.size() < 3)
+        {
+            throw new MissingOperandException(operator, arguments);
+        }
         context.processOperator("Tw", arguments.subList(0,1));
         context.processOperator("Tc", arguments.subList(1,2));
         context.processOperator("'", arguments.subList(2,3));
