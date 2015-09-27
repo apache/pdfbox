3f4a890ab40406d6087551525cec6e4c1ca47080
Andreas Lehmkühler <lehmi@apache.org>
2015-09-27 18:04:42 +0000

    PDFBOX-2905: create a new package for PDFDebugger
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1705561 13f79535-47bb-0310-9956-ffa450edef68

--- debugger/src/main/java/org/apache/pdfbox/tools/util/ExtensionFileFilter.java
+++ debugger/src/main/java/org/apache/pdfbox/tools/util/ExtensionFileFilter.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.pdfbox.tools;
+package org.apache.pdfbox.tools.util;
 
 import java.io.File;
 
