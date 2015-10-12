48b8687a47651a61c21a8f1bebeba2253d0d9032
Tilman Hausherr <tilman@apache.org>
2015-10-12 20:55:07 +0000

    PDFBOX-2852: make fields final, remove unneeded casts
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1708242 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/text/PDFMarkedContentExtractor.java
+++ pdfbox/src/main/java/org/apache/pdfbox/text/PDFMarkedContentExtractor.java
@@ -38,10 +38,10 @@ import org.apache.pdfbox.contentstream.operator.markedcontent.EndMarkedContentSe
  */
 public class PDFMarkedContentExtractor extends PDFTextStreamEngine
 {
-    private boolean suppressDuplicateOverlappingText = true;
-    private List<PDMarkedContent> markedContents = new ArrayList<PDMarkedContent>();
-    private Stack<PDMarkedContent> currentMarkedContents = new Stack<PDMarkedContent>();
-    private Map<String, List<TextPosition>> characterListMapping = new HashMap<String, List<TextPosition>>();
+    private final boolean suppressDuplicateOverlappingText = true;
+    private final List<PDMarkedContent> markedContents = new ArrayList<PDMarkedContent>();
+    private final Stack<PDMarkedContent> currentMarkedContents = new Stack<PDMarkedContent>();
+    private final Map<String, List<TextPosition>> characterListMapping = new HashMap<String, List<TextPosition>>();
 
     /**
      * Instantiate a new PDFTextStripper object.
@@ -151,7 +151,7 @@ public class PDFMarkedContentExtractor extends PDFTextStreamEngine
             float tolerance = (text.getWidth()/textCharacter.length())/3.0f;
             for (TextPosition sameTextCharacter : sameTextCharacters)
             {
-                TextPosition character = (TextPosition) sameTextCharacter;
+                TextPosition character = sameTextCharacter;
                 String charCharacter = character.getUnicode();
                 float charX = character.getX();
                 float charY = character.getY();
@@ -194,7 +194,7 @@ public class PDFMarkedContentExtractor extends PDFTextStreamEngine
                  * Note that we are making an assumption that we need to only look back
                  * one TextPosition to find what we are overlapping.  
                  * This may not always be true. */
-                TextPosition previousTextPosition = (TextPosition)textList.get(textList.size()-1);
+                TextPosition previousTextPosition = textList.get(textList.size()-1);
                 if(text.isDiacritic() && previousTextPosition.contains(text))
                 {
                     previousTextPosition.mergeDiacritic(text);
