c626977f6dfbe7be830f5585e2f305515c91dd99
Maruan Sahyoun <msahyoun@apache.org>
2015-10-18 08:48:24 +0000

    PDFBOX-3030: move existing content
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1709244 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/interactive/form/FillFormField.java
+++ examples/src/main/java/org/apache/pdfbox/examples/interactive/form/FillFormField.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.pdfbox.examples.acroforms;
+package org.apache.pdfbox.examples.interactive.form;
 
 import java.io.File;
 import java.io.IOException;
