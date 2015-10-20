51645e75683f0bb507174180ea56a7cc90916d84
Tilman Hausherr <tilman@apache.org>
2015-10-20 16:32:27 +0000

    PDFBOX-3037: add DrawObject method for content extractor engine
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1709639 13f79535-47bb-0310-9956-ffa450edef68

---
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/markedcontent/DrawObject.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pdfbox.contentstream.operator.markedcontent;
+
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
+import org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject;
+import org.apache.pdfbox.pdmodel.graphics.form.PDTransparencyGroup;
+import org.apache.pdfbox.text.PDFMarkedContentExtractor;
+
+import java.io.IOException;
+import java.util.List;
+import org.apache.pdfbox.contentstream.operator.MissingOperandException;
+import org.apache.pdfbox.contentstream.operator.Operator;
+import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
+
+/**
+ * Do: Draws an XObject.
+ *
+ * @author Ben Litchfield
+ * @author Mario Ivankovits
+ */
+public class DrawObject extends OperatorProcessor
+{
+    @Override
+    public void process(Operator operator, List<COSBase> arguments) throws IOException
+    {
+        if (arguments.size() < 1)
+        {
+            throw new MissingOperandException(operator, arguments);
+        }
+        COSBase base0 = arguments.get(0);
+        if (!(base0 instanceof COSName))
+        {
+            return;
+        }
+        COSName name = (COSName) base0;
+        PDXObject xobject =  context.getResources().getXObject(name);
+        ((PDFMarkedContentExtractor) context).xobject(xobject);
+
+        if (xobject instanceof PDTransparencyGroup)
+        {
+            context.showTransparencyGroup((PDTransparencyGroup) xobject);
+        }
+        else if (xobject instanceof PDFormXObject)
+        {
+            PDFormXObject form = (PDFormXObject) xobject;
+            context.showForm(form);
+        }
+    }
+
+    @Override
+    public String getName()
+    {
+        return "Do";
+    }
+}
