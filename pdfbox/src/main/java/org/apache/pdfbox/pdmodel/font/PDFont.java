5321e968aa5928b6720d67ffc95559df1224ebb8
Tilman Hausherr <tilman@apache.org>
2016-01-02 21:37:50 +0000

    PDFBOX-3123: "undocumented case of using Identity-H/V as a ToUnicode CMap" only if it is a COSName, not if it is a stream
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1722668 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDFont.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDFont.java
@@ -401,11 +401,13 @@ public abstract class PDFont implements COSObjectable, PDFontLike
         // if the font dictionary containsName a ToUnicode CMap, use that CMap
         if (toUnicodeCMap != null)
         {
-            if (toUnicodeCMap.getName() != null && toUnicodeCMap.getName().startsWith("Identity-"))
+            if (toUnicodeCMap.getName() != null && toUnicodeCMap.getName().startsWith("Identity-") && 
+                    dict.getDictionaryObject(COSName.TO_UNICODE) instanceof COSName)
             {
                 // handle the undocumented case of using Identity-H/V as a ToUnicode CMap, this
-                // isn't  actually valid as the Identity-x CMaps are code->CID maps, not
+                // isn't actually valid as the Identity-x CMaps are code->CID maps, not
                 // code->Unicode maps. See sample_fonts_solidconvertor.pdf for an example.
+                // PDFBOX-3123: do this only if the /ToUnicode entry is a name
                 return new String(new char[] { (char) code });
             }
             else
