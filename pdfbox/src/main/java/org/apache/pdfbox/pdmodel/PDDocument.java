6bd5f83ffb66a2fd5f99e04528513c285446b9f9
Tilman Hausherr <tilman@apache.org>
2016-02-18 16:53:31 +0000

    PDFBOX-2852: clarify javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1731095 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDDocument.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDDocument.java
@@ -500,8 +500,13 @@ public class PDDocument implements Closeable
      * This will import and copy the contents from another location. Currently the content stream is stored in a scratch
      * file. The scratch file is associated with the document. If you are adding a page to this document from another
      * document and want to copy the contents to this document's scratch file then use this method otherwise just use
-     * the addPage method.
+     * the {@link #addPage} method.
      * 
+     * Unlike {@link #addPage}, this method does a deep copy. If your page has annotations, and if
+     * these link to pages not in the target document, then the target document might become huge.
+     * What you need to do is to delete page references of such annotations. See
+     * <a href="http://stackoverflow.com/a/35477351/535646">here</a> for how to do this.
+     *
      * @param page The page to import.
      * @return The page that was imported.
      * 
