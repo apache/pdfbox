6fa9ac305fafcd16d18ab243f6b6384933d89e45
Andreas Lehmkühler <lehmi@apache.org>
2016-03-30 16:29:07 +0000

    PDFBOX-3295: compare the object stream index instead of simply checking if it exists
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1737130 13f79535-47bb-0310-9956-ffa450edef68

--- preflight/src/main/java/org/apache/pdfbox/preflight/parser/PreflightParser.java
+++ preflight/src/main/java/org/apache/pdfbox/preflight/parser/PreflightParser.java
@@ -833,7 +833,8 @@ public class PreflightParser extends PDFParser
                     for (COSObject next : parser.getObjects())
                     {
                         COSObjectKey stmObjKey = new COSObjectKey(next);
-                        if (xrefTrailerResolver.getXrefTable().containsKey(stmObjKey))
+                        Long offset = xrefTrailerResolver.getXrefTable().get(stmObjKey); 
+                        if (offset != null && offset == -objstmObjNr)
                         {
                             COSObject stmObj = document.getObjectFromPool(stmObjKey);
                             stmObj.setObject(next.getObject());
