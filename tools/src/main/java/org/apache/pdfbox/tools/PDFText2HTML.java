1f33c4565c2f040565ec8134172ea6bb2d3fe177
Andreas Lehmkühler <lehmi@apache.org>
2016-04-20 15:58:55 +0000

    PDFBOX-3281: ignore encoding parameter when writing html output
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1740161 13f79535-47bb-0310-9956-ffa450edef68

--- tools/src/main/java/org/apache/pdfbox/tools/PDFText2HTML.java
+++ tools/src/main/java/org/apache/pdfbox/tools/PDFText2HTML.java
@@ -39,7 +39,6 @@ public class PDFText2HTML extends PDFTextStripper
 {
     private static final int INITIAL_PDF_TO_HTML_BYTES = 8192;
 
-    private boolean onFirstPage = true;
     private final FontState fontState = new FontState();
 
     /**
@@ -64,34 +63,26 @@ public class PDFText2HTML extends PDFTextStripper
      *
      * @throws IOException
      *             If there is a problem writing out the header to the document.
+     * @deprecated deprecated, use {@link #startDocument(PDDocument)}
      */
     protected void writeHeader() throws IOException
+    {
+    }
+
+    @Override
+    protected void startDocument(PDDocument document) throws IOException
     {
         StringBuilder buf = new StringBuilder(INITIAL_PDF_TO_HTML_BYTES);
         buf.append("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"" + "\n"
                 + "\"http://www.w3.org/TR/html4/loose.dtd\">\n");
         buf.append("<html><head>");
         buf.append("<title>").append(escape(getTitle())).append("</title>\n");
-        buf.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=\"UTF-16\">\n");
+        buf.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=\"UTF-8\">\n");
         buf.append("</head>\n");
         buf.append("<body>\n");
         super.writeString(buf.toString());
     }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void writePage() throws IOException
-    {
-        if (onFirstPage)
-        {
-            writeHeader();
-            onFirstPage = false;
-        }
-        super.writePage();
-    }
-
+    
     /**
      * {@inheritDoc}
      */
