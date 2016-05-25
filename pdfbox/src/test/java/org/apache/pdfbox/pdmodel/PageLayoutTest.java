2a79543f0a2b655607225697aef830bc9709ce92
Tilman Hausherr <tilman@apache.org>
2016-05-25 15:49:20 +0000

    PDFBOX-3362: add test
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1745509 13f79535-47bb-0310-9956-ffa450edef68

---
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/PageLayoutTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2016 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pdfbox.pdmodel;
+
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * @author Tilman Hausherr
+ */
+public class PageLayoutTest
+{
+    /**
+     * Test for completeness (PDFBOX-3362).
+     */
+    @Test
+    public void testValues()
+    {
+        Set<PageLayout> pageLayoutSet = EnumSet.noneOf(PageLayout.class);
+        Set<String> stringSet = new HashSet<String>();
+        for (PageLayout pl : PageLayout.values())
+        {
+            String s = pl.stringValue();
+            stringSet.add(s);
+            pageLayoutSet.add(PageLayout.fromString(s));
+        }
+        assertEquals(PageLayout.values().length, pageLayoutSet.size());
+        assertEquals(PageLayout.values().length, stringSet.size());
+    }
+}
