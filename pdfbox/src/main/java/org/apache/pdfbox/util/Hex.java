368a083895d098e3f4880032858e43503a8028b0
Tilman Hausherr <tilman@apache.org>
2016-06-21 16:25:28 +0000

    PDFBOX-2852: fix javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1749566 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/util/Hex.java
+++ pdfbox/src/main/java/org/apache/pdfbox/util/Hex.java
@@ -97,7 +97,7 @@ public final class Hex
 
     /** 
      * Writes the given byte array as hex value to the given output stream.
-     * @param b the byte array to be written
+     * @param bytes the byte array to be written
      * @param output the output stream to be written to
      * @throws IOException exception if anything went wrong
      */
