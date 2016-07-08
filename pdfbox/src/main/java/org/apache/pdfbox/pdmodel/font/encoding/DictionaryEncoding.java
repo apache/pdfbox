92f3ee8369e462bcc3dd818d791f66581959d16e
John Hewson <jahewson@apache.org>
2016-07-08 16:41:05 +0000

    PDFBOX-3403: Handle invalid BaseEncoding names
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1751942 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/encoding/DictionaryEncoding.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/encoding/DictionaryEncoding.java
@@ -19,6 +19,8 @@ package org.apache.pdfbox.pdmodel.font.encoding;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.pdfbox.cos.COSArray;
 import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
@@ -32,6 +34,8 @@ import org.apache.pdfbox.cos.COSNumber;
  */
 public class DictionaryEncoding extends Encoding
 {
+    private static final Log LOG = LogFactory.getLog(DictionaryEncoding.class);
+    
     private final COSDictionary encoding;
     private final Encoding baseEncoding;
     private final Map<Integer, String> differences = new HashMap<Integer, String>();
@@ -91,7 +95,8 @@ public class DictionaryEncoding extends Encoding
         encoding = fontEncoding;
 
         Encoding base = null;
-        if (encoding.containsKey(COSName.BASE_ENCODING))
+        boolean hasBaseEncoding = encoding.containsKey(COSName.BASE_ENCODING);
+        if (hasBaseEncoding)
         {
             COSName name = encoding.getCOSName(COSName.BASE_ENCODING);
             base = Encoding.getInstance(name); // may be null
@@ -111,8 +116,18 @@ public class DictionaryEncoding extends Encoding
                 {
                     base = builtIn;
                 }
+                else if (hasBaseEncoding)
+                {
+                    String name = encoding.getCOSName(COSName.BASE_ENCODING).getName();
+                    LOG.warn("Invalid BaseEncoding: " + name + ", using StandardEncoding instead");
+                    
+                    // fallback to standard encoding (not in PDF spec, see PDFBOX-3403)
+                    base = StandardEncoding.INSTANCE;
+                }
                 else
                 {
+                    // triggering this error indicates a bug in PDFBox. Every font should always have
+                    // a built-in encoding, if not, we parsed it incorrectly.
                     throw new IllegalArgumentException("Symbolic fonts must have a built-in " + 
                                                        "encoding");
                 }
