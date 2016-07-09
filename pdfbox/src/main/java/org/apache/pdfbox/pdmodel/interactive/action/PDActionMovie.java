ba952c4db83779426b10f9e42676237bab728839
Tilman Hausherr <tilman@apache.org>
2016-07-09 15:26:08 +0000

    PDFBOX-3324: deprecate or delete /S
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1752015 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionMovie.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionMovie.java
@@ -18,7 +18,6 @@
 package org.apache.pdfbox.pdmodel.interactive.action;
 
 import org.apache.pdfbox.cos.COSDictionary;
-import org.apache.pdfbox.cos.COSName;
 
 /**
  * @author Timur Kamalov
@@ -49,27 +48,4 @@ public class PDActionMovie extends PDAction
     {
         super(a);
     }
-
-    /**
-     * This will get the type of action that the actions dictionary describes. It must be Movie for
-     * a Movie action.
-     *
-     * @return The S entry of the specific Movie action dictionary.
-     */
-    public String getS()
-    {
-        return action.getNameAsString(COSName.S);
-    }
-
-    /**
-     * This will set the type of action that the actions dictionary describes. It must be Movie for
-     * a Movie action.
-     *
-     * @param s The Movie action.
-     */
-    public void setS(String s)
-    {
-        action.setName(COSName.S, s);
-    }
-
 }
