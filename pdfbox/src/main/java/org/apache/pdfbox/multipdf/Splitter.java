b3bb871847fb914cc467a40803e2d3028d78477f
Tilman Hausherr <tilman@apache.org>
2016-08-01 17:06:16 +0000

    PDFBOX-3428: remove calls that are now done in importPage
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1754780 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/multipdf/Splitter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/multipdf/Splitter.java
@@ -204,11 +204,7 @@ public class Splitter
         createNewDocumentIfNecessary();
         
         PDPage imported = getDestinationDocument().importPage(page);
-        imported.setCropBox(page.getCropBox());
-        imported.setMediaBox(page.getMediaBox());
-        // only the resources of the page will be copied
         imported.setResources(page.getResources());
-        imported.setRotation(page.getRotation());
         // remove page links to avoid copying not needed resources 
         processAnnotations(imported);
     }
