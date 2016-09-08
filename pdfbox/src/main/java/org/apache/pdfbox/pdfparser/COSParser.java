e4717bdeae0a5c9dcf7f26032c56f1384e4d5227
Tilman Hausherr <tilman@apache.org>
2016-09-08 17:36:35 +0000

    PDFBOX-2852: fix imports
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1759870 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdfparser/COSParser.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfparser/COSParser.java
@@ -24,7 +24,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
