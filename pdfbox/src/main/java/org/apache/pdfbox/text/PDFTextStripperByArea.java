3b5206c1265ca08d4259b7bc9374b4d00038d241
Tilman Hausherr <tilman@apache.org>
2016-09-08 17:36:41 +0000

    PDFBOX-2852: fix imports
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1759871 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/text/PDFTextStripperByArea.java
+++ pdfbox/src/main/java/org/apache/pdfbox/text/PDFTextStripperByArea.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import org.apache.pdfbox.pdmodel.PDPage;
