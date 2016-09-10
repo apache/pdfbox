59ac367aadef11980f5ea3d5371eced32960db14
Tilman Hausherr <tilman@apache.org>
2016-09-10 09:32:35 +0000

    PDFBOX-2852: fix imports
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1760141 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/ttf/GlyfCompositeDescript.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/GlyfCompositeDescript.java
@@ -21,7 +21,6 @@ package org.apache.fontbox.ttf;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
