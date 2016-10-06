741f13f2f2cd409107ac021f537a1ccff124048c
Tilman Hausherr <tilman@apache.org>
2016-10-06 16:06:07 +0000

    PDFBOX-3523: disable high quality interpolation if image is scaled up by ctm AND xform (temporary commit)
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1763621 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
@@ -766,8 +766,10 @@ public class PageDrawer extends PDFGraphicsStreamEngine
 
         if (!pdImage.getInterpolate())
         {
-            boolean isScaledUp = pdImage.getWidth() < Math.round(at.getScaleX()) ||
-                                 pdImage.getHeight() < Math.round(at.getScaleY());
+            Matrix m = new Matrix(xform);
+            m.concatenate(ctm);
+            boolean isScaledUp = pdImage.getWidth() < Math.round(Math.abs(m.getScalingFactorX())) ||
+                                pdImage.getHeight() < Math.round(Math.abs(m.getScalingFactorY()));
 
             // if the image is scaled down, we use smooth interpolation, eg PDFBOX-2364
             // only when scaled up do we use nearest neighbour, eg PDFBOX-2302 / mori-cvpr01.pdf
