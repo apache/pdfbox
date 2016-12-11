043c36880d2cc42b2c55b2b70c77aae51c2a7011
Tilman Hausherr <tilman@apache.org>
2016-12-11 11:43:17 +0000

    PDFBOX-2941: add mnemonic
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1773565 13f79535-47bb-0310-9956-ffa450edef68

--- debugger/src/main/java/org/apache/pdfbox/debugger/PDFDebugger.java
+++ debugger/src/main/java/org/apache/pdfbox/debugger/PDFDebugger.java
@@ -438,6 +438,7 @@ public class PDFDebugger extends JFrame
 
         JMenu fileMenu = new JMenu("File");
         fileMenu.add(openMenuItem);
+        fileMenu.setMnemonic('F');
 
         JMenuItem openUrlMenuItem = new JMenuItem("Open URL...");
         openUrlMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_U, SHORCUT_KEY_MASK));
@@ -544,6 +545,7 @@ public class PDFDebugger extends JFrame
     private JMenu createEditMenu()
     {
         JMenu editMenu = new JMenu("Edit");
+        editMenu.setMnemonic('E');
         
         JMenuItem cutMenuItem = new JMenuItem("Cut");
         cutMenuItem.setEnabled(false);
@@ -569,6 +571,7 @@ public class PDFDebugger extends JFrame
     private JMenu createViewMenu()
     {
         JMenu viewMenu = new JMenu("View");
+        viewMenu.setMnemonic('V');
         if (isPageMode)
         {
             viewModeItem = new JMenuItem("Show Internal Structure");
