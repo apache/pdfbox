2223b7499c189ad70bddf2210772f35ec7e26b60
Tilman Hausherr <tilman@apache.org>
2016-12-20 17:33:45 +0000

    PDFBOX-3638: draw line only if border color is set
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1775328 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/AppearanceGeneratorHelper.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/AppearanceGeneratorHelper.java
@@ -228,7 +228,7 @@ class AppearanceGeneratorHelper
                 lineWidth = borderStyle.getWidth();
             }
 
-            if (lineWidth > 0)
+            if (lineWidth > 0 && borderColour != null)
             {
                 if (lineWidth != 1)
                 {
