c7ccf63f5324af990f41608f8e7f8dbe755dc5ec
Tilman Hausherr <tilman@apache.org>
2017-01-30 19:42:26 +0000

    PDFBOX-3669: add setMiterLimit()
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1780974 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/PDAbstractContentStream.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/PDAbstractContentStream.java
@@ -1284,6 +1284,22 @@ public abstract class PDAbstractContentStream implements Closeable
     }
 
     /**
+     * Set the miter limit.
+     *
+     * @param miterLimit the new miter limit.
+     * @throws IOException If the content stream could not be written.
+     */
+    public void setMiterLimit(float miterLimit) throws IOException
+    {
+        if (inTextMode)
+        {
+            throw new IllegalStateException("Error: setMiterLimit is not allowed within a text block.");
+        }
+        writeOperand(miterLimit);
+        writeOperator("M");
+    }
+
+    /**
      * Begin a marked content sequence.
      *
      * @param tag the tag
