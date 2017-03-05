981246a2499c236f2a40e2696dfcadb3b6e6fe53
Tilman Hausherr <tilman@apache.org>
2017-03-05 10:29:31 +0000

    PDFBOX-3708: clarify javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1785539 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDVectorFont.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDVectorFont.java
@@ -30,7 +30,7 @@ public interface PDVectorFont
     /**
      * Returns the glyph path for the given character code.
      *
-     * @param code character code
+     * @param code character code in a PDF. Not to be confused with unicode.
      * @throws java.io.IOException if the font could not be read
      */
     GeneralPath getPath(int code) throws IOException;
@@ -38,7 +38,7 @@ public interface PDVectorFont
     /**
      * Returns true if this font contains a glyph for the given character code.
      *
-     * @param code character code
+     * @param code character code in a PDF. Not to be confused with unicode.
      */
     boolean hasGlyph(int code) throws IOException;
 }
