5de2bac7b86ed34e11c513db74da140a887365c2
Tilman Hausherr <tilman@apache.org>
2017-03-06 19:24:36 +0000

    PDFBOX-2852: use switch
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1785746 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/type1/Type1Lexer.java
+++ fontbox/src/main/java/org/apache/fontbox/type1/Type1Lexer.java
@@ -378,54 +378,50 @@ class Type1Lexer
             char c = getChar();
 
             // string context
-            if (c == '(')
+            switch (c)
             {
-                openParens++;
-                sb.append('(');
-            }
-            else if (c == ')')
-            {
-                if (openParens == 0)
-                {
-                    // end of string
-                    return new Token(sb.toString(), Token.STRING);
-                }
-                else
-                {
+                case '(':
+                    openParens++;
+                    sb.append('(');
+                    break;
+                case ')':
+                    if (openParens == 0)
+                    {
+                        // end of string
+                        return new Token(sb.toString(), Token.STRING);
+                    }
                     sb.append(')');
                     openParens--;
-                }
-            }
-            else if (c == '\\')
-            {
-                // escapes: \n \r \t \b \f \\ \( \)
-                char c1 = getChar();
-                switch (c1)
-                {
-                    case 'n':
-                    case 'r': sb.append("\n"); break;
-                    case 't': sb.append('\t'); break;
-                    case 'b': sb.append('\b'); break;
-                    case 'f': sb.append('\f'); break;
-                    case '\\': sb.append('\\'); break;
-                    case '(': sb.append('('); break;
-                    case ')': sb.append(')'); break;
-                }
-                // octal \ddd
-                if (Character.isDigit(c1))
-                {
-                    String num = String.valueOf(new char[] { c1, getChar(), getChar() });
-                    Integer code = Integer.parseInt(num, 8);
-                    sb.append((char)(int)code);
-                }
-            }
-            else if (c == '\r' || c == '\n')
-            {
-                sb.append("\n");
-            }
-            else
-            {
-                sb.append(c);
+                    break;
+                case '\\':
+                    // escapes: \n \r \t \b \f \\ \( \)
+                    char c1 = getChar();
+                    switch (c1)
+                    {
+                        case 'n':
+                        case 'r': sb.append("\n"); break;
+                        case 't': sb.append('\t'); break;
+                        case 'b': sb.append('\b'); break;
+                        case 'f': sb.append('\f'); break;
+                        case '\\': sb.append('\\'); break;
+                        case '(': sb.append('('); break;
+                        case ')': sb.append(')'); break;
+                    }   
+                    // octal \ddd
+                    if (Character.isDigit(c1))
+                    {
+                        String num = String.valueOf(new char[] { c1, getChar(), getChar() });
+                        Integer code = Integer.parseInt(num, 8);
+                        sb.append((char)(int)code);
+                    }
+                    break;
+                case '\r':
+                case '\n':
+                    sb.append("\n");
+                    break;
+                default:
+                    sb.append(c);
+                    break;
             }
         }
         return null;
