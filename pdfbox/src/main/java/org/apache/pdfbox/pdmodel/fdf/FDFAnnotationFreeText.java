36f02d58886900449b1742996494353f2cd82647
Tilman Hausherr <tilman@apache.org>
2017-03-17 20:01:59 +0000

    PDFBOX-2852: remove super()
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1787489 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fdf/FDFAnnotationFreeText.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fdf/FDFAnnotationFreeText.java
@@ -48,7 +48,6 @@ public class FDFAnnotationFreeText extends FDFAnnotation
      */
     public FDFAnnotationFreeText()
     {
-        super();
         annot.setName(COSName.SUBTYPE, SUBTYPE);
     }
 
