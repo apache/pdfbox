52d163ded00147ecb013136d40c0598f572e4941
Tilman Hausherr <tilman@apache.org>
2017-04-02 15:49:28 +0000

    PDFBOX-3740: don't get standard 14 AFM if font is embedded
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1789890 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDTrueTypeFont.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDTrueTypeFont.java
@@ -220,7 +220,7 @@ public class PDTrueTypeFont extends PDSimpleFont implements PDVectorFont
     @Override
     protected Encoding readEncodingFromFont() throws IOException
     {
-        if (getStandard14AFM() != null)
+        if (!isEmbedded() && getStandard14AFM() != null)
         {
             // read from AFM
             return new Type1Encoding(getStandard14AFM());
