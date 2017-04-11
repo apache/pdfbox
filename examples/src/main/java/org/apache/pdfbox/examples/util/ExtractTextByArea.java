48c618b0cadd39e8c621c88e184abc998a659be9
Tilman Hausherr <tilman@apache.org>
2017-04-11 19:48:27 +0000

    PDFBOX-2852: use JDK7 syntax
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1791017 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/util/ExtractTextByArea.java
+++ examples/src/main/java/org/apache/pdfbox/examples/util/ExtractTextByArea.java
@@ -20,7 +20,7 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.text.PDFTextStripperByArea;
 
-import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.io.IOException;
 
@@ -52,10 +52,8 @@ public final class ExtractTextByArea
         }
         else
         {
-            PDDocument document = null;
-            try
+            try (PDDocument document = PDDocument.load(new File(args[0])))
             {
-                document = PDDocument.load( new File(args[0]) );
                 PDFTextStripperByArea stripper = new PDFTextStripperByArea();
                 stripper.setSortByPosition( true );
                 Rectangle rect = new Rectangle( 10, 280, 275, 60 );
@@ -64,13 +62,6 @@ public final class ExtractTextByArea
                 stripper.extractRegions( firstPage );
                 System.out.println( "Text in the area:" + rect );
                 System.out.println( stripper.getTextForRegion( "class1" ) );
-            }
-            finally
-            {
-                if( document != null )
-                {
-                    document.close();
-                }
             }
         }
     }
