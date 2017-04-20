05943c2d953868c9ece76d9fe0d5efe6c24b556a
Tilman Hausherr <tilman@apache.org>
2017-04-20 18:51:40 +0000

    PDFBOX-2684: improve javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1792109 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/cos/COSUpdateInfo.java
+++ pdfbox/src/main/java/org/apache/pdfbox/cos/COSUpdateInfo.java
@@ -19,15 +19,17 @@ package org.apache.pdfbox.cos;
 public interface COSUpdateInfo 
 {
     /**
-     * Get the update state for the COSWriter.
-     * 
+     * Get the update state for the COSWriter. This indicates whether an object is to be written
+     * when there is an incremental save.
+     *
      * @return the update state.
      */
     boolean isNeedToBeUpdated();
 
     /**
-     * Set the update state of the dictionary for the COSWriter.
-     * 
+     * Set the update state of the dictionary for the COSWriter. This indicates whether an object is
+     * to be written when there is an incremental save.
+     *
      * @param flag the update state.
      */
     void setNeedToBeUpdated(boolean flag);
