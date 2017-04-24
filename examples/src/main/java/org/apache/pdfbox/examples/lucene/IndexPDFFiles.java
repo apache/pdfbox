5bc5a5c558e34300c85178115c98db70c2be10b2
Tilman Hausherr <tilman@apache.org>
2017-04-24 16:44:25 +0000

    PDFBOX-2852: Sonar fix
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1792517 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/lucene/IndexPDFFiles.java
+++ examples/src/main/java/org/apache/pdfbox/examples/lucene/IndexPDFFiles.java
@@ -79,6 +79,8 @@ public final class IndexPDFFiles
                 case "-update":
                     create = false;
                     break;
+                default:
+                    break;
             }
         }
 
