01c16d1e256107cb47c8a5c77910f272de9e942c
Tilman Hausherr <tilman@apache.org>
2017-04-26 17:03:13 +0000

    PDFBOX-3766: replace method deprecated in jdk9
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1792771 13f79535-47bb-0310-9956-ffa450edef68

--- preflight/src/main/java/org/apache/pdfbox/preflight/annotation/AnnotationValidatorFactory.java
+++ preflight/src/main/java/org/apache/pdfbox/preflight/annotation/AnnotationValidatorFactory.java
@@ -85,12 +85,12 @@ public abstract class AnnotationValidatorFactory
         {
             try
             {
-                Constructor<? extends AnnotationValidator> constructor = clazz.getConstructor(PreflightContext.class,
-                        COSDictionary.class);
+                Constructor<? extends AnnotationValidator> constructor =
+                        clazz.getDeclaredConstructor(PreflightContext.class, COSDictionary.class);
                 result = constructor.newInstance(ctx, annotDic);
                 result.setFactory(this);
             }
-            catch (Exception e)
+            catch (ReflectiveOperationException e)
             {
                 throw new ValidationException(e.getMessage(), e);
             }
