342d6d8414cef4fbb861a101dee0b96d4f9fd9ef
Tilman Hausherr <tilman@apache.org>
2017-06-11 13:04:37 +0000

    PDFBOX-3824: get the length of a ttf datastream without decoding it
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1798362 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/TrueTypeEmbedder.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/TrueTypeEmbedder.java
@@ -110,7 +110,6 @@ abstract class TrueTypeEmbedder implements Subsetter
     public void buildFontFile2(InputStream ttfStream) throws IOException
     {
         PDStream stream = new PDStream(document, ttfStream, COSName.FLATE_DECODE);
-        stream.getCOSObject().setInt(COSName.LENGTH1, stream.toByteArray().length);
 
         // as the stream was closed within the PDStream constructor, we have to recreate it
         InputStream input = null;
@@ -136,7 +135,7 @@ abstract class TrueTypeEmbedder implements Subsetter
         {
             IOUtils.closeQuietly(input);
         }
-
+        stream.getCOSObject().setLong(COSName.LENGTH1, ttf.getOriginalDataSize());
         fontDescriptor.setFontFile2(stream);
     }
 
