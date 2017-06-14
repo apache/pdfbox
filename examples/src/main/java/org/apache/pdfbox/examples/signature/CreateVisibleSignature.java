527080563ee824e13af718d73b8e90af94bc22a2
Tilman Hausherr <tilman@apache.org>
2017-06-14 14:55:53 +0000

    PDFBOX-3821: certify only if version at least 1.5
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1798695 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/CreateVisibleSignature.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/CreateVisibleSignature.java
@@ -164,7 +164,10 @@ public class CreateVisibleSignature extends CreateSignatureBase
             }
 
             // Optional: certify
-            if (accessPermissions == 0)
+            // can be done only if version is at least 1.5 and if not already set
+            // doing this on a PDF/A-1b file fails validation by Adobe preflight (PDFBOX-3821)
+            // PDF/A-1b requires PDF version 1.4 max, so don't increase the version on such files.
+            if (doc.getVersion() >= 1.5f && accessPermissions == 0)
             {
                 setMDPPermission(doc, signature, 2);
             }
