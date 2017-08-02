62372a027c291590e120efce27e91ac9ee7f85aa
Tilman Hausherr <tilman@apache.org>
2017-08-02 16:04:47 +0000

    PDFBOX-2852: remove unneeded nullcheck
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1803845 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/ttf/TTFSubsetter.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/TTFSubsetter.java
@@ -453,6 +453,7 @@ public final class TTFSubsetter
         return bos.toByteArray();
     }
 
+    // never returns null
     private byte[] buildLocaTable(long[] newOffsets) throws IOException
     {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -555,9 +556,11 @@ public final class TTFSubsetter
                 glyphIds.addAll(glyphIdsToAdd);
             }
             hasNested = glyphIdsToAdd != null;
-        } while (hasNested);
+        }
+        while (hasNested);
     }
 
+    // never returns null
     private byte[] buildGlyfTable(long[] newOffsets) throws IOException
     {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -980,17 +983,11 @@ public final class TTFSubsetter
             {
                 tables.put("cmap", cmap);
             }
-            if (glyf != null)
-            {
-                tables.put("glyf", glyf); 
-            }
+            tables.put("glyf", glyf); 
             tables.put("head", head);
             tables.put("hhea", hhea);
             tables.put("hmtx", hmtx);
-            if (loca != null)
-            {
-                tables.put("loca", loca);
-            }
+            tables.put("loca", loca);
             tables.put("maxp", maxp);
             if (name != null)
             {
