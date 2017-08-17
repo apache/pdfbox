361d54d5b9a9ac2067bf22b060482a48015c575b
Tilman Hausherr <tilman@apache.org>
2017-08-17 16:06:31 +0000

    PDFBOX-2852: don't use default locale
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1805311 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/ttf/GlyphRenderer.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/GlyphRenderer.java
@@ -162,7 +162,7 @@ class GlyphRenderer
         path.lineTo(point.x, point.y);
         if (LOG.isDebugEnabled())
         {
-            LOG.trace("lineTo: " + String.format(Locale.US"%d,%d", point.x, point.y));
+            LOG.trace("lineTo: " + String.format(Locale.US, "%d,%d", point.x, point.y));
         }
     }
 
@@ -171,7 +171,7 @@ class GlyphRenderer
         path.quadTo(ctrlPoint.x, ctrlPoint.y, point.x, point.y);
         if (LOG.isDebugEnabled())
         {
-            LOG.trace("quadTo: " + String.format(Locale.US"%d,%d %d,%d", ctrlPoint.x, ctrlPoint.y,
+            LOG.trace("quadTo: " + String.format(Locale.US, "%d,%d %d,%d", ctrlPoint.x, ctrlPoint.y,
                     point.x, point.y));
         }
     }
