af68dd7042db779d524ade67ecef86a20a98c4ca
Tilman Hausherr <tilman@apache.org>
2017-09-17 09:30:46 +0000

    PDFBOX-3913: support utf8 URIs
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1808608 13f79535-47bb-0310-9956-ffa450edef68

---
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/interactive/interactive/PDActionURITest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pdfbox.pdmodel.interactive.action;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author Tilman Hausherr
+ */
+public class PDActionURITest
+{
+    /**
+     * PDFBOX-3913: Check that URIs encoded in UTF-8 are also supported.
+     */
+    @Test
+    public void testUTF8URI()
+    {
+        PDActionURI actionURI = new PDActionURI();
+        actionURI.setURI("http://çµ„åŒ¶æ›¿ç¶Ž.com/");
+        assertEquals("http://経営承継.com/", actionURI.getURI());
+    }
+}
