e704f0d4a3ee148bef4948f7c0e228c29ec97375
Tilman Hausherr <tilman@apache.org>
2017-09-20 17:02:36 +0000

    PDFBOX-3938: add testcase from issue 2079 by Tim Allison
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1809054 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/test/java/org/apache/pdfbox/pdfparser/EndstreamOutputStreamTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdfparser/EndstreamOutputStreamTest.java
@@ -17,16 +17,29 @@
 package org.apache.pdfbox.pdfparser;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import junit.framework.TestCase;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
+import org.apache.pdfbox.io.IOUtils;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
+import org.apache.pdfbox.pdmodel.PDDocumentNameDictionary;
+import org.apache.pdfbox.pdmodel.PDEmbeddedFilesNameTreeNode;
+import org.apache.pdfbox.pdmodel.common.filespecification.PDComplexFileSpecification;
+import org.apache.pdfbox.pdmodel.common.filespecification.PDEmbeddedFile;
 import org.junit.Assert;
+import org.junit.Test;
 
 /**
  *
  * @author Tilman Hausherr
  */
-public class EndstreamOutputStreamTest extends TestCase
+public class EndstreamOutputStreamTest
 {
+    @Test
     public void testEndstreamOutputStream() throws IOException
     {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -93,5 +106,33 @@ public class EndstreamOutputStreamTest extends TestCase
         byte[] expectedResult5 = { 1, 2, 3, 4, '\r', '\n', 5, 6, 7, '\r', 8, 9, '\n', '\r'};
         Assert.assertArrayEquals(expectedResult5, baos.toByteArray());
     }
-    
+
+    @Test
+    public void testPDFBox2079EmbeddedFile() throws IOException
+    {
+        // there should be 17660 bytes in the zip file.
+        // in PDFBox 1.8.5, windows newline is appended to the byte stream
+        // yielding 17662 bytes, which causes a problem for ZipFile in Java 1.6
+
+        // Modification of embedded_zip.pdf for 2.0:
+        // /Length entry removed to force usage of EndstreamOutputStream
+        try (PDDocument doc = PDDocument.load(
+                new File("src/test/resources/org/apache/pdfbox/pdfparser", "embedded_zip.pdf")))
+        {
+            PDDocumentCatalog catalog = doc.getDocumentCatalog();
+            PDDocumentNameDictionary names = catalog.getNames();
+            PDEmbeddedFilesNameTreeNode node = names.getEmbeddedFiles();
+            Map<String, PDComplexFileSpecification> map = node.getNames();
+            Assert.assertEquals(1, map.size());
+            PDComplexFileSpecification spec = map.get("My first attachment");
+            PDEmbeddedFile file = spec.getEmbeddedFile();
+            InputStream input = file.createInputStream();
+            File f = new File("target/test-output", spec.getFile());
+            try (OutputStream os = new FileOutputStream(f))
+            {
+                IOUtils.copy(input, os);
+            }
+            Assert.assertEquals(17660, f.length());
+        }
+    }
 }
