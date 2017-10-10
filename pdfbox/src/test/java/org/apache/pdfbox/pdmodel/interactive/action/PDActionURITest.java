21084c3eb5320e9a04991c0f5dab89458871882c
Tilman Hausherr <tilman@apache.org>
2017-10-10 15:44:12 +0000

    PDFBOX-3958: support UTF-16
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1811718 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/test/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionURITest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionURITest.java
@@ -15,6 +15,9 @@
  */
 package org.apache.pdfbox.pdmodel.interactive.action;
 
+import java.io.IOException;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.cos.COSString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import org.junit.Test;
@@ -36,5 +39,46 @@ public class PDActionURITest
         assertNull(actionURI.getURI());
         actionURI.setURI("http://çµ„åŒ¶æ›¿ç¶Ž.com/");
         assertEquals("http://経営承継.com/", actionURI.getURI());
+    }
+
+    /**
+     * PDFBOX-3913: Check that URIs encoded in UTF16 (BE) are also supported.
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testUTF16BEURI() throws IOException
+    {
+        PDActionURI actionURI = new PDActionURI();
+        
+        // found in govdocs file 534948.pdf
+        COSString utf16URI = COSString.parseHex("FEFF0068007400740070003A002F002F00770077"
+                + "0077002E006E00610070002E006500640075002F0063006100740061006C006F006700"
+                + "2F00310031003100340030002E00680074006D006C");
+        actionURI.getCOSObject().setItem(COSName.URI, utf16URI);
+        assertEquals("http://www.nap.edu/catalog/11140.html", actionURI.getURI());
+    }
+
+    /**
+     * PDFBOX-3913: Check that URIs encoded in UTF16 (LE) are also supported.
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void testUTF16LEURI() throws IOException
+    {
+        PDActionURI actionURI = new PDActionURI();
+        
+        COSString utf16URI = COSString.parseHex("FFFE68007400740070003A00");
+        actionURI.getCOSObject().setItem(COSName.URI, utf16URI);
+        assertEquals("http:", actionURI.getURI());
+    }
+
+    @Test
+    public void testUTF7URI()
+    {
+        PDActionURI actionURI = new PDActionURI();
+        actionURI.setURI("http://pdfbox.apache.org/");
+        assertEquals("http://pdfbox.apache.org/", actionURI.getURI());
     }
 }
