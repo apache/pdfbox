58455e40da42ad848934412ff5833429cc7c681c
Tilman Hausherr <tilman@apache.org>
2017-10-25 18:35:49 +0000

    PDFBOX-1848: remove MDP methods from base class; adjust calls
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1813336 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/CreateVisibleSignature.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/CreateVisibleSignature.java
@@ -195,7 +195,7 @@ public class CreateVisibleSignature extends CreateSignatureBase
 
         try (PDDocument doc = PDDocument.load(inputFile))
         {
-            int accessPermissions = getMDPPermission(doc);
+            int accessPermissions = SigUtils.getMDPPermission(doc);
             if (accessPermissions == 1)
             {
                 throw new IllegalStateException("No changes to the document are permitted due to DocMDP transform parameters dictionary");
@@ -221,7 +221,7 @@ public class CreateVisibleSignature extends CreateSignatureBase
             // PDF/A-1b requires PDF version 1.4 max, so don't increase the version on such files.
             if (doc.getVersion() >= 1.5f && accessPermissions == 0)
             {
-                setMDPPermission(doc, signature, 2);
+                SigUtils.setMDPPermission(doc, signature, 2);
             }
 
             PDAcroForm acroForm = doc.getDocumentCatalog().getAcroForm();
