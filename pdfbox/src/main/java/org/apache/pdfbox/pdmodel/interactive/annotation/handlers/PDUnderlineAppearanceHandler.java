60669cea9e9b02afa2be430f5b790d39bbc500cd
Tilman Hausherr <tilman@apache.org>
2018-01-20 21:57:44 +0000

    PDFBOX-3353: add handler for underline annotations
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1821763 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDUnderlineAppearanceHandler.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDUnderlineAppearanceHandler.java
@@ -16,9 +16,13 @@
 
 package org.apache.pdfbox.pdmodel.interactive.annotation.handlers;
 
+import java.io.IOException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationUnderline;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAppearanceContentStream;
 
 /**
  *
@@ -43,7 +47,73 @@ public class PDUnderlineAppearanceHandler extends PDAbstractAppearanceHandler
     @Override
     public void generateNormalAppearance()
     {
-        //TODO
+        PDAnnotationUnderline annotation = (PDAnnotationUnderline) getAnnotation();
+        PDRectangle rect = annotation.getRectangle();
+        float[] pathsArray = annotation.getQuadPoints();
+        if (pathsArray == null)
+        {
+            return;
+        }
+        AnnotationBorder ab = AnnotationBorder.getAnnotationBorder(annotation, annotation.getBorderStyle());
+        if (ab.color.getComponents().length == 0)
+        {
+            return;
+        }
+        if (ab.width == 0)
+        {
+            // value found in adobe reader
+            ab.width = 1.5f;
+        }
+
+        // Adjust rectangle even if not empty, see PLPDF.com-MarkupAnnotations.pdf
+        //TODO in a class structure this should be overridable
+        // this is similar to polyline but different data type
+        // all coordinates (unlike painting) are used because I'm lazy
+        float minX = Float.MAX_VALUE;
+        float minY = Float.MAX_VALUE;
+        float maxX = Float.MIN_VALUE;
+        float maxY = Float.MIN_VALUE;
+        for (int i = 0; i < pathsArray.length / 2; ++i)
+        {
+            float x = pathsArray[i * 2];
+            float y = pathsArray[i * 2 + 1];
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+        }
+        rect.setLowerLeftX(Math.min(minX - ab.width / 2, rect.getLowerLeftX()));
+        rect.setLowerLeftY(Math.min(minY - ab.width / 2, rect.getLowerLeftY()));
+        rect.setUpperRightX(Math.max(maxX + ab.width, rect.getUpperRightX()));
+        rect.setUpperRightY(Math.max(maxY + ab.width, rect.getUpperRightY()));
+        annotation.setRectangle(rect);
+
+        try
+        {
+            try (PDAppearanceContentStream cs = getNormalAppearanceAsContentStream())
+            {
+                cs.setStrokingColor(ab.color);
+                if (ab.dashArray != null)
+                {
+                    cs.setLineDashPattern(ab.dashArray, 0);
+                }
+                cs.setLineWidth(ab.width);
+
+                // spec is incorrect
+                // https://stackoverflow.com/questions/9855814/pdf-spec-vs-acrobat-creation-quadpoints
+                for (int i = 0; i < pathsArray.length / 8; ++i)
+                {
+                    // only lower coords are used
+                    cs.moveTo(pathsArray[i * 8 + 4], pathsArray[i * 8 + 5]);
+                    cs.lineTo(pathsArray[i * 8 + 6], pathsArray[i * 8 + 7]);
+                }
+                cs.stroke();
+            }
+        }
+        catch (IOException ex)
+        {
+            LOG.error(ex);
+        }
     }
 
     @Override
