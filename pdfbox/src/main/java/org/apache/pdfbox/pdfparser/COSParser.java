c5ec30001109ad9fabedccb1fc71bac0ca578698
Andreas Lehmkühler <lehmi@apache.org>
2018-01-24 06:58:56 +0000

    PDFBOX-4052: allow all whitespace when searching for objects
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1822077 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdfparser/COSParser.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfparser/COSParser.java
@@ -1465,7 +1465,7 @@ public class COSParser extends BaseParser
             source.seek(currentOffset);
             int nextChar = source.read();
             currentOffset++;
-            if (nextChar == ' ' && isString(OBJ_MARKER))
+            if (isWhitespace(nextChar) && isString(OBJ_MARKER))
             {
                 long tempOffset = currentOffset - 2;
                 source.seek(tempOffset);
@@ -1476,9 +1476,9 @@ public class COSParser extends BaseParser
                     genID -= 48;
                     tempOffset--;
                     source.seek(tempOffset);
-                    if (isSpace())
+                    if (isWhitespace())
                     {
-                        while (tempOffset > MINIMUM_SEARCH_OFFSET && isSpace())
+                        while (tempOffset > MINIMUM_SEARCH_OFFSET && isWhitespace())
                         {
                             source.seek(--tempOffset);
                         }
