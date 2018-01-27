3c6ff009c52ad0f1320f1c46e8d69064b2cf5539
Tilman Hausherr <tilman@apache.org>
2018-01-27 12:03:10 +0000

    PDFBOX-4084: avoid ClassCastException
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1822397 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDPage.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDPage.java
@@ -657,14 +657,10 @@ public class PDPage implements COSObjectable, PDContentStream
      */
     public List<PDAnnotation> getAnnotations() throws IOException
     {
-        COSArrayList<PDAnnotation> retval;
-        COSArray annots = (COSArray) page.getDictionaryObject(COSName.ANNOTS);
-        if (annots == null)
+        COSBase base = page.getDictionaryObject(COSName.ANNOTS);
+        if (base instanceof COSArray)
         {
-            return new COSArrayList<>(page, COSName.ANNOTS);
-        }
-        else
-        {
+            COSArray annots = (COSArray) base;
             List<PDAnnotation> actuals = new ArrayList<>();
             for (int i = 0; i < annots.size(); i++)
             {
@@ -675,9 +671,9 @@ public class PDPage implements COSObjectable, PDContentStream
                 }
                 actuals.add(PDAnnotation.createAnnotation(item));
             }
-            retval = new COSArrayList<>(actuals, annots);
+            return new COSArrayList<>(actuals, annots);
         }
-        return retval;
+        return new COSArrayList<>(page, COSName.ANNOTS);
     }
 
     /**
