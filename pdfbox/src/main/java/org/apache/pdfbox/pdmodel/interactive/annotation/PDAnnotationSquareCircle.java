09fa39f2c5b5d93c148ad5b7a31963c04ec4ac10
Maruan Sahyoun <msahyoun@apache.org>
2018-02-01 17:36:02 +0000

    PDFBOX-4092: support setting, getting the /RD value (margin) for Square and Circle annotation
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1822891 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/PDAnnotationSquareCircle.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/PDAnnotationSquareCircle.java
@@ -19,6 +19,7 @@ package org.apache.pdfbox.pdmodel.interactive.annotation;
 import org.apache.pdfbox.cos.COSArray;
 import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.cos.COSFloat;
 import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.io.ScratchFile;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
@@ -184,6 +185,48 @@ public abstract class PDAnnotationSquareCircle extends PDAnnotationMarkup
             return new PDBorderStyleDictionary((COSDictionary) bs);
         }
         return null;
+    }
+    
+    /**
+     * This will set the margin between the annotations "outer" rectangle defined by
+     * /Rect and the border.
+     * 
+     * @param margin
+     */
+    public void setMargins(float margin) {
+        setMargins(margin, margin, margin, margin);
+    }
+    
+    /**
+     * This will set the margin between the annotations "outer" rectangle defined by
+     * /Rect and the border.
+     * 
+     * @param margin
+     */
+    public void setMargins(float marginLeft, float marginTop, float marginRight, float marginBottom)
+    {
+        COSArray margins = new COSArray();
+        margins.add(new COSFloat(marginLeft));
+        margins.add(new COSFloat(marginTop));
+        margins.add(new COSFloat(marginRight));
+        margins.add(new COSFloat(marginBottom));
+        getCOSObject().setItem(COSName.RD, margins);    
+    }
+    
+    /**
+     * This will get the margin between the annotations "outer" rectangle defined by
+     * /Rect and the border.
+     * 
+     * @return the margins. If the entry hasn't been set defaults to 0 on all sides.
+     */
+    public float[] getMargins()
+    {
+        COSBase margin = getCOSObject().getItem(COSName.RD);
+        if (margin instanceof COSArray)
+        {
+            return ((COSArray) margin).toFloatArray();
+        }
+        return new float[]{0f, 0f, 0f, 0f};
     }
 
 }
