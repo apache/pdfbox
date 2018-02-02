61b6cdf664f803e54087615426e1495db48ef2b7
Maruan Sahyoun <msahyoun@apache.org>
2018-02-02 09:08:02 +0000

    PDFBOX-4092: return empty array if the /RD entry hasn't been set to allow to check for that condition
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1822932 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/PDAnnotationFreeText.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/PDAnnotationFreeText.java
@@ -153,7 +153,7 @@ public class PDAnnotationFreeText extends PDAnnotationMarkup
      * This will get the margin between the annotations "outer" rectangle defined by
      * /Rect and the border.
      * 
-     * @return the margins. If the entry hasn't been set defaults to 0 on all sides.
+     * @return the margins. If the entry hasn't been set am empty array is returned.
      */
     public float[] getMargins()
     {
@@ -162,7 +162,7 @@ public class PDAnnotationFreeText extends PDAnnotationMarkup
         {
             return ((COSArray) margin).toFloatArray();
         }
-        return new float[]{0f, 0f, 0f, 0f};
+        return new float[]{};
     }
     
     /**
