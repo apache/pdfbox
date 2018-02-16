7a36b09fadd579bc5970113a886f6af063ca57bb
Tilman Hausherr <tilman@apache.org>
2018-02-16 06:46:53 +0000

    PDFBOX-4106: add @deprecated to javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1824420 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/TrueTypeEmbedder.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/TrueTypeEmbedder.java
@@ -58,8 +58,15 @@ abstract class TrueTypeEmbedder implements Subsetter
     private final PDDocument document;
     protected TrueTypeFont ttf;
     protected PDFontDescriptor fontDescriptor;
+    
+    /**
+     * For API backwards compatibility.
+     * 
+     * @deprecated
+     */
     @Deprecated
-    protected final CmapSubtable cmap; // for API backwards compatibility
+    protected final CmapSubtable cmap;
+
     protected final CmapLookup cmapLookup;
     private final Set<Integer> subsetCodePoints = new HashSet<Integer>();
     private final boolean embedSubset;
