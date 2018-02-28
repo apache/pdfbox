2c889331fa0ebf7b75b3f12c044e98032dbc593e
Tilman Hausherr <tilman@apache.org>
2018-02-28 21:49:23 +0000

    PDFBOX-4071: remove unused code, as suggested by Itai in the dev mailing list
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1825604 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/filter/JBIG2Filter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/filter/JBIG2Filter.java
@@ -69,8 +69,6 @@ final class JBIG2Filter extends Filter
         {
             logLevigoDonated();
         }
-        DecodeResult result = new DecodeResult(new COSDictionary());
-        result.getParameters().addAll(parameters);
 
         int bits = parameters.getInt(COSName.BITS_PER_COMPONENT, 1);
         COSDictionary params = getDecodeParams(parameters, index);
@@ -141,13 +139,6 @@ final class JBIG2Filter extends Filter
             }
             reader.dispose();
         }
-
-        // repair missing color space
-        if (!parameters.containsKey(COSName.COLORSPACE))
-        {
-            result.getParameters().setName(COSName.COLORSPACE, COSName.DEVICEGRAY.getName());
-        }
-
         return new DecodeResult(parameters);
     }
 
