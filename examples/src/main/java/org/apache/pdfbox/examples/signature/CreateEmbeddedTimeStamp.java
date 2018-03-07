25c38bc642cd1c97b91d3c9cbb3b84979884e912
Tilman Hausherr <tilman@apache.org>
2018-03-07 17:13:38 +0000

    PDFBOX-4071: fix typo
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1826138 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/CreateEmbeddedTimeStamp.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/CreateEmbeddedTimeStamp.java
@@ -38,7 +38,7 @@ import org.bouncycastle.cms.CMSException;
 import org.bouncycastle.cms.CMSSignedData;
 
 /**
- * An example for timestamp-singing a PDF for PADeS-Specification. The document will only be changed
+ * An example for timestamp-signing a PDF for PADeS-Specification. The document will only be changed
  * in its existing signature by a signed timestamp (A timestamp and the Hash-Value of the document
  * are signed by a Time Stamp Authority (TSA)).
  *
