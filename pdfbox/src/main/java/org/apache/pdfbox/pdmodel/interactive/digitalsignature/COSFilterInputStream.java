d21ae1a958531432ce0ac8dce02e3128758fe2ac
Tilman Hausherr <tilman@apache.org>
2018-03-07 17:46:43 +0000

    PDFBOX-4071: simplify code
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1826151 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/digitalsignature/COSFilterInputStream.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/digitalsignature/COSFilterInputStream.java
@@ -17,10 +17,10 @@
 package org.apache.pdfbox.pdmodel.interactive.digitalsignature;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import org.apache.pdfbox.io.IOUtils;
 
 
 public class COSFilterInputStream extends FilterInputStream
@@ -119,13 +119,6 @@ public class COSFilterInputStream extends FilterInputStream
   
   public byte[] toByteArray() throws IOException 
   {
-    ByteArrayOutputStream byteOS = new ByteArrayOutputStream();
-    byte[] buffer = new byte[1024];
-    int c;
-    while ((c = this.read(buffer)) != -1)
-    {
-        byteOS.write(buffer, 0, c);
-    }
-    return byteOS.toByteArray();
+      return IOUtils.toByteArray(this);
   }
 }
