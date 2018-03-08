7b0c5389e0feb7d7c71657d11141851a25f0f04d
Maruan Sahyoun <msahyoun@apache.org>
2018-03-08 09:13:08 +0000

    PDFBOX-4071: remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1826191 13f79535-47bb-0310-9956-ffa450edef68

--- debugger/src/main/java/org/apache/pdfbox/debugger/streampane/StreamPane.java
+++ debugger/src/main/java/org/apache/pdfbox/debugger/streampane/StreamPane.java
@@ -26,7 +26,6 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
