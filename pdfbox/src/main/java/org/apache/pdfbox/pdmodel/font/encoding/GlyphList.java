33771c475cc74776a5742d246c0f40b211cd826c
Tilman Hausherr <tilman@apache.org>
2018-04-03 18:03:21 +0000

    PDFBOX-4181: use ConcurrentHashMap instead of HashMap because of concurrent writing, as suggested by Michael Klink
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1828266 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/encoding/GlyphList.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/encoding/GlyphList.java
@@ -25,6 +25,7 @@ import org.apache.commons.logging.LogFactory;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * PostScript glyph list, maps glyph names to sequences of Unicode characters.
@@ -97,7 +98,7 @@ public final class GlyphList
     private final Map<String, String> unicodeToName;
     
     // additional read/write cache for uniXXXX names
-    private final Map<String, String> uniNameToUnicodeCache = new HashMap<>();
+    private final Map<String, String> uniNameToUnicodeCache = new ConcurrentHashMap<>();
 
     /**
      * Creates a new GlyphList from a glyph list file.
