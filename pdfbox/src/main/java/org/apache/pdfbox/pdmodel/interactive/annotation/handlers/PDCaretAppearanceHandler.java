a53b04f59f455bf6164c756155bd6611594a0ca9
Tilman Hausherr <tilman@apache.org>
2018-04-23 17:13:42 +0000

    PDFBOX-3353: add caret annotation handler
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1829909 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDCaretAppearanceHandler.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDCaretAppearanceHandler.java
@@ -15,10 +15,19 @@
  */
 package org.apache.pdfbox.pdmodel.interactive.annotation.handlers;
 
+import java.io.IOException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationCaret;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAppearanceContentStream;
 
+/**
+ * Handler to generate the caret annotations appearance.
+ *
+ * @author Tilman Hausherr
+ */
 public class PDCaretAppearanceHandler extends PDAbstractAppearanceHandler
 {
     private static final Log LOG = LogFactory.getLog(PDCaretAppearanceHandler.class);
@@ -39,7 +48,46 @@ public class PDCaretAppearanceHandler extends PDAbstractAppearanceHandler
     @Override
     public void generateNormalAppearance()
     {
-        // TODO to be implemented
+        float lineWidth = 1f;
+        //TODO Adobe creates the /RD entry, but it is unclear how it
+        // gets the (identical) numbers. The numbers from there are then substracted/added from /BBox
+        // and used in the translation in the matrix and also for the line width.
+
+        try
+        {
+            PDAnnotationCaret annotation = (PDAnnotationCaret) getAnnotation();
+            try (PDAppearanceContentStream contentStream = getNormalAppearanceAsContentStream())
+            {
+                contentStream.setStrokingColor(getColor());
+                contentStream.setLineWidth(lineWidth);
+                contentStream.setNonStrokingColor(getColor());
+                
+                handleOpacity(annotation.getConstantOpacity());
+
+                PDRectangle rect = getRectangle();
+                PDRectangle bbox = new PDRectangle(rect.getWidth(), rect.getHeight());
+                annotation.getNormalAppearanceStream().setBBox(bbox);
+
+                float halfX = rect.getWidth() / 2;
+                float halfY = rect.getHeight() / 2;
+                contentStream.moveTo(0, 0);
+                contentStream.curveTo(halfX, 0,
+                                      halfX, halfY, 
+                                      halfX, rect.getHeight());
+                contentStream.curveTo(halfX, halfY, 
+                                      halfX, 0,
+                                      rect.getWidth(), 0);
+                // closeAndFillAndStroke() would bring a thicker "thin top" shape.
+                contentStream.closePath();
+                contentStream.fill();
+                contentStream.stroke();
+                //TODO test whether the stroke() and setLineWidth() calls have any effect at all.
+            }
+        }
+        catch (IOException e)
+        {
+            LOG.error(e);
+        }
     }
 
     @Override
