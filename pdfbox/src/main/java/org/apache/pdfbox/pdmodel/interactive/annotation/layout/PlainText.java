6cdd00dbf73466f1b2d8fae6f06df16da59911e1
Maruan Sahyoun <msahyoun@apache.org>
2018-06-03 16:09:21 +0000

    PDFBOX-4212: changes to package names were not persisted - fix package name
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1832776 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/layout/PlainText.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/layout/PlainText.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.pdfbox.pdmodel.interactive.annotation.text;
+package org.apache.pdfbox.pdmodel.interactive.annotation.layout;
 
 import java.io.IOException;
 import java.text.AttributedString;
