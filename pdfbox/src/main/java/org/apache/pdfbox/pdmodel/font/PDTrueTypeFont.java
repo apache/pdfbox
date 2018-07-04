188aed755a694de82791216623b3344e7e63ee56
Tilman Hausherr <tilman@apache.org>
2018-07-04 18:19:20 +0000

    PDFBOX-4071: remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1835075 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDTrueTypeFont.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDTrueTypeFont.java
@@ -18,7 +18,6 @@ package org.apache.pdfbox.pdmodel.font;
 
 import java.awt.geom.GeneralPath;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
