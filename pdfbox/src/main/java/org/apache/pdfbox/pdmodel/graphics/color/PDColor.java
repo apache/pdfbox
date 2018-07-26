971fb322c223d199ae81ecb0e1a7a599c714259a
Tilman Hausherr <tilman@apache.org>
2018-07-26 19:10:02 +0000

    PDFBOX-4279: avoid NPE with empty annotation color that have no colorspace
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1836753 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDColor.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDColor.java
@@ -113,9 +113,11 @@ public final class PDColor
      */
     public float[] getComponents()
     {
-        if (colorSpace instanceof PDPattern)
+        if (colorSpace instanceof PDPattern || colorSpace == null)
         {
             // colorspace of the pattern color isn't known, so just clone
+            // null colorspace can happen with empty annotation color
+            // see PDFBOX-3351-538928-p4.pdf
             return components.clone();
         }
         // PDFBOX-4279: copyOf instead of clone in case array is too small
