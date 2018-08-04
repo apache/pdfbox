a6deb9d7ffec79986cac82345db724b343ca3e5d
Tilman Hausherr <tilman@apache.org>
2018-08-04 17:29:55 +0000

    PDFBOX-4283: treat invalid elements as 0
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1837440 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/cos/COSArray.java
+++ pdfbox/src/main/java/org/apache/pdfbox/cos/COSArray.java
@@ -534,9 +534,11 @@ public class COSArray extends COSBase implements Iterable<COSBase>, COSUpdateInf
     public float[] toFloatArray()
     {
         float[] retval = new float[size()];
-        for( int i=0; i<size(); i++ )
+        for (int i = 0; i < size(); i++)
         {
-            retval[i] = ((COSNumber)getObject( i )).floatValue();
+            COSBase base = getObject(i);
+            retval[i] =
+                base instanceof COSNumber ? ((COSNumber) base).floatValue() : 0;
         }
         return retval;
     }
