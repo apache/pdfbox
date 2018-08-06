333f61d5947e1919f25d445b3c00063a43cc0f1c
Tilman Hausherr <tilman@apache.org>
2018-08-06 20:33:05 +0000

    PDFBOX-4285: pass quality parameter
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1837562 13f79535-47bb-0310-9956-ffa450edef68

--- tools/src/main/java/org/apache/pdfbox/tools/imageio/ImageIOUtil.java
+++ tools/src/main/java/org/apache/pdfbox/tools/imageio/ImageIOUtil.java
@@ -185,7 +185,7 @@ public final class ImageIOUtil
     public static boolean writeImage(BufferedImage image, String formatName, OutputStream output,
             int dpi, float quality) throws IOException
     {
-        return writeImage(image, formatName, output, dpi, 1.0f, "");
+        return writeImage(image, formatName, output, dpi, quality, "");
     }
 
     /**
