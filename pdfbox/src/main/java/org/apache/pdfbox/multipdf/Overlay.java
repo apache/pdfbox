1ed0b7b4a74a261bc311f172a62a724fe60c132b
Tilman Hausherr <tilman@apache.org>
2018-08-22 16:09:03 +0000

    PDFBOX-4298: avoid NPE; use similar text
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1838652 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/multipdf/Overlay.java
+++ pdfbox/src/main/java/org/apache/pdfbox/multipdf/Overlay.java
@@ -322,7 +322,8 @@ public class Overlay
         }
         else
         {
-            throw new IOException("Contents are unknown type:" + contents.getClass().getName());
+            throw new IOException("Unknown content type: " +
+                    (contents == null ? "(null)" : contents.getClass().getName()));
         }
         return contentStreams;
     }
@@ -371,7 +372,8 @@ public class Overlay
         }
         else
         {
-            throw new IOException("Unknown content type:" + contents.getClass().getName());
+            throw new IOException("Unknown content type: " +
+                    (contents == null ? "(null)" : contents.getClass().getName()));
         }
     }
 
