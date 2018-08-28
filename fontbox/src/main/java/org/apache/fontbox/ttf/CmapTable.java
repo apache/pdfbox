8865512cebf18b4ed49abb699e0cb8ce5ee376f4
Tilman Hausherr <tilman@apache.org>
2018-08-28 04:24:35 +0000

    PDFBOX-4071: SonarQube fix
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1839404 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/ttf/CmapTable.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/CmapTable.java
@@ -71,7 +71,7 @@ public class CmapTable extends TTFTable
     @Override
     public void read(TrueTypeFont ttf, TTFDataStream data) throws IOException
     {
-        @SuppressWarnings({"squid:S1854", "squid:S1481"})
+        @SuppressWarnings({"unused", "squid:S1854", "squid:S1481"})
         int version = data.readUnsignedShort();
         int numberOfTables = data.readUnsignedShort();
         cmaps = new CmapSubtable[numberOfTables];
