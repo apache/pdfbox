f1e24ab5805d70dc0b2971c7c37662f77a4f05de
Tilman Hausherr <tilman@apache.org>
2018-09-03 15:54:03 +0000

    PDFBOX-4308: prevent object that is referenced several times from being written direct and indirect, thus getting encrypted with wrong object number or getting encrypted twice
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1839972 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdfwriter/COSWriter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfwriter/COSWriter.java
@@ -927,8 +927,12 @@ public class COSWriter implements ICOSVisitor, Closeable
             else if( current instanceof COSObject )
             {
                 COSBase subValue = ((COSObject)current).getObject();
-                if (incrementalUpdate || subValue instanceof COSDictionary || subValue == null)
+                if (willEncrypt || incrementalUpdate || subValue instanceof COSDictionary || subValue == null)
                 {
+                    // PDFBOX-4308: added willEncrypt to prevent an object
+                    // that is referenced several times from being written
+                    // direct and indirect, thus getting encrypted
+                    // with wrong object number or getting encrypted twice
                     addObjectToWrite( current );
                     writeReference( current );
                 }
@@ -1025,8 +1029,12 @@ public class COSWriter implements ICOSVisitor, Closeable
                 else if( value instanceof COSObject )
                 {
                     COSBase subValue = ((COSObject)value).getObject();
-                    if (incrementalUpdate || subValue instanceof COSDictionary || subValue == null)
+                    if (willEncrypt || incrementalUpdate || subValue instanceof COSDictionary || subValue == null)
                     {
+                        // PDFBOX-4308: added willEncrypt to prevent an object
+                        // that is referenced several times from being written
+                        // direct and indirect, thus getting encrypted
+                        // with wrong object number or getting encrypted twice
                         addObjectToWrite( value );
                         writeReference( value );
                     }
