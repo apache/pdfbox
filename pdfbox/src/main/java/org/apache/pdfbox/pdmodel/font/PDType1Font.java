ff092a0961aaa630f634fa28c9778b75986f20e3
Tilman Hausherr <tilman@apache.org>
2018-09-17 16:02:48 +0000

    PDFBOX-4318: correct bug that wrong key was used for caching, thanks Daniel Wildschut
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1841081 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType1Font.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType1Font.java
@@ -454,7 +454,7 @@ public class PDType1Font extends PDSimpleFont implements PDVectorFont
         Map<String, Integer> inverted = encoding.getNameToCodeMap();
         int code = inverted.get(name);
         bytes = new byte[] { (byte)code };
-        codeToBytesMap.put(code, bytes);
+        codeToBytesMap.put(unicode, bytes);
         return bytes;
     }
 
