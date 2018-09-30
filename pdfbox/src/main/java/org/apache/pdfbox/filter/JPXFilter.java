e86e76cc4db539ed2a8da782675d3d658b4ba7db
Tilman Hausherr <tilman@apache.org>
2018-09-30 14:23:20 +0000

    PDFBOX-4071: use base class
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1842382 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/filter/JPXFilter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/filter/JPXFilter.java
@@ -22,7 +22,7 @@ import java.awt.image.DataBufferByte;
 import java.awt.image.DataBufferUShort;
 import java.awt.image.IndexColorModel;
 import java.awt.image.MultiPixelPackedSampleModel;
-import java.awt.image.WritableRaster;
+import java.awt.image.Raster;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -62,7 +62,7 @@ public final class JPXFilter extends Filter
         result.getParameters().addAll(parameters);
         BufferedImage image = readJPX(encoded, options, result);
 
-        WritableRaster raster = image.getRaster();
+        Raster raster = image.getRaster();
         switch (raster.getDataBuffer().getDataType())
         {
             case DataBuffer.TYPE_BYTE:
