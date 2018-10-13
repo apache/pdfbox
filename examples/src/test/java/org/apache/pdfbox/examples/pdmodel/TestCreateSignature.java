abc07ead0a33c0d1ed1f0b6e398003a6832d2191
Tilman Hausherr <tilman@apache.org>
2018-10-13 13:09:25 +0000

    PDFBOX-4071: remove unneeded cast
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1843748 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/test/java/org/apache/pdfbox/examples/pdmodel/TestCreateSignature.java
+++ examples/src/test/java/org/apache/pdfbox/examples/pdmodel/TestCreateSignature.java
@@ -36,7 +36,6 @@ import java.util.Collection;
 import java.util.List;
 
 import org.apache.pdfbox.cos.COSName;
-import org.apache.pdfbox.cos.COSObject;
 import org.apache.pdfbox.cos.COSString;
 import org.apache.pdfbox.examples.signature.CreateEmptySignatureForm;
 import org.apache.pdfbox.examples.signature.CreateSignature;
@@ -296,7 +295,7 @@ public class TestCreateSignature
         try (PDDocument document = PDDocument.load(origFile))
         {
             // get string representation of pages COSObject
-            origPageKey = ((COSObject) document.getDocumentCatalog().getCOSObject().getItem(COSName.PAGES)).toString();
+            origPageKey = document.getDocumentCatalog().getCOSObject().getItem(COSName.PAGES).toString();
         }
         try (PDDocument document = PDDocument.load(signedFile))
         {
