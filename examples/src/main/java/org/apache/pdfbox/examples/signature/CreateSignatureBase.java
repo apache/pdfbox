8e7eaa76efdab418c3c33004827f1333c18f9337
Tilman Hausherr <tilman@apache.org>
2018-10-21 06:49:55 +0000

    PDFBOX-3017: check key usage when signing
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1844462 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/CreateSignatureBase.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/CreateSignatureBase.java
@@ -84,6 +84,8 @@ public abstract class CreateSignatureBase implements SignatureInterface
             {
                 // avoid expired certificate
                 ((X509Certificate) cert).checkValidity();
+                
+                SigUtils.checkCertificateUsage((X509Certificate) cert);
             }
             break;
         }
