f14427284dc114fb10461d48c0ba09de0e864a4d
Tilman Hausherr <tilman@apache.org>
2018-10-23 18:13:52 +0000

    PDFBOX-3017: add revocation date to exception
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1844686 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/validation/RevokedCertificateException.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/validation/RevokedCertificateException.java
@@ -16,6 +16,8 @@
  */
 package org.apache.pdfbox.examples.signature.validation;
 
+import java.util.Date;
+
 /**
  * Exception to handle a revoked Certificate explicitly
  * 
@@ -24,9 +26,23 @@ package org.apache.pdfbox.examples.signature.validation;
 public class RevokedCertificateException extends Exception
 {
     private static final long serialVersionUID = 3543946618794126654L;
+    
+    private final Date revocationTime;
 
     public RevokedCertificateException(String message)
     {
         super(message);
+        this.revocationTime = null;
+    }
+
+    public RevokedCertificateException(String message, Date revocationTime)
+    {
+        super(message);
+        this.revocationTime = revocationTime;
+    }
+
+    public Date getRevocationTime()
+    {
+        return revocationTime;
     }
 }
