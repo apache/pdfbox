6bc40f55b639c24bbeac189c018f065825c3d0c4
Tilman Hausherr <tilman@apache.org>
2018-11-16 18:56:09 +0000

    PDFBOX-3017: move two classes to cert package
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1846748 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/cert/RevokedCertificateException.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/cert/RevokedCertificateException.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.pdfbox.examples.signature.validation;
+package org.apache.pdfbox.examples.signature.cert;
 
 import java.util.Date;
 
