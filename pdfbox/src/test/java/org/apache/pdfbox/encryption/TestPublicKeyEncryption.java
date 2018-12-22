3a263898ba11fc2a16538e88e2747b199309551a
Tilman Hausherr <tilman@apache.org>
2018-12-22 18:16:28 +0000

    PDFBOX-4413: correct javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1849562 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/test/java/org/apache/pdfbox/encryption/TestPublicKeyEncryption.java
+++ pdfbox/src/test/java/org/apache/pdfbox/encryption/TestPublicKeyEncryption.java
@@ -245,8 +245,7 @@ public class TestPublicKeyEncryption extends TestCase
     }
 
     /**
-     * Reloads the given document by writing it to a temporary byte array
-     * and loading a fresh document from that byte array.
+     * Reloads the given document from a file and check some contents.
      *
      * @param file input file
      * @param decryptionPassword password to be used to decrypt the doc
