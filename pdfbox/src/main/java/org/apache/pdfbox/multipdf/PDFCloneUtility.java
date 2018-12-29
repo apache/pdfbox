3c4f6dd33f4ed86277d2fb6e114f57ed24493660
Tilman Hausherr <tilman@apache.org>
2018-12-29 16:17:26 +0000

    PDFBOX-4417: don't clone a clone
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1849921 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/multipdf/PDFCloneUtility.java
+++ pdfbox/src/main/java/org/apache/pdfbox/multipdf/PDFCloneUtility.java
@@ -79,8 +79,14 @@ public class PDFCloneUtility
           if( retval != null )
           {
               //we are done, it has already been converted.
+              return retval;
           }
-          else if( base instanceof List)
+          if (base instanceof COSBase && clonedVersion.containsValue(base))
+          {
+              // Don't clone a clone
+              return (COSBase) base;
+          }
+          if (base instanceof List)
           {
               COSArray array = new COSArray();
               List<?> list = (List<?>) base;
@@ -169,7 +175,8 @@ public class PDFCloneUtility
               return;
               //we are done, it has already been converted. // ### Is that correct for cloneMerge???
           }
-          else if (!(base instanceof COSBase))
+          //TODO what when clone-merging a clone? Does it ever happen?
+          if (!(base instanceof COSBase))
           {
               cloneMerge(base.getCOSObject(), target.getCOSObject());
               clonedVersion.put(base, retval);
