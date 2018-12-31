edeb7f26267cf9c2f095699b8fbc59c20c82f461
Tilman Hausherr <tilman@apache.org>
2018-12-31 10:48:33 +0000

    PDFBOX-4003: add new class that can accomodate both COSArray and COSDictionary
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1850034 13f79535-47bb-0310-9956-ffa450edef68

---
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/documentinterchange/logicalstructure/PDParentTreeValue.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pdfbox.pdmodel.documentinterchange.logicalstructure;
+
+import org.apache.pdfbox.cos.COSArray;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.pdmodel.common.COSObjectable;
+
+/**
+ * Described in the PDF specification in the ParentTree segment of the table "Entries in the
+ * structure tree root". This is either a dictionary or an array.
+ *
+ * @author Tilman Hausherr
+ */
+public class PDParentTreeValue implements COSObjectable
+{
+    COSObjectable obj;
+
+    public PDParentTreeValue(COSArray obj)
+    {
+        this.obj = obj;
+    }
+
+    public PDParentTreeValue(COSDictionary obj)
+    {
+        this.obj = obj;
+    }
+
+    @Override
+    public COSBase getCOSObject()
+    {
+        return obj.getCOSObject();
+    }
+
+    @Override
+    public String toString()
+    {
+        return obj.toString();
+    }
+}
