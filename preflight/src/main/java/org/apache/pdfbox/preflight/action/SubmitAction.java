824643421ce78ea4f6ae162757a0d6c80e4c9946
Tilman Hausherr <tilman@apache.org>
2019-01-02 18:20:04 +0000

    PDFBOX-4420: fix javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1850192 13f79535-47bb-0310-9956-ffa450edef68

--- preflight/src/main/java/org/apache/pdfbox/preflight/action/SubmitAction.java
+++ preflight/src/main/java/org/apache/pdfbox/preflight/action/SubmitAction.java
@@ -49,7 +49,7 @@ public class SubmitAction extends AbstractActionManager
     /*
      * (non-Javadoc)
      * 
-     * @see net.awl.edoc.pdfa.validation.actions.AbstractActionManager#valid(java.util .List)
+     * @see org.apache.pdfbox.preflight.action.AbstractActionManager#valid(java.util .List)
      */
     @Override
     protected boolean innerValid()
