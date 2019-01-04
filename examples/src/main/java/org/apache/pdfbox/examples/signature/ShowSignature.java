bfa188ebfff04a8e6e2bed6d1702b23eae8c92c6
Tilman Hausherr <tilman@apache.org>
2019-01-04 19:06:46 +0000

    PDFBOX-4071: remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1850430 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/ShowSignature.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/ShowSignature.java
@@ -40,7 +40,6 @@ import java.util.Set;
 import org.apache.pdfbox.cos.COSArray;
 import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
-import org.apache.pdfbox.cos.COSInputStream;
 import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.cos.COSObject;
 import org.apache.pdfbox.cos.COSStream;
