35ecc35354b57549f606f83832103cc0cb769c0d
Tilman Hausherr <tilman@apache.org>
2019-01-08 20:09:13 +0000

    PDFBOX-4399: remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1850788 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/markedcontent/BeginMarkedContentSequence.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/markedcontent/BeginMarkedContentSequence.java
@@ -21,7 +21,6 @@ import java.util.List;
 
 import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSName;
-import org.apache.pdfbox.text.PDFMarkedContentExtractor;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
 
