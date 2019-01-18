2d8ea9cc6a28eee8cfbcd859d05327e73454cc98
Tilman Hausherr <tilman@apache.org>
2019-01-18 08:04:43 +0000

    PDFBOX-4434: Sonar fix
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1851607 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/interactive/form/CreateCheckBox.java
+++ examples/src/main/java/org/apache/pdfbox/examples/interactive/form/CreateCheckBox.java
@@ -44,7 +44,11 @@ import org.apache.pdfbox.pdmodel.interactive.form.PDCheckBox;
  */
 public class CreateCheckBox
 {
-    public static void main(String arg[]) throws IOException
+    private CreateCheckBox()
+    {
+    }
+
+    public static void main(String[] args) throws IOException
     {
         try (PDDocument document = new PDDocument())
         {
