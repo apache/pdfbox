30cbc93dea0795af9f9b07c23b9374923a707108
Tilman Hausherr <tilman@apache.org>
2019-02-03 12:10:40 +0000

    PDFBOX-4450: catch more recursions, avoid multiple checking of certain COS elements that isn't caught in recursion check
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1852825 13f79535-47bb-0310-9956-ffa450edef68

--- preflight/src/main/java/org/apache/pdfbox/preflight/PreflightContext.java
+++ preflight/src/main/java/org/apache/pdfbox/preflight/PreflightContext.java
@@ -23,13 +23,16 @@ package org.apache.pdfbox.preflight;
 
 import java.io.Closeable;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.activation.DataSource;
 
 import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.pdfparser.XrefTrailerResolver;
+import org.apache.pdfbox.pdmodel.common.COSObjectable;
 import org.apache.pdfbox.preflight.ValidationResult.ValidationError;
 import org.apache.pdfbox.preflight.font.container.FontContainer;
 import org.apache.pdfbox.preflight.graphic.ICCProfileWrapper;
@@ -76,6 +79,8 @@ public class PreflightContext implements Closeable
     private PreflightConfiguration config = null;
 
     private PreflightPath validationPath = new PreflightPath();
+
+    private final Set<COSObjectable> processedSet = new HashSet<COSObjectable>();
 
     private Integer currentPageNumber = null;
     
@@ -287,5 +292,26 @@ public class PreflightContext implements Closeable
     public long getFileLen()
     {
         return fileLen;
+    }
+
+    /**
+     * Add the argument to the set of processed elements,
+     *
+     * @param cos
+     */
+    public void addToProcessedSet(COSObjectable cos)
+    {
+        processedSet.add(cos);
+    }
+
+    /**
+     * Tell if the argument is in the set of processed elements.
+     *
+     * @param cos
+     * @return true if in the set, false if not.
+     */
+    public boolean isInProcessedSet(COSObjectable cos)
+    {
+        return processedSet.contains(cos);
     }
 }
