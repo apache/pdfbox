1515617a99975393b0115da9db78e862840f74e6
Tilman Hausherr <tilman@apache.org>
2019-02-25 19:20:05 +0000

    PDFBOX-3017: move segment to bottom so that output comes together with signature verification
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1854335 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/ShowSignature.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/ShowSignature.java
@@ -151,30 +151,6 @@ public final class ShowSignature
 
                     System.out.println("Signature found");
 
-                    int[] byteRange = sig.getByteRange();
-                    if (byteRange.length != 4)
-                    {
-                        System.err.println("Signature byteRange must have 4 items");
-                    }
-                    else
-                    {
-                        long fileLen = infile.length();
-                        long rangeMax = byteRange[2] + (long) byteRange[3];
-                        // multiply content length with 2 (because it is in hex in the PDF) and add 2 for < and >
-                        int contentLen = contents.getString().length() * 2 + 2;
-                        if (fileLen != rangeMax || byteRange[0] != 0 || byteRange[1] + contentLen != byteRange[2])
-                        {
-                            // a false result doesn't necessarily mean that the PDF is a fake
-                            // see this answer why:
-                            // https://stackoverflow.com/a/48185913/535646
-                            System.out.println("Signature does not cover whole document");
-                        }
-                        else
-                        {
-                            System.out.println("Signature covers whole document");
-                        }
-                    }
-
                     if (sig.getName() != null)
                     {
                         System.out.println("Name:     " + sig.getName());
@@ -277,6 +253,30 @@ public final class ShowSignature
                     else
                     {
                         throw new IOException("Missing subfilter for cert dictionary");
+                    }
+
+                    int[] byteRange = sig.getByteRange();
+                    if (byteRange.length != 4)
+                    {
+                        System.err.println("Signature byteRange must have 4 items");
+                    }
+                    else
+                    {
+                        long fileLen = infile.length();
+                        long rangeMax = byteRange[2] + (long) byteRange[3];
+                        // multiply content length with 2 (because it is in hex in the PDF) and add 2 for < and >
+                        int contentLen = contents.getString().length() * 2 + 2;
+                        if (fileLen != rangeMax || byteRange[0] != 0 || byteRange[1] + contentLen != byteRange[2])
+                        {
+                            // a false result doesn't necessarily mean that the PDF is a fake
+                            // see this answer why:
+                            // https://stackoverflow.com/a/48185913/535646
+                            System.out.println("Signature does not cover whole document");
+                        }
+                        else
+                        {
+                            System.out.println("Signature covers whole document");
+                        }
                     }
                 }
                 analyseDSS(document);
