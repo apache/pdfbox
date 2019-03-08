41a4b08a9e4e369a4ba2efd9f8ac18ff9a35bb6c
Tilman Hausherr <tilman@apache.org>
2019-03-08 18:21:29 +0000

    PDFBOX-3017: check whether gap contains a hex value equal byte-by-byte to the Content value, as suggested by mkl in SO 55049270 comment
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1855060 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/ShowSignature.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/ShowSignature.java
@@ -53,6 +53,7 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
 import org.apache.pdfbox.pdmodel.encryption.SecurityProvider;
 import org.apache.pdfbox.pdmodel.interactive.digitalsignature.PDSignature;
+import org.apache.pdfbox.util.Charsets;
 import org.apache.pdfbox.util.Hex;
 import org.bouncycastle.asn1.ASN1Object;
 import org.bouncycastle.asn1.cms.Attribute;
@@ -274,6 +275,7 @@ public final class ShowSignature
                         {
                             System.out.println("Signature covers whole document");
                         }
+                        checkContentValueWithFile(infile, byteRange, contents);
                     }
                 }
                 analyseDSS(document);
@@ -295,6 +297,58 @@ public final class ShowSignature
             }
             System.out.println("Analyzed: " + args[1]);
         }
+    }
+
+    private void checkContentValueWithFile(File file, int[] byteRange, COSString contents) throws IOException
+    {
+        // https://stackoverflow.com/questions/55049270
+        // comment by mkl: check whether gap contains a hex value equal
+        // byte-by-byte to the Content value, to prevent attacker from using a literal string
+        // to allow extra space
+        RandomAccessBufferedFileInputStream raf = new RandomAccessBufferedFileInputStream(file);
+        raf.seek(byteRange[1]);
+        int c = raf.read();
+        if (c != '<')
+        {
+            System.err.println("'<' expected at offset " + byteRange[1] + ", but got " + (char) c);
+        }
+        byte[] contentFromFile = raf.readFully(byteRange[2] - byteRange[1] - 2);
+        byte[] contentAsHex = Hex.getString(contents.getBytes()).getBytes(Charsets.US_ASCII);
+        if (contentFromFile.length != contentAsHex.length)
+        {
+            System.err.println("Raw content length from file is " +
+                    contentFromFile.length +
+                    ", but internal content string in hex has length " +
+                    contentAsHex.length);
+        }
+        // Compare the two, we can't do byte comparison because of upper/lower case
+        // also check that it is really hex
+        for (int i = 0; i < contentFromFile.length; ++i)
+        {
+            try
+            {
+                if (Integer.parseInt(String.valueOf((char) contentFromFile[i]), 16) !=
+                    Integer.parseInt(String.valueOf((char) contentAsHex[i]), 16))
+                {
+                    System.err.println("Possible manipulation at file offset " +
+                                       (byteRange[1] + i + 1) + " in signature content");
+                    break;
+                }
+            }
+            catch (NumberFormatException ex)
+            {
+                System.err.println("Incorrect hex value");
+                System.err.println("Possible manipulation at file offset " +
+                                    (byteRange[1] + i + 1) + " in signature content");
+                break;
+            }
+        }
+        c = raf.read();
+        if (c != '>')
+        {
+            System.err.println("'>' expected at offset " + byteRange[2] + ", but got " + (char) c);
+        }
+        raf.close();
     }
 
     private void verifyETSIdotRFC3161(byte[] buf, COSString contents)
