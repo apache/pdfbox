7d4d83d7563452dd909fdd7b500abc1f65699ecf
Tilman Hausherr <tilman@apache.org>
2019-04-07 08:25:45 +0000

    PDFBOX-4508: improve javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1857073 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDVariableText.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDVariableText.java
@@ -96,11 +96,21 @@ public abstract class PDVariableText extends PDTerminalField
     /**
      * Set the default appearance.
      * 
-     * This will set the local default appearance for the variable text field only not 
+     * This will set the local default appearance for the variable text field only, not 
      * affecting a default appearance in the parent hierarchy.
      * 
      * Providing null as the value will remove the local default appearance.
-     * 
+     * <p>
+     * This method can also be used to change the font of a field, by replacing the font name from
+     * this string with another font name found in the AcroForm default resources <u>before</u>
+     * calling {@link #setValue(java.lang.String) setValue(String)}, see also
+     * <a href="https://stackoverflow.com/questions/47995062/pdfbox-api-how-to-handle-cyrillic-values">this
+     * stackoverflow answer</a>. For example, "/Helv 10 Tf 0 g" can be replaced with "/F1 10 Tf 0
+     * g". Performance may go down (see
+     * <a href="https://issues.apache.org/jira/browse/PDFBOX-4508">PDFBOX-4508)</a> if this is done
+     * for many fields and with a very large font (e.g. ArialUni); to avoid this, save and reload
+     * the file after changing all fields.
+     *
      * @param daValue a string describing the default appearance
      */
     public void setDefaultAppearance(String daValue)
