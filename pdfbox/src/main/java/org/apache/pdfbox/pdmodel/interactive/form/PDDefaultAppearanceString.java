1e4bd044702e02be7c454fb33b6354c0ad1d3e20
Tilman Hausherr <tilman@apache.org>
2019-04-10 17:57:24 +0000

    PDFBOX-4071: improve javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1857264 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDDefaultAppearanceString.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDDefaultAppearanceString.java
@@ -284,7 +284,11 @@ class PDDefaultAppearanceString
     }
 
     /**
-     * Writes the DA string to the given content stream.
+     * Write font name, font size and color from the /DA string to the given content stream.
+     *
+     * @param contents The content stream.
+     * @param zeroFontSize The calculated font size to use if the /DA string has a size 0
+     * (autosize). Otherwise the size from the /DA string is used.
      */
     void writeTo(PDAppearanceContentStream contents, float zeroFontSize) throws IOException
     {
