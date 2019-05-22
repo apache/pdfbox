3b66218a279021a3260e1f5012c761136244c1af
Tilman Hausherr <tilman@apache.org>
2019-05-22 17:11:30 +0000

    PDFBOX-4551: prevent printing when not authorized
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1859744 13f79535-47bb-0310-9956-ffa450edef68

--- tools/src/main/java/org/apache/pdfbox/tools/PrintPDF.java
+++ tools/src/main/java/org/apache/pdfbox/tools/PrintPDF.java
@@ -26,6 +26,7 @@ import java.util.Map;
 import javax.print.PrintService;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
 import org.apache.pdfbox.printing.Orientation;
 import org.apache.pdfbox.printing.PDFPageable;
 
@@ -152,6 +153,12 @@ public final class PrintPDF
         try
         {
             document = PDDocument.load(new File(pdfFile), password);
+
+            AccessPermission ap = document.getCurrentAccessPermission();
+            if (!ap.canPrint())
+            {
+                throw new IOException("You do not have permission to print");
+            }
 
             PrinterJob printJob = PrinterJob.getPrinterJob();
             printJob.setJobName(new File(pdfFile).getName());
