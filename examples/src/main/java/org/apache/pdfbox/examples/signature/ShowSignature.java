3df9e8a581183f32cbf5d9a54868d08b80ccf015
Tilman Hausherr <tilman@apache.org>
2019-06-08 08:46:30 +0000

    PDFBOX-4071: add cast / type; avoid "unchecked" warnings
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1860816 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/ShowSignature.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/ShowSignature.java
@@ -235,9 +235,9 @@ public final class ShowSignature
 
                                 if (sig.getSignDate() != null)
                                 {
-                                    verifyCertificateChain(new JcaCertStore(certs),
-                                            cert,
-                                            sig.getSignDate().getTime());
+                                    @SuppressWarnings("unchecked")
+                                    Store<X509CertificateHolder> store = new JcaCertStore(certs);
+                                    verifyCertificateChain(store, cert, sig.getSignDate().getTime());
                                 }
                             }
                         }
@@ -444,8 +444,9 @@ public final class ShowSignature
             // https://www.quovadisglobal.com/~/media/Files/Repository/QV_RCA1_RCA3_CPCPS_V4_11.ashx
             // also 021496.pdf and 036351.pdf from digitalcorpora
             validateTimestampToken(timeStampToken);
+            @SuppressWarnings("unchecked") // TimeStampToken.getSID() is untyped
             Collection<X509CertificateHolder> tstMatches =
-                timeStampToken.getCertificates().getMatches(timeStampToken.getSID());
+                timeStampToken.getCertificates().getMatches((Selector<X509CertificateHolder>) timeStampToken.getSID());
             X509CertificateHolder tstCertHolder = tstMatches.iterator().next();
             X509Certificate certFromTimeStamp = new JcaX509CertificateConverter().getCertificate(tstCertHolder);
             // merge both stores using a set to remove duplicates
@@ -582,8 +583,9 @@ public final class ShowSignature
             throws IOException, CertificateException, TSPException, OperatorCreationException
     {
         // https://stackoverflow.com/questions/42114742/
+        @SuppressWarnings("unchecked") // TimeStampToken.getSID() is untyped
         Collection<X509CertificateHolder> tstMatches =
-                timeStampToken.getCertificates().getMatches(timeStampToken.getSID());
+                timeStampToken.getCertificates().getMatches((Selector<X509CertificateHolder>) timeStampToken.getSID());
         X509CertificateHolder holder = tstMatches.iterator().next();
         X509Certificate tstCert = new JcaX509CertificateConverter().getCertificate(holder);
         SignerInformationVerifier siv = new JcaSimpleSignerInfoVerifierBuilder().setProvider(SecurityProvider.getProvider()).build(tstCert);
