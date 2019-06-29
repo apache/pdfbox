a93de33b7dd4d720a0c5047a248d305bfeed6f53
Tilman Hausherr <tilman@apache.org>
2019-06-29 03:34:37 +0000

    PDFBOX-4587: fix typo
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1862299 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/SaslPrep.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/SaslPrep.java
@@ -266,7 +266,7 @@ class SaslPrep
     private static boolean privateUse(int codepoint)
     {
         return 0xE000 <= codepoint && codepoint <= 0xF8FF
-                || 0xF000 <= codepoint && codepoint <= 0xFFFFD
+                || 0xF0000 <= codepoint && codepoint <= 0xFFFFD
                 || 0x100000 <= codepoint && codepoint <= 0x10FFFD;
     }
 
