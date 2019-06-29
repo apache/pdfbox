ed7b5532afae1d0f18248eb8ae1b3041c6057f9b
Tilman Hausherr <tilman@apache.org>
2019-06-29 16:14:16 +0000

    PDFBOX-4583: remember and apply initial clip of a graphics device
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1862319 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
@@ -134,6 +134,9 @@ public class PageDrawer extends PDFGraphicsStreamEngine
     
     // last clipping path
     private Area lastClip;
+
+    // clip when drawPage() is called, can be null, must be intersected when clipping
+    private Shape initialClip;
     
     // shapes of glyphs being drawn to be used for clipping
     private List<Shape> textClippings;
@@ -245,6 +248,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
     {
         graphics = (Graphics2D) g;
         xform = graphics.getTransform();
+        initialClip = graphics.getClip();
         this.pageSize = pageSize;
 
         setRenderingHints();
@@ -288,6 +292,8 @@ public class PageDrawer extends PDFGraphicsStreamEngine
 
         Area oldLastClip = lastClip;
         lastClip = null;
+        Shape oldInitialClip = initialClip;
+        initialClip = null;
         
         boolean oldFlipTG = flipTG;
         flipTG = true;
@@ -299,6 +305,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
         graphics = oldGraphics;
         linePath = oldLinePath;
         lastClip = oldLastClip;
+        initialClip = oldInitialClip;
         clipWindingRule = oldClipWindingRule;
     }
 
@@ -365,6 +372,11 @@ public class PageDrawer extends PDFGraphicsStreamEngine
         if (clippingPath != lastClip)
         {
             graphics.setClip(clippingPath);
+            if (initialClip != null)
+            {
+                // apply the remembered initial clip, but transform it first
+                graphics.clip(graphics.getTransform().createTransformedShape(initialClip));
+            }
             lastClip = clippingPath;
         }
     }
@@ -1413,6 +1425,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
         {
             Graphics2D g2dOriginal = graphics;
             Area lastClipOriginal = lastClip;
+            Shape oldInitialClip = initialClip;
 
             // get the CTM x Form Matrix transform
             Matrix transform = Matrix.concatenate(ctm, form.getMatrix());
@@ -1556,6 +1569,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
                 lastClip = lastClipOriginal;
                 graphics.dispose();
                 graphics = g2dOriginal;
+                initialClip = oldInitialClip;
                 clipWindingRule = clipWindingRuleOriginal;
                 linePath = linePathOriginal;
                 pageSize = pageSizeOriginal;
