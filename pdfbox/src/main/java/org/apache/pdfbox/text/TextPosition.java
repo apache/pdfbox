8ee505ae6749210e37162d5599f9111c2ed8e2af
Tilman Hausherr <tilman@apache.org>
2019-07-07 16:22:46 +0000

    PDFBOX-4071: SonarQube fix: remove conditions that are always true
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/issue4569@1862700 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/text/TextPosition.java
+++ pdfbox/src/main/java/org/apache/pdfbox/text/TextPosition.java
@@ -602,20 +602,20 @@ public final class TextPosition
             }
             // diacritic completely covers this character and therefore we assume that this is the
             // character the diacritic belongs to
-            else if (diacXStart < currCharXStart && diacXEnd > currCharXEnd)
+            else if (diacXStart < currCharXStart)
             {
                 insertDiacritic(i, diacritic);
                 wasAdded = true;
             }
             // otherwise, The diacritic modifies this character because its completely
             // contained by the character width
-            else if (diacXStart >= currCharXStart && diacXEnd <= currCharXEnd)
+            else if (diacXEnd <= currCharXEnd)
             {
                 insertDiacritic(i, diacritic);
                 wasAdded = true;
             }
             // last character in the TextPosition so we add diacritic to the end
-            else if (diacXStart >= currCharXStart && diacXEnd > currCharXEnd && i == strLen - 1)
+            else if (i == strLen - 1)
             {
                 insertDiacritic(i, diacritic);
                 wasAdded = true;
