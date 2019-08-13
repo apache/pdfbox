4ac391fdc39bc63cbfde3945c47d3a0ede8a5c39
Tilman Hausherr <tilman@apache.org>
2019-08-13 17:37:44 +0000

    PDFBOX-4080: pass PDDocument to construct appearances, as suggested by Ghenadii Batalski
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1865060 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
@@ -1299,8 +1299,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
         PDAppearanceDictionary appearance = annotation.getAppearance();
         if (appearance == null || appearance.getNormalAppearance() == null)
         {
-            // TODO: Improve memory consumption by passing a ScratchFile
-            annotation.constructAppearances();
+            annotation.constructAppearances(renderer.document);
         }
         if (annotation.isNoRotate() && getCurrentPage().getRotation() != 0)
         {
