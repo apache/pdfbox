ca16c6cc44bb46df483da67027d5d3d22d842086
Tilman Hausherr <tilman@apache.org>
2019-08-23 16:06:06 +0000

    PDFBOX-4071: SonarQube fix: these UI classes won't be serialized
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1865771 13f79535-47bb-0310-9956-ffa450edef68

--- debugger/src/main/java/org/apache/pdfbox/debugger/PDFDebugger.java
+++ debugger/src/main/java/org/apache/pdfbox/debugger/PDFDebugger.java
@@ -127,7 +127,7 @@ import org.apache.pdfbox.printing.PDFPageable;
  * @author Ben Litchfield
  * @author Khyrul Bashar
  */
-@SuppressWarnings({"serial","squid:MaximumInheritanceDepth"})
+@SuppressWarnings({"serial","squid:MaximumInheritanceDepth","squid:S1948"})
 public class PDFDebugger extends JFrame
 {
     private static final Set<COSName> SPECIALCOLORSPACES =
@@ -146,7 +146,6 @@ public class PDFDebugger extends JFrame
     
     private TreeStatusPane statusPane;
     private RecentFiles recentFiles;
-    @SuppressWarnings("squid:S1948")
     private WindowPrefs windowPrefs;
     private boolean isPageMode;
 
