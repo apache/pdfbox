318fdbfede62710568bc95f58ed83181d4b139ad
Tilman Hausherr <tilman@apache.org>
2019-08-24 04:41:04 +0000

    PDFBOX-4071: SonarQube fix
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/issue45@1865808 13f79535-47bb-0310-9956-ffa450edef68

--- debugger/src/main/java/org/apache/pdfbox/debugger/streampane/StreamPane.java
+++ debugger/src/main/java/org/apache/pdfbox/debugger/streampane/StreamPane.java
@@ -416,7 +416,7 @@ public class StreamPane implements ActionListener
                     }
                     else
                     {
-                        String str = "" + (char)chr;
+                        String str = Character.toString((char) chr);
                         docu.insertString(docu.getLength(), str, STRING_STYLE);
                     }
                 }
