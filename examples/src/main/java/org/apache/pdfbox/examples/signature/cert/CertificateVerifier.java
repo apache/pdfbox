534e48facd5e8de6d4354a6f1da2175a0ee602ee
Tilman Hausherr <tilman@apache.org>
2019-10-07 18:40:54 +0000

    PDFBOX-3017: use constant.equals; use base class
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1868093 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/cert/CertificateVerifier.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/cert/CertificateVerifier.java
@@ -49,6 +49,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.pdfbox.io.IOUtils;
 import org.apache.pdfbox.pdmodel.encryption.SecurityProvider;
+import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.ASN1OctetString;
 import org.bouncycastle.asn1.ASN1Primitive;
@@ -285,8 +286,8 @@ public final class CertificateVerifier
         {
             // AccessDescription
             ASN1Sequence obj = (ASN1Sequence) objects.nextElement();
-            ASN1ObjectIdentifier oid = (ASN1ObjectIdentifier) obj.getObjectAt(0);
-            if (!oid.equals(X509ObjectIdentifiers.id_ad_caIssuers))
+            ASN1Encodable oid = obj.getObjectAt(0);
+            if (!X509ObjectIdentifiers.id_ad_caIssuers.equals(oid))
             {
                 continue;
             }
@@ -395,7 +396,7 @@ public final class CertificateVerifier
                 ASN1ObjectIdentifier oid = (ASN1ObjectIdentifier) obj.getObjectAt(0);
                 // accessLocation
                 ASN1TaggedObject location = (ASN1TaggedObject) obj.getObjectAt(1);
-                if (oid.equals(X509ObjectIdentifiers.id_ad_ocsp)
+                if (X509ObjectIdentifiers.id_ad_ocsp.equals(oid)
                         && location.getTagNo() == GeneralName.uniformResourceIdentifier)
                 {
                     ASN1OctetString url = (ASN1OctetString) location.getObject();
