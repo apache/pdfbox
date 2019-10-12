e0e039d7998a1cbf8a5d88bc7b01965e4e19b51d
Tilman Hausherr <tilman@apache.org>
2019-10-12 13:56:54 +0000

    PDFBOX-4341: update javadoc about drawbacks of this method, by Emmeran Seehuber
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1868345 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/util/Matrix.java
+++ pdfbox/src/main/java/org/apache/pdfbox/util/Matrix.java
@@ -702,7 +702,10 @@ public final class Matrix implements Cloneable
     }
 
     /**
-     * Returns a COS array which represents this matrix.
+     * Returns a COS array which represent the geometric relevant
+     * components of the matrix. The last column of the matrix is ignored,
+     * only the first two columns are returned. This is analog to the
+     * Matrix(COSArray) constructor.
      */
     public COSArray toCOSArray()
     {
