e6f182e6c2ebfeaa93c90afa514fb1818d0ce637
Tilman Hausherr <tilman@apache.org>
2019-11-13 17:22:31 +0000

    PDFBOX-4686: add text to AcroForm examples, make the examples more similar
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1869759 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/interactive/form/CreateSimpleFormWithEmbeddedFont.java
+++ examples/src/main/java/org/apache/pdfbox/examples/interactive/form/CreateSimpleFormWithEmbeddedFont.java
@@ -18,14 +18,20 @@
 package org.apache.pdfbox.examples.interactive.form;
 
 import java.io.IOException;
+import org.apache.pdfbox.cos.COSDictionary;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.PDResources;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.color.PDColor;
+import org.apache.pdfbox.pdmodel.graphics.color.PDDeviceRGB;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationWidget;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAppearanceCharacteristicsDictionary;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDTextField;
 
@@ -47,7 +53,7 @@ public class CreateSimpleFormWithEmbeddedFont
     {
         // Create a new document with an empty page.
         PDDocument doc = new PDDocument();
-        PDPage page = new PDPage();
+        PDPage page = new PDPage(PDRectangle.A4);
         doc.addPage(page);
         PDAcroForm acroForm = new PDAcroForm(doc);
         doc.getDocumentCatalog().setAcroForm(acroForm);
@@ -79,9 +85,26 @@ public class CreateSimpleFormWithEmbeddedFont
         widget.setPage(page);
         page.getAnnotations().add(widget);
 
+        // set green border and yellow background
+        // if you prefer defaults, just delete this code block
+        PDAppearanceCharacteristicsDictionary fieldAppearance
+                = new PDAppearanceCharacteristicsDictionary(new COSDictionary());
+        fieldAppearance.setBorderColour(new PDColor(new float[]{0,1,0}, PDDeviceRGB.INSTANCE));
+        fieldAppearance.setBackground(new PDColor(new float[]{1,1,0}, PDDeviceRGB.INSTANCE));
+        widget.setAppearanceCharacteristics(fieldAppearance);
+
         // set the field value. Note that the last character is a turkish capital I with a dot,
         // which is not part of WinAnsiEncoding
         textBox.setValue("Sample field İ");
+
+        // put some text near the field
+        PDPageContentStream cs = new PDPageContentStream(doc, page);
+        cs.beginText();
+        cs.setFont(PDType1Font.HELVETICA, 15);
+        cs.newLineAtOffset(50, 760);
+        cs.showText("Field:");
+        cs.endText();
+        cs.close();
 
         doc.save("target/SimpleFormWithEmbeddedFont.pdf");
         doc.close();
