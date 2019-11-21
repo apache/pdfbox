2424a9e28f2215ea78543e7c02095400e52b215f
Tilman Hausherr <tilman@apache.org>
2019-11-21 18:55:53 +0000

    PDFBOX-4071: SonarQube fix
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/issue4569@1870109 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/graphics/FillNonZeroAndStrokePath.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/graphics/FillNonZeroAndStrokePath.java
@@ -22,7 +22,7 @@ import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
 
-import java.awt.geom.GeneralPath;
+import java.awt.geom.Path2D;
 import java.io.IOException;
 
 /**
@@ -36,7 +36,7 @@ public class FillNonZeroAndStrokePath extends GraphicsOperatorProcessor
     @Override
     public void process(Operator operator, List<COSBase> operands) throws IOException
     {
-        context.fillAndStrokePath(GeneralPath.WIND_NON_ZERO);
+        context.fillAndStrokePath(Path2D.WIND_NON_ZERO);
     }
 
     @Override
