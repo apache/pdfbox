539897ac68befd2511f007855cd609cfb04b49ff
Tilman Hausherr <tilman@apache.org>
2019-11-23 10:43:33 +0000

    PDFBOX-4071: disable irrelevant SonarQube vulnerabilities warnings on self-created XML
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/issue45@1870220 13f79535-47bb-0310-9956-ffa450edef68

--- preflight/src/main/java/org/apache/pdfbox/preflight/parser/XmlResultParser.java
+++ preflight/src/main/java/org/apache/pdfbox/preflight/parser/XmlResultParser.java
@@ -47,6 +47,7 @@ public class XmlResultParser
     {
         try
         {
+            @SuppressWarnings({"squid:S2755"}) // self-created XML
             Document rdocument = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
             return validate(rdocument,dataSource);
         }
