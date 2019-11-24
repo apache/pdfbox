7b1eeff8bfc6ff72bf788fa9a1b362d9aee7d933
Tilman Hausherr <tilman@apache.org>
2019-11-24 16:13:09 +0000

    PDFBOX-4696: avoid multiple certificate downloads
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1870312 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/validation/CertInformationCollector.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/validation/CertInformationCollector.java
@@ -64,6 +64,7 @@ public class CertInformationCollector
     private static final int MAX_CERTIFICATE_CHAIN_DEPTH = 5;
 
     private final Set<X509Certificate> certificateSet = new HashSet<X509Certificate>();
+    private final Set<String> urlSet = new HashSet<String>();
 
     private final JcaX509CertificateConverter certConverter = new JcaX509CertificateConverter();
 
@@ -277,6 +278,11 @@ public class CertInformationCollector
     private void getAlternativeIssuerCertificate(CertSignatureInformation certInfo, int maxDepth)
             throws CertificateProccessingException
     {
+        if (urlSet.contains(certInfo.issuerUrl))
+        {
+            return;
+        }
+        urlSet.add(certInfo.issuerUrl);
         LOG.info("Get alternative issuer certificate from: " + certInfo.issuerUrl);
         try
         {
