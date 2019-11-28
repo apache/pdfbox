5756df970b80a23a5541f9fd9145029212f0057f
Tilman Hausherr <tilman@apache.org>
2019-11-28 07:32:42 +0000

    PDFBOX-3017: use certificateHolder BC class where possible
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/issue4569@1870531 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/test/java/org/apache/pdfbox/examples/pdmodel/TestCreateSignature.java
+++ examples/src/test/java/org/apache/pdfbox/examples/pdmodel/TestCreateSignature.java
@@ -349,10 +349,9 @@ public class TestCreateSignature
                 Collection<X509CertificateHolder> matches = certificatesStore
                         .getMatches((Selector<X509CertificateHolder>) signerInformation.getSID());
                 X509CertificateHolder certificateHolder = (X509CertificateHolder) matches.iterator().next();
-                X509Certificate certFromSignedData = new JcaX509CertificateConverter().getCertificate(certificateHolder);
-                Assert.assertEquals(certificate, certFromSignedData);
+                Assert.assertArrayEquals(certificate.getEncoded(), certificateHolder.getEncoded());
                 // CMSVerifierCertificateNotValidException means that the keystore wasn't valid at signing time
-                if (!signerInformation.verify(new JcaSimpleSignerInfoVerifierBuilder().build(certFromSignedData)))
+                if (!signerInformation.verify(new JcaSimpleSignerInfoVerifierBuilder().build(certificateHolder)))
                 {
                     Assert.fail("Signature verification failed");
                 }
