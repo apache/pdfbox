71050dc8285a7662f8c86b8d7e2cbab19fbee915
Tilman Hausherr <tilman@apache.org>
2019-12-06 04:29:05 +0000

    PDFBOX-4071: remove deprecated
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/issue4569@1870910 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/test/java/org/apache/pdfbox/pdfwriter/COSWriterXRefEntryTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdfwriter/COSWriterXRefEntryTest.java
@@ -17,15 +17,10 @@
 package org.apache.pdfbox.pdfwriter;
 
 import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class COSWriterXRefEntryTest
 {
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
     @Test
     public void compareToInputNullOutputNegative()
     {
--- pdfbox/src/test/java/org/apache/pdfbox/pdmodel/PageModeTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/PageModeTest.java
@@ -23,9 +23,6 @@ import org.junit.rules.ExpectedException;
 
 public class PageModeTest
 {
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
     @Test
     public void fromStringInputNotNullOutputNotNull()
     {
@@ -107,27 +104,13 @@ public class PageModeTest
     @Test
     public void fromStringInputNotNullOutputIllegalArgumentException()
     {
-        // Arrange
-        final String value = "";
-
-        // Act
-        thrown.expect(IllegalArgumentException.class);
-        PageMode.fromString(value);
-
-        // Method is not expected to return due to exception thrown
+        Assert.assertThrows(IllegalArgumentException.class, () -> PageMode.fromString(""));
     }
 
     @Test
     public void fromStringInputNotNullOutputIllegalArgumentException2()
     {
-        // Arrange
-        final String value = "Dulacb`ecj";
-
-        // Act
-        thrown.expect(IllegalArgumentException.class);
-        PageMode.fromString(value);
-
-        // Method is not expected to return due to exception thrown
+        Assert.assertThrows(IllegalArgumentException.class, () -> PageMode.fromString("Dulacb`ecj"));
     }
 
     @Test
