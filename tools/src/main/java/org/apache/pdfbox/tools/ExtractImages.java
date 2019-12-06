234f91228975e1f39ad82a2923a36ffd786cd83f
Tilman Hausherr <tilman@apache.org>
2019-12-06 02:42:39 +0000

    PDFBOX-4071: remove "throws" declaration that isn't thrown
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1870897 13f79535-47bb-0310-9956-ffa450edef68

--- tools/src/main/java/org/apache/pdfbox/tools/ExtractImages.java
+++ tools/src/main/java/org/apache/pdfbox/tools/ExtractImages.java
@@ -197,7 +197,7 @@ public final class ExtractImages
 
     private class ImageGraphicsEngine extends PDFGraphicsStreamEngine
     {
-        protected ImageGraphicsEngine(PDPage page) throws IOException
+        protected ImageGraphicsEngine(PDPage page)
         {
             super(page);
         }
