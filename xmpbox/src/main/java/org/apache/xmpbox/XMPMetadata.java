c361f70c046bcad226e69681d8ca6eabb4999b96
Tilman Hausherr <tilman@apache.org>
2019-12-12 04:59:10 +0000

    PDFBOX-4071: fix typo found by codespell on fossies.org
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/1.8@1871228 13f79535-47bb-0310-9956-ffa450edef68

--- xmpbox/src/main/java/org/apache/xmpbox/XMPMetadata.java
+++ xmpbox/src/main/java/org/apache/xmpbox/XMPMetadata.java
@@ -66,7 +66,7 @@ public class XMPMetadata
     private TypeMapping typeMapping;
 
     /**
-     * Contructor of an empty default XMPMetaData
+     * Constructor of an empty default XMPMetaData
      * 
      * @throws CreateXMPMetadataException
      *             If DOM Document associated could not be created
