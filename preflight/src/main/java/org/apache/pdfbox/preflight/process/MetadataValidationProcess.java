b541548ae1d538e8882baf6c6176af22214ebe5a
Tilman Hausherr <tilman@apache.org>
2019-12-12 04:48:07 +0000

    PDFBOX-4071: fix typo found by codespell on fossies.org
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1871221 13f79535-47bb-0310-9956-ffa450edef68

--- preflight/src/main/java/org/apache/pdfbox/preflight/process/MetadataValidationProcess.java
+++ preflight/src/main/java/org/apache/pdfbox/preflight/process/MetadataValidationProcess.java
@@ -234,7 +234,7 @@ public class MetadataValidationProcess extends AbstractProcess
         if (bim.getWidth() != tb.getWidth())
         {
             addValidationError(ctx, new ValidationError(PreflightConstants.ERROR_METADATA_FORMAT,
-                    "xapGImg:witdh does not match the actual base64-encoded thumbnail image data"));
+                    "xapGImg:width does not match the actual base64-encoded thumbnail image data"));
         }
     }
     
@@ -280,7 +280,7 @@ public class MetadataValidationProcess extends AbstractProcess
             // should not be defined
             ValidationError error = new ValidationError(
                     PreflightConstants.ERROR_SYNTAX_STREAM_INVALID_FILTER,
-                    "Filter specified in metadata dictionnary");
+                    "Filter specified in metadata dictionary");
             throw new XpacketParsingException("Failed while retrieving xpacket", error);
         }
 
