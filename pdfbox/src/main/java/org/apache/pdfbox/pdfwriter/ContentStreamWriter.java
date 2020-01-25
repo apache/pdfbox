81ba75ffb9e8dfaa8a4019f0feaa8d50e0aee059
Tilman Hausherr <tilman@apache.org>
2020-01-25 09:11:51 +0000

    PDFBOX-4750: allow to rewrite null object, as suggested by Tomas Kochan
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1873133 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdfwriter/ContentStreamWriter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfwriter/ContentStreamWriter.java
@@ -30,6 +30,7 @@ import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSFloat;
 import org.apache.pdfbox.cos.COSInteger;
 import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.cos.COSNull;
 import org.apache.pdfbox.cos.COSString;
 
 /**
@@ -194,6 +195,12 @@ public class ContentStreamWriter
                 output.write( op.getName().getBytes(StandardCharsets.ISO_8859_1) );
                 output.write( EOL );
             }
+        }
+        else if (o instanceof COSNull)
+        {
+            output.write(SPACE);
+            output.write("null".getBytes(StandardCharsets.US_ASCII));
+            output.write(SPACE);
         }
         else
         {
