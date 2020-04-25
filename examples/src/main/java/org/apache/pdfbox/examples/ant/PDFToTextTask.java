c243e18d2b86abb3051d6e951a2849a771332be4
Tilman Hausherr <tilman@apache.org>
2020-04-25 07:41:43 +0000

    PDFBOX-4071: use functional syntax
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1876961 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/ant/PDFToTextTask.java
+++ examples/src/main/java/org/apache/pdfbox/examples/ant/PDFToTextTask.java
@@ -17,19 +17,18 @@
 package org.apache.pdfbox.examples.ant;
 
 import java.io.File;
+import java.io.IOException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.pdfbox.tools.ExtractText;
-import org.apache.tools.ant.DirectoryScanner;
 import org.apache.tools.ant.Task;
 
 import org.apache.tools.ant.types.FileSet;
 
 /**
- * This is an Ant task that will allow pdf documents to be converted using an
- * Ant task.
+ * This is an Ant task that will allow pdf documents to be converted using an Ant task.
  *
  * @author Ben Litchfield
  */
@@ -55,31 +54,29 @@ public class PDFToTextTask extends Task
     {
         log( "PDFToTextTask executing" );
 
-        for (FileSet fileSet : fileSets)
-        {
-            DirectoryScanner dirScanner = fileSet.getDirectoryScanner(getProject());
-            dirScanner.scan();
-            String[] files = dirScanner.getIncludedFiles();
-            for (String file : files)
+        fileSets.stream().
+            map(fileSet -> fileSet.getDirectoryScanner(getProject())).
+            forEachOrdered(dirScanner ->
             {
-                File f = new File(dirScanner.getBasedir(), file);
-                log( "processing: " + f.getAbsolutePath() );
-                String pdfFile = f.getAbsolutePath();
-                if( pdfFile.toUpperCase().endsWith( ".PDF" ) )
+                dirScanner.scan();
+                for (String file : dirScanner.getIncludedFiles())
                 {
-                    String textFile = pdfFile.substring( 0, pdfFile.length() -3 );
-                    textFile = textFile + "txt";
-                    try
+                    File f = new File(dirScanner.getBasedir(), file);
+                    log("processing: " + f.getAbsolutePath());
+                    String pdfFile = f.getAbsolutePath();
+                    if (pdfFile.toUpperCase().endsWith(".PDF"))
                     {
-                        ExtractText.main(new String[]{pdfFile, textFile});
-                    }
-                    catch( Exception e )
-                    {
-                        log( "Error processing " + pdfFile + e.getMessage() );
+                        String textFile = pdfFile.substring(0, pdfFile.length() - 3) + "txt";
+                        try
+                        {
+                            ExtractText.main(new String[] {pdfFile, textFile});
+                        }
+                        catch (IOException e)
+                        {
+                            log("Error processing " + pdfFile + e.getMessage());
+                        }
                     }
                 }
-            }
-
-        }
+            });
     }
 }
