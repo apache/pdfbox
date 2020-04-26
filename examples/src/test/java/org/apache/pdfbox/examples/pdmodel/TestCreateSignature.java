e424a2e054140e2a5c704143027f104add320180
Tilman Hausherr <tilman@apache.org>
2020-04-26 20:14:09 +0000

    PDFBOX-3017: rename variable to avoid confusion
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1877040 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/test/java/org/apache/pdfbox/examples/pdmodel/TestCreateSignature.java
+++ examples/src/test/java/org/apache/pdfbox/examples/pdmodel/TestCreateSignature.java
@@ -175,7 +175,7 @@ public class TestCreateSignature
         // mock TSA server (RFC 3161)
         MockHttpServer mockServer = new MockHttpServer(15371);
         mockServer.startServer();
-        String tsaUrl = "http://localhost:" + mockServer.getServerPort() + "/";
+        String brokenMockTSA = "http://localhost:" + mockServer.getServerPort() + "/";
         MockHttpServer.MockHttpServerResponse response = new MockHttpServer.MockHttpServerResponse();
         response.setMockResponseContent(content);
         response.setMockResponseContentType("application/timestamp-reply");
@@ -194,7 +194,7 @@ public class TestCreateSignature
         {
             CreateSignature signing = new CreateSignature(keystore, PASSWORD.toCharArray());
             signing.setExternalSigning(externallySign);
-            signing.signDetached(new File(inPath), new File(outPath), tsaUrl);
+            signing.signDetached(new File(inPath), new File(outPath), brokenMockTSA);
             Assert.fail("This should have failed");
         }
         catch (IOException e)
