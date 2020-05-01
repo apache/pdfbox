c6e39e27e8659027b11edd5c8e1f99ab54d2dd45
Tilman Hausherr <tilman@apache.org>
2020-05-01 11:24:51 +0000

    PDFBOX-4071: fix deprecated annotation, remove double deprecated, remove super()
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/issue45@1877256 13f79535-47bb-0310-9956-ffa450edef68

--- tools/src/main/java/org/apache/pdfbox/tools/PDFText2HTML.java
+++ tools/src/main/java/org/apache/pdfbox/tools/PDFText2HTML.java
@@ -47,7 +47,6 @@ public class PDFText2HTML extends PDFTextStripper
      */
     public PDFText2HTML() throws IOException
     {
-        super();
         setLineSeparator(LINE_SEPARATOR);
         setParagraphStart("<p>");
         setParagraphEnd("</p>"+ LINE_SEPARATOR);
@@ -63,8 +62,9 @@ public class PDFText2HTML extends PDFTextStripper
      *
      * @throws IOException
      *             If there is a problem writing out the header to the document.
-     * @deprecated deprecated, use {@link #startDocument(PDDocument)}
+     * @deprecated use {@link #startDocument(PDDocument)}
      */
+    @Deprecated
     protected void writeHeader() throws IOException
     {
     }
