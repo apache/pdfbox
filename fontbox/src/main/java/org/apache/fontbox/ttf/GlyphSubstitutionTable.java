6dc5f21eba8ad795326a4c18abd859f829de8743
Tilman Hausherr <tilman@apache.org>
2020-05-11 06:16:54 +0000

    PDFBOX-4828: avoid ArrayIndexOutOfBoundsException
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1877561 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/ttf/GlyphSubstitutionTable.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/GlyphSubstitutionTable.java
@@ -409,14 +409,15 @@ public class GlyphSubstitutionTable extends TTFTable
         for (LangSysTable langSysTable : langSysTables)
         {
             int required = langSysTable.requiredFeatureIndex;
-            if (required != 0xffff) // if no required features = 0xFFFF
+            if (required != 0xffff && required < featureList.length) // if no required features = 0xFFFF
             {
                 result.add(featureList[required]);
             }
             for (int featureIndex : langSysTable.featureIndices)
             {
-                if (enabledFeatures == null
-                        || enabledFeatures.contains(featureList[featureIndex].featureTag))
+                if (enabledFeatures == null || 
+                        featureIndex < featureList.length && 
+                        enabledFeatures.contains(featureList[featureIndex].featureTag))
                 {
                     result.add(featureList[featureIndex]);
                 }
