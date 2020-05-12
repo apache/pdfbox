84324e873705d093995d9ef017c2f57dc21415ce
Tilman Hausherr <tilman@apache.org>
2020-05-12 17:15:15 +0000

    PDFBOX-4824: remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1877667 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/FileSystemFontProvider.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/FileSystemFontProvider.java
@@ -28,8 +28,6 @@ import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.security.AccessControlException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
