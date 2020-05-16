6d821e8541e4bd5b6266b0818e11189bbaa6604f
Andreas Lehmkühler <lehmi@apache.org>
2020-05-16 12:00:01 +0000

    PDFBOX-4071: ensure to skip 2 bytes
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1877809 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/filter/FlateFilter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/filter/FlateFilter.java
@@ -66,7 +66,8 @@ final class FlateFilter extends Filter
     { 
         byte[] buf = new byte[2048];
         // skip zlib header
-        in.read(buf,0,2);
+        in.read();
+        in.read();
         int read = in.read(buf); 
         if (read > 0) 
         { 
