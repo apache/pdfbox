4721ab07c9756ce48463eab5b7feb325fa44c659
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2020-05-29 08:29:54 +0200

    PDFBOX-4784 : fixed javadoc

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/SecurityHandler.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/SecurityHandler.java
@@ -140,7 +140,7 @@ public abstract class SecurityHandler
     /**
      * Set the custom SecureRandom.
      * 
-     * @param secureRandom the custom SecureRandom for AES encryption
+     * @param customSecureRandom the custom SecureRandom for AES encryption
      */
     public void setCustomSecureRandom(SecureRandom customSecureRandom)
     {
