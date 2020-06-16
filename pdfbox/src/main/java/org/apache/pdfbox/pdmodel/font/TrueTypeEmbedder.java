e08c12693cbee58a7d32a4a4bbfe3d7249c25974
Tilman Hausherr <tilman@apache.org>
2020-06-16 18:23:44 +0000

    PDFBOX-4887: check whether mark() and reset() are supported
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/issue45@1878899 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/TrueTypeEmbedder.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/TrueTypeEmbedder.java
@@ -94,13 +94,21 @@ abstract class TrueTypeEmbedder implements Subsetter
             // TrueType collections are not supported
             InputStream is = ttf.getOriginalData();
             byte b[] = new byte[4];
-            is.mark(b.length); // "is" is FileInputStream or ByteArrayInputStream
+            is.mark(b.length);
             if (is.read(b) == b.length && new String(b).equals("ttcf"))
             {
                 is.close();
                 throw new IOException("Full embedding of TrueType font collections not supported");
             }
-            is.reset();
+            if (is.markSupported())
+            {
+                is.reset();
+            }
+            else
+            {
+                is.close();
+                is = ttf.getOriginalData();
+            }
             PDStream stream = new PDStream(document, is, COSName.FLATE_DECODE);
             stream.getCOSObject().setLong(COSName.LENGTH1, ttf.getOriginalDataSize());
             fontDescriptor.setFontFile2(stream);
