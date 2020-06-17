f3c49ba23d2dc37e2bdba88d7962b672337fc5c2
Andreas Lehmkühler <lehmi@apache.org>
2020-06-17 06:13:56 +0000

    PDFBOX-3888: close stream after reading the object numbers
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1878912 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdfparser/PDFObjectStreamParser.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfparser/PDFObjectStreamParser.java
@@ -63,8 +63,8 @@ public class PDFObjectStreamParser extends BaseParser
     }
 
     /**
-     * Search for/parse the object with the given object number. This will close the stream when it is finished parsing.
-     * The stream is closed after parsing the object with the given number.
+     * Search for/parse the object with the given object number. The stream is closed after parsing the object with the
+     * given number.
      * 
      * @param objectNumber the number of the object to b e parsed
      * @return the parsed object or null if the object with the given number can't be found
@@ -75,7 +75,7 @@ public class PDFObjectStreamParser extends BaseParser
         COSBase streamObject = null;
         try
         {
-            Integer objectOffset = readObjectNumbers().get(objectNumber);
+            Integer objectOffset = privateReadObjectNumbers().get(objectNumber);
             if (objectOffset != null) 
             {
                 // jump to the offset of the first object
@@ -92,18 +92,12 @@ public class PDFObjectStreamParser extends BaseParser
         finally
         {
             source.close();
+            document = null;
         }
         return streamObject;
     }
 
-    /**
-     * Read all object numbers from the compressed object stream. The stream is not closed after reading the object
-     * numbers.
-     * 
-     * @return a map off all object numbers and the corresponding offset within the object stream.
-     * @throws IOException if there is an error while parsing the stream
-     */
-    public Map<Long, Integer> readObjectNumbers() throws IOException
+    private Map<Long, Integer> privateReadObjectNumbers() throws IOException
     {
         Map<Long, Integer> objectNumbers = new HashMap<>(numberOfObjects);
         for (int i = 0; i < numberOfObjects; i++)
@@ -111,6 +105,27 @@ public class PDFObjectStreamParser extends BaseParser
             long objectNumber = readObjectNumber();
             int offset = (int) readLong();
             objectNumbers.put(objectNumber, offset);
+        }
+        return objectNumbers;
+    }
+
+    /**
+     * Read all object numbers from the compressed object stream. The stream is closed after reading the object numbers.
+     * 
+     * @return a map off all object numbers and the corresponding offset within the object stream.
+     * @throws IOException if there is an error while parsing the stream
+     */
+    public Map<Long, Integer> readObjectNumbers() throws IOException
+    {
+        Map<Long, Integer> objectNumbers = null;
+        try
+        {
+            objectNumbers = privateReadObjectNumbers();
+        }
+        finally
+        {
+            source.close();
+            document = null;
         }
         return objectNumbers;
     }
