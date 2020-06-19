1979286ce82fe5b5fc53ec2336a50967ddd6dc4b
Tilman Hausherr <tilman@apache.org>
2020-06-19 06:40:45 +0000

    PDFBOX-4890: remove debug logging
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1878980 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/ttf/BufferedRandomAccessFile.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/BufferedRandomAccessFile.java
@@ -19,8 +19,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 /**
  * This class is a version of the one published at
@@ -35,8 +33,6 @@ import org.apache.commons.logging.LogFactory;
  */
 public class BufferedRandomAccessFile extends RandomAccessFile
 {
-    private static final Log LOG = LogFactory.getLog(BufferedRandomAccessFile.class);
-
     /**
      * Uses a byte instead of a char buffer for efficiency reasons.
      */
@@ -70,7 +66,6 @@ public class BufferedRandomAccessFile extends RandomAccessFile
             throws FileNotFoundException
     {
         super(filename, mode);
-LOG.debug("filename: " + filename);
         BUFSIZE = bufsize;
         buffer = new byte[BUFSIZE];
     }
@@ -91,7 +86,6 @@ LOG.debug("filename: " + filename);
             throws FileNotFoundException
     {
         super(file, mode);
-LOG.debug("file: " + file);
         BUFSIZE = bufsize;
         buffer = new byte[BUFSIZE];
     }
@@ -154,7 +148,6 @@ LOG.debug("file: " + file);
     @Override
     public int read(byte[] b, int off, int len) throws IOException
     {
-LOG.debug("b size: " + b.length + ", off: " + off + ", len: " + len);
         int leftover = bufend - bufpos;
         if (len <= leftover)
         {
@@ -166,9 +159,7 @@ LOG.debug("b size: " + b.length + ", off: " + off + ", len: " + len);
         bufpos += leftover;
         if (fillBuffer() > 0)
         {
-LOG.debug("leftover: " + leftover);
             int bytesRead = read(b, off + leftover, len - leftover);
-LOG.debug("bytesRead: " + bytesRead);
             if (bytesRead > 0)
             {
                 leftover += bytesRead;
