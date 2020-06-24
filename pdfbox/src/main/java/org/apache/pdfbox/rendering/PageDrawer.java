40b6be57f731bbe42b5267a0dc0054909527e446
Tilman Hausherr <tilman@apache.org>
2020-06-24 16:29:48 +0000

    PDFBOX-4898: use rendering hints of the "parent" graphics device to improve scaling
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1879163 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
@@ -1011,6 +1011,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
                 Graphics2D g = (Graphics2D) renderedPaint.getGraphics();
                 g.translate(-bounds.getMinX(), -bounds.getMinY());
                 g.setPaint(paint);
+                g.setRenderingHints(graphics.getRenderingHints());
                 g.fill(bounds);
                 g.dispose();
 
@@ -1022,6 +1023,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
                 AffineTransform imageTransform = new AffineTransform(at);
                 imageTransform.scale(1.0 / mask.getWidth(), -1.0 / mask.getHeight());
                 imageTransform.translate(0, -mask.getHeight());
+                g.setRenderingHints(graphics.getRenderingHints());
                 g.drawImage(mask, imageTransform, null);
                 g.dispose();
 
