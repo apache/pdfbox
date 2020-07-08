344b387802606b6d973b1d764653a9eddb6e961f
Tilman Hausherr <tilman@apache.org>
2020-07-08 04:23:25 +0000

    PDFBOX-4911: catch ArrayIndexOutOfBoundsException
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/1.8@1879620 13f79535-47bb-0310-9956-ffa450edef68

--- preflight/src/main/java/org/apache/pdfbox/preflight/process/CatalogValidationProcess.java
+++ preflight/src/main/java/org/apache/pdfbox/preflight/process/CatalogValidationProcess.java
@@ -458,9 +458,15 @@ public class CatalogValidationProcess extends AbstractProcess
             addValidationError(ctx, new ValidationError(ERROR_GRAPHIC_OUTPUT_INTENT_ICC_PROFILE_INVALID,
                     "DestOutputProfile isn't a valid ICCProfile. Caused by : " + e.getMessage()));
         }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            // this is not a ICC_Profile
+            addValidationError(ctx, new ValidationError(ERROR_GRAPHIC_OUTPUT_INTENT_ICC_PROFILE_INVALID,
+                    "DestOutputProfile isn't a valid ICCProfile. Caused by : " + e.getMessage()));
+        }
         catch (IOException e)
         {
             throw new ValidationException("Unable to parse the ICC Profile.", e);
         }
-            }
+    }
 }
