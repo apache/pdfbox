c28d10d9c208787be02fe5a683b67853078a629f
Andreas Lehmkühler <lehmi@apache.org>
2020-07-16 05:55:50 +0000

    PDFBOX-4915: skip 0 offsets within the xref table
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1879922 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdfparser/COSParser.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfparser/COSParser.java
@@ -2389,9 +2389,13 @@ public class COSParser extends BaseParser implements ICOSParser
                     try
                     {
                         long currOffset = Long.parseLong(splitString[0]);
-                        int currGenID = Integer.parseInt(splitString[1]);
-                        COSObjectKey objKey = new COSObjectKey(currObjID, currGenID);
-                        xrefTrailerResolver.setXRef(objKey, currOffset);
+                        // skip 0 offsets
+                        if (currOffset > 0)
+                        {
+                            int currGenID = Integer.parseInt(splitString[1]);
+                            COSObjectKey objKey = new COSObjectKey(currObjID, currGenID);
+                            xrefTrailerResolver.setXRef(objKey, currOffset);
+                        }
                     }
                     catch(NumberFormatException e)
                     {
