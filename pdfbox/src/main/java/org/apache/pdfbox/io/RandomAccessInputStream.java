ab0d4e428dbb21c018b1b3a08436d137dcffb817
Andreas Lehmkühler <lehmi@apache.org>
2020-08-13 06:24:28 +0000

    PDFBOX-4836: optimize available
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1880821 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/io/RandomAccessInputStream.java
+++ pdfbox/src/main/java/org/apache/pdfbox/io/RandomAccessInputStream.java
@@ -55,13 +55,7 @@ public class RandomAccessInputStream extends InputStream
     @Override
     public int available() throws IOException
     {
-        restorePosition();
-        long available = input.length() - input.getPosition();
-        if (available > Integer.MAX_VALUE)
-        {
-            return Integer.MAX_VALUE;
-        }
-        return (int)available;
+        return (int) Math.min(input.length() - position, Integer.MAX_VALUE);
     }
 
     @Override
