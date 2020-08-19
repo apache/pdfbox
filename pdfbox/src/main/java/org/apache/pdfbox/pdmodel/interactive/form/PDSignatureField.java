1b1a2988954584b7c70bf4ab4ef15f1a718bfa81
Tilman Hausherr <tilman@apache.org>
2020-08-19 17:55:54 +0000

    PDFBOX-3017: improve log message, inspired by SO question 63457413
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1881003 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDSignatureField.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDSignatureField.java
@@ -215,8 +215,11 @@ public class PDSignatureField extends PDTerminalField
                 return;
             }
 
-            // TODO: implement appearance generation for signatures
-            LOG.warn("Appearance generation for signature fields not yet implemented - you need to generate/update that manually");
+            // TODO: implement appearance generation for signatures (PDFBOX-3524)
+            LOG.warn("Appearance generation for signature fields not implemented here. "
+                    + "You need to generate/update that manually, see the "
+                    + "CreateVisibleSignature*.java files in the examples subproject "
+                    + "of the source code download");
         }
     }
 }
