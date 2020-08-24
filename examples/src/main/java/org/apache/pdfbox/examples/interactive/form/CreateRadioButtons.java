7b3fdf10f8aa0d3f4b34c31f13cce2032996397b
Tilman Hausherr <tilman@apache.org>
2020-08-24 17:24:09 +0000

    PDFBOX-4939: add tests for CreateCheckBox and CreateRadioButtons examples
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/issue45@1881153 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/interactive/form/CreateRadioButtons.java
+++ examples/src/main/java/org/apache/pdfbox/examples/interactive/form/CreateRadioButtons.java
@@ -115,7 +115,7 @@ public class CreateRadioButtons
 
         radioButton.setValue("c");
 
-        document.save("RadioButtonsSample.pdf");
+        document.save("target/RadioButtonsSample.pdf");
         document.close();
     }
 
