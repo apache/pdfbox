06aa5380989d5b7f54c11675904ca6c25d720811
Tilman Hausherr <tilman@apache.org>
2020-10-10 09:08:31 +0000

    PDFBOX-4297: introduce fast method to get the signature contents, as suggested by Ralf Hauser
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1882382 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/digitalsignature/PDSignature.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/digitalsignature/PDSignature.java
@@ -23,6 +23,7 @@ import java.io.InputStream;
 import java.util.Calendar;
 
 import org.apache.pdfbox.cos.COSArray;
+import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSInteger;
 import org.apache.pdfbox.cos.COSName;
@@ -302,6 +303,22 @@ public class PDSignature implements COSObjectable
             ary[i] = byteRange.getInt(i);
         }
         return ary;
+    }
+
+    /**
+     * Returns the /Contents string as a byte array, i.e. the embedded signature between the
+     * byterange gap.
+     *
+     * @return a byte array containing the signature, or an empty array if there isn't any.
+     */
+    public byte[] getContents()
+    {
+        COSBase base = dictionary.getDictionaryObject(COSName.CONTENTS);
+        if (base instanceof COSString)
+        {
+            return ((COSString) base).getBytes();
+        }
+        return new byte[0];
     }
 
     /**
