d5678f1ae64e99fc85ca72211fe9f4a3bfb667d9
Tilman Hausherr <tilman@apache.org>
2020-11-08 13:21:45 +0000

    PDFBOX-5009, PDFBOX-3953: improve javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1883201 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDPageTree.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDPageTree.java
@@ -233,6 +233,10 @@ public class PDPageTree implements COSObjectable, Iterable<PDPage>
      * Returns the page at the given index.
      *
      * @param index zero-based index
+     * 
+     * @throws IllegalStateException if the requested index isn't found or doesn't point to a valid
+     * page dictionary
+     * @throws IndexOutOfBoundsException if the requested index is higher than the page count
      */
     public PDPage get(int index)
     {
@@ -265,6 +269,8 @@ public class PDPageTree implements COSObjectable, Iterable<PDPage>
      * @param node page tree node to search
      * @param encountered number of pages encountered so far
      * @return COS dictionary of the Page object
+     * @throws IllegalStateException if the requested page number isn't found
+     * @throws IndexOutOfBoundsException if the requested page number is higher than the page count
      */
     private COSDictionary get(int pageNum, COSDictionary node, int encountered)
     {
