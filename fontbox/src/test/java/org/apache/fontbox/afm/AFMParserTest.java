89859f9455d93ea4e3808c74dfa78e47c04985d0
Andreas Lehmkühler <lehmi@apache.org>
2020-11-14 17:06:57 +0000

    PDFBOX-5001: DRY-refactoring, split tests
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1883425 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/test/java/org/apache/fontbox/afm/AFMParserTest.java
+++ fontbox/src/test/java/org/apache/fontbox/afm/AFMParserTest.java
@@ -108,139 +108,121 @@ public class AFMParserTest
     }
 
     @Test
-    public void testAFMParser() throws IOException
+    public void testHelveticaFontMetrics() throws IOException
+    {
+        AFMParser parser = new AFMParser(
+                new FileInputStream("src/test/resources/afm/Helvetica.afm"));
+        checkHelveticaFontMetrics(parser.parse());
+    }
+
+    @Test
+    public void testHelveticaCharMetrics() throws IOException
     {
         AFMParser parser = new AFMParser(
                 new FileInputStream("src/test/resources/afm/Helvetica.afm"));
         FontMetrics fontMetrics = parser.parse();
 
-        assertEquals(4.1f, fontMetrics.getAFMVersion(), 0f);
-        assertEquals("Helvetica", fontMetrics.getFontName());
-        assertEquals("Helvetica", fontMetrics.getFullName());
-        assertEquals("Helvetica", fontMetrics.getFamilyName());
-        assertEquals("Medium", fontMetrics.getWeight());
-        BoundingBox fontBBox = fontMetrics.getFontBBox();
-        assertNotNull(fontBBox);
-        assertEquals(-166f, fontBBox.getLowerLeftX(),0f);
-        assertEquals(-225f, fontBBox.getLowerLeftY(), 0f);
-        assertEquals(1000f, fontBBox.getUpperRightX(), 0f);
-        assertEquals(931f, fontBBox.getUpperRightY(), 0f);
-        assertEquals("002.000", fontMetrics.getFontVersion());
-        assertEquals(
-                "Copyright (c) 1985, 1987, 1989, 1990, 1997 Adobe Systems Incorporated.  All Rights Reserved.Helvetica is a trademark of Linotype-Hell AG and/or its subsidiaries.",
-                fontMetrics.getNotice());
-        assertEquals("AdobeStandardEncoding", fontMetrics.getEncodingScheme());
-        assertEquals(0, fontMetrics.getMappingScheme());
-        assertEquals(0, fontMetrics.getEscChar());
-        assertEquals("ExtendedRoman", fontMetrics.getCharacterSet());
-        assertEquals(0, fontMetrics.getCharacters());
-        assertTrue(fontMetrics.getIsBaseFont());
-        assertNull(fontMetrics.getVVector());
-        assertFalse(fontMetrics.getIsFixedV());
-        assertEquals(718f, fontMetrics.getCapHeight(), 0f);
-        assertEquals(523f, fontMetrics.getXHeight(), 0f);
-        assertEquals(718f, fontMetrics.getAscender(), 0f);
-        assertEquals(-207f, fontMetrics.getDescender(), 0f);
-        assertEquals(76f, fontMetrics.getStandardHorizontalWidth(), 0f);
-        assertEquals(88f, fontMetrics.getStandardVerticalWidth(), 0f);
-        List<String> comments = fontMetrics.getComments();
-        assertEquals(4, comments.size());
-        assertEquals(
-                "Copyright (c) 1985, 1987, 1989, 1990, 1997 Adobe Systems Incorporated.  All Rights Reserved.",
-                comments.get(0));
-        assertEquals("UniqueID 43054", comments.get(2));
-        assertEquals(-100f, fontMetrics.getUnderlinePosition(), 0f);
-        assertEquals(50f, fontMetrics.getUnderlineThickness(), 0f);
-        assertEquals(0f, fontMetrics.getItalicAngle(), 0f);
-        assertNull(fontMetrics.getCharWidth());
-        assertFalse(fontMetrics.getIsFixedPitch());
         // char metrics
-        List<CharMetric> charMetrics = fontMetrics.getCharMetrics();
-        assertEquals(315, charMetrics.size());
-        // check "space" metrics
-        Optional<CharMetric> space = charMetrics.stream()//
-                .filter(c -> "space".equals(c.getName())).findFirst();
-        assertTrue(space.isPresent());
-        CharMetric spaceCharMetric = space.get();
-        assertEquals(278f, spaceCharMetric.getWx(), 0f);
-        assertEquals(32, spaceCharMetric.getCharacterCode());
-        BoundingBox spaceBBox = spaceCharMetric.getBoundingBox();
-        assertNotNull(spaceBBox);
-        assertEquals(0, spaceBBox.getLowerLeftX(), 0f);
-        assertEquals(0, spaceBBox.getLowerLeftY(), 0f);
-        assertEquals(0, spaceBBox.getUpperRightX(), 0f);
-        assertEquals(0, spaceBBox.getUpperRightY(), 0f);
-        assertTrue(spaceCharMetric.getLigatures().isEmpty());
-        assertNull(spaceCharMetric.getW());
-        assertNull(spaceCharMetric.getW0());
-        assertNull(spaceCharMetric.getW1());
-        assertNull(spaceCharMetric.getVv());
-        // check "ring" metrics
-        Optional<CharMetric> ring = charMetrics.stream()//
-                .filter(c -> "ring".equals(c.getName())).findFirst();
-        assertTrue(ring.isPresent());
-        CharMetric ringCharMetric = ring.get();
-        assertEquals(333f, ringCharMetric.getWx(), 0f);
-        assertEquals(202, ringCharMetric.getCharacterCode());
-        BoundingBox ringBBox = ringCharMetric.getBoundingBox();
-        assertNotNull(ringBBox);
-        assertEquals(75, ringBBox.getLowerLeftX(), 0f);
-        assertEquals(572, ringBBox.getLowerLeftY(), 0f);
-        assertEquals(259, ringBBox.getUpperRightX(), 0f);
-        assertEquals(756, ringBBox.getUpperRightY(), 0f);
-        assertTrue(ringCharMetric.getLigatures().isEmpty());
-        assertNull(ringCharMetric.getW());
-        assertNull(ringCharMetric.getW0());
-        assertNull(ringCharMetric.getW1());
-        assertNull(ringCharMetric.getVv());
+        checkHelveticaCharMetrics(fontMetrics.getCharMetrics());
+    }
+
+    @Test
+    public void testHelveticaKernPairs() throws IOException
+    {
+        AFMParser parser = new AFMParser(
+                new FileInputStream("src/test/resources/afm/Helvetica.afm"));
+        FontMetrics fontMetrics = parser.parse();
+
         // KernPairs
         List<KernPair> kernPairs = fontMetrics.getKernPairs();
         assertEquals(2705, kernPairs.size());
         // check "KPX A Ucircumflex -50"
-        Optional<KernPair> A_Ucircumflex = kernPairs.stream() //
-                .filter(k -> "A".equals(k.getFirstKernCharacter())) //
-                .filter(k -> "Ucircumflex".equals(k.getSecondKernCharacter())) //
-                .findFirst();
-        assertTrue(A_Ucircumflex.isPresent());
-        assertEquals(-50f, A_Ucircumflex.get().getX(), 0f);
-        assertEquals(0f, A_Ucircumflex.get().getY(), 0f);
+        checkKernPair(kernPairs, "A", "Ucircumflex", -50, 0);
         // check "KPX W agrave -40"
-        Optional<KernPair> W_agrave = kernPairs.stream() //
-                .filter(k -> "W".equals(k.getFirstKernCharacter())) //
-                .filter(k -> "agrave".equals(k.getSecondKernCharacter())) //
-                .findFirst();
-        assertTrue(W_agrave.isPresent());
-        assertEquals(-40f, W_agrave.get().getX(), 0f);
-        assertEquals(0f, W_agrave.get().getY(), 0f);
+        checkKernPair(kernPairs, "W", "agrave", -40, 0);
         // KernPairs0
-        List<KernPair> kernPairs0 = fontMetrics.getKernPairs0();
-        assertTrue(kernPairs0.isEmpty());
+        assertTrue(fontMetrics.getKernPairs0().isEmpty());
         // KernPairs1
-        List<KernPair> kernPairs1 = fontMetrics.getKernPairs1();
-        assertTrue(kernPairs1.isEmpty());
+        assertTrue(fontMetrics.getKernPairs1().isEmpty());
         // composite data
-        List<Composite> composites = fontMetrics.getComposites();
-        assertTrue(composites.isEmpty());
+        assertTrue(fontMetrics.getComposites().isEmpty());
     }
 
     @Test
-    public void testAFMParserReducedDataset() throws IOException
+    public void testHelveticaFontMetricsReducedDataset() throws IOException
+    {
+        AFMParser parser = new AFMParser(
+                new FileInputStream("src/test/resources/afm/Helvetica.afm"));
+        checkHelveticaFontMetrics(parser.parse(true));
+    }
+
+    @Test
+    public void testHelveticaCharMetricsReducedDataset() throws IOException
     {
         AFMParser parser = new AFMParser(
                 new FileInputStream("src/test/resources/afm/Helvetica.afm"));
         FontMetrics fontMetrics = parser.parse(true);
 
+        // char metrics
+        checkHelveticaCharMetrics(fontMetrics.getCharMetrics());
+    }
+
+    @Test
+    public void testHelveticaKernPairsReducedDataset() throws IOException
+    {
+        AFMParser parser = new AFMParser(
+                new FileInputStream("src/test/resources/afm/Helvetica.afm"));
+        FontMetrics fontMetrics = parser.parse(true);
+
+        // KernPairs, empty due to reducedDataset == true
+        assertTrue(fontMetrics.getKernPairs().isEmpty());
+        // KernPairs0
+        assertTrue(fontMetrics.getKernPairs0().isEmpty());
+        // KernPairs1
+        assertTrue(fontMetrics.getKernPairs1().isEmpty());
+        // composite data
+        assertTrue(fontMetrics.getComposites().isEmpty());
+    }
+
+    private void checkHelveticaCharMetrics(List<CharMetric> charMetrics)
+    {
+        assertEquals(315, charMetrics.size());
+        // check "space" metrics
+        Optional<CharMetric> space = charMetrics.stream()//
+                .filter(c -> "space".equals(c.getName())).findFirst();
+        assertTrue(space.isPresent());
+        CharMetric spaceCharMetric = space.get();
+        assertEquals(278f, spaceCharMetric.getWx(), 0f);
+        assertEquals(32, spaceCharMetric.getCharacterCode());
+        checkBBox(spaceCharMetric.getBoundingBox(), 0, 0, 0, 0);
+        assertTrue(spaceCharMetric.getLigatures().isEmpty());
+        assertNull(spaceCharMetric.getW());
+        assertNull(spaceCharMetric.getW0());
+        assertNull(spaceCharMetric.getW1());
+        assertNull(spaceCharMetric.getVv());
+        // check "ring" metrics
+        Optional<CharMetric> ring = charMetrics.stream()//
+                .filter(c -> "ring".equals(c.getName())).findFirst();
+        assertTrue(ring.isPresent());
+        CharMetric ringCharMetric = ring.get();
+        assertEquals(333f, ringCharMetric.getWx(), 0f);
+        assertEquals(202, ringCharMetric.getCharacterCode());
+        checkBBox(ringCharMetric.getBoundingBox(), 75, 572, 259, 756);
+        assertTrue(ringCharMetric.getLigatures().isEmpty());
+        assertNull(ringCharMetric.getW());
+        assertNull(ringCharMetric.getW0());
+        assertNull(ringCharMetric.getW1());
+        assertNull(ringCharMetric.getVv());
+    }
+
+    private void checkHelveticaFontMetrics(FontMetrics fontMetrics)
+    {
         assertEquals(4.1f, fontMetrics.getAFMVersion(), 0f);
         assertEquals("Helvetica", fontMetrics.getFontName());
         assertEquals("Helvetica", fontMetrics.getFullName());
         assertEquals("Helvetica", fontMetrics.getFamilyName());
         assertEquals("Medium", fontMetrics.getWeight());
-        BoundingBox fontBBox = fontMetrics.getFontBBox();
-        assertNotNull(fontBBox);
-        assertEquals(-166f, fontBBox.getLowerLeftX(), 0f);
-        assertEquals(-225f, fontBBox.getLowerLeftY(), 0f);
-        assertEquals(1000f, fontBBox.getUpperRightX(), 0f);
-        assertEquals(931f, fontBBox.getUpperRightY(), 0f);
+        checkBBox(fontMetrics.getFontBBox(), -166f, -225f, 1000f, 931f);
         assertEquals("002.000", fontMetrics.getFontVersion());
         assertEquals(
                 "Copyright (c) 1985, 1987, 1989, 1990, 1997 Adobe Systems Incorporated.  All Rights Reserved.Helvetica is a trademark of Linotype-Hell AG and/or its subsidiaries.",
@@ -270,56 +252,27 @@ public class AFMParserTest
         assertEquals(0f, fontMetrics.getItalicAngle(), 0f);
         assertNull(fontMetrics.getCharWidth());
         assertFalse(fontMetrics.getIsFixedPitch());
-        // char metrics
-        List<CharMetric> charMetrics = fontMetrics.getCharMetrics();
-        assertEquals(315, charMetrics.size());
-        // check "space" metrics
-        Optional<CharMetric> space = charMetrics.stream()//
-                .filter(c -> "space".equals(c.getName())).findFirst();
-        assertTrue(space.isPresent());
-        CharMetric spaceCharMetric = space.get();
-        assertEquals(278f, spaceCharMetric.getWx(), 0f);
-        assertEquals(32, spaceCharMetric.getCharacterCode());
-        BoundingBox spaceBBox = spaceCharMetric.getBoundingBox();
-        assertNotNull(spaceBBox);
-        assertEquals(0, spaceBBox.getLowerLeftX(), 0f);
-        assertEquals(0, spaceBBox.getLowerLeftY(), 0f);
-        assertEquals(0, spaceBBox.getUpperRightX(), 0f);
-        assertEquals(0, spaceBBox.getUpperRightY(), 0f);
-        assertTrue(spaceCharMetric.getLigatures().isEmpty());
-        assertNull(spaceCharMetric.getW());
-        assertNull(spaceCharMetric.getW0());
-        assertNull(spaceCharMetric.getW1());
-        assertNull(spaceCharMetric.getVv());
-        // check "ring" metrics
-        Optional<CharMetric> ring = charMetrics.stream()//
-                .filter(c -> "ring".equals(c.getName())).findFirst();
-        assertTrue(ring.isPresent());
-        CharMetric ringCharMetric = ring.get();
-        assertEquals(333f, ringCharMetric.getWx(), 0f);
-        assertEquals(202, ringCharMetric.getCharacterCode());
-        BoundingBox ringBBox = ringCharMetric.getBoundingBox();
-        assertNotNull(ringBBox);
-        assertEquals(75, ringBBox.getLowerLeftX(), 0f);
-        assertEquals(572, ringBBox.getLowerLeftY(), 0f);
-        assertEquals(259, ringBBox.getUpperRightX(), 0f);
-        assertEquals(756, ringBBox.getUpperRightY(), 0f);
-        assertTrue(ringCharMetric.getLigatures().isEmpty());
-        assertNull(ringCharMetric.getW());
-        assertNull(ringCharMetric.getW0());
-        assertNull(ringCharMetric.getW1());
-        assertNull(ringCharMetric.getVv());
-        // KernPairs, empty due to reducedDataset == true
-        List<KernPair> kernPairs = fontMetrics.getKernPairs();
-        assertTrue(kernPairs.isEmpty());
-        // KernPairs0
-        List<KernPair> kernPairs0 = fontMetrics.getKernPairs0();
-        assertTrue(kernPairs0.isEmpty());
-        // KernPairs1
-        List<KernPair> kernPairs1 = fontMetrics.getKernPairs1();
-        assertTrue(kernPairs1.isEmpty());
-        // composite data
-        List<Composite> composites = fontMetrics.getComposites();
-        assertTrue(composites.isEmpty());
+    }
+
+    private void checkBBox(BoundingBox bBox, float lowerX, float lowerY, float upperX, float upperY)
+    {
+        assertNotNull(bBox);
+        assertEquals(lowerX, bBox.getLowerLeftX(), 0f);
+        assertEquals(lowerY, bBox.getLowerLeftY(), 0f);
+        assertEquals(upperX, bBox.getUpperRightX(), 0f);
+        assertEquals(upperY, bBox.getUpperRightY(), 0f);
+    }
+
+    private void checkKernPair(List<KernPair> kernPairs, String firstKernChar,
+            String secondKernChar, float x, float y)
+    {
+        Optional<KernPair> kernPair = kernPairs.stream() //
+                .filter(k -> firstKernChar.equals(k.getFirstKernCharacter())) //
+                .filter(k -> secondKernChar.equals(k.getSecondKernCharacter())) //
+                .findFirst();
+        assertTrue(kernPair.isPresent());
+        assertEquals(x, kernPair.get().getX(), 0f);
+        assertEquals(y, kernPair.get().getY(), 0f);
+
     }
 }
