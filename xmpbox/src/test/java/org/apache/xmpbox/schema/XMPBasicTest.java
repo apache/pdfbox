3f2edf99bb73eca9c451a985246f17b7ce31c0e8
Maruan Sahyoun <msahyoun@apache.org>
2020-11-16 11:41:30 +0000

    PDFBOX-5017: use new test class, JUnit5 parameterized tests
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1883473 13f79535-47bb-0310-9956-ffa450edef68

--- xmpbox/src/test/java/org/apache/xmpbox/schema/XMPBasicTest.java
+++ xmpbox/src/test/java/org/apache/xmpbox/schema/XMPBasicTest.java
@@ -21,56 +21,48 @@
 
 package org.apache.xmpbox.schema;
 
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Collection;
-import java.util.List;
 
 import org.apache.xmpbox.XMPMetadata;
 import org.apache.xmpbox.type.Cardinality;
-import org.apache.xmpbox.type.PropertyType;
 import org.apache.xmpbox.type.Types;
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-@RunWith(Parameterized.class)
-public class XMPBasicTest extends AbstractXMPSchemaTest
+public class XMPBasicTest
 {
-
-    public XMPBasicTest(String prop, PropertyType type, Object val)
+    @ParameterizedTest
+    @MethodSource("initializeParameters")
+    public void testElementValue(XMPSchemaTester xmpSchemaTester) throws Exception
     {
-        super(prop, type, val);
+        xmpSchemaTester.testGetSetValue();
     }
 
-    @Before
-    public void initTempMetaData() throws Exception
+    @ParameterizedTest
+    @MethodSource("initializeParameters")
+    public void testElementProperty(XMPSchemaTester xmpSchemaTester) throws Exception
     {
-        metadata = XMPMetadata.createXMPMetadata();
-        schema = metadata.createAndAddXMPBasicSchema();
-        schemaClass = XMPBasicSchema.class;
+        xmpSchemaTester.testGetSetProperty();
     }
 
-    @Parameters
-    public static Collection<Object[]> initializeParameters() throws Exception
+    static XMPSchemaTester[] initializeParameters() throws Exception
     {
-        List<Object[]> data = new ArrayList<>();
+        XMPMetadata metadata = XMPMetadata.createXMPMetadata();
+        XMPSchema schema = metadata.createAndAddXMPBasicSchema();
+        Class<?> schemaClass = XMPBasicSchema.class;
 
-        data.add(wrapProperty("Advisory", Types.XPath, Cardinality.Bag, new String[] { "xpath1", "xpath2" }));
-        data.add(wrapProperty("BaseURL", Types.URL, "URL"));
-        data.add(wrapProperty("CreateDate", Types.Date, Calendar.getInstance()));
-        data.add(wrapProperty("CreatorTool", Types.AgentName, "CreatorTool"));
-        data.add(wrapProperty("Identifier", Types.Text, Cardinality.Bag, new String[] { "id1", "id2" }));
-        data.add(wrapProperty("Label", Types.Text, "label"));
-        data.add(wrapProperty("MetadataDate", Types.Date, Calendar.getInstance()));
-        data.add(wrapProperty("ModifyDate", Types.Date, Calendar.getInstance()));
-        data.add(wrapProperty("Nickname", Types.Text, "nick name"));
-        data.add(wrapProperty("Rating", Types.Integer, 7));
-
-        data.add(wrapProperty("Thumbnails", Types.Thumbnail, Cardinality.Alt, null));
-
-        return data;
+        return new XMPSchemaTester[] {
+            new XMPSchemaTester(metadata, schema, schemaClass, "Advisory", XMPSchemaTester.createPropertyType(Types.XPath, Cardinality.Bag), new String[] { "xpath1", "xpath2" }),
+            new XMPSchemaTester(metadata, schema, schemaClass, "BaseURL", XMPSchemaTester.createPropertyType(Types.URL), "URL"),
+            new XMPSchemaTester(metadata, schema, schemaClass, "CreateDate", XMPSchemaTester.createPropertyType(Types.Date), Calendar.getInstance()),
+            new XMPSchemaTester(metadata, schema, schemaClass, "CreatorTool", XMPSchemaTester.createPropertyType(Types.AgentName), "CreatorTool"),
+            new XMPSchemaTester(metadata, schema, schemaClass, "Identifier", XMPSchemaTester.createPropertyType(Types.Text, Cardinality.Bag), new String[] { "id1", "id2" }),
+            new XMPSchemaTester(metadata, schema, schemaClass, "Label", XMPSchemaTester.createPropertyType(Types.Text), "label"),
+            new XMPSchemaTester(metadata, schema, schemaClass, "MetadataDate", XMPSchemaTester.createPropertyType(Types.Date), Calendar.getInstance()),
+            new XMPSchemaTester(metadata, schema, schemaClass, "ModifyDate", XMPSchemaTester.createPropertyType(Types.Date), Calendar.getInstance()),
+            new XMPSchemaTester(metadata, schema, schemaClass, "Nickname", XMPSchemaTester.createPropertyType(Types.Text), "nick name"),
+            new XMPSchemaTester(metadata, schema, schemaClass, "Rating", XMPSchemaTester.createPropertyType(Types.Integer), 7),
+            new XMPSchemaTester(metadata, schema, schemaClass, "Thumbnails", XMPSchemaTester.createPropertyType(Types.Thumbnail, Cardinality.Alt), null)
+        };
     }
-
 }
--- xmpbox/src/test/java/org/apache/xmpbox/schema/XmpRightsSchemaTest.java
+++ xmpbox/src/test/java/org/apache/xmpbox/schema/XmpRightsSchemaTest.java
@@ -21,52 +21,47 @@
 
 package org.apache.xmpbox.schema;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.xmpbox.XMPMetadata;
 import org.apache.xmpbox.type.Cardinality;
-import org.apache.xmpbox.type.PropertyType;
 import org.apache.xmpbox.type.Types;
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-@RunWith(Parameterized.class)
-public class XmpRightsSchemaTest extends AbstractXMPSchemaTest
+public class XmpRightsSchemaTest
 {
-
-    public XmpRightsSchemaTest(String property, PropertyType type, Object value)
+    @ParameterizedTest
+    @MethodSource("initializeParameters")
+    public void testElementValue(XMPSchemaTester xmpSchemaTester) throws Exception
     {
-        super(property, type, value);
+        xmpSchemaTester.testGetSetValue();
     }
 
-    @Before
-    public void initTempMetaData() throws Exception
+    @ParameterizedTest
+    @MethodSource("initializeParameters")
+    public void testElementProperty(XMPSchemaTester xmpSchemaTester) throws Exception
     {
-        metadata = XMPMetadata.createXMPMetadata();
-        schema = metadata.createAndAddXMPRightsManagementSchema();
-        schemaClass = XMPRightsManagementSchema.class;
+        xmpSchemaTester.testGetSetProperty();
     }
 
-    @Parameters
-    public static Collection<Object[]> initializeParameters() throws Exception
+    static XMPSchemaTester[] initializeParameters() throws Exception
     {
-        List<Object[]> data = new ArrayList<>();
-        data.add(wrapProperty("Certificate", Types.URL, "http://une.url.vers.un.certificat/moncert.cer"));
-        data.add(wrapProperty("Marked", Types.Boolean, true));
-        data.add(wrapProperty("Owner", Types.ProperName, Cardinality.Bag, new String[] { "OwnerName" }));
+        XMPMetadata metadata = XMPMetadata.createXMPMetadata();
+        XMPSchema schema = metadata.createAndAddXMPRightsManagementSchema();
+        Class<?> schemaClass = XMPRightsManagementSchema.class;
 
         Map<String, String> desc = new HashMap<>(2);
         desc.put("fr", "Termes d'utilisation");
         desc.put("en", "Usage Terms");
-        data.add(wrapProperty("UsageTerms", Types.LangAlt, desc));
-        data.add(wrapProperty("WebStatement", Types.URL, "http://une.url.vers.une.page.fr/"));
-        return data;
-    }
 
+        return new XMPSchemaTester[] {
+            new XMPSchemaTester(metadata, schema, schemaClass, "Certificate", XMPSchemaTester.createPropertyType(Types.URL), "http://une.url.vers.un.certificat/moncert.cer"),
+            new XMPSchemaTester(metadata, schema, schemaClass, "Marked", XMPSchemaTester.createPropertyType(Types.Boolean), true),
+            new XMPSchemaTester(metadata, schema, schemaClass, "Owner", XMPSchemaTester.createPropertyType(Types.ProperName, Cardinality.Bag), new String[] { "OwnerName" }),
+            new XMPSchemaTester(metadata, schema, schemaClass, "UsageTerms", XMPSchemaTester.createPropertyType(Types.LangAlt), desc),
+            new XMPSchemaTester(metadata, schema, schemaClass, "WebStatement", XMPSchemaTester.createPropertyType(Types.URL), "http://une.url.vers.une.page.fr/"),
+        };
+    }
 }
