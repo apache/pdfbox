0f5d88fc5b0c6194370a5a59833f1b5f86eb3fd1
Maruan Sahyoun <msahyoun@apache.org>
2020-12-09 19:35:20 +0000

    PDFBOX-2602: use picocli for command line parsing
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1884268 13f79535-47bb-0310-9956-ffa450edef68

--- tools/src/main/java/org/apache/pdfbox/tools/PrintPDF.java
+++ tools/src/main/java/org/apache/pdfbox/tools/PrintPDF.java
@@ -21,8 +21,9 @@ import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.PrintStream;
+import java.util.concurrent.Callable;
+
 import javax.print.PrintService;
 
 import org.apache.pdfbox.Loader;
@@ -31,123 +32,76 @@ import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
 import org.apache.pdfbox.printing.Orientation;
 import org.apache.pdfbox.printing.PDFPageable;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
 /**
  * This is a command line program that will print a PDF document.
  * 
  * @author Ben Litchfield
  */
-public final class PrintPDF
+@Command(name = "PrintPDF", description = "Print a PDF.")
+public final class PrintPDF implements Callable<Integer>
 {
-    @SuppressWarnings({"squid:S2068"})
-    private static final String PASSWORD = "-password";
-    private static final String SILENT = "-silentPrint";
-    private static final String PRINTER_NAME = "-printerName";
-    private static final String ORIENTATION = "-orientation";
-    private static final String BORDER = "-border";
-    private static final String DPI = "-dpi";
-    private static final String NOCOLOROPT = "-noColorOpt";
+    // Expected for CLI app to write to System.out/Sytem.err
+    @SuppressWarnings("squid:S106")
+    private static final PrintStream SYSERR = System.err;
 
-    /**
-     * private constructor.
-     */
-    private PrintPDF()
-    {
-        // static class
-    }
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "display this help message")
+    boolean usageHelpRequested;
+
+    @Option(names = "-password", description = "the password to decrypt the document.")    
+    private String password;
+
+    @Option(names = "-silentPrint", description = "print without printer dialog box.")    
+    private boolean silentPrint;
+
+    @Option(names = "-printerName", description = "print to specific printer.")    
+    private String printerName;
+
+    @Option(names = "-orientation", description = "print using orientation [${COMPLETION-CANDIDATES}] (default: ${DEFAULT-VALUE}).")    
+    private Orientation orientation = Orientation.AUTO;
+
+    @Option(names = "-border", description = "print with border.")    
+    private boolean border;
+
+    @Option(names = "-dpi", description = "render into intermediate image with specific dpi and then print")
+    private int dpi;
+
+    @Option(names = "-noColorOpt", description = "disable color optimizations (useful when printing barcodes)")
+    private boolean noColorOpt;
+
+    @Parameters(paramLabel = "inputfile", description = "the PDF file to print.")
+    private File infile;
 
     /**
      * Infamous main method.
      * 
      * @param args Command line arguments, should be one and a reference to a file.
-     * @throws PrinterException if the specified service cannot support the Pageable and Printable interfaces.
-     * @throws IOException if there is an error parsing the file.
      */
-    public static void main(String[] args) throws PrinterException, IOException
+    public static void main(String[] args)
     {
         // suppress the Dock icon on OS X
         System.setProperty("apple.awt.UIElement", "true");
+        int exitCode = new CommandLine(new PrintPDF()).execute(args);
+        System.exit(exitCode);
+    }
 
-        @SuppressWarnings({"squid:S2068"})
-        String password = "";
-        String pdfFile = null;
-        boolean silentPrint = false;
-        String printerName = null;
-        Orientation orientation = Orientation.AUTO;
-        boolean showPageBorder = false;
-        int dpi = 0;
-        Map <String,Orientation> orientationMap = new HashMap<>();
-        orientationMap.put("auto", Orientation.AUTO);
-        orientationMap.put("landscape", Orientation.LANDSCAPE);
-        orientationMap.put("portrait", Orientation.PORTRAIT);
+    public Integer call()
+    {
         RenderingHints renderingHints = null;
 
-        for (int i = 0; i < args.length; i++)
+        if (noColorOpt)
         {
-            switch (args[i])
-            {
-                case PASSWORD:
-                    i++;
-                    if (i >= args.length)
-                    {
-                        usage();
-                    }
-                    password = args[i];
-                    break;
-                case PRINTER_NAME:
-                    i++;
-                    if (i >= args.length)
-                    {
-                        usage();
-                    }
-                    printerName = args[i];
-                    break;
-                case SILENT:
-                    silentPrint = true;
-                    break;
-                case ORIENTATION:
-                    i++;
-                    if (i >= args.length)
-                    {
-                        usage();
-                    }
-                    orientation = orientationMap.get(args[i]);
-                    if (orientation == null)
-                    {
-                        usage();
-                    }
-                    break;
-                case BORDER:
-                    showPageBorder = true;
-                    break;
-                case DPI:
-                    i++;
-                    if (i >= args.length)
-                    {
-                        usage();
-                    }
-                    dpi = Integer.parseInt(args[i]);
-                    break;
-                case NOCOLOROPT:
-                    renderingHints = new RenderingHints(null);
-                    renderingHints.put(RenderingHints.KEY_INTERPOLATION,
-                                       RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
-                    renderingHints.put(RenderingHints.KEY_RENDERING,
-                                       RenderingHints.VALUE_RENDER_QUALITY);
-                    renderingHints.put(RenderingHints.KEY_ANTIALIASING,
-                                       RenderingHints.VALUE_ANTIALIAS_OFF);
-                    break;
-                default:
-                    pdfFile = args[i];
-                    break;
-            }
+            renderingHints = new RenderingHints(null);
+            renderingHints.put(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+            renderingHints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            renderingHints.put(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
         }
 
-        if (pdfFile == null)
-        {
-            usage();
-        }
-
-        try (PDDocument document = Loader.loadPDF(new File(pdfFile), password))
+        try (PDDocument document = Loader.loadPDF(infile, password))
         {
             AccessPermission ap = document.getCurrentAccessPermission();
             if (!ap.canPrint())
@@ -156,7 +110,7 @@ public final class PrintPDF
             }
 
             PrinterJob printJob = PrinterJob.getPrinterJob();
-            printJob.setJobName(new File(pdfFile).getName());
+            printJob.setJobName(infile.getName());
 
             if (printerName != null)
             {
@@ -172,11 +126,11 @@ public final class PrintPDF
                 }
                 if (!printerFound)
                 {
-                    System.err.println("printer '" + printerName + "' not found, using default");
+                    SYSERR.println("printer '" + printerName + "' not found, using default");
                     showAvailablePrinters();
                 }
             }
-            PDFPageable pageable = new PDFPageable(document, orientation, showPageBorder, dpi);
+            PDFPageable pageable = new PDFPageable(document, orientation, border, dpi);
             pageable.setRenderingHints(renderingHints);
             printJob.setPageable(pageable);
 
@@ -190,37 +144,21 @@ public final class PrintPDF
                 printJob.print();
             }
         }
-    }
-
-    /**
-     * This will print the usage requirements and exit.
-     */
-    private static void usage()
-    {
-        String message = "Usage: java -jar pdfbox-app-x.y.z.jar PrintPDF [options] <inputfile>\n"
-                + "\nOptions:\n"
-                + "  -password  <password>                : Password to decrypt document\n"
-                + "  -printerName <name>                  : Print to specific printer\n"
-                + "  -orientation auto|portrait|landscape : Print using orientation\n"
-                + "                                           (default: auto)\n"
-                + "  -border                              : Print with border\n"
-                + "  -dpi                                 : Render into intermediate image with\n"
-                + "                                           specific dpi and then print\n"
-                + "  -noColorOpt                          : Disable color optimizations\n"
-                + "                                           (useful when printing barcodes)\n"
-                + "  -silentPrint                         : Print without printer dialog box\n";
-        System.err.println(message);
-        showAvailablePrinters();
-        System.exit(1);
+        catch (IOException | PrinterException ex)
+        {
+            SYSERR.println("Error printing document: " + ex.getMessage());
+            return 4;
+        }
+        return 0;
     }
 
     private static void showAvailablePrinters()
     {
-        System.err.println("Available printer names:");
+        SYSERR.println("Available printer names:");
         PrintService[] printServices = PrinterJob.lookupPrintServices();
         for (PrintService printService : printServices)
         {
-            System.err.println("    " + printService.getName());
+            SYSERR.println("    " + printService.getName());
         }
     }
 }
