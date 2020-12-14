4936e47c58aa8744b3203387661ad60c1cb1c98f
Tilman Hausherr <tilman@apache.org>
2020-12-14 04:14:52 +0000

    PDFBOX-4892: avoid IllegalArgumentException in Arrays.copyOfRange()
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1884402 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType1Font.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType1Font.java
@@ -235,9 +235,13 @@ public class PDType1Font extends PDSimpleFont implements PDVectorFont
 
                     // repair Length1 and Length2 if necessary
                     byte[] bytes = fontFile.toByteArray();
+                    if (bytes.length == 0)
+                    {
+                        throw new IOException("Font data unavailable");
+                    }
                     length1 = repairLength1(bytes, length1);
                     length2 = repairLength2(bytes, length1, length2);
-                    
+
                     if (bytes.length > 0 && (bytes[0] & 0xff) == PFB_START_MARKER)
                     {
                         // some bad files embed the entire PFB, see PDFBOX-2607
@@ -246,6 +250,11 @@ public class PDType1Font extends PDSimpleFont implements PDVectorFont
                     else
                     {
                         // the PFB embedded as two segments back-to-back
+                        if (length1 < 0 || length1 > length1 + length2)
+                        {
+                            throw new IOException("Invalid length data, actual length: " +
+                                    bytes.length + ", /Length1: " + length1 + ", /Length2: " + length2);
+                        }
                         byte[] segment1 = Arrays.copyOfRange(bytes, 0, length1);
                         byte[] segment2 = Arrays.copyOfRange(bytes, length1, length1 + length2);
 
