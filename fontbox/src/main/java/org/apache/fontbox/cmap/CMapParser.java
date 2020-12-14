6470a52434126faede0a7f50a558d7d70ce4e0e6
Tilman Hausherr <tilman@apache.org>
2020-12-14 13:28:42 +0000

    PDFBOX-4892: rethrow IllegalArgumentException (pdfium issue 1478)
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1884421 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/cmap/CMapParser.java
+++ fontbox/src/main/java/org/apache/fontbox/cmap/CMapParser.java
@@ -244,7 +244,14 @@ public class CMapParser
             }
             byte[] startRange = (byte[]) nextToken;
             byte[] endRange = (byte[]) parseNextToken(cmapStream);
-            result.addCodespaceRange(new CodespaceRange(startRange, endRange));
+            try
+            {
+                result.addCodespaceRange(new CodespaceRange(startRange, endRange));
+            }
+            catch (IllegalArgumentException ex)
+            {
+                throw new IOException(ex);
+            }
         }
     }
 
