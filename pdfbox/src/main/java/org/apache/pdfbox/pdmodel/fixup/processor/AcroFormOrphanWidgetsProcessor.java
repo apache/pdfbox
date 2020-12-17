5ed53bad1e1c2064c2039bcf39451413e04cd827
Tilman Hausherr <tilman@apache.org>
2020-12-17 18:46:57 +0000

    PDFBOX-5050: avoid NPE
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1884566 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fixup/processor/AcroFormOrphanWidgetsProcessor.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fixup/processor/AcroFormOrphanWidgetsProcessor.java
@@ -114,7 +114,8 @@ public class AcroFormOrphanWidgetsProcessor extends AbstractProcessor
             {
                 addFontFromWidget(acroFormResources, annot);
 
-                if (annot.getCOSObject().containsKey(COSName.PARENT))
+                COSDictionary parent = annot.getCOSObject().getCOSDictionary(COSName.PARENT);
+                if (parent != null)
                 {
                     PDField resolvedField = resolveNonRootField(acroForm, (PDAnnotationWidget) annot, nonTerminalFieldsMap);
                     if (resolvedField != null)
@@ -175,6 +176,10 @@ public class AcroFormOrphanWidgetsProcessor extends AbstractProcessor
         while (parent.containsKey(COSName.PARENT))
         {
             parent = parent.getCOSDictionary(COSName.PARENT);
+            if (parent == null)
+            {
+                return null;
+            }
         }
         
         if (nonTerminalFieldsMap.get(parent.getString(COSName.T)) == null)
