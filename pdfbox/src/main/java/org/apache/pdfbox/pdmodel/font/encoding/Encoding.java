1e06f6af1580203599e2112c8e7f72d380ee8f76
Tilman Hausherr <tilman@apache.org>
2020-12-28 12:49:54 +0000

    PDFBOX-5056: fix double-checked locking, as suggested by Mike Kaplinskiy
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1884866 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/encoding/Encoding.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/encoding/Encoding.java
@@ -63,7 +63,7 @@ public abstract class Encoding implements COSObjectable
 
     protected final Map<Integer, String> codeToName = new HashMap<Integer, String>(250);
     protected final Map<String, Integer> inverted = new HashMap<String, Integer>(250);
-    private Set<String> names;
+    private volatile Set<String> names;
 
     /**
      * Returns an unmodifiable view of the code -&gt; name mapping.
@@ -140,11 +140,13 @@ public abstract class Encoding implements COSObjectable
         {
             synchronized(this)
             {
-                // PDFBOX-3404: avoid possibility that one thread ends up with newly created empty map from other thread
-                Set<String> tmpSet = new HashSet<String>(codeToName.values());
-                // make sure that assignment is done after initialisation is complete
-                names = tmpSet;
-                // note that it might still happen that 'names' is initialized twice, but this is harmless
+                if (names == null)
+                {
+                    // PDFBOX-3404: avoid possibility that one thread ends up with newly created empty map from other thread
+                    Set<String> tmpSet = new HashSet<String>(codeToName.values());
+                    // make sure that assignment is done after initialisation is complete
+                    names = tmpSet;
+                }
             }
             // at this point, names will never be null.
         }
