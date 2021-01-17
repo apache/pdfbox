8add01c33251194592b52dfa41334405913665be
Andreas Lehmkühler <lehmi@apache.org>
2021-01-17 14:09:50 +0000

    PDFBOX-5078: add missing package declaration
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1885601 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/util/SplitBooklet.java
+++ examples/src/main/java/org/apache/pdfbox/examples/util/SplitBooklet.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.pdfbox.examples.util;
 
 import java.io.File;
 import java.io.IOException;
