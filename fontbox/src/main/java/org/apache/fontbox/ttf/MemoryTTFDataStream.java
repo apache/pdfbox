0b06a6d1ddfc3d1bb7c5ed35ab7b67693b5ad64b
Tilman Hausherr <tilman@apache.org>
2021-03-01 18:54:05 +0000

    PDFBOX-4892: remove useless shift, as suggested by valerybokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1887042 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/ttf/MemoryTTFDataStream.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/MemoryTTFDataStream.java
@@ -83,7 +83,7 @@ class MemoryTTFDataStream extends TTFDataStream
         {
             throw new EOFException();
         }
-        return ((ch1 << 24) + (ch2 << 16) + (ch3 << 8) + (ch4 << 0));
+        return ((ch1 << 24) + (ch2 << 16) + (ch3 << 8) + ch4);
     }
     
     /**
@@ -118,7 +118,7 @@ class MemoryTTFDataStream extends TTFDataStream
         {
             throw new EOFException();
         }
-        return (ch1 << 8) + (ch2 << 0);
+        return (ch1 << 8) + ch2;
     }
     
     /**
@@ -136,7 +136,7 @@ class MemoryTTFDataStream extends TTFDataStream
         {
             throw new EOFException();
         }
-        return (short)((ch1 << 8) + (ch2 << 0));
+        return (short)((ch1 << 8) + ch2);
     }
     
     /**
