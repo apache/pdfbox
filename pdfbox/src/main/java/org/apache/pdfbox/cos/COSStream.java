f7679454e55b1fb4fd7a264c2cd3fa39b93a9bf4
Tilman Hausherr <tilman@apache.org>
2021-03-02 18:24:45 +0000

    PDFBOX-4892: set individual initial ArrayList size, as suggested by valerybokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1887103 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/cos/COSStream.java
+++ pdfbox/src/main/java/org/apache/pdfbox/cos/COSStream.java
@@ -317,15 +317,17 @@ public class COSStream extends COSDictionary implements Closeable
      */
     private List<Filter> getFilterList() throws IOException
     {
-        List<Filter> filterList = new ArrayList<>();
+        List<Filter> filterList;
         COSBase filters = getFilters();
         if (filters instanceof COSName)
         {
+            filterList = new ArrayList<>(1);
             filterList.add(FilterFactory.INSTANCE.getFilter((COSName)filters));
         }
         else if (filters instanceof COSArray)
         {
             COSArray filterArray = (COSArray)filters;
+            filterList = new ArrayList<>(filterArray.size());
             for (int i = 0; i < filterArray.size(); i++)
             {
                 COSBase base = filterArray.get(i);
@@ -336,6 +338,10 @@ public class COSStream extends COSDictionary implements Closeable
                 }
                 filterList.add(FilterFactory.INSTANCE.getFilter((COSName) base));
             }
+        }
+        else
+        {
+            filterList = new ArrayList<Filter>();
         }
         return filterList;
     }
