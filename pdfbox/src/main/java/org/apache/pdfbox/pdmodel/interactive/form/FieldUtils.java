51a3c67eb7ba7a7b6028d4f1f413be3193087d16
valery.bokov <67366451+valerybokov@users.noreply.github.com>
2021-03-08 10:17:05 +0200

    performance improvements for FieldUtils.toKeyValueList

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/FieldUtils.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/FieldUtils.java
@@ -85,7 +85,7 @@ public final class FieldUtils
      */
     static List<KeyValue> toKeyValueList(List<String> key, List<String> value)
     {
-        List<KeyValue> list = new ArrayList<>();
+        List<KeyValue> list = new ArrayList<>(key.size());
         for(int i =0; i<key.size(); i++)
         {
             list.add(new FieldUtils.KeyValue(key.get(i),value.get(i)));
@@ -139,7 +139,7 @@ public final class FieldUtils
         
         if (items instanceof COSString)
         {
-            List<String> array = new ArrayList<>();
+            List<String> array = new ArrayList<>(1);
             array.add(((COSString) items).getString());
             return array;
         }
