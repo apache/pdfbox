f7660b52fd073ac94e55ce95014bd31efa0b54a4
Tilman Hausherr <tilman@apache.org>
2021-03-12 07:01:16 +0000

    PDFBOX-4892: set individual initial ArrayList size and simplify code, as suggested by valerybokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1887529 13f79535-47bb-0310-9956-ffa450edef68

--- xmpbox/src/main/java/org/apache/xmpbox/type/ArrayProperty.java
+++ xmpbox/src/main/java/org/apache/xmpbox/type/ArrayProperty.java
@@ -23,7 +23,6 @@ package org.apache.xmpbox.type;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.xmpbox.XMPMetadata;
@@ -72,17 +71,10 @@ public class ArrayProperty extends AbstractComplexProperty
 
     public List<String> getElementsAsString()
     {
-        List<String> retval;
-        retval = new ArrayList<>();
-        Iterator<AbstractField> it = getContainer().getAllProperties().iterator();
-        AbstractSimpleProperty tmp;
-        while (it.hasNext())
-        {
-            tmp = (AbstractSimpleProperty) it.next();
-            retval.add(tmp.getStringValue());
-        }
-        retval = Collections.unmodifiableList(retval);
-        return retval;
+        List<AbstractField> allProperties = getContainer().getAllProperties();
+        List<String> retval = new ArrayList<>(allProperties.size());
+        allProperties.forEach(tmp -> retval.add(((AbstractSimpleProperty) tmp).getStringValue()));
+        return Collections.unmodifiableList(retval);
     }
 
     /**
