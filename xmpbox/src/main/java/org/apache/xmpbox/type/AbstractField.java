9f28d2c10613ab70170fcacbed0b97f4c53e13ad
valery.bokov <67366451+valerybokov@users.noreply.github.com>
2021-03-12 08:49:45 +0200

    Revert "updating AbstractField.setAttribute"
    This reverts commit f4480d5e867e27bae2872a8494ce93e5836325ec.

--- xmpbox/src/main/java/org/apache/xmpbox/type/AbstractField.java
+++ xmpbox/src/main/java/org/apache/xmpbox/type/AbstractField.java
@@ -81,9 +81,11 @@ public abstract class AbstractField
      */
     public final void setAttribute(Attribute value)
     {
-        // if same name in element, attribute will be replaced
-        attributes.remove(value.getName());
-
+        if (attributes.containsKey(value.getName()))
+        {
+            // if same name in element, attribute will be replaced
+            attributes.remove(value.getName());
+        }
         attributes.put(value.getName(), value);
     }
 
