960b4df5b1397e8663b8c9ae642af14ff3c2ac29
Tilman Hausherr <tilman@apache.org>
2021-03-20 18:21:07 +0000

    PDFBOX-4892: create array only when needed, as suggested by valerybokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1887874 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDFreeTextAppearanceHandler.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDFreeTextAppearanceHandler.java
@@ -81,7 +81,7 @@ public class PDFreeTextAppearanceHandler extends PDAbstractAppearanceHandler
     public void generateNormalAppearance()
     {
         PDAnnotationFreeText annotation = (PDAnnotationFreeText) getAnnotation();
-        float[] pathsArray = new float[0];
+        float[] pathsArray;
         if (PDAnnotationFreeText.IT_FREE_TEXT_CALLOUT.equals(annotation.getIntent()))
         {
             pathsArray = annotation.getCallout();
@@ -89,6 +89,10 @@ public class PDFreeTextAppearanceHandler extends PDAbstractAppearanceHandler
             {
                 pathsArray = new float[0];
             }
+        }
+        else
+        {
+            pathsArray = new float[0];
         }
         AnnotationBorder ab = AnnotationBorder.getAnnotationBorder(annotation, annotation.getBorderStyle());
 
