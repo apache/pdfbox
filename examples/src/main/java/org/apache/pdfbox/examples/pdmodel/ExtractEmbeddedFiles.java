f46df9deae88ead6ea2b40c5e7a40a86b78d258f
Tilman Hausherr <tilman@apache.org>
2021-03-21 19:19:21 +0000

    PDFBOX-4892: avoid NPE
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1887910 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/pdmodel/ExtractEmbeddedFiles.java
+++ examples/src/main/java/org/apache/pdfbox/examples/pdmodel/ExtractEmbeddedFiles.java
@@ -111,6 +111,10 @@ public final class ExtractEmbeddedFiles
         else
         {
             List<PDNameTreeNode<PDComplexFileSpecification>> kids = efTree.getKids();
+            if (kids == null)
+            {
+                return;
+            }
             for (PDNameTreeNode<PDComplexFileSpecification> node : kids)
             {
                 names = node.getNames();
