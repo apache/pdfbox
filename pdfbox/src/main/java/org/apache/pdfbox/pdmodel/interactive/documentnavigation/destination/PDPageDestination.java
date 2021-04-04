119592322d20ce2dfbf2c7780b4036601405106b
valery.bokov <67366451+valerybokov@users.noreply.github.com>
2021-04-04 16:04:22 +0300

    performance improvements for PDPageDestination.indexOfPageTree

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/documentnavigation/destination/PDPageDestination.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/documentnavigation/destination/PDPageDestination.java
@@ -138,9 +138,15 @@ public abstract class PDPageDestination extends PDDestination
     private int indexOfPageTree(COSDictionary pageDict)
     {
         COSDictionary parent = pageDict;
-        while (parent.getCOSDictionary(COSName.PARENT, COSName.P) != null)
+        COSDictionary prevParent;
+        while (true)
         {
-            parent = parent.getCOSDictionary(COSName.PARENT, COSName.P);
+            prevParent = parent.getCOSDictionary(COSName.PARENT, COSName.P);
+
+            if (prevParent == null)
+                break;
+
+            parent = prevParent;
         }
         if (parent.containsKey(COSName.KIDS)
                 && COSName.PAGES.equals(parent.getCOSName(COSName.TYPE)))
