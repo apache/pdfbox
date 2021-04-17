8757e7a8c30208ea81a1b676cebce01820426147
Tilman Hausherr <tilman@apache.org>
2021-04-17 10:38:41 +0000

    PDFBOX-5168: make non empty dash with only zero invisible (corrected,
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1888854 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
@@ -678,12 +678,29 @@ public class PageDrawer extends PDFGraphicsStreamEngine
         }
 
         PDLineDashPattern dashPattern = state.getLineDashPattern();
+        // PDFBOX-5168: show an all-zero dash array line invisible like Adobe does
+        // must do it here because getDashArray() sets minimum width because of JVM bugs
+        float[] dashArray = dashPattern.getDashArray();
+        if (dashArray.length > 0)
+        {
+            boolean allZero = true;
+            for (int i = 0; i < dashArray.length; ++i)
+            {
+                if (dashArray[i] != 0)
+                {
+                    allZero = false;
+                    break;
+                }
+            }
+            if (allZero)
+            {
+                return (Shape p) -> new Area();
+            }
+        }
         float phaseStart = dashPattern.getPhase();
-        float[] dashArray = getDashArray(dashPattern);
+        dashArray = getDashArray(dashPattern);
         phaseStart = transformWidth(phaseStart);
-        boolean allZero = true;
 
-        // empty dash array is illegal
         // avoid also infinite and NaN values (PDFBOX-3360)
         if (dashArray.length == 0 || Float.isInfinite(phaseStart) || Float.isNaN(phaseStart))
         {
@@ -693,22 +710,11 @@ public class PageDrawer extends PDFGraphicsStreamEngine
         {
             for (int i = 0; i < dashArray.length; ++i)
             {
-                float dash = dashArray[i];
-                if (Float.isInfinite(dash) || Float.isNaN(dash))
+                if (Float.isInfinite(dashArray[i]) || Float.isNaN(dashArray[i]))
                 {
                     dashArray = null;
-                    allZero = false;
                     break;
                 }
-                if (dash != 0)
-                {
-                    allZero = false;
-                }
-            }
-            if (allZero)
-            {
-                // PDFBOX-5168: make it invisible like Adobe
-                return (Shape p) -> new Area();
             }
         }
         int lineCap = Math.min(2, Math.max(0, state.getLineCap())); // legal values 0..2
