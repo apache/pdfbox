33d7a3897ebaa0f6d785c16c9d8515cafcd0c653
valery.bokov <67366451+valerybokov@users.noreply.github.com>
2021-05-05 10:57:43 +0300

    fixed bug with wrong indexes in a PDJPXColorSpace.getDefaultDecode.

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDJPXColorSpace.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDJPXColorSpace.java
@@ -59,8 +59,7 @@ public final class PDJPXColorSpace extends PDColorSpace
     {
         int n = getNumberOfComponents();
         float[] decode = new float[n * 2];
-        int j = 0;
-        for (int i = 0; i < n; i += 2, ++j)
+        for (int i = 0, j = 0; i < decode.length; i += 2, ++j)
         {
             decode[i] = awtColorSpace.getMinValue(j);
             decode[i + 1] = awtColorSpace.getMaxValue(j);
