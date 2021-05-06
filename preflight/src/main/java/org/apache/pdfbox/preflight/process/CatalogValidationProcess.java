ba0504628af5ec878d3318f2e9689288ed79c2ce
Tilman Hausherr <tilman@apache.org>
2021-05-06 06:35:52 +0000

    PDFBOX-4892: avoid ClassCastException, as suggested by valerybokov; remove unneeded code
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1889552 13f79535-47bb-0310-9956-ffa450edef68

--- preflight/src/main/java/org/apache/pdfbox/preflight/process/CatalogValidationProcess.java
+++ preflight/src/main/java/org/apache/pdfbox/preflight/process/CatalogValidationProcess.java
@@ -272,16 +272,10 @@ public class CatalogValidationProcess extends AbstractProcess
             {
         try
         {
-            if (destOutputProfile == null)
-            {
-                return;
-            }
-
-            COSBase localDestOutputProfile = destOutputProfile;
             // destOutputProfile should be an instance of COSObject because of this is a object reference
-            if (localDestOutputProfile instanceof COSObject)
+            if (destOutputProfile instanceof COSObject)
             {
-                if (mapDestOutputProfile.containsKey(new COSObjectKey((COSObject) localDestOutputProfile)))
+                if (mapDestOutputProfile.containsKey(new COSObjectKey((COSObject) destOutputProfile)))
                 {
                     // the profile is already checked. continue
                     return;
@@ -294,13 +288,18 @@ public class CatalogValidationProcess extends AbstractProcess
                     return;
                 }
                 // else the profile will be kept in the mapDestOutputProfile if it is valid
-
-                localDestOutputProfile = ((COSObject) localDestOutputProfile).getObject();
+            }
+            else
+            {
+                addValidationError(ctx, new ValidationError(ERROR_GRAPHIC_OUTPUT_INTENT_INVALID_ENTRY,
+                        "OutputIntent object should be a reference: " + destOutputProfile));
+                return;
             }
 
             // keep reference to avoid multiple profile definition
             mapDestOutputProfile.put(new COSObjectKey((COSObject) destOutputProfile), true);
 
+            COSBase localDestOutputProfile = ((COSObject) destOutputProfile).getObject();
             if (!(localDestOutputProfile instanceof COSStream))
             {
                 addValidationError(ctx, new ValidationError(ERROR_GRAPHIC_OUTPUT_INTENT_INVALID_ENTRY,
