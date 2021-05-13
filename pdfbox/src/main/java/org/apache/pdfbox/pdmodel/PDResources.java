a4e14cdf4188e6a4784295243d570c9de90112b5
Tilman Hausherr <tilman@apache.org>
2021-05-13 09:56:01 +0000

    PDFBOX-4892: don't cache null keys, as suggested by valerybokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1889828 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDResources.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDResources.java
@@ -204,7 +204,7 @@ public final class PDResources implements COSObjectable
         }
 
         // we can't cache PDPattern, because it holds page resources, see PDFBOX-2370
-        if (cache != null && !(colorSpace instanceof PDPattern))
+        if (cache != null && indirect != null && !(colorSpace instanceof PDPattern))
         {
             cache.put(indirect, colorSpace);
         }
@@ -250,7 +250,7 @@ public final class PDResources implements COSObjectable
             extGState = new PDExtendedGraphicsState((COSDictionary) base);
         }
 
-        if (cache != null)
+        if (cache != null && indirect != null)
         {
             cache.put(indirect, extGState);
         }
@@ -286,7 +286,7 @@ public final class PDResources implements COSObjectable
             shading = PDShading.create((COSDictionary) base);
         }
         
-        if (cache != null)
+        if (cache != null && indirect != null)
         {
             cache.put(indirect, shading);
         }
@@ -322,7 +322,7 @@ public final class PDResources implements COSObjectable
             pattern = PDAbstractPattern.create((COSDictionary) base, getResourceCache());
         }
 
-        if (cache != null)
+        if (cache != null && indirect != null)
         {
             cache.put(indirect, pattern);
         }
@@ -356,7 +356,7 @@ public final class PDResources implements COSObjectable
             propertyList = PDPropertyList.create((COSDictionary) base);
         }
 
-        if (cache != null)
+        if (cache != null && indirect != null)
         {
             cache.put(indirect, propertyList);
         }
@@ -425,7 +425,7 @@ public final class PDResources implements COSObjectable
         {
             xobject = PDXObject.createXObject(value, this);
         }
-        if (cache != null && isAllowedCache(xobject))
+        if (cache != null && indirect != null && isAllowedCache(xobject))
         {
             cache.put(indirect, xobject);
         }
