eef31a9d0de2db32305894d2cd86701710918064
Tilman Hausherr <tilman@apache.org>
2021-05-16 09:05:02 +0000

    PDFBOX-4892: fix comment, as suggested by valerybokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1889927 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/measurement/PDNumberFormatDictionary.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/measurement/PDNumberFormatDictionary.java
@@ -153,7 +153,8 @@ public class PDNumberFormatDictionary implements COSObjectable
 
     /** 
      * This will set the value for the manner to display a fractional value.
-     * Allowed values are "D", "F", "R" and "T"
+     * Allowed values are "D", "F", "R", "T" and null.
+     * 
      * @param fractionalDisplay the manner to display a fractional value
      */
     public void setFractionalDisplay(String fractionalDisplay)
@@ -301,9 +302,9 @@ public class PDNumberFormatDictionary implements COSObjectable
     }
 
     /**
-     * This will set the value indicating the ordering of the label specified by U to the calculated unit value.
-     * Possible values are "S" and "P"
-     * 
+     * This will set the value indicating the ordering of the label specified by U to the calculated
+     * unit value. Possible values are "S", "P" and null.
+     *
      * @param labelPositionToValue label position 
      */
     public void setLabelPositionToValue(String labelPositionToValue)
