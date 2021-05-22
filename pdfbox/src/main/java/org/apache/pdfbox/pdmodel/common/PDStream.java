c86542ce9cb3ae755ada6eb9517f8ef7871efa66
Tilman Hausherr <tilman@apache.org>
2021-05-22 13:47:10 +0000

    PDFBOX-5199: avoid possible memory leak
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1890103 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/PDStream.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/PDStream.java
@@ -222,8 +222,14 @@ public class PDStream implements COSObjectable
                 else
                 {
                     Filter filter = FilterFactory.INSTANCE.getFilter(nextFilter);
-                    filter.decode(is, os, stream, i);
-                    IOUtils.closeQuietly(is);
+                    try
+                    {
+                        filter.decode(is, os, stream, i);
+                    }
+                    finally
+                    {
+                        IOUtils.closeQuietly(is);
+                    }
                     is = new ByteArrayInputStream(os.toByteArray());
                     os.reset();
                 }
