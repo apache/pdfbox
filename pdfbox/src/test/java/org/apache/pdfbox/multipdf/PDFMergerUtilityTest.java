da6985ea8ecc83b4978fed956871e4c3f65db82d
Tilman Hausherr <tilman@apache.org>
2021-05-29 03:00:59 +0000

    PDFBOX-5198: add test
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1890302 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/test/java/org/apache/pdfbox/multipdf/PDFMergerUtilityTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/multipdf/PDFMergerUtilityTest.java
@@ -56,6 +56,7 @@ import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPa
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.apache.pdfbox.rendering.PDFRenderer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
@@ -707,6 +708,67 @@ class PDFMergerUtilityTest
         Files.delete(inFile1.toPath());
         Files.delete(inFile2.toPath());
         Files.delete(outFile.toPath());
+    }
+
+    /**
+     * Check that there is a top level Document and Parts below in a merge of 2 documents.
+     *
+     * @param file
+     * @throws IOException 
+     */
+    @Test
+    void testPDFBox5198_2() throws IOException
+    {
+        PDFMergerUtility pdfMergerUtility = new PDFMergerUtility();
+        pdfMergerUtility.addSource(new File(SRCDIR, "PDFA3A.pdf"));
+        pdfMergerUtility.addSource(new File(SRCDIR, "PDFA3A.pdf"));
+        pdfMergerUtility.setDestinationFileName(TARGETTESTDIR + "PDFA3A-merged2.pdf");
+        pdfMergerUtility.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly());
+
+        checkParts(new File(TARGETTESTDIR + "PDFA3A-merged2.pdf"));
+    }
+    
+    /**
+     * Check that there is a top level Document and Parts below in a merge of 3 documents.
+     *
+     * @param file
+     * @throws IOException 
+     */
+    @Test
+    void testPDFBox5198_3() throws IOException
+    {
+        PDFMergerUtility pdfMergerUtility = new PDFMergerUtility();
+        pdfMergerUtility.addSource(new File(SRCDIR, "PDFA3A.pdf"));
+        pdfMergerUtility.addSource(new File(SRCDIR, "PDFA3A.pdf"));
+        pdfMergerUtility.addSource(new File(SRCDIR, "PDFA3A.pdf"));
+        pdfMergerUtility.setDestinationFileName(TARGETTESTDIR + "PDFA3A-merged3.pdf");
+        pdfMergerUtility.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly());
+
+        checkParts(new File(TARGETTESTDIR + "PDFA3A-merged3.pdf"));
+    }
+
+    /**
+     * Check that there is a top level Document and Parts below.
+     * @param file
+     * @throws IOException 
+     */
+    private void checkParts(File file) throws IOException
+    {
+        try (PDDocument doc = Loader.loadPDF(file))
+        {
+            PDStructureTreeRoot structureTreeRoot = doc.getDocumentCatalog().getStructureTreeRoot();
+            COSDictionary topDict = (COSDictionary) structureTreeRoot.getK();
+            assertEquals(COSName.DOCUMENT, topDict.getItem(COSName.S));
+            assertEquals(structureTreeRoot.getCOSObject(), topDict.getCOSDictionary(COSName.P));
+            COSArray kArray = topDict.getCOSArray(COSName.K);
+            assertEquals(doc.getNumberOfPages(), kArray.size());
+            for (int i = 0; i < kArray.size(); ++i)
+            {
+                COSDictionary dict = (COSDictionary) kArray.getObject(i);
+                assertEquals(COSName.PART, dict.getItem(COSName.S));
+                assertEquals(topDict, dict.getCOSDictionary(COSName.P));
+            }
+        }
     }
 
     private void checkForPageOrphans(PDDocument doc) throws IOException
