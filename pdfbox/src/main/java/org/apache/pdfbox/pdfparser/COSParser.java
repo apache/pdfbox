3536fe3845002c337cda46888ed39f096af6c02d
Tilman Hausherr <tilman@apache.org>
2021-06-05 05:20:39 +0000

    PDFBOX-4892: simplify code, as suggested by valerybokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1890486 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdfparser/COSParser.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfparser/COSParser.java
@@ -2239,7 +2239,7 @@ public class COSParser extends BaseParser implements ICOSParser
         if ( headerStart > 0 )
         {
             //trim off any leading characters
-            header = header.substring( headerStart, header.length() );
+            header = header.substring(headerStart);
         }
     
         // This is used if there is garbage after the header on the same line
@@ -2253,7 +2253,7 @@ public class COSParser extends BaseParser implements ICOSParser
             }
             else
             {
-                String headerGarbage = header.substring(headerMarker.length() + 3, header.length()) + "\n";
+                String headerGarbage = header.substring(headerMarker.length() + 3) + "\n";
                 header = header.substring(0, headerMarker.length() + 3);
                 source.rewind(headerGarbage.getBytes(StandardCharsets.ISO_8859_1).length);
             }
