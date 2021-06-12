c70bb4fabfd11842b2fd34bd80ed3e76887a4da7
Tilman Hausherr <tilman@apache.org>
2021-06-12 05:07:33 +0000

    PDFBOX-4892: make field final, as suggested by valerybokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1890709 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/cff/DataInput.java
+++ fontbox/src/main/java/org/apache/fontbox/cff/DataInput.java
@@ -26,7 +26,7 @@ import java.io.IOException;
 public class DataInput
 {
 
-    private byte[] inputBuffer = null;
+    private final byte[] inputBuffer;
     private int bufferPosition = 0;
 
     /**
