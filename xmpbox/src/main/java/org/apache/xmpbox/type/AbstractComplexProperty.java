ce4fa612f67bccf097102b211d0513ecc902584b
valery.bokov <67366451+valerybokov@users.noreply.github.com>
2021-06-14 18:37:28 +0300

    AbstractComplexProperty changes
    - dangling comment removed
    - code style changes

--- xmpbox/src/main/java/org/apache/xmpbox/type/AbstractComplexProperty.java
+++ xmpbox/src/main/java/org/apache/xmpbox/type/AbstractComplexProperty.java
@@ -1,5 +1,4 @@
-/*****************************************************************************
- * 
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,17 +6,16 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
- ****************************************************************************/
+ */
 
 package org.apache.xmpbox.type;
 
@@ -29,7 +27,6 @@ import org.apache.xmpbox.XMPMetadata;
 
 public abstract class AbstractComplexProperty extends AbstractField
 {
-
     private final ComplexPropertyContainer container;
 
     private final Map<String, String> namespaceToPrefix;
@@ -132,5 +129,4 @@ public abstract class AbstractComplexProperty extends AbstractField
     {
         return container.getFirstEquivalentProperty(localName, type);
     }
-
 }
