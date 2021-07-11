29e7ff39380dfd20a139867b6b56a22ad8a47699
Tilman Hausherr <tilman@apache.org>
2021-07-11 15:23:19 +0000

    PDFBOX-4892: simply code, avoid using available()
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1891458 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/filter/FlateFilter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/filter/FlateFilter.java
@@ -26,6 +26,7 @@ import java.util.zip.Inflater;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.io.IOUtils;
 
 /**
  * Decompresses data encoded using the zlib/deflate compression method,
@@ -129,16 +130,7 @@ final class FlateFilter extends Filter
         Deflater deflater = new Deflater(compressionLevel);
         try (DeflaterOutputStream out = new DeflaterOutputStream(encoded,deflater))
         {
-            int amountRead;
-            int mayRead = input.available();
-            if (mayRead > 0)
-            {
-                byte[] buffer = new byte[Math.min(mayRead,BUFFER_SIZE)];
-                while ((amountRead = input.read(buffer, 0, Math.min(mayRead,BUFFER_SIZE))) != -1)
-                {
-                    out.write(buffer, 0, amountRead);
-                }
-            }
+            IOUtils.copy(input, out);
         }
         encoded.flush();
         deflater.end();
