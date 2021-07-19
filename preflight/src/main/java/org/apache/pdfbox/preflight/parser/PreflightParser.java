4ca8716ef392dca037c983ee49a3d1d032b05adf
Tilman Hausherr <tilman@apache.org>
2021-07-19 18:20:22 +0000

    PDFBOX-4892: optimize, as suggested by valerybokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1891662 13f79535-47bb-0310-9956-ffa450edef68

--- preflight/src/main/java/org/apache/pdfbox/preflight/parser/PreflightParser.java
+++ preflight/src/main/java/org/apache/pdfbox/preflight/parser/PreflightParser.java
@@ -410,6 +410,8 @@ public class PreflightParser extends PDFParser
         // signal start of new XRef
         xrefTrailerResolver.nextXrefObj(startByteOffset,XRefType.TABLE);
 
+        Pattern pattern = Pattern.compile("(\\d+)\\s(\\d+)(\\s*)");
+
         // Xref tables can have multiple sections. Each starts with a starting object id and a count.
         while (true)
         {
@@ -421,7 +423,6 @@ public class PreflightParser extends PDFParser
 
             long offset = source.getPosition();
             String line = readLine();
-            Pattern pattern = Pattern.compile("(\\d+)\\s(\\d+)(\\s*)");
             Matcher matcher = pattern.matcher(line);
             if (matcher.matches())
             {
