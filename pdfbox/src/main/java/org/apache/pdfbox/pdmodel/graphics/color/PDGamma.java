c2e9938a30b8a9ff470df30e1419c0187583e363
valery.bokov <67366451+valerybokov@users.noreply.github.com>
2021-09-24 18:39:47 +0300

    make field final for PDGamma

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDGamma.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDGamma.java
@@ -30,7 +30,7 @@ import org.apache.pdfbox.pdmodel.common.COSObjectable;
  */
 public final class PDGamma implements COSObjectable
 {
-    private COSArray values = null;
+    private final COSArray values;
 
     /**
      * Creates a new gamma.
