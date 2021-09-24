7a7d7d4953bfa84774a9aa8a8e093b74d6631a10
valery.bokov <67366451+valerybokov@users.noreply.github.com>
2021-09-24 19:14:06 +0300

    performance improvements for PDActionEmbeddedGoTo.getOpenInNewWindow

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionEmbeddedGoTo.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/action/PDActionEmbeddedGoTo.java
@@ -127,9 +127,10 @@ public class PDActionEmbeddedGoTo extends PDAction
      */
     public OpenMode getOpenInNewWindow()
     {
-        if (getCOSObject().getDictionaryObject(COSName.NEW_WINDOW) instanceof COSBoolean)
+        COSBase object = getCOSObject().getDictionaryObject(COSName.NEW_WINDOW);
+        if (object instanceof COSBoolean)
         {
-            COSBoolean b = (COSBoolean) getCOSObject().getDictionaryObject(COSName.NEW_WINDOW);
+            COSBoolean b = (COSBoolean) object;
             return b.getValue() ? OpenMode.NEW_WINDOW : OpenMode.SAME_WINDOW;
         }
         return OpenMode.USER_PREFERENCE;
