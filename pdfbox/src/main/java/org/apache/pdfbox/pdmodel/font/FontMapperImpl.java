e342b7d3f52e075d5fdc66edc7bce5130dc193ff
Tilman Hausherr <tilman@apache.org>
2021-10-02 14:11:11 +0000

    PDFBOX-4892: reduce memory usage, as suggested by valerybokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1893824 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/FontMapperImpl.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/FontMapperImpl.java
@@ -188,7 +188,7 @@ final class FontMapperImpl implements FontMapper
      */
     private Set<String> getPostScriptNames(String postScriptName)
     {
-        Set<String> names = new HashSet<String>();
+        Set<String> names = new HashSet<String>(2);
     
         // built-in PostScript name
         names.add(postScriptName);
