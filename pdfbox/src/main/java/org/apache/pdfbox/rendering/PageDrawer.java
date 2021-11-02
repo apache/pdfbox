3d52620a4eb12deac9b14c79ef1b76e79637a875
Henry Iguaro <henry@canva.com>
2021-11-02 12:26:18 +1100

    Check if graphics.getClip() != null before invoking intersect

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
@@ -820,7 +820,10 @@ public class PageDrawer extends PDFGraphicsStreamEngine
         {
             // apply clip to path to avoid oversized device bounds in shading contexts (PDFBOX-2901)
             Area area = new Area(linePath);
-            area.intersect(new Area(graphics.getClip()));
+            if (graphics.getClip() != null)
+            {
+                area.intersect(new Area(graphics.getClip()));
+            }
             intersectShadingBBox(getGraphicsState().getNonStrokingColor(), area);
             shape = area;
         }
