9353a6962e6fa4bae1160bbf4b104a828d466602
Tilman Hausherr <tilman@apache.org>
2021-11-11 03:14:43 +0000

    PDFBOX-5319: restore reversion but use colorspace instead of Paint to decide whether to intersect
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1894944 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
@@ -797,8 +797,8 @@ public class PageDrawer extends PDFGraphicsStreamEngine
     @Override
     public void fillPath(int windingRule) throws IOException
     {
-        graphics.setComposite(getGraphicsState().getNonStrokingJavaComposite());
-        graphics.setPaint(getNonStrokingPaint());
+        PDGraphicsState graphicsState = getGraphicsState();
+        graphics.setComposite(graphicsState.getNonStrokingJavaComposite());
         setClip();
         linePath.setWindingRule(windingRule);
 
@@ -816,7 +816,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
         }
 
         Shape shape;
-        if (!(graphics.getPaint() instanceof Color))
+        if (graphicsState.getNonStrokingColorSpace() instanceof PDPattern)
         {
             // apply clip to path to avoid oversized device bounds in shading contexts (PDFBOX-2901)
             Area area = new Area(linePath);
@@ -825,7 +825,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
             {
                 area.intersect(new Area(clip));
             }
-            intersectShadingBBox(getGraphicsState().getNonStrokingColor(), area);
+            intersectShadingBBox(graphicsState.getNonStrokingColor(), area);
             shape = area;
         }
         else
@@ -834,6 +834,8 @@ public class PageDrawer extends PDFGraphicsStreamEngine
         }
         if (isContentRendered() && !shape.getPathIterator(null).isDone())
         {
+            // creating Paint is sometimes a costly operation, so avoid if possible
+            graphics.setPaint(getNonStrokingPaint());
             graphics.fill(shape);
         }
         
