748901efa2698c19cbc14ce858bf7dc41910d2e5
Tilman Hausherr <tilman@apache.org>
2021-11-21 11:31:47 +0000

    PDFBOX-4892: optimize, as suggested by valerybokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1895224 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/PDNameTreeNode.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/PDNameTreeNode.java
@@ -217,32 +217,29 @@ public abstract class PDNameTreeNode<T extends COSObjectable> implements COSObje
     {
         T retval = null;
         Map<String, T> names = getNames();
-        if( names != null )
+        if (names != null)
         {
-            retval = names.get( name );
+            return names.get(name);
+        }
+        List<PDNameTreeNode<T>> kids = getKids();
+        if (kids != null)
+        {
+            for (int i = 0; i < kids.size(); i++)
+            {
+                PDNameTreeNode<T> childNode = kids.get(i);
+                String upperLimit = childNode.getUpperLimit();
+                String lowerLimit = childNode.getLowerLimit();
+                if (upperLimit == null || lowerLimit == null || 
+                    upperLimit.compareTo(lowerLimit) < 0 ||
+                    (lowerLimit.compareTo(name) <= 0 && upperLimit.compareTo(name) >= 0))
+                {
+                    retval = childNode.getValue( name );
+                }
+            }
         }
         else
         {
-            List<PDNameTreeNode<T>> kids = getKids();
-            if (kids != null)
-            {
-                for( int i=0; i<kids.size() && retval == null; i++ )
-                {
-                    PDNameTreeNode<T> childNode = kids.get( i );
-                    String upperLimit = childNode.getUpperLimit();
-                    String lowerLimit = childNode.getLowerLimit();
-                    if (upperLimit == null || lowerLimit == null || 
-                        upperLimit.compareTo(lowerLimit) < 0 ||
-                        (lowerLimit.compareTo(name) <= 0 && upperLimit.compareTo(name) >= 0))
-                    {
-                        retval = childNode.getValue( name );
-                    }
-                }
-            }
-            else
-            {
-                LOG.warn("NameTreeNode does not have \"names\" nor \"kids\" objects.");
-            }
+            LOG.warn("NameTreeNode does not have \"names\" nor \"kids\" objects.");
         }
         return retval;
     }
