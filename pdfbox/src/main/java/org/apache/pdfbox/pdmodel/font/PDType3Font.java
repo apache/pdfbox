2063dc598dfa4d6a43349d44e651c6247bb45ea7
Tilman Hausherr <tilman@apache.org>
2021-12-25 15:07:24 +0000

    PDFBOX-5339: avoid NullPointerException
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1896390 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType3Font.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType3Font.java
@@ -369,6 +369,10 @@ public class PDType3Font extends PDSimpleFont
      */
     public PDType3CharProc getCharProc(int code)
     {
+        if (getEncoding() == null)
+        {
+            return null;
+        }
         String name = getEncoding().getName(code);
         if (getCharProcs() == null)
         {
