9c36fcd1ac2b71b707f8bb5ea8ee8315b739ebb5
Tilman Hausherr <tilman@apache.org>
2021-12-26 16:14:14 +0000

    PDFBOX-5339: avoid IndexOutOfBoundsException
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1896426 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/SetTextLeading.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/text/SetTextLeading.java
@@ -16,8 +16,10 @@
  */
 package org.apache.pdfbox.contentstream.operator.text;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.pdfbox.contentstream.operator.MissingOperandException;
 import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSNumber;
 import org.apache.pdfbox.contentstream.operator.Operator;
@@ -32,8 +34,12 @@ import org.apache.pdfbox.contentstream.operator.OperatorProcessor;
 public class SetTextLeading extends OperatorProcessor
 {
     @Override
-    public void process(Operator operator, List<COSBase> arguments)
+    public void process(Operator operator, List<COSBase> arguments) throws IOException
     {
+        if (arguments.size() < 1)
+        {
+            throw new MissingOperandException(operator, arguments);
+        }
         COSBase base = arguments.get(0);
         if (!(base instanceof COSNumber))
         {
