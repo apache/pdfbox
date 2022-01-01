564503c75702e29fc07cea1bce037bcf223b090c
Tilman Hausherr <tilman@apache.org>
2022-01-01 11:37:23 +0000

    PDFBOX-4892: fix typos, as suggested by valerybokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1896594 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fixup/processor/AcroFormOrphanWidgetsProcessor.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fixup/processor/AcroFormOrphanWidgetsProcessor.java
@@ -97,7 +97,7 @@ public class AcroFormOrphanWidgetsProcessor extends AbstractProcessor
 
         acroForm.setFields(fields);
 
-        // ensure that PDVariableText fields have the neccesary resources
+        // ensure that PDVariableText fields have the necessary resources
         PDResources resources = acroForm.getDefaultResources();
         for (PDField field : acroForm.getFieldTree())
         {
@@ -158,7 +158,7 @@ public class AcroFormOrphanWidgetsProcessor extends AbstractProcessor
                         if (acroFormResources.getFont(fontName) == null)
                         {
                             acroFormResources.put(fontName, widgetResources.getFont(fontName));
-                            LOG.debug("qdded font resource to AcroForm from widget for font name " + fontName.getName());
+                            LOG.debug("added font resource to AcroForm from widget for font name " + fontName.getName());
                         }
                     }
                     catch (IOException ioe)
