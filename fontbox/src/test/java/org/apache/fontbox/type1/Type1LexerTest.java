5aa11f109ccd55cbc158bd416885f5ef2fc8523d
Tilman Hausherr <tilman@apache.org>
2022-01-05 03:25:58 +0000

    PDFBOX-4892: increase test coverage
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1896690 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/test/java/org/apache/fontbox/type1/Type1LexerTest.java
+++ fontbox/src/test/java/org/apache/fontbox/type1/Type1LexerTest.java
@@ -43,7 +43,7 @@ class Type1LexerTest
     @Test
     void testRealNumbers() throws IOException
     {
-        String s = "/FontMatrix [1e-3 0e-3 0e-3 1E-3 0 0] readonly def";
+        String s = "/FontMatrix [1e-3 0e-3 0e-3 1E-3 0 0 1.23 -1.23 ] readonly def";
         Type1Lexer t1l = new Type1Lexer(s.getBytes(StandardCharsets.US_ASCII));
         Token nextToken;
         List<Token> tokens = new ArrayList<>();
@@ -65,15 +65,19 @@ class Type1LexerTest
         Assertions.assertEquals(Token.REAL, tokens.get(5).getKind());
         Assertions.assertEquals(Token.INTEGER, tokens.get(6).getKind());
         Assertions.assertEquals(Token.INTEGER, tokens.get(7).getKind());
+        Assertions.assertEquals(Token.REAL, tokens.get(8).getKind());
+        Assertions.assertEquals(Token.REAL, tokens.get(9).getKind());
         Assertions.assertEquals("1e-3", tokens.get(2).getText());
         Assertions.assertEquals("0e-3", tokens.get(3).getText());
         Assertions.assertEquals("0e-3", tokens.get(4).getText());
         Assertions.assertEquals("1E-3", tokens.get(5).getText());
         Assertions.assertEquals("0", tokens.get(6).getText());
         Assertions.assertEquals("0", tokens.get(7).getText());
-        Assertions.assertEquals(Token.END_ARRAY, tokens.get(8).getKind());
-        Assertions.assertEquals(Token.NAME, tokens.get(9).getKind());
-        Assertions.assertEquals(Token.NAME, tokens.get(10).getKind());
+        Assertions.assertEquals("1.23", tokens.get(8).getText());
+        Assertions.assertEquals("-1.23", tokens.get(9).getText());
+        Assertions.assertEquals(Token.END_ARRAY, tokens.get(10).getKind());
+        Assertions.assertEquals(Token.NAME, tokens.get(11).getKind());
+        Assertions.assertEquals(Token.NAME, tokens.get(12).getKind());
     }
 
     @Test
