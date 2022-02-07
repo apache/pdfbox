52ac979b2f236688cbe19f1dbde5b8ebe5e51f3e
Tilman Hausherr <tilman@apache.org>
2022-02-07 18:15:43 +0000

    PDFBOX-4892: remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1897822 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/AppearanceGeneratorHelper.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/AppearanceGeneratorHelper.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.fontbox.util.BoundingBox;
