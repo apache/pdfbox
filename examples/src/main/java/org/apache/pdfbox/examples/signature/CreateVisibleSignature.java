5b00807463279f1002e24531aa0f07c12ee03b0d
Tilman Hausherr <tilman@apache.org>
2022-03-20 12:08:45 +0000

    PDFBOX-5382: revert, but keep a comment for now
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1899086 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/CreateVisibleSignature.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/CreateVisibleSignature.java
@@ -219,8 +219,10 @@ public class CreateVisibleSignature extends CreateSignatureBase
             doc = Loader.loadPDF(inputFile, memoryUsageSetting);
         }
 
-        SigUtils.checkCrossReferenceTable(doc);
-        
+        // call SigUtils.checkCrossReferenceTable(doc) if Adobe complains
+        // and read https://stackoverflow.com/a/71293901/535646
+        // and https://issues.apache.org/jira/browse/PDFBOX-5382
+
         try (FileOutputStream fos = new FileOutputStream(signedFile))
         {
             int accessPermissions = SigUtils.getMDPPermission(doc);
--- examples/src/main/java/org/apache/pdfbox/examples/signature/CreateVisibleSignature2.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/CreateVisibleSignature2.java
@@ -167,7 +167,9 @@ public class CreateVisibleSignature2 extends CreateSignatureBase
         try (FileOutputStream fos = new FileOutputStream(signedFile);
                 PDDocument doc = Loader.loadPDF(inputFile))
         {
-            SigUtils.checkCrossReferenceTable(doc);
+            // call SigUtils.checkCrossReferenceTable(doc) if Adobe complains
+            // and read https://stackoverflow.com/a/71293901/535646
+            // and https://issues.apache.org/jira/browse/PDFBOX-5382
 
             int accessPermissions = SigUtils.getMDPPermission(doc);
             if (accessPermissions == 1)
