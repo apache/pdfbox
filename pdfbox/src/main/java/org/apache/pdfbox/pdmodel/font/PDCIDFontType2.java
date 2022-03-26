74ab826a4559124373c6809792a70e1de155bdfd
Tilman Hausherr <tilman@apache.org>
2022-03-26 04:18:24 +0000

    PDFBOX-5402: avoid NPE
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1899202 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDCIDFontType2.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDCIDFontType2.java
@@ -356,10 +356,13 @@ public class PDCIDFontType2 extends PDCIDFont
             if (cid == -1)
             {
                 CMap toUnicodeCMap = parent.getToUnicodeCMap();
-                byte[] codes = toUnicodeCMap.getCodesFromUnicode(Character.toString((char) unicode));
-                if (codes != null)
+                if (toUnicodeCMap != null)
                 {
-                    return codes;
+                    byte[] codes = toUnicodeCMap.getCodesFromUnicode(Character.toString((char) unicode));
+                    if (codes != null)
+                    {
+                        return codes;
+                    }
                 }
                 cid = 0;
             }
