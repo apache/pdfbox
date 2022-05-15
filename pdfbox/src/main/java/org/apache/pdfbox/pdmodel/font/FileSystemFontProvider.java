ffb877b3d915a0b1e1e71457399d22c274c58a3c
Tilman Hausherr <tilman@apache.org>
2022-05-15 12:05:57 +0000

    PDFBOX-4892: optimize, as suggested by Valery Bokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1900923 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/FileSystemFontProvider.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/FileSystemFontProvider.java
@@ -401,15 +401,20 @@ final class FileSystemFontProvider extends FontProvider
     private File getDiskCacheFile()
     {
         String path = System.getProperty("pdfbox.fontcache");
-        if (path == null || !new File(path).isDirectory() || !new File(path).canWrite())
+        if (isBadPath(path))
         {
             path = System.getProperty("user.home");
-            if (path == null || !new File(path).isDirectory() || !new File(path).canWrite())
+            if (isBadPath(path))
             {
                 path = System.getProperty("java.io.tmpdir");
             }
         }
         return new File(path, ".pdfbox.cache");
+    }
+
+    private static boolean isBadPath(String path)
+    {
+        return path == null || !new File(path).isDirectory() || !new File(path).canWrite();
     }
 
     /**
