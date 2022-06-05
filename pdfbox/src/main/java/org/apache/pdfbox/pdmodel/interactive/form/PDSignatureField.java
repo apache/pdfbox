cd750e6a98ce887f645b8a09f198312ee58e51e2
Tilman Hausherr <tilman@apache.org>
2022-06-05 13:36:59 +0000

    PDFBOX-4892: optimize / refactor, as suggested by Valery Bokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1901671 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDSignatureField.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDSignatureField.java
@@ -47,8 +47,9 @@ public class PDSignatureField extends PDTerminalField
     {
         super(acroForm);
         getCOSObject().setItem(COSName.FT, COSName.SIG);
-        getWidgets().get(0).setLocked(true);
-        getWidgets().get(0).setPrinted(true);
+        PDAnnotationWidget firstWidget = getWidgets().get(0);
+        firstWidget.setLocked(true);
+        firstWidget.setPrinted(true);
         setPartialName(generatePartialName());
     }
     
