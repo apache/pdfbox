d88c09c1202dfc76e6d615ca494a0670c0587bed
Andreas Lehmkühler <lehmi@apache.org>
2022-07-24 12:18:07 +0000

    PDFBOX-4892: sonar fix
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1902985 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDPanose.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDPanose.java
@@ -47,7 +47,7 @@ public class PDPanose
      */
     public int getFamilyClass()
     {
-        return bytes[0] << 8 | bytes[1]; 
+        return bytes[0] << 8 | (bytes[1] & 0xff);
     }
 
     /**
