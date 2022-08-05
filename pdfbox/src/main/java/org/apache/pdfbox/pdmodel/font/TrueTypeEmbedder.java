936814d9d0b0ad5c7857671efd943cbbd93e4eb7
valery.bokov <67366451+valerybokov@users.noreply.github.com>
2022-08-05 18:45:08 +0300

    introduce 2 variables for TrueTypeEmbedder.createFontDescriptor

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/TrueTypeEmbedder.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/TrueTypeEmbedder.java
@@ -183,22 +183,23 @@ abstract class TrueTypeEmbedder implements Subsetter
     private PDFontDescriptor createFontDescriptor(TrueTypeFont ttf) throws IOException
     {
         OS2WindowsMetricsTable os2 = ttf.getOS2Windows();
+        String ttfName = ttf.getName();
         if (os2 == null)
         {
-            throw new IOException("os2 table is missing in font " + ttf.getName());
+            throw new IOException("os2 table is missing in font " + ttfName);
         }
         PostScriptTable post = ttf.getPostScript();
         if (post == null)
         {
-            throw new IOException("post table is missing in font " + ttf.getName());            
+            throw new IOException("post table is missing in font " + ttfName);
         }
 
         PDFontDescriptor fd = new PDFontDescriptor();
-        fd.setFontName(ttf.getName());
+        fd.setFontName(ttfName);
 
+        HorizontalHeaderTable hHeader = ttf.getHorizontalHeader();
         // Flags
-        fd.setFixedPitch(post.getIsFixedPitch() > 0 ||
-                         ttf.getHorizontalHeader().getNumberOfHMetrics() == 1);
+        fd.setFixedPitch(post.getIsFixedPitch() > 0 || hHeader.getNumberOfHMetrics() == 1);
 
         int fsSelection = os2.getFsSelection();
         fd.setItalic(((fsSelection & (ITALIC | OBLIQUE)) != 0));
@@ -238,7 +239,6 @@ abstract class TrueTypeEmbedder implements Subsetter
         fd.setFontBoundingBox(rect);
 
         // Ascent, Descent
-        HorizontalHeaderTable hHeader = ttf.getHorizontalHeader();
         fd.setAscent(hHeader.getAscender() * scaling);
         fd.setDescent(hHeader.getDescender() * scaling);
 
