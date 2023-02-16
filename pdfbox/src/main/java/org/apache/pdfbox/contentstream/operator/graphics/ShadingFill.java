f87219700e07e244a5d7f44912ac6f5729376b9e
Tilman Hausherr <tilman@apache.org>
2023-02-16 18:25:48 +0000

    PDFBOX-5566: avoid ClassCastException
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1907705 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/graphics/ShadingFill.java
+++ pdfbox/src/main/java/org/apache/pdfbox/contentstream/operator/graphics/ShadingFill.java
@@ -39,7 +39,12 @@ public final class ShadingFill extends GraphicsOperatorProcessor
         {
             throw new MissingOperandException(operator, operands);
         }
-        context.shadingFill((COSName) operands.get(0));
+        COSBase base = operands.get(0);
+        if (!(base instanceof COSName))
+        {
+            throw new MissingOperandException(operator, operands);
+        }
+        context.shadingFill((COSName) base);
     }
 
     @Override
