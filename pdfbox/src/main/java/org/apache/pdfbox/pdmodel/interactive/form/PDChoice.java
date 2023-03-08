59b6f6ca39b0b288dc38fef9cade4be8effad461
Tilman Hausherr <tilman@apache.org>
2023-03-08 13:29:19 +0000

    PDFBOX-4892: optimize, as suggested by valerybokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1908206 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDChoice.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDChoice.java
@@ -412,12 +412,13 @@ public abstract class PDChoice extends PDVariableText
             {
                 throw new IllegalArgumentException("The list box does not allow multiple selections.");
             }
-            if (!getOptions().containsAll(values))
+            List<String> options = getOptions();
+            if (!options.containsAll(values))
             {
                 throw new IllegalArgumentException("The values are not contained in the selectable options.");
             }
             getCOSObject().setItem(COSName.V, COSArrayList.convertStringListToCOSStringCOSArray(values));
-            updateSelectedOptionsIndex(values);
+            updateSelectedOptionsIndex(values, options);
         }
         else
         {
@@ -477,10 +478,9 @@ public abstract class PDChoice extends PDVariableText
     /**
      * Update the 'I' key based on values set.
      */
-    private void updateSelectedOptionsIndex(List<String> values)
+    private void updateSelectedOptionsIndex(List<String> values, List<String> options)
     {
-        List<String> options = getOptions();
-        List<Integer> indices = new ArrayList<Integer>();
+        List<Integer> indices = new ArrayList<Integer>(values.size());
 
         for (String value : values)
         {
