27570ad974fb49cd2d51b68cd185b0abbb401f98
Tilman Hausherr <tilman@apache.org>
2023-03-18 04:15:46 +0000

    PDFBOX-5572: fix some logging inconsistencies, by Axel Howind
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1908465 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/ttf/GlyphRenderer.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/GlyphRenderer.java
@@ -151,7 +151,7 @@ class GlyphRenderer
     private void moveTo(GeneralPath path, Point point)
     {
         path.moveTo(point.x, point.y);
-        if (LOG.isDebugEnabled())
+        if (LOG.isTraceEnabled())
         {
             LOG.trace("moveTo: " + String.format(Locale.US, "%d,%d", point.x, point.y));
         }
@@ -160,7 +160,7 @@ class GlyphRenderer
     private void lineTo(GeneralPath path, Point point)
     {
         path.lineTo(point.x, point.y);
-        if (LOG.isDebugEnabled())
+        if (LOG.isTraceEnabled())
         {
             LOG.trace("lineTo: " + String.format(Locale.US, "%d,%d", point.x, point.y));
         }
@@ -169,7 +169,7 @@ class GlyphRenderer
     private void quadTo(GeneralPath path, Point ctrlPoint, Point point)
     {
         path.quadTo(ctrlPoint.x, ctrlPoint.y, point.x, point.y);
-        if (LOG.isDebugEnabled())
+        if (LOG.isTraceEnabled())
         {
             LOG.trace("quadTo: " + String.format(Locale.US, "%d,%d %d,%d", ctrlPoint.x, ctrlPoint.y,
                     point.x, point.y));
