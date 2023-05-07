5ad85ebbf6c26332456463f4c03b998047b71aca
Tilman Hausherr <tilman@apache.org>
2023-05-07 16:06:45 +0000

    PDFBOX-5596: fix test
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1909665 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/test/java/org/apache/pdfbox/examples/rendering/CustomPageDrawerTest.java
+++ examples/src/test/java/org/apache/pdfbox/examples/rendering/CustomPageDrawerTest.java
@@ -34,7 +34,7 @@ import org.junit.Test;
 public class CustomPageDrawerTest
 {
     @Test
-    public void testCreateGradientShading() throws IOException
+    public void testCustomPageDrawer() throws IOException
     {
         CustomPageDrawer.main(new String[]{});
         BufferedImage bim = ImageIO.read(new File("target","custom-render.png"));
