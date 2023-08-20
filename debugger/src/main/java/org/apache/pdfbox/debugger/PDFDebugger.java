45879998db44c363e87e26c9bb22b59d93811a59
Tilman Hausherr <tilman@apache.org>
2023-08-20 12:05:32 +0000

    PDFBOX-5663: add about dialog inspired from tika
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1911804 13f79535-47bb-0310-9956-ffa450edef68

--- debugger/src/main/java/org/apache/pdfbox/debugger/PDFDebugger.java
+++ debugger/src/main/java/org/apache/pdfbox/debugger/PDFDebugger.java
@@ -17,6 +17,7 @@
 package org.apache.pdfbox.debugger;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.Dimension;
@@ -27,11 +28,13 @@ import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -40,6 +43,8 @@ import java.io.PrintStream;
 import java.lang.reflect.Method;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -55,7 +60,10 @@ import javax.print.attribute.PrintRequestAttributeSet;
 import javax.print.attribute.standard.Sides;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.Box;
 import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -69,6 +77,8 @@ import javax.swing.KeyStroke;
 import javax.swing.TransferHandler;
 import javax.swing.UIManager;
 import javax.swing.border.BevelBorder;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -153,7 +163,7 @@ import picocli.CommandLine.Model.CommandSpec;
  */
 @SuppressWarnings({ "serial", "squid:MaximumInheritanceDepth", "squid:S1948" })
 @Command(name = "pdfdebugger", description = "Analyzes and inspects the internal structure of a PDF document")
-public class PDFDebugger extends JFrame implements Callable<Integer>
+public class PDFDebugger extends JFrame implements Callable<Integer>, HyperlinkListener
 {
     private static Log LOG; // needs late initialization
 
@@ -437,6 +447,24 @@ public class PDFDebugger extends JFrame implements Callable<Integer>
         ViewMenu viewMenu = ViewMenu.getInstance(this);
         menuBar.add(viewMenu.getMenu());
         setJMenuBar(menuBar);
+
+        menuBar.add(Box.createHorizontalGlue());
+        JMenu help = new JMenu("Help");
+        help.setMnemonic(KeyEvent.VK_H);
+
+        JMenuItem item = new JMenuItem("About PDFBox", KeyEvent.VK_A);
+        item.setActionCommand("about");
+        item.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent ae)
+            {
+                textDialog("About Apache PDFBox", PDFDebugger.class.getResource("about.html"));
+            }
+        });
+        help.add(item);
+
+        menuBar.add(help);
 
         setExtendedState(windowPrefs.getExtendedState());
         setBounds(windowPrefs.getBounds());
@@ -1592,5 +1620,61 @@ public class PDFDebugger extends JFrame implements Callable<Integer>
             }
         }
         return null;
+    }
+
+    private void textDialog(String title, URL resource)
+    {
+        try
+        {
+            JDialog dialog = new JDialog(this, title);
+            JEditorPane editor = new JEditorPane(resource);
+            editor.setContentType("text/html");
+            editor.setEditable(false);
+            editor.setBackground(Color.WHITE);
+            editor.setPreferredSize(new Dimension(400, 250));
+            editor.addHyperlinkListener(this);
+            dialog.add(editor);
+            dialog.pack();
+            dialog.setVisible(true);
+        }
+        catch (IOException ex)
+        {
+            new ErrorDialog(ex).setVisible(true);
+        }
+    }
+    
+    @Override
+    public void hyperlinkUpdate(HyperlinkEvent he)
+    {
+        if (he.getEventType() == HyperlinkEvent.EventType.ACTIVATED)
+        {
+            try
+            {
+                URL url = he.getURL();
+                try (InputStream stream = url.openStream())
+                {
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    IOUtils.copy(stream, baos);
+                    JEditorPane editor
+                            = new JEditorPane("text/plain", baos.toString(StandardCharsets.UTF_8));
+                    editor.setEditable(false);
+                    editor.setBackground(Color.WHITE);
+                    editor.setCaretPosition(0);
+                    editor.setPreferredSize(new Dimension(600, 400));
+
+                    String name = url.toString();
+                    name = name.substring(name.lastIndexOf('/') + 1);
+
+                    JDialog dialog = new JDialog(this, "Apache PDFBox: " + name);
+                    dialog.add(new JScrollPane(editor));
+                    dialog.pack();
+                    dialog.setVisible(true);
+                }
+            }
+            catch (IOException ex)
+            {
+                new ErrorDialog(ex).setVisible(true);
+            }
+        }
     }
 }
