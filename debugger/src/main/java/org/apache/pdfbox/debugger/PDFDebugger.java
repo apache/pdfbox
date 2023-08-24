73e1a1a2a44193e232867bbc9ee4dbec9965adfe
Tilman Hausherr <tilman@apache.org>
2023-08-24 03:49:18 +0000

    PDFBOX-5660: remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1911884 13f79535-47bb-0310-9956-ffa450edef68

--- debugger/src/main/java/org/apache/pdfbox/debugger/PDFDebugger.java
+++ debugger/src/main/java/org/apache/pdfbox/debugger/PDFDebugger.java
@@ -29,7 +29,6 @@ import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
