0699c25dbd8a698184f06e020c7593f537132086
Andreas Lehmkühler <lehmi@apache.org>
2023-12-03 17:45:27 +0000

    PDFBOX-5723: remove already cached hashCode a proposed by Axel Howind
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1914306 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/cos/COSName.java
+++ pdfbox/src/main/java/org/apache/pdfbox/cos/COSName.java
@@ -622,7 +622,6 @@ public final class COSName extends COSBase implements Comparable<COSName>
 
     // fields
     private final String name;
-    private final int hashCode;
 
     /**
      * This will get a COSName object with that name.
@@ -662,7 +661,6 @@ public final class COSName extends COSBase implements Comparable<COSName>
     private COSName(String aName, boolean staticValue)
     {
         name = aName;
-        hashCode = name.hashCode();
         if (staticValue)
         {
             commonNameMap.put(aName, this);
@@ -708,7 +706,7 @@ public final class COSName extends COSBase implements Comparable<COSName>
     @Override
     public int hashCode()
     {
-        return hashCode;
+        return name.hashCode();
     }
 
     @Override
