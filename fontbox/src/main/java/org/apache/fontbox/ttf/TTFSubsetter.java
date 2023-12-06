c319cfb5d9c2af215d13fbe077cdaa279c07a991
Tilman Hausherr <tilman@apache.org>
2023-12-06 15:56:37 +0000

    PDFBOX-5728: check whether there are any glyph names in the POST table
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1914390 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/ttf/TTFSubsetter.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/TTFSubsetter.java
@@ -832,7 +832,8 @@ public final class TTFSubsetter
     private byte[] buildPostTable() throws IOException
     {
         PostScriptTable post = ttf.getPostScript();
-        if (post == null || keepTables != null && !keepTables.contains(PostScriptTable.TAG))
+        if (post == null || post.getGlyphNames() == null || 
+                keepTables != null && !keepTables.contains(PostScriptTable.TAG))
         {
             return null;
         }
