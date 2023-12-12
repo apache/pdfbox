a415f497f79aeb470632851403df5d7057af0803
Tilman Hausherr <tilman@apache.org>
2023-12-12 10:07:28 +0000

    PDFBOX-5728: add test
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1914555 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/pdmodel/EmbeddedFonts.java
+++ examples/src/main/java/org/apache/pdfbox/examples/pdmodel/EmbeddedFonts.java
@@ -18,12 +18,21 @@
 package org.apache.pdfbox.examples.pdmodel;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import static org.apache.pdfbox.pdmodel.font.Standard14Fonts.FontName.HELVETICA;
+import static org.apache.pdfbox.pdmodel.font.Standard14Fonts.FontName.SYMBOL;
+import static org.apache.pdfbox.pdmodel.font.Standard14Fonts.FontName.ZAPF_DINGBATS;
+import org.apache.pdfbox.pdmodel.graphics.state.RenderingMode;
 
 /**
  * An example of using an embedded TrueType font with Unicode text.
@@ -33,6 +42,8 @@ import org.apache.pdfbox.pdmodel.font.PDType0Font;
  */
 public final class EmbeddedFonts
 {
+    private static final Log LOG = LogFactory.getLog(EmbeddedFonts.class);
+
 
     private EmbeddedFonts()
     {
@@ -55,8 +66,9 @@ public final class EmbeddedFonts
                 stream.setLeading(12 * 1.2f);
 
                 stream.newLineAtOffset(50, 600);
-                stream.showText("PDFBox's Unicode with Embedded TrueType Font");
+                stream.showText("PDFBox's Unicode with Embedded TrueType Font \u00AD - affection affiliate film float");
                 stream.newLine();
+                System.out.println("liberation sans done");
                 
                 stream.showText("Supports full Unicode text ☺");
                 stream.newLine();
@@ -65,12 +77,78 @@ public final class EmbeddedFonts
                 stream.newLine();
                 
                 // ligature
-                stream.showText("Ligatures: \uFB01lm \uFB02ood");
+                stream.showText("Ligatures: \uFB01lm \uFB02ood / generated: effective, affiliation, float, film, affluent");
+                stream.newLine();
                 
+//                PDType0Font font3 = PDType0Font.load(document, new File("c:/windows/fonts/arabtype.ttf"));
+//                stream.setFont(font3, 12);
+//                stream.showText("نديم");
+//                stream.newLine();
+//                stream.showText(new StringBuilder("نديم").reverse().toString());
+//                stream.newLine();
+//                PDType0Font font2 = PDType0Font.load(document, new File("c:/windows/fonts/simhei.ttf"));
+//                stream.setFont(font2, 12);
+//                stream.showText("中国你好! simhei.ttf");
+                
+//            PDDocument doc2 = new PDDocument();
+//            PDPage page2 = new PDPage();
+//            PDPageContentStream cs2 = new PDPageContentStream(doc2, page2);
+//            cs2.setFont(font2, 1);
+//            cs2.close();
+//            doc2.save(new ByteArrayOutputStream());
+                
+                PDType0Font font4 = PDType0Font.load(document, new FileInputStream(new File("c:/windows/fonts/arialuni.ttf")));
+                stream.setFont(font4, 12);
+                stream.newLine();
+                stream.showText("α \uFF0C \u4E8B The quick brown fox jumps over the lazy dog äüöÄÜÖß 电信 arialuni.ttf effective affluent");
+                
+                stream.setFont(new PDType1Font(HELVETICA), 12);
+                stream.newLine();
+                stream.showText("\u2022 The\u00A0quick brown fox jumps over the lazy dog äüöÄÜÖß Helvetica type1");
+                
+                stream.setFont(new PDType1Font(ZAPF_DINGBATS), 12);
+                stream.newLine();
+                stream.showText("\u2714\u27a2    \u273F\u271D\u0020\u275E");
+                
+                PDFont font6 = new PDType1Font(SYMBOL);
+                stream.setFont(font6, 12);
+                stream.newLine();
+                stream.setRenderingMode(RenderingMode.FILL_STROKE);
+                stream.showText("\u2206ααα\u21D1\u21B5");
+                stream.setRenderingMode(RenderingMode.FILL);
+                
+                PDFont font7 = PDType0Font.load(document, new File("c:/windows/fonts/webdings.ttf"));
+                stream.setFont(font7, 12);
+                stream.newLine();
+                stream.showText("\uf061");
+                
+                PDFont font8 = PDType0Font.load(document, new File("c:/windows/fonts/arial.ttf"));
+                //PDFont font8 = PDType0Font.load(document, new File("c:/windows/fonts/NotoMono-Regular.ttf"));
+                //PDFont font8 = PDType0Font.load(document, new File("c:/users/tilman/downloads/dejavusans.ttf"));
+                stream.setFont(font8, 12);
+                stream.newLine();
+                stream.showText("font8 Rupee ₹ \u062C  effective affluent");
+                
+                //TrueTypeFont ligttf = new TTFParser().parse(new RandomAccessReadBufferedFile("c:/windows/fonts/calibri.ttf"));
+                stream.newLine();
+                //PDFont ligFont = PDType0Font.load(document, ligttf, true);
+                //PDFont ligFont = PDType0Font.load(document, new File("c:/windows/fonts/calibri.ttf"));
+                PDFont ligFont = PDType0Font.load(document, new File("c:/users/tilman/downloads/dejavusans.ttf"));
+                stream.setFont(ligFont, 12);
+                stream.showText("Generated DejaVu ligatures: effective, affiliation, float, film, affluent");
+
+                LOG.info("*** FIRA code test ***");
+                stream.newLine();
+                PDFont firaFont = PDType0Font.load(document, new File("c:/users/tilman/downloads/FiraCode-Regular.ttf"));
+                stream.setFont(firaFont, 12);
+                stream.showText("fira code = == => <= >= ==> === ");
+                LOG.info("*** FIRA code test end ***");
+                
+
                 stream.endText();
             }
             
-            document.save("example.pdf");
+            document.save("C:\\Users\\Tilman\\Downloads\\example.pdf");
         }
     }
 }
