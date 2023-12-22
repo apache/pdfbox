70ef7d3d80b23432dd4b8c3ccd3164ab9c3d1066
Tilman Hausherr <tilman@apache.org>
2023-12-22 13:15:17 +0000

    PDFBOX-5730: seek to the subTableOffsets
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1914852 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/ttf/GlyphSubstitutionTable.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/GlyphSubstitutionTable.java
@@ -320,30 +320,27 @@ public class GlyphSubstitutionTable extends TTFTable
             // https://learn.microsoft.com/en-us/typography/opentype/spec/gsub#ES
             for (int i = 0; i < subTableCount; i++)
             {
-                long baseOffset = data.getCurrentPosition();
+                data.seek(offset + subTableOffsets[i]);
                 int substFormat = data.readUnsignedShort(); // always 1
+                if (substFormat != 1)
+                {
+                    LOG.error("The expected SubstFormat for ExtensionSubstFormat1 subtable is " +
+                            substFormat + " but should be 1 at offset " + (offset + subTableOffsets[i]));
+                    continue;
+                }
                 int extensionLookupType = data.readUnsignedShort();
                 if (lookupType != 7 && lookupType != extensionLookupType)
                 {
                     // "If a lookup table uses extension subtables, then all of the extension
                     //  subtables must have the same extensionLookupType"
                     LOG.error("extensionLookupType changed from " +
-                            lookupType + " to " + extensionLookupType);
-                    data.seek(baseOffset + 8);
+                            lookupType + " to " + extensionLookupType + " at offset " + (offset + subTableOffsets[i] + 2));
                     continue;
                 }
                 lookupType = extensionLookupType;
                 long extensionOffset = data.readUnsignedInt();
-                if (substFormat != 1)
-                {
-                    LOG.error("The expected SubstFormat for ExtensionSubstFormat1 subtable is " +
-                            substFormat + " but should be 1");
-                }
-                else
-                {
-                    subTables[i] = readLookupSubtable(data, baseOffset + extensionOffset, extensionLookupType);
-                }
-                data.seek(baseOffset + 8);
+                long extensionLookupTableAddress = offset + subTableOffsets[i] + extensionOffset;
+                subTables[i] = readLookupSubtable(data, extensionLookupTableAddress, extensionLookupType);
             }
             break;
         default:
