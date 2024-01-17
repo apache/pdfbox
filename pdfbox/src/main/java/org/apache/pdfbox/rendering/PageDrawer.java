a24fb0b8edbb85273f303870adb45d81374fc624
Tilman Hausherr <tilman@apache.org>
2024-01-17 10:19:59 +0000

    PDFBOX-5754: cache hasBlendMode results
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1915284 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
@@ -174,6 +174,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
     private final RenderingHints renderingHints;
     private final float imageDownscalingOptimizationThreshold;
     private LookupTable invTable = null;
+    private final Map<COSBase,Boolean> blendModeMap = new HashMap<>();
 
     /**
     * Default annotations filter, returns all annotations
@@ -1965,14 +1966,21 @@ public class PageDrawer extends PDFGraphicsStreamEngine
     {
         if (groupsDone.contains(group.getCOSObject()))
         {
-            // The group was already processed. Avoid endless recursion.
+            // The group is being processed. Avoid endless recursion.
             return false;
         }
         groupsDone.add(group.getCOSObject());
 
+        Boolean val = blendModeMap.get(group.getCOSObject());
+        if (val != null)
+        {
+            return val;
+        }
+
         PDResources resources = group.getResources();
         if (resources == null)
         {
+            blendModeMap.put(group.getCOSObject(), false);
             return false;
         }
         for (COSName name : resources.getExtGStateNames())
@@ -1985,6 +1993,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
             BlendMode blendMode = extGState.getBlendMode();
             if (blendMode != BlendMode.NORMAL)
             {
+                blendModeMap.put(group.getCOSObject(), true);
                 return true;
             }
         }
@@ -2004,10 +2013,12 @@ public class PageDrawer extends PDFGraphicsStreamEngine
             if (xObject instanceof PDTransparencyGroup &&
                 hasBlendMode((PDTransparencyGroup)xObject, groupsDone))
             {
+                blendModeMap.put(group.getCOSObject(), true);
                 return true;
             }
         }
 
+        blendModeMap.put(group.getCOSObject(), false);
         return false;
     }
 
