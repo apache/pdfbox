9e50cd26de3d5c3e93e3fda4157adb999912bbeb
Andreas Lehmkühler <lehmi@apache.org>
2024-03-02 16:44:51 +0000

    PDFBOX-5752: eliminate overlapping object keys after importing some external content
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1916079 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdfwriter/compress/COSWriterCompressionPool.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfwriter/compress/COSWriterCompressionPool.java
@@ -127,6 +127,12 @@ public class COSWriterCompressionPool
             {
                 return current;
             }
+            // check if the key of the indirect object matches the key of the referenced object
+            // otherwise update the key
+            if (!actualKey.equals(key) && base instanceof COSObject)
+            {
+                base.setKey(actualKey);
+            }
             topLevelObjects.add(actualKey);
             return current;
         }
@@ -137,7 +143,12 @@ public class COSWriterCompressionPool
         {
             return current;
         }
-
+        // check if the key of the indirect object matches the key of the referenced object
+        // otherwise update the key
+        if (!actualKey.equals(key) && base instanceof COSObject)
+        {
+            base.setKey(actualKey);
+        }
         // Append it to an object stream.
         this.objectStreamObjects.add(actualKey);
         return current;
@@ -194,7 +205,13 @@ public class COSWriterCompressionPool
                 COSObject cosObject = (COSObject) value;
                 if (cosObject.getKey() != null && objectPool.contains(cosObject.getKey()))
                 {
-                    continue;
+                    // check if the stored object matches the referenced object otherwise replace the key with a new one
+                    // there may differences if some imported content uses the same object numbers than the target pdf
+                    if (objectPool.getObject(cosObject.getKey()).equals(cosObject.getObject()))
+                    {
+                        continue;
+                    }
+                    cosObject.setKey(null);
                 }
                 if (cosObject.getObject() != null)
                 {
