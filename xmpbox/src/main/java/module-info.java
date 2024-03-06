96f00ad8e3dad7884bc0aab38f34eb55bad8a8f3
Alessandro Autiero <alautiero@gmail.com>
2024-03-06 21:09:50 +0100

    Added explicit module-info.java for all submodules that previously provided an automatic module name Removed automatic module name from all pom.xml that defined one

---
+++ xmpbox/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.apache.xmpbox {
+    requires java.xml;
+
+    exports org.apache.xmpbox;
+    exports org.apache.xmpbox.schema;
+    exports org.apache.xmpbox.type;
+    exports org.apache.xmpbox.xml;
+}
