66ca8fdadfcfce0e6fe5ac4b83d080a423eeaa96
Tilman Hausherr <tilman@apache.org>
2024-03-30 12:25:20 +0000

    PDFBOX-5792: use a map instead of a set to make sure that the source page (that contains the link to a destination page) is in the current split target document
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1916652 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/multipdf/Splitter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/multipdf/Splitter.java
@@ -79,7 +79,7 @@ public class Splitter
     private Map<COSDictionary, COSDictionary> pageDictMap;
     private Map<COSDictionary, COSDictionary> structDictMap;
     private Map<COSDictionary, COSDictionary> annotDictMap;
-    private Set<PDPageDestination> destToFixSet;
+    private Map<PDPageDestination,PDPage> destToFixMap;
     private Set<String> idSet;
     private Set<COSName> roleSet;
 
@@ -124,7 +124,7 @@ public class Splitter
         destinationDocuments = new ArrayList<>();
         sourceDocument = document;
         pageDictMap = new HashMap<>();
-        destToFixSet = new HashSet<>();
+        destToFixMap = new HashMap<>();
         annotDictMap = new HashMap<>();
         idSet = new HashSet<>();
         roleSet = new HashSet<>();
@@ -141,25 +141,27 @@ public class Splitter
     }
     
     /**
-     * Replace the page destinations, if the destination page is in the target document. This must
-     * be called after all pages (and its annotations) are processed.
+     * Replace the page destinations, if the source and destination pages are in the target
+     * document. This must be called after all pages (and its annotations) are processed.
      *
      * @param destinationDocument
      */
     private void fixDestinations(PDDocument destinationDocument)
     {
         PDPageTree pageTree = destinationDocument.getPages();
-        for (PDPageDestination pageDestination : destToFixSet)
+        for (Map.Entry<PDPageDestination,PDPage> entry : destToFixMap.entrySet())
         {
-            if (pageDestination.getPage() == null)
+            PDPageDestination pageDestination = entry.getKey();
+            // Find whether source page is inside or outside
+            PDPage srcPage = entry.getValue();
+            if (pageTree.indexOf(srcPage) < 0)
             {
-                //TODO avoid NPE until proper solution, see PDFBOX-5792
                 continue;
             }
             COSDictionary srcPageDict = pageDestination.getPage().getCOSObject();
             COSDictionary dstPageDict = pageDictMap.get(srcPageDict);
             PDPage dstPage = new PDPage(dstPageDict);
-            // Find whether destination is inside or outside
+            // Find whether destination page is inside or outside
             if (pageTree.indexOf(dstPage) >= 0)
             {
                 pageDestination.setPage(dstPage);
@@ -751,7 +753,7 @@ public class Splitter
                                 (PDPageDestination) PDDestination.create(clonedDestinationArray);
 
                         // remember the destination to adjust / remove page later
-                        destToFixSet.add(dstDestination);
+                        destToFixMap.put(dstDestination, imported);
 
                         if (action != null)
                         {
