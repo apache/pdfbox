01082a743ef02a70a90c7106d53378e4fa51aa7a
Andreas Lehmkühler <lehmi@apache.org>
2024-04-14 10:44:23 +0000

    PDFBOX-5660: bugfix rendering
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1916982 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/cff/Type1CharString.java
+++ fontbox/src/main/java/org/apache/fontbox/cff/Type1CharString.java
@@ -322,6 +322,7 @@ public class Type1CharString
             // indicates a PDFBox bug
             throw new IllegalArgumentException("Unhandled command: " + type1KeyWord);
         }
+        numbers.clear();
     }
 
     /**
