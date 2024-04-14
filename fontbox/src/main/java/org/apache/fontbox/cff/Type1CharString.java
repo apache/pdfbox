2e94a963e535e72444fed46ac48108337dc4b691
Andreas Lehmkühler <lehmi@apache.org>
2024-04-14 09:20:30 +0000

    PDFBOX-5660: optimize, as suggested by cinstool; closes #188
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1916977 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/cff/Type1CharString.java
+++ fontbox/src/main/java/org/apache/fontbox/cff/Type1CharString.java
@@ -22,7 +22,6 @@ import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.apache.logging.log4j.Logger;
@@ -150,9 +149,7 @@ public class Type1CharString
         type1Sequence.forEach(obj -> {
             if (obj instanceof CharStringCommand)
             {
-                List<Number> results = handleType1Command(numbers, (CharStringCommand) obj);
-                numbers.clear();
-                numbers.addAll(results);
+                handleType1Command(numbers, (CharStringCommand) obj);
             }
             else
             {
@@ -161,7 +158,7 @@ public class Type1CharString
         });
     }
 
-    private List<Number> handleType1Command(List<Number> numbers, CharStringCommand command)
+    private void handleType1Command(List<Number> numbers, CharStringCommand command)
     {
         commandCount++;
         Type1KeyWord type1KeyWord = command.getType1KeyWord();
@@ -169,7 +166,8 @@ public class Type1CharString
         {
             // indicates an invalid charstring
             LOG.warn("Unknown charstring command in glyph {} of font {}", glyphName, fontName);
-            return Collections.emptyList();
+            numbers.clear();
+            return;
         }
         switch(type1KeyWord)
         {
@@ -298,11 +296,10 @@ public class Type1CharString
 
                 float result = a / b;
 
-                List<Number> list = new ArrayList<>(numbers);
-                list.remove(list.size() - 1);
-                list.remove(list.size() - 1);
-                list.add(result);
-                return list;
+                numbers.remove(numbers.size() - 1);
+                numbers.remove(numbers.size() - 1);
+                numbers.add(result);
+                return;
             }
             break;
         case HSTEM:
@@ -325,7 +322,6 @@ public class Type1CharString
             // indicates a PDFBox bug
             throw new IllegalArgumentException("Unhandled command: " + type1KeyWord);
         }
-        return Collections.emptyList();
     }
 
     /**
