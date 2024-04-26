68e30547419e45023f5a4f41c9728e2bb3d289b9
Tilman Hausherr <tilman@apache.org>
2024-04-26 11:40:00 +0000

    PDFBOX-5808: add string util instead of CompoundCharacterTokenizer to split on space, as suggested by Fabrice Calafat
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1917359 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDAbstractContentStream.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDAbstractContentStream.java
@@ -37,7 +37,6 @@ import java.util.Set;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.fontbox.ttf.CmapLookup;
-import org.apache.fontbox.ttf.gsub.CompoundCharacterTokenizer;
 import org.apache.fontbox.ttf.gsub.GsubWorker;
 import org.apache.fontbox.ttf.gsub.GsubWorkerFactory;
 import org.apache.fontbox.ttf.model.GsubData;
@@ -1616,7 +1615,7 @@ abstract class PDAbstractContentStream implements Closeable
                                  Set<Integer> glyphIds, PDType0Font font, String text) throws IOException
     {
         // break the entire chunk of text into words by splitting it with space
-        List<String> words = new CompoundCharacterTokenizer(StringUtil.PATTERN_SPACE).tokenize(text);
+        String[] words = StringUtil.tokenizeOnSpace(text);
 
         ByteArrayOutputStream out = new ByteArrayOutputStream();
 
