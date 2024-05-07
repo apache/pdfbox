368ffd61e14a54c1fe43aff979a97ae4b39e1b06
Tilman Hausherr <tilman@apache.org>
2024-05-07 12:38:47 +0000

    PDFBOX-5648: add test that uses the table type
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1917548 13f79535-47bb-0310-9956-ffa450edef68

---
+++ fontbox/src/test/java/org/apache/fontbox/ttf/gsub/GsubWorkerForSmcp.java
@@ -0,0 +1,113 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.fontbox.ttf.gsub;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.fontbox.ttf.CmapLookup;
+import org.apache.fontbox.ttf.model.GsubData;
+import org.apache.fontbox.ttf.model.ScriptFeature;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * 
+ * GSUB worker to test "smcp", code is copied from the latin worker except for the features.
+ * 
+ * @author Palash Ray
+ * @author Tilman Hausherr
+ *
+ */
+public class GsubWorkerForSmcp implements GsubWorker
+{
+    private static final Logger LOG = LogManager.getLogger(GsubWorkerForSmcp.class);
+
+    private static final List<String> FEATURES_IN_ORDER = Arrays.asList("smcp");
+
+    private final CmapLookup cmapLookup;
+    private final GsubData gsubData;
+
+    GsubWorkerForSmcp(CmapLookup cmapLookup, GsubData gsubData)
+    {
+        this.cmapLookup = cmapLookup;
+        this.gsubData = gsubData;
+    }
+
+    @Override
+    public List<Integer> applyTransforms(List<Integer> originalGlyphIds)
+    {
+        List<Integer> intermediateGlyphsFromGsub = originalGlyphIds;
+
+        for (String feature : FEATURES_IN_ORDER)
+        {
+            if (!gsubData.isFeatureSupported(feature))
+            {
+                LOG.debug("the feature {} was not found", feature);
+                continue;
+            }
+
+            LOG.debug("applying the feature {}", feature);
+
+            ScriptFeature scriptFeature = gsubData.getFeature(feature);
+
+            intermediateGlyphsFromGsub = applyGsubFeature(scriptFeature,
+                    intermediateGlyphsFromGsub);
+        }
+
+        return Collections.unmodifiableList(intermediateGlyphsFromGsub);
+    }
+
+    private List<Integer> applyGsubFeature(ScriptFeature scriptFeature,
+            List<Integer> originalGlyphs)
+    {
+        if (scriptFeature.getAllGlyphIdsForSubstitution().isEmpty())
+        {
+            LOG.debug("getAllGlyphIdsForSubstitution() for {} is empty",
+                        scriptFeature.getName());
+            return originalGlyphs;
+        }
+        
+        GlyphArraySplitter glyphArraySplitter = new GlyphArraySplitterRegexImpl(
+                scriptFeature.getAllGlyphIdsForSubstitution());
+
+        List<List<Integer>> tokens = glyphArraySplitter.split(originalGlyphs);
+        List<Integer> gsubProcessedGlyphs = new ArrayList<>();
+
+        for (List<Integer> chunk : tokens)
+        {
+            if (scriptFeature.canReplaceGlyphs(chunk))
+            {
+                // gsub system kicks in, you get the glyphId directly
+                List<Integer> replacementForGlyphs = scriptFeature.getReplacementForGlyphs(chunk);
+                gsubProcessedGlyphs.addAll(replacementForGlyphs);
+            }
+            else
+            {
+                gsubProcessedGlyphs.addAll(chunk);
+            }
+        }
+
+        LOG.debug("originalGlyphs: {}, gsubProcessedGlyphs: {}", originalGlyphs, gsubProcessedGlyphs);
+
+        return gsubProcessedGlyphs;
+    }
+}
---
+++ fontbox/src/test/java/org/apache/fontbox/ttf/gsub/GsubWorkerForSmcpTest.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.fontbox.ttf.gsub;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.fontbox.ttf.CmapLookup;
+import org.apache.fontbox.ttf.TTFParser;
+import org.apache.fontbox.ttf.TrueTypeFont;
+import org.apache.pdfbox.io.RandomAccessReadBufferedFile;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test the "smcp" type 2 tables of a font.
+ * 
+ * @author Tilman Hausherr
+ */
+class GsubWorkerForSmcpTest
+{
+    
+    @Test
+    void testCalibri() throws IOException
+    {
+        File file = new File("c:/windows/fonts/calibri.ttf");
+        Assumptions.assumeTrue(file.exists(), "calibri smcp test skipped");
+
+        CmapLookup cmapLookup;
+        GsubWorker gsubWorkerForSmcp;
+        try (TrueTypeFont ttf = new TTFParser().parse(new RandomAccessReadBufferedFile(file)))
+        {
+            cmapLookup = ttf.getUnicodeCmapLookup();
+            gsubWorkerForSmcp = new GsubWorkerForSmcp(cmapLookup, ttf.getGsubData());
+        }
+
+        // Values should be the same you get by looking at the GSUB lookup list 24 a font tool
+        // This one converts "ﬀ" (single-ff-ligature glyph) into "FF" small capitals
+        assertEquals(Arrays.asList(165, 165),
+                gsubWorkerForSmcp.applyTransforms(getGlyphIds("\ufb00", cmapLookup)));
+    }
+
+    private List<Integer> getGlyphIds(String word, CmapLookup cmapLookup)
+    {
+        List<Integer> originalGlyphIds = new ArrayList<>();
+
+        for (char unicodeChar : word.toCharArray())
+        {
+            int glyphId = cmapLookup.getGlyphId(unicodeChar);
+            assertTrue(glyphId > 0);
+            originalGlyphIds.add(glyphId);
+        }
+
+        return originalGlyphIds;
+    }
+}
