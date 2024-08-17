1c20e499f743a94b696ddfd88c832d66237aaaa4
Tilman Hausherr <tilman@apache.org>
2024-08-17 17:13:26 +0000

    PDFBOX-5869: fix apache header for checkstyle
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1919964 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/ScriptingHandler.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/ScriptingHandler.java
@@ -1,4 +1,3 @@
-package org.apache.pdfbox.pdmodel.interactive.form;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -15,6 +14,8 @@ package org.apache.pdfbox.pdmodel.interactive.form;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.pdfbox.pdmodel.interactive.form;
+
 import org.apache.pdfbox.pdmodel.interactive.action.PDActionJavaScript;
 
 public interface ScriptingHandler
