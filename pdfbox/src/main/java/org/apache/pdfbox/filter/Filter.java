107eaef45efce970fe3942ed4153fd2f55610faf
Tilman Hausherr <tilman@apache.org>
2024-08-25 10:54:03 +0000

    PDFBOX-5872: avoid NPE on image library / allow JPEG2000 library that doesn't support raster, by GÃ¡bor Stefanik; closes #197
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1920175 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/filter/Filter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/filter/Filter.java
@@ -149,7 +149,7 @@ public abstract class Filter
     }
 
     /**
-     * Finds a suitable image raster reader for an image format.
+     * Finds a suitable image reader for an image format.
      *
      * @param formatName The image format to search for.
      * @param errorCause The probably cause if something goes wrong.
@@ -163,11 +163,37 @@ public abstract class Filter
         while (readers.hasNext())
         {
             ImageReader reader = readers.next();
-            if (reader.canReadRaster())
+            if (reader != null)
             {
                 return reader;
             }
-            reader.dispose();
+        }
+        throw new MissingImageReaderException("Cannot read " + formatName + " image: " + errorCause);
+    }
+
+    /**
+     * Finds a suitable image raster reader for an image format.
+     *
+     * @param formatName The image format to search for.
+     * @param errorCause The probably cause if something goes wrong.
+     * @return The image reader for the format.
+     * @throws MissingImageReaderException if no image reader is found.
+     */
+    public static final ImageReader findRasterReader(String formatName, String errorCause)
+            throws MissingImageReaderException
+    {
+        Iterator<ImageReader> readers = ImageIO.getImageReadersByFormatName(formatName);
+        while (readers.hasNext())
+        {
+            ImageReader reader = readers.next();
+            if (reader != null)
+            {
+                if (reader.canReadRaster())
+                {
+                    return reader;
+                }
+                reader.dispose();
+            }
         }
         throw new MissingImageReaderException("Cannot read " + formatName + " image: " + errorCause);
     }
