e5b6b263018b12bb9af89468d3e52e6ba0347ceb
Tilman Hausherr <tilman@apache.org>
2024-09-17 07:20:56 +0000

    PDFBOX-5879: avoid ClassCastException
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1920728 13f79535-47bb-0310-9956-ffa450edef68

--- tools/src/main/java/org/apache/pdfbox/tools/ExtractText.java
+++ tools/src/main/java/org/apache/pdfbox/tools/ExtractText.java
@@ -333,7 +333,7 @@ public final class ExtractText  implements Callable<Integer>
                         stripper.writeText(document, output);
 
                         // remove prepended transformation
-                        ((COSArray) page.getCOSObject().getItem(COSName.CONTENTS)).remove(0);
+                        page.getCOSObject().getCOSArray(COSName.CONTENTS).remove(0);
                     }
                     page.setRotation(rotation);
                 }
