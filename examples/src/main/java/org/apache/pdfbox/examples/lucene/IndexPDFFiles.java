96b6f372d39d7a39800ac205e9cab83e913ba46c
Tilman Hausherr <tilman@apache.org>
2024-10-04 18:13:03 +0000

    PDFBOX-5881: add comment
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1921118 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/lucene/IndexPDFFiles.java
+++ examples/src/main/java/org/apache/pdfbox/examples/lucene/IndexPDFFiles.java
@@ -39,6 +39,9 @@ import org.apache.lucene.store.FSDirectory;
  * usage information.
  * <p>
  * It's based on a demo provided by the lucene project.
+ * <p>
+ * <b>Important:</b> The pom.xml uses an outdated lucene version. Replace that version with the
+ * latest version to avoid security risks like CVE-2024-45772.
  */
 public final class IndexPDFFiles
 {
