0708a50147979b603e4282a85d89f3357244bb91
Tilman Hausherr <tilman@apache.org>
2024-10-09 13:24:41 +0000

    PDFBOX-5884: Sonar fix
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1921206 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
@@ -2179,7 +2179,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
             else if (base instanceof COSDictionary)
             {
                 // Another OCG
-                PDPropertyList prop = PDOptionalContentGroup.create((COSDictionary) base);
+                PDPropertyList prop = PDPropertyList.create((COSDictionary) base);
                 boolean isHidden = isHiddenOCG(prop);
                 if (isHidden)
                 {
@@ -2208,7 +2208,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
             else if (base instanceof COSDictionary)
             {
                 // Another OCG
-                PDPropertyList prop = PDOptionalContentGroup.create((COSDictionary) base);
+                PDPropertyList prop = PDPropertyList.create((COSDictionary) base);
                 boolean isHidden = isHiddenOCG(prop);
                 if (!isHidden)
                 {
@@ -2234,7 +2234,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
         else if (base instanceof COSDictionary)
         {
             // Another OCG
-            PDPropertyList prop = PDOptionalContentGroup.create((COSDictionary) base);
+            PDPropertyList prop = PDPropertyList.create((COSDictionary) base);
             return !isHiddenOCG(prop);
         }
         return false;
