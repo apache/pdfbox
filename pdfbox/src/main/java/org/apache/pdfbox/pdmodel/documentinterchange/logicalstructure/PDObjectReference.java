7c2d1c04d99961054c62a8a4bf420893170f7a78
Tilman Hausherr <tilman@apache.org>
2024-10-15 14:04:22 +0000

    PDFBOX-5887: add page getter/setter
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1921334 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/documentinterchange/logicalstructure/PDObjectReference.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/documentinterchange/logicalstructure/PDObjectReference.java
@@ -22,6 +22,7 @@ import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.cos.COSStream;
+import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.common.COSObjectable;
 import org.apache.pdfbox.pdmodel.graphics.PDXObject;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
@@ -140,4 +141,30 @@ public class PDObjectReference implements COSObjectable
         this.getCOSObject().setItem(COSName.OBJ, xobject);
     }
 
+    /**
+     * Get the page on which the object shall be rendered.
+     *
+     * @return the referenced page or null.
+     */
+    public PDPage getPage()
+    {
+        COSDictionary pageDict = this.getCOSObject().getCOSDictionary(COSName.PG);
+        if (pageDict != null)
+        {
+            return new PDPage(pageDict);
+        }
+        return null;
+    }
+
+    /**
+     * Sets the page on which the object shall be rendered. This is optional and overrides the /PG
+     * entry in the structure element containing the object reference; shall be used if the
+     * structure element contained no such entry.
+     *
+     * @param page
+     */
+    public void setPage(PDPage page)
+    {
+        this.getCOSObject().setItem(COSName.PG, page);
+    }
 }
