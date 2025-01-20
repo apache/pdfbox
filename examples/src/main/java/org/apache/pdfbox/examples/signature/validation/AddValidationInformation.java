9d060cb6e8e34df7077746d1dc95ef4faeed7e5c
Tilman Hausherr <tilman@apache.org>
2025-01-20 11:05:39 +0000

    PDFBOX-5936:  refactor for future change, the list of potention issuers is already in getCertificateSet() result
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1923255 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/validation/AddValidationInformation.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/validation/AddValidationInformation.java
@@ -495,9 +495,9 @@ public class AddValidationInformation
             CertificateVerificationException
     {
         X509CRL crl = CRLVerifier.downloadCRLFromWeb(certInfo.getCrlUrl());
-        X509Certificate issuerCertificate = certInfo.getIssuerCertificate();
 
         // find the issuer certificate (usually issuer of signature certificate)
+        X509Certificate issuerCertificate = null;
         for (X509Certificate certificate : certInformationHelper.getCertificateSet())
         {
             if (certificate.getSubjectX500Principal().equals(crl.getIssuerX500Principal()))
@@ -505,6 +505,10 @@ public class AddValidationInformation
                 issuerCertificate = certificate;
                 break;
             }
+        }
+        if (issuerCertificate == null)
+        {
+            throw new CertificateVerificationException("Can't find issuer of CRL for " + certInfo.getCrlUrl());
         }
         crl.verify(issuerCertificate.getPublicKey(), SecurityProvider.getProvider().getName());
         CRLVerifier.checkRevocation(crl, certInfo.getCertificate(), signDate.getTime(), certInfo.getCrlUrl());
