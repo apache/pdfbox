c28064622fdc56e534a4d2062a8770980aac8008
Tilman Hausherr <tilman@apache.org>
2025-01-22 09:49:48 +0000

    PDFBOX-5938: implement missing method and use it
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1923312 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/form/PDFormXObject.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/form/PDFormXObject.java
@@ -121,9 +121,9 @@ public class PDFormXObject extends PDXObject implements PDContentStream
     }
 
     /**
-     * Returns the group attributes dictionary.
+     * Returns the transparency group attributes dictionary.
      *
-     * @return the group attributes dictionary
+     * @return the transparency group attributes dictionary, never null.
      */
     public PDTransparencyGroupAttributes getGroup()
     {
@@ -137,7 +137,18 @@ public class PDFormXObject extends PDXObject implements PDContentStream
         }
         return group;
     }
-    
+
+    /**
+     * Sets the transparency group attributes dictionary.
+     *
+     * @param group a transparency group attributes dictionary.
+     */
+    public void setGroup(PDTransparencyGroupAttributes group)
+    {
+        this.group = group;
+        getCOSObject().setItem(COSName.GROUP, group);
+    }
+
     public PDStream getContentStream()
     {
         return new PDStream(getCOSObject());
