b654cbac2944714845386ca705183ea4fffd989c
Tilman Hausherr <tilman@apache.org>
2025-02-06 16:07:06 +0000

    PDFBOX-5660: remove unused
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1923616 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDPolylineAppearanceHandler.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDPolylineAppearanceHandler.java
@@ -20,9 +20,6 @@ package org.apache.pdfbox.pdmodel.interactive.annotation.handlers;
 import java.io.IOException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.pdfbox.cos.COSArray;
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSNumber;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.graphics.color.PDColor;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
@@ -30,8 +27,6 @@ import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationPolyline;
 import org.apache.pdfbox.pdmodel.PDAppearanceContentStream;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import static org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationLine.LE_NONE;
-import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationMarkup;
-import org.apache.pdfbox.pdmodel.interactive.annotation.PDBorderStyleDictionary;
 import org.apache.pdfbox.util.Matrix;
 
 /**
@@ -212,46 +207,5 @@ public class PDPolylineAppearanceHandler extends PDAbstractAppearanceHandler
     public void generateDownAppearance()
     {
         // No down appearance generated for a polyline annotation
-    }
-
-    //TODO DRY, this code is from polygonAppearanceHandler so it's double
-    
-    /**
-     * Get the line with of the border.
-     * 
-     * Get the width of the line used to draw a border around the annotation.
-     * This may either be specified by the annotation dictionaries Border
-     * setting or by the W entry in the BS border style dictionary. If both are
-     * missing the default width is 1.
-     * 
-     * @return the line width
-     */
-    // TODO: according to the PDF spec the use of the BS entry is annotation
-    // specific
-    // so we will leave that to be implemented by individual handlers.
-    // If at the end all annotations support the BS entry this can be handled
-    // here and removed from the individual handlers.
-    float getLineWidth()
-    {
-        PDAnnotationMarkup annotation = (PDAnnotationMarkup) getAnnotation();
-
-        PDBorderStyleDictionary bs = annotation.getBorderStyle();
-
-        if (bs != null)
-        {
-            return bs.getWidth();
-        }
-
-        COSArray borderCharacteristics = annotation.getBorder();
-        if (borderCharacteristics.size() >= 3)
-        {
-            COSBase base = borderCharacteristics.getObject(2);
-            if (base instanceof COSNumber)
-            {
-                return ((COSNumber) base).floatValue();
-            }
-        }
-
-        return 1;
     }
 }
