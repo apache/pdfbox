810e2b8a8814bdbc8fe88703b66e2cfbf0b0b8fd
Tilman Hausherr <tilman@apache.org>
2025-02-08 05:23:03 +0000

    PDFBOX-5949: consider the font matrix when scaling
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1923658 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDTextAppearanceHandler.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDTextAppearanceHandler.java
@@ -386,6 +386,10 @@ public class PDTextAppearanceHandler extends PDAbstractAppearanceHandler
     private void drawCrossHairs(PDAnnotationText annotation, final PDAppearanceContentStream contentStream)
             throws IOException
     {
+        List<Number> fontMatrix = new PDType1Font(FontName.SYMBOL).getFontBoxFont().getFontMatrix();
+        float xScale = (float) fontMatrix.get(0);
+        float yScale = (float) fontMatrix.get(3);
+
         PDRectangle bbox = adjustRectAndBBox(annotation, 20, 20);
 
         float min = Math.min(bbox.getWidth(), bbox.getHeight());
@@ -395,7 +399,7 @@ public class PDTextAppearanceHandler extends PDAbstractAppearanceHandler
         contentStream.setLineCapStyle(0);
         contentStream.setLineWidth(0.61f); // value from Adobe
 
-        contentStream.transform(Matrix.getScaleInstance(0.001f * min / 1.5f, 0.001f * min / 1.5f));
+        contentStream.transform(Matrix.getScaleInstance(xScale * min * 1.3333f, yScale * min * 1.3333f));
         contentStream.transform(Matrix.getTranslateInstance(0, 50));
 
         // we get the shape of a Symbol crosshair (0x2295) and use that one.
