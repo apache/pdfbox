af2f5a4d6342b23c2034100e9bb5d627f1dceb7d
Tilman Hausherr <tilman@apache.org>
2025-02-10 18:47:16 +0000

    PDFBOX-5951: ignore OpenAction that points to orphan page
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1923708 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/multipdf/PDFMergerUtility.java
+++ pdfbox/src/main/java/org/apache/pdfbox/multipdf/PDFMergerUtility.java
@@ -570,6 +570,11 @@ public class PDFMergerUtility
                 if (page != null)
                 {
                     pageIndexOpenActionDest = srcCatalog.getPages().indexOf(page);
+                    if (pageIndexOpenActionDest == -1)
+                    {
+                        LOG.warn("OpenAction entry ignored because destination page doesn't exist");
+                        openAction = null;
+                    }
                 }
             }
 
