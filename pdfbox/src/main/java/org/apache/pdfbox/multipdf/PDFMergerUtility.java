0f71c0b6a13756c61883e5862313bb2a535cc5d9
Tilman Hausherr <tilman@apache.org>
2025-03-05 05:12:52 +0000

    PDFBOX-5968: avoid NPE
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1924175 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/multipdf/PDFMergerUtility.java
+++ pdfbox/src/main/java/org/apache/pdfbox/multipdf/PDFMergerUtility.java
@@ -812,7 +812,12 @@ public class PDFMergerUtility
             {
                 int srcKey = entry.getKey();
                 maxSrcKey = Math.max(srcKey, maxSrcKey);
-                destNumberTreeAsMap.put(destParentTreeNextKey + srcKey, cloner.cloneForNewDocument(entry.getValue()));
+                COSObjectable value = entry.getValue();
+                if (value != null)
+                {
+                    value = cloner.cloneForNewDocument(value.getCOSObject());
+                    destNumberTreeAsMap.put(destParentTreeNextKey + srcKey, value);
+                }
             }
             destParentTreeNextKey += maxSrcKey + 1;
             PDNumberTreeNode newParentTreeNode = new PDNumberTreeNode(PDParentTreeValue.class);
@@ -1098,8 +1103,12 @@ public class PDFMergerUtility
             }
             else
             {
-                destNames.put(entry.getKey(),
-                              new PDStructureElement((COSDictionary) cloner.cloneForNewDocument(entry.getValue().getCOSObject())));
+                if (entry.getValue() != null)
+                {
+                    PDStructureElement structureElement = new PDStructureElement(
+                            (COSDictionary) cloner.cloneForNewDocument(entry.getValue().getCOSObject()));
+                    destNames.put(entry.getKey(), structureElement);
+                }
             }
         }
         destIDTree = new PDStructureElementNameTreeNode();
