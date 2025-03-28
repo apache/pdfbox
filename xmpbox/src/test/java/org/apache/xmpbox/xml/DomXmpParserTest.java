d0cde3aa5dc9591448dcb86c12d00fad5a619dc0
Tilman Hausherr <tilman@apache.org>
2025-03-28 04:07:20 +0000

    PDFBOX-5976: remove unused throws
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1924669 13f79535-47bb-0310-9956-ffa450edef68

--- xmpbox/src/test/java/org/apache/xmpbox/xml/DomXmpParserTest.java
+++ xmpbox/src/test/java/org/apache/xmpbox/xml/DomXmpParserTest.java
@@ -23,7 +23,6 @@ package org.apache.xmpbox.xml;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.xmpbox.XMPMetadata;
@@ -67,7 +66,7 @@ class DomXmpParserTest
     }
 
     @Test
-    void testPDFBox5976() throws XmpParsingException, UnsupportedEncodingException
+    void testPDFBox5976() throws XmpParsingException
     {
         String s = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
                    "<?xpacket begin=\"\" id=\"W5M0MpCehiHzreSzNTczkc9d\"?>\n" +
