b1de2919949fd86c4d69b99b646ada0dbc6de565
Tilman Hausherr <tilman@apache.org>
2025-04-05 18:11:22 +0000

    PDFBOX-5660: Sonar fix
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1924819 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/test/java/org/apache/pdfbox/examples/pdmodel/TestHelloWorld.java
+++ examples/src/test/java/org/apache/pdfbox/examples/pdmodel/TestHelloWorld.java
@@ -38,7 +38,7 @@ class TestHelloWorld
     private static final String OUTPUT_DIR = "target/test-output";
 
     @BeforeAll
-    public static void init() throws Exception
+    void setUp()
     {
         new File(OUTPUT_DIR).mkdirs();
     }
