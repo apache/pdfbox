9d29212523def1d777948df71ebb5cf78945c529
Tilman Hausherr <tilman@apache.org>
2025-04-12 17:41:30 +0000

    PDFBOX-2941: refactor
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1925056 13f79535-47bb-0310-9956-ffa450edef68

--- debugger/src/main/java/org/apache/pdfbox/debugger/ui/Tree.java
+++ debugger/src/main/java/org/apache/pdfbox/debugger/ui/Tree.java
@@ -38,9 +38,10 @@ import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.MouseEvent;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringJoiner;
@@ -295,11 +296,10 @@ public class Tree extends JTree
             {
                 File temp = File.createTempFile("pdfbox", "." + extension);
                 temp.deleteOnExit();
-                
-                try (InputStream is = cosStream.createInputStream();
-                        FileOutputStream os = new FileOutputStream(temp))
+
+                try (InputStream is = cosStream.createInputStream())
                 {
-                    IOUtils.copy(is, os);
+                    Files.copy(is, temp.toPath(), StandardCopyOption.REPLACE_EXISTING);
                 }
                 Desktop.getDesktop().open(temp);
             }
