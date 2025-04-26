424482afa727f25b6c22fc3e47b361cce5db634a
Tilman Hausherr <tilman@apache.org>
2025-04-26 08:46:13 +0000

    PDFBOX-5660: Sonar fix
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1925275 13f79535-47bb-0310-9956-ffa450edef68

--- xmpbox/src/test/java/org/apache/xmpbox/XMPMetaDataTest.java
+++ xmpbox/src/test/java/org/apache/xmpbox/XMPMetaDataTest.java
@@ -174,7 +174,6 @@ class XMPMetaDataTest
                 + "<?xpacket end=\"w\"?>";
         DomXmpParser xmpParser = new DomXmpParser();
         xmpParser.setStrictParsing(false);
-        //IOUtils.copy(meta.createInputStream(),System.out);
         XMPMetadata xmp = xmpParser.parse(xmpmeta.getBytes());
         XMPBasicSchema basicSchema = xmp.getXMPBasicSchema();
         Calendar createDate1 = basicSchema.getCreateDate();
