abc8572b191ec3c6b4e7ae506d331ea0310b32b9
Tilman Hausherr <tilman@apache.org>
2025-05-24 11:31:14 +0000

    PDFBOX-6009: remove empty OBJR dictionary
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1925775 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/multipdf/Splitter.java
+++ pdfbox/src/main/java/org/apache/pdfbox/multipdf/Splitter.java
@@ -472,6 +472,10 @@ public class Splitter
                 {
                     removePossibleOrphanAnnotation(srcObj, srcDict, currentPageDict, dstDict);
                 }
+                if (dstDict.size() == 1)
+                {
+                    return null;
+                }
             }
             else
             {
