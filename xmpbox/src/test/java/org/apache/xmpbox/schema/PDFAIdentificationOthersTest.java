24a1e3a37057c26b5fc4de225504f2feab2377f6
Tilman Hausherr <tilman@apache.org>
2025-06-28 11:29:02 +0000

    PDFBOX-6026: test serializer results
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1926801 13f79535-47bb-0310-9956-ffa450edef68

--- xmpbox/src/test/java/org/apache/xmpbox/schema/PDFAIdentificationOthersTest.java
+++ xmpbox/src/test/java/org/apache/xmpbox/schema/PDFAIdentificationOthersTest.java
@@ -21,17 +21,22 @@
 
 package org.apache.xmpbox.schema;
 
+import java.io.ByteArrayOutputStream;
+import javax.xml.transform.TransformerException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.apache.xmpbox.XMPMetadata;
 import org.apache.xmpbox.type.BadFieldValueException;
+import org.apache.xmpbox.xml.DomXmpParser;
+import org.apache.xmpbox.xml.XmpParsingException;
+import org.apache.xmpbox.xml.XmpSerializer;
 import org.junit.jupiter.api.Test;
 
 class PDFAIdentificationOthersTest
 {
     @Test
-    void testPDFAIdentification() throws BadFieldValueException
+    void testPDFAIdentification() throws BadFieldValueException, TransformerException, XmpParsingException
     {
         XMPMetadata metadata = XMPMetadata.createXMPMetadata();
         PDFAIdentificationSchema pdfaid = metadata.createAndAddPDFAIdentificationSchema();
@@ -55,7 +60,18 @@ class PDFAIdentificationOthersTest
         // check retrieve this schema in metadata
         assertEquals(pdfaid, metadata.getPDFAIdentificationSchema());
 
-        // SaveMetadataHelper.serialize(metadata, true, System.out);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        new XmpSerializer().serialize(metadata, bos, true);
+        XMPMetadata rxmp = new DomXmpParser().parse(bos.toByteArray());
+        pdfaid = rxmp.getPDFAIdentificationSchema();
+
+        assertEquals(versionId, pdfaid.getPart());
+        assertEquals(amdId, pdfaid.getAmendment());
+        assertEquals(conformance, pdfaid.getConformance());
+
+        assertEquals("" + versionId, pdfaid.getPartProperty().getStringValue());
+        assertEquals(amdId, pdfaid.getAmdProperty().getStringValue());
+        assertEquals(conformance, pdfaid.getConformanceProperty().getStringValue());
     }
 
     @Test
