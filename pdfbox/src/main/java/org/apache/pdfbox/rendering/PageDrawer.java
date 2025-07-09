e0c9e212cb5a6223ad5184fa3e3b99479ad53662
Tilman Hausherr <tilman@apache.org>
2025-07-09 11:28:13 +0000

    PDFBOX-6033: move some clipping operations from endPath ("n") to clip ("W")
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1927088 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
@@ -1040,6 +1040,22 @@ public class PageDrawer extends PDFGraphicsStreamEngine
     {
         // the clipping path will not be updated until the succeeding painting operator is called
         clipWindingRule = windingRule;
+        if (clipWindingRule != -1)
+        {
+            linePath.setWindingRule(clipWindingRule);
+
+            if (!linePath.getPathIterator(null).isDone())
+            {
+                // PDFBOX-4949 / PDF.js 12306: don't clip if "W n" only
+                getGraphicsState().intersectClippingPath(adjustClip(linePath));
+            }
+
+            // PDFBOX-3836: lastClip needs to be reset, because after intersection it is still the same 
+            // object, thus setClip() would believe that it is cached.
+            lastClips = null;
+
+            clipWindingRule = -1;
+        }
     }
 
     @Override
@@ -1075,22 +1091,6 @@ public class PageDrawer extends PDFGraphicsStreamEngine
     @Override
     public void endPath()
     {
-        if (clipWindingRule != -1)
-        {
-            linePath.setWindingRule(clipWindingRule);
-
-            if (!linePath.getPathIterator(null).isDone())
-            {
-                // PDFBOX-4949 / PDF.js 12306: don't clip if "W n" only
-                getGraphicsState().intersectClippingPath(adjustClip(linePath));
-            }
-
-            // PDFBOX-3836: lastClip needs to be reset, because after intersection it is still the same 
-            // object, thus setClip() would believe that it is cached.
-            lastClips = null;
-
-            clipWindingRule = -1;
-        }
         linePath.reset();
     }
     
