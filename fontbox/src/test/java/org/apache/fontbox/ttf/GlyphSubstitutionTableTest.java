abb028244e56d48e6a15edda1574a74bda3b8ebe
Tilman Hausherr <tilman@apache.org>
2025-07-10 17:18:17 +0000

    PDFBOX-5660: avoid default encoding
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1927112 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/test/java/org/apache/fontbox/ttf/GlyphSubstitutionTableTest.java
+++ fontbox/src/test/java/org/apache/fontbox/ttf/GlyphSubstitutionTableTest.java
@@ -23,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -90,7 +91,8 @@ class GlyphSubstitutionTableTest
         Map<List<Integer>, Integer> gsubData = new HashMap<>();
 
         try (BufferedReader br = new BufferedReader(
-             new InputStreamReader(TestTTFParser.class.getResourceAsStream(pathToResource))))
+             new InputStreamReader(
+                     TestTTFParser.class.getResourceAsStream(pathToResource), StandardCharsets.US_ASCII)))
         {
             while (true)
             {
