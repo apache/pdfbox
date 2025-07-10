5e2f1aeded7c75767242374c73b7f74b11bbd0fc
Tilman Hausherr <tilman@apache.org>
2025-07-10 17:26:01 +0000

    PDFBOX-5660: avoid default encoding
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1927114 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/pfb/PfbParser.java
+++ fontbox/src/main/java/org/apache/fontbox/pfb/PfbParser.java
@@ -21,6 +21,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -190,7 +191,8 @@ public class PfbParser
                 continue;
             }
             byte[] ar = barrList.get(i);
-            if (i == typeList.size() - 1 && ar.length < 600 && new String(ar).contains("cleartomark"))
+            if (i == typeList.size() - 1 && ar.length < 600 &&
+                    new String(ar, StandardCharsets.US_ASCII).contains("cleartomark"))
             {
                 cleartomarkSegment = ar;
                 continue;
