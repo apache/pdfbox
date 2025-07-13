d5040bc82681447ae5c8ba3a0a5382f17b9045a6
Tilman Hausherr <tilman@apache.org>
2025-07-13 07:08:13 +0000

    PDFBOX-5660: avoid default encoding
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1927170 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fdf/FDFAnnotationStamp.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/fdf/FDFAnnotationStamp.java
@@ -116,7 +116,7 @@ public class FDFAnnotationStamp extends FDFAnnotation
         }
         if (base64EncodedAppearance != null && !base64EncodedAppearance.isEmpty())
         {
-            LOG.debug("Decoded XML: {}", () -> new String(decodedAppearanceXML, StandardCharsets.UTF_8));
+            LOG.debug("Decoded XML:\n====\n{}\n====", () -> new String(decodedAppearanceXML, StandardCharsets.UTF_8));
 
             Document stampAppearance = XMLUtil
                     .parse(new ByteArrayInputStream(decodedAppearanceXML));
@@ -253,8 +253,16 @@ public class FDFAnnotationStamp extends FDFAnnotation
                         {
                             try (OutputStream os = stream.createOutputStream())
                             {
-                                // not sure about charset
-                                os.write(child.getTextContent().getBytes());
+                                String encoding = child.getOwnerDocument().getXmlEncoding();
+                                if (encoding == null)
+                                {
+                                    encoding = child.getOwnerDocument().getInputEncoding();
+                                }
+                                if (encoding == null)
+                                {
+                                    encoding = "UTF-8";
+                                }
+                                os.write(child.getTextContent().getBytes(encoding));
                                 LOG.debug("{} => Data was streamed", parentAttrKey);
                             }
                         }
