6a936a4d6745773da4f1e2374ec6c0383c164d92
Tilman Hausherr <tilman@apache.org>
2025-07-13 10:13:31 +0000

    PDFBOX-6022: don't use identity if there's a predefined cmap
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1927185 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType0Font.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType0Font.java
@@ -544,7 +544,8 @@ public class PDType0Font extends PDFont implements PDVectorFont
 
         // Use identity mapping if the given ToUnicode CMap doesn't provide any valid mapping
         // a predefined map shall only be used if there isn't any ToUnicode CMap
-        if (getToUnicodeCMap() != null)
+        // PDFBOX-6022: not when there's a predefined cmap
+        if (getToUnicodeCMap() != null && !isCMapPredefined)
         {
             return Character.toString((char) code);
         }
