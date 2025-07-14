841b1bf2199015b4302cb21f7a53ab42f1a3a821
ilgoo.kim <ilgoo.kim@navercorp.com>
2025-07-14 23:22:58 +0900

    PDFBOX-6032: add JavaDoc for CustomFactory

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/image/CustomFactory.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/image/CustomFactory.java
@@ -4,7 +4,19 @@ import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 
+/**
+ * A functional interface that allows users to define a custom strategy for converting
+ * image data as a byte array into a {@link PDImageXObject}.
+ */
 @FunctionalInterface
 public interface CustomFactory {
+	/**
+	 * Creates a {@link PDImageXObject} from the given image byte array and document context.
+	 *
+	 * @param document the document that shall use this PDImageXObject.
+	 * @param byteArray the image data as a byte array
+	 * @return a PDImageXObject.
+	 * @throws IOException if there is an error when creating the PDImageXObject.
+	 */
 	PDImageXObject createFromByteArray(PDDocument document, byte[] byteArray) throws IOException;
 }
