04b2e12cc8d1d4a9dfcfc6fa92ee7a16cd1292a0
Tilman Hausherr <tilman@apache.org>
2025-07-23 08:10:35 +0000

    PDFBOX-6044: avoid potential OOM
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1927414 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/pfb/PfbParser.java
+++ fontbox/src/main/java/org/apache/fontbox/pfb/PfbParser.java
@@ -154,6 +154,11 @@ public class PfbParser
             size += in.read() << 16;
             size += in.read() << 24;
             LOG.debug("record type: {}, segment size: {}", recordType, size);
+            if (size > pfb.length)
+            {
+                // PDFBOX-6044: avoid potential OOM
+                throw new IOException("record size " + size + "would be larger than the input");
+            }
             byte[] ar = new byte[size];
             int got = in.read(ar);
             if (got != size)
