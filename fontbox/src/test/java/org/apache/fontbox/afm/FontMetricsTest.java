4a601d6e88b4c980e3ced2d794fba83b78167db7
Tilman Hausherr <tilman@apache.org>
2025-08-25 10:45:18 +0000

    PDFBOX-5660: fix typo
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1927993 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/test/java/org/apache/fontbox/afm/FontMetricsTest.java
+++ fontbox/src/test/java/org/apache/fontbox/afm/FontMetricsTest.java
@@ -132,7 +132,7 @@ class FontMetricsTest
             fontMetrics.setMetricSets(-1);
             fail("An IllegalArgumentException should have been thrown");
         }
-        catch (IllegalArgumentException excpetion)
+        catch (IllegalArgumentException exception)
         {
             // do nothing
         }
@@ -143,7 +143,7 @@ class FontMetricsTest
             fontMetrics.setMetricSets(3);
             fail("An IllegalArgumentException should have been thrown");
         }
-        catch (IllegalArgumentException excpetion)
+        catch (IllegalArgumentException exception)
         {
             // do nothing
         }
