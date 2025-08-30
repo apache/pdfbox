5b57c3619bd9e2220521a339569f88326c63d118
Tilman Hausherr <tilman@apache.org>
2025-08-30 19:20:39 +0000

    PDFBOX-5660: optimize, as suggested by Valery Bokov; closes #237
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1928139 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/PDNumberTreeNode.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/common/PDNumberTreeNode.java
@@ -180,12 +180,13 @@ public class PDNumberTreeNode implements COSObjectable
         if (numBase instanceof COSArray)
         {
             COSArray numbersArray = (COSArray) numBase;
-            indices = new HashMap<Integer, COSObjectable>();
-            if (numbersArray.size() % 2 != 0)
+            int size = numbersArray.size();
+            indices = new HashMap<Integer, COSObjectable>(size / 2);
+            if (size % 2 != 0)
             {
-                LOG.warn("Numbers array has odd size: " + numbersArray.size());
+                LOG.warn("Numbers array has odd size: " + size);
             }
-            for (int i = 0; i + 1 < numbersArray.size(); i += 2)
+            for (int i = 0; i + 1 < size; i += 2)
             {
                 COSBase base = numbersArray.getObject(i);
                 if (!(base instanceof COSInteger))
