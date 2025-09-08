672240f59493ff12b8989333cb7738ca4d1dd756
Tilman Hausherr <tilman@apache.org>
2025-09-08 09:56:43 +0000

    PDFBOX-6064: improve logging, fix typo
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1928283 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdfparser/PDFStreamParser.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdfparser/PDFStreamParser.java
@@ -49,6 +49,7 @@ public class PDFStreamParser extends BaseParser
     private static final int MAX_BIN_CHAR_TEST_LENGTH = 10;
     private final byte[] binCharTestArr = new byte[MAX_BIN_CHAR_TEST_LENGTH];
     private int inlineImageDepth = 0;
+    private long inlineOffset = 0;
     
     /**
      * Constructor.
@@ -241,7 +242,12 @@ public class PDFStreamParser extends BaseParser
                     if (inlineImageDepth > 1)
                     {
                         // PDFBOX-6038
-                        throw new IOException("Nested '" + OperatorName.BEGIN_INLINE_IMAGE + "' operator not allowed");
+                        throw new IOException("Nested '" + OperatorName.BEGIN_INLINE_IMAGE +
+                                "' operator not allowed at offset " + source.getPosition() + ", first: " + inlineOffset);
+                    }
+                    else
+                    {
+                        inlineOffset = source.getPosition();
                     }
                     COSDictionary imageParams = new COSDictionary();
                     beginImageOP.setImageParameters( imageParams );
@@ -267,6 +273,11 @@ public class PDFStreamParser extends BaseParser
                         }
                         beginImageOP.setImageData(imageData.getImageData());
                         inlineImageDepth--;
+                    }
+                    else
+                    {
+                        LOG.warn("nextToken " + nextToken + " at position " + source.getPosition() +
+                                ", expected " + OperatorName.BEGIN_INLINE_IMAGE_DATA + "?!");
                     }
                 }
                 return beginImageOP;
@@ -376,7 +387,7 @@ public class PDFStreamParser extends BaseParser
                 if (!"Q".equals(s) && !"EMC".equals(s) && !"S".equals(s) &&
                     !s.matches("^\\d*\\.?\\d*$"))
                 {
-                    // operator is not Q, not EMC, not S, nur a number -> assume binary data
+                    // operator is not Q, not EMC, not S, nor a number -> assume binary data
                     noBinData = false;
                 }
             }
