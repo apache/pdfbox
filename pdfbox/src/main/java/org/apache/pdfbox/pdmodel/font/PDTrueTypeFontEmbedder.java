7c2ac9e1713421fe53631a16b8def7c1566f08a5
valery.bokov <vlry.bkv@gmail.com>
2025-09-13 18:21:27 +0200

    improve performance for PDTrueTypeFontEmbedder.setWidths method

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDTrueTypeFontEmbedder.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDTrueTypeFontEmbedder.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import org.apache.fontbox.ttf.HorizontalMetricsTable;
 import org.apache.fontbox.ttf.TrueTypeFont;
 import org.apache.pdfbox.cos.COSArray;
@@ -80,8 +81,9 @@ final class PDTrueTypeFontEmbedder extends TrueTypeEmbedder
 
         Map<Integer, String> codeToName = getFontEncoding().getCodeToNameMap();
 
-        int firstChar = Collections.min(codeToName.keySet());
-        int lastChar = Collections.max(codeToName.keySet());
+        Set<Integer> codes = codeToName.keySet();
+        int firstChar = Collections.min(codes);
+        int lastChar = Collections.max(codes);
 
         List<Integer> widths = new ArrayList<>(lastChar - firstChar + 1);
         for (int i = 0; i < lastChar - firstChar + 1; i++)
