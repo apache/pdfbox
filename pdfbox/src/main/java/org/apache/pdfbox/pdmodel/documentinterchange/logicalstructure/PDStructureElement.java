2476f674b26e659c59b1047e4c0e7fd51602ffb4
Tilman Hausherr <tilman@apache.org>
2025-09-14 15:34:17 +0000

    PDFBOX-5660: refactor
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1928468 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/documentinterchange/logicalstructure/PDStructureElement.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/documentinterchange/logicalstructure/PDStructureElement.java
@@ -18,7 +18,6 @@ package org.apache.pdfbox.pdmodel.documentinterchange.logicalstructure;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.pdfbox.cos.COSArray;
@@ -164,16 +163,11 @@ public class PDStructureElement extends PDStructureNode
         COSBase a = this.getCOSObject().getDictionaryObject(COSName.A);
         if (a instanceof COSArray)
         {
-            COSArray aa = (COSArray) a;
-            Iterator<COSBase> it = aa.iterator();
+            COSArray array = (COSArray) a;
             PDAttributeObject ao = null;
-            while (it.hasNext())
+            for (int i = 0; i < array.size(); ++i)
             {
-                COSBase item = it.next();
-                if (item instanceof COSObject)
-                {
-                    item = ((COSObject) item).getObject();
-                }
+                COSBase item = array.getObject(i);
                 if (item instanceof COSDictionary)
                 {
                     ao = PDAttributeObject.create((COSDictionary) item);
@@ -182,6 +176,8 @@ public class PDStructureElement extends PDStructureNode
                 }
                 else if (item instanceof COSInteger)
                 {
+                    // Read "14.7.5.3 Attribute Revision Numbers"
+                    // This is additional to the /R entry
                     attributes.setRevisionNumber(ao, ((COSNumber) item).intValue());
                 }
             }
@@ -337,15 +333,10 @@ public class PDStructureElement extends PDStructureNode
         if (c instanceof COSArray)
         {
             COSArray array = (COSArray) c;
-            Iterator<COSBase> it = array.iterator();
             String className = null;
-            while (it.hasNext())
+            for (int i = 0; i < array.size(); ++i)
             {
-                COSBase item = it.next();
-                if (item instanceof COSObject)
-                {
-                    item = ((COSObject) item).getObject();
-                }
+                COSBase item = array.getObject(i);
                 if (item instanceof COSName)
                 {
                     className = ((COSName) item).getName();
@@ -353,6 +344,8 @@ public class PDStructureElement extends PDStructureNode
                 }
                 else if (item instanceof COSInteger)
                 {
+                    // Read "14.7.5.3 Attribute Revision Numbers"
+                    // This is additional to the /R entry
                     classNames.setRevisionNumber(className, ((COSNumber) item).intValue());
                 }
             }
