e6c46de78f7ffcfd681c0c76f33f3d92d353d4a2
Tilman Hausherr <tilman@apache.org>
2025-09-21 11:10:31 +0000

    PDFBOX-5660: fix test, as discovered by Valery Bokov; closes #261
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1928663 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/test/java/org/apache/pdfbox/pdmodel/interactive/form/PDAcroFormFromAnnotsTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/interactive/form/PDAcroFormFromAnnotsTest.java
@@ -279,7 +279,7 @@ public class PDAcroFormFromAnnotsTest
         String sourceUrl = "https://issues.apache.org/jira/secure/attachment/12881055/merge-test.pdf";
         String acrobatSourceUrl = "https://issues.apache.org/jira/secure/attachment/13014447/merge-test-na-acrobat.pdf";
 
-        // will build the expected font respurce names and font decriptor names using the acrobat source document
+        // will build the expected font resource names and font decriptor names using the acrobat source document
         Map<String, String> fontNames = new HashMap<String, String>();
 
         PDDocument testPdf = null;
@@ -296,8 +296,8 @@ public class PDAcroFormFromAnnotsTest
                     try
                     {
                         PDFont font = acroFormResources.getFont(fontName);
-                        font.getFontDescriptor().getFontName();
-                        fontNames.put(fontName.getName(), font.getName());
+                        String pdfBoxFontName = font.getFontDescriptor().getFontName();
+                        fontNames.put(fontName.getName(), pdfBoxFontName);
                     }
                     catch (IOException ioe)
                     {
