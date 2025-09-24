18642ec502cc2024ddc4c99f39861dde2a1d45ef
Tilman Hausherr <tilman@apache.org>
2025-09-24 14:49:42 +0000

    PDFBOX-5660: optimize, as suggested by Valery Bokov; closes #262
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1928736 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDAbstractAppearanceHandler.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDAbstractAppearanceHandler.java
@@ -529,13 +529,13 @@ public abstract class PDAbstractAppearanceHandler implements PDAppearanceHandler
         float[] rectDifferences = annotation.getRectDifferences();
         if (rectDifferences.length == 0)
         {
-            borderBox = getPaddedRectangle(getRectangle(), lineWidth / 2);
+            PDRectangle rect = getRectangle();
+            borderBox = getPaddedRectangle(rect, lineWidth / 2);
             // the differences rectangle
             annotation.setRectDifferences(lineWidth / 2);
-            annotation.setRectangle(addRectDifferences(getRectangle(), annotation.getRectDifferences()));
+            annotation.setRectangle(addRectDifferences(rect, annotation.getRectDifferences()));
             // when the normal appearance stream was generated BBox and Matrix have been set to the
             // values of the original /Rect. As the /Rect was changed that needs to be adjusted too.
-            PDRectangle rect = getRectangle();
             PDAppearanceStream appearanceStream = annotation.getNormalAppearanceStream();
             AffineTransform transform =
                     AffineTransform.getTranslateInstance(-rect.getLowerLeftX(), -rect.getLowerLeftY());
