e265a91cad57d3ccfaf4adbf65314c2b72f5c59b
Tilman Hausherr <tilman@apache.org>
2025-09-27 16:59:09 +0000

    PDFBOX-5660: optimize, as suggested by Valery Bokov; closes #266
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1928792 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/AppearanceGeneratorHelper.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/AppearanceGeneratorHelper.java
@@ -678,11 +678,11 @@ class AppearanceGeneratorHelper {
         int quadding = field.getQ();
         int numChars = Math.min(value.length(), maxLen);
 
-        float combWidth = appearanceStream.getBBox().getWidth() / maxLen;
+        PDRectangle bBox = appearanceStream.getBBox();
+        float combWidth = bBox.getWidth() / maxLen;
         float ascentAtFontSize = font.getFontDescriptor().getAscent() / FONTSCALE * fontSize;
 
-        float baselineOffset = appearanceStream.getBBox().getLowerLeftY() +  
-                (appearanceStream.getBBox().getHeight() - ascentAtFontSize)/2;
+        float baselineOffset = bBox.getLowerLeftY() + (bBox.getHeight() - ascentAtFontSize) / 2;
 
         float prevCharWidth = 0f;
 
