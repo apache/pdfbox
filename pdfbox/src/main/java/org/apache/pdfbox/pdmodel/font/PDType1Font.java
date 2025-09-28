d69fb0a5da2c71e7da4e7e165becb1333d4ebb14
Tilman Hausherr <tilman@apache.org>
2025-09-28 12:38:26 +0000

    PDFBOX-5660: optimize logging
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1928811 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType1Font.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType1Font.java
@@ -307,10 +307,7 @@ public class PDType1Font extends PDSimpleFont implements PDVectorFont
 
         if (length1 - offset != 0 && offset > 0)
         {
-            if (LOG.isWarnEnabled())
-            {
-                LOG.warn("Ignored invalid Length1 {} for Type 1 font {}", length1, getName());
-            }
+            LOG.warn("Ignored invalid Length1 {} for Type 1 font {}", () -> length1, () -> getName());
             return offset;
         }
 
