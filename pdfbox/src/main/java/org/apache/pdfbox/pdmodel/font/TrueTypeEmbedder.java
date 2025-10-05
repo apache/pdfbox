1dd5103911ae7e961e76a093dc7d091a226224aa
Tilman Hausherr <tilman@apache.org>
2025-10-05 12:44:16 +0000

    PDFBOX-5660: improve code, as suggested by Andreas LehmkÃ¼hler
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1928959 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/TrueTypeEmbedder.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/TrueTypeEmbedder.java
@@ -23,7 +23,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -55,22 +55,10 @@ abstract class TrueTypeEmbedder implements Subsetter
     private static final String BASE25 = "BCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     // PDF spec required tables (if present), all others will be removed
-    private static final List<String> TABLES = new ArrayList<>(10);
-
-    static
-    {
-        TABLES.add("head");
-        TABLES.add("hhea");
-        TABLES.add("loca");
-        TABLES.add("maxp");
-        TABLES.add("cvt ");
-        TABLES.add("prep");
-        TABLES.add("glyf");
-        TABLES.add("hmtx");
-        TABLES.add("fpgm");
-        // Windows ClearType
-        TABLES.add("gasp");
-    }
+    private static final List<String> TABLES =
+            Arrays.asList("head", "hhea", "loca", "maxp", "cvt ", "prep", "glyf", "hmtx", "fpgm",
+                    // Windows ClearType
+                    "gasp");
 
     private final PDDocument document;
     protected TrueTypeFont ttf;
