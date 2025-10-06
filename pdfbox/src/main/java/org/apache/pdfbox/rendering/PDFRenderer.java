92e9de6c75ead173ca52fe11fadf2c5c342f0337
Tilman Hausherr <tilman@apache.org>
2025-10-06 11:06:36 +0000

    PDFBOX-5660: improve javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1928974 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/PDFRenderer.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/PDFRenderer.java
@@ -188,6 +188,9 @@ public class PDFRenderer
      * @param pageIndex the zero-based index of the page to be converted.
      * @return the rendered page image
      * @throws IOException if the PDF cannot be read
+     * @throws IllegalStateException if the requested index isn't found or doesn't point to a valid
+     * page dictionary
+     * @throws IndexOutOfBoundsException if the requested index is higher than the page count
      */
     public BufferedImage renderImage(int pageIndex) throws IOException
     {
@@ -201,6 +204,9 @@ public class PDFRenderer
      * @param scale the scaling factor, where 1 = 72 DPI
      * @return the rendered page image
      * @throws IOException if the PDF cannot be read
+     * @throws IllegalStateException if the requested index isn't found or doesn't point to a valid
+     * page dictionary
+     * @throws IndexOutOfBoundsException if the requested index is higher than the page count
      */
     public BufferedImage renderImage(int pageIndex, float scale) throws IOException
     {
@@ -213,6 +219,9 @@ public class PDFRenderer
      * @param dpi the DPI (dots per inch) to render at
      * @return the rendered page image
      * @throws IOException if the PDF cannot be read
+     * @throws IllegalStateException if the requested index isn't found or doesn't point to a valid
+     * page dictionary
+     * @throws IndexOutOfBoundsException if the requested index is higher than the page count
      */
     public BufferedImage renderImageWithDPI(int pageIndex, float dpi) throws IOException
     {
@@ -226,6 +235,9 @@ public class PDFRenderer
      * @param imageType the type of image to return
      * @return the rendered page image
      * @throws IOException if the PDF cannot be read
+     * @throws IllegalStateException if the requested index isn't found or doesn't point to a valid
+     * page dictionary
+     * @throws IndexOutOfBoundsException if the requested index is higher than the page count
      */
     public BufferedImage renderImageWithDPI(int pageIndex, float dpi, ImageType imageType)
             throws IOException
@@ -240,6 +252,9 @@ public class PDFRenderer
      * @param imageType the type of image to return
      * @return the rendered page image
      * @throws IOException if the PDF cannot be read
+     * @throws IllegalStateException if the requested index isn't found or doesn't point to a valid
+     * page dictionary
+     * @throws IndexOutOfBoundsException if the requested index is higher than the page count
      */
     public BufferedImage renderImage(int pageIndex, float scale, ImageType imageType)
             throws IOException
@@ -256,6 +271,9 @@ public class PDFRenderer
      * @param destination controlling visibility of optional content groups
      * @return the rendered page image
      * @throws IOException if the PDF cannot be read
+     * @throws IllegalStateException if the requested index isn't found or doesn't point to a valid
+     * page dictionary
+     * @throws IndexOutOfBoundsException if the requested index is higher than the page count
      */
     public BufferedImage renderImage(int pageIndex, float scale, ImageType imageType, RenderDestination destination)
             throws IOException
@@ -419,6 +437,9 @@ public class PDFRenderer
      * @param scaleY the scale to draw the page at for the y-axis, where 1 = 72 DPI
      * @param destination controlling visibility of optional content groups
      * @throws IOException if the PDF cannot be read
+     * @throws IllegalStateException if the requested index isn't found or doesn't point to a valid
+     * page dictionary
+     * @throws IndexOutOfBoundsException if the requested index is higher than the page count
      */
     public void renderPageToGraphics(int pageIndex, Graphics2D graphics, float scaleX, float scaleY, RenderDestination destination)
             throws IOException
