f9eac0e9ff7305ab39b215feb7ab43224ecd59e1
valery.bokov <vlry.bkv@gmail.com>
2025-10-10 12:18:21 +0200

    fix typos in Matrix class

--- pdfbox/src/main/java/org/apache/pdfbox/util/Matrix.java
+++ pdfbox/src/main/java/org/apache/pdfbox/util/Matrix.java
@@ -381,8 +381,8 @@ public final class Matrix implements Cloneable
      * 0 y 0
      * 0 0 1
      *
-     * @param x The xscale operator.
-     * @param y The yscale operator.
+     * @param x The x-scale operator.
+     * @param y The y-scale operator.
      * @return A new matrix with just the x/y scaling
      */
     public static Matrix getScaleInstance(float x, float y)
@@ -518,7 +518,7 @@ public final class Matrix implements Cloneable
     /**
      * Returns the x-shear element of this matrix.
      * 
-     * @return the x-sheard element of the matrix
+     * @return the x-shear element of the matrix
      */
     public float getShearX()
     {
