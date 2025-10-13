95b9d38f88cd5ac8d916ffd5763bc532ac56d5d8
Tilman Hausherr <tilman@apache.org>
2025-10-13 18:51:21 +0000

    PDFBOX-6084: remove dead code, as suggested by Valery Bokov
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1929129 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/text/PDFMarkedContentExtractor.java
+++ pdfbox/src/main/java/org/apache/pdfbox/text/PDFMarkedContentExtractor.java
@@ -203,48 +203,9 @@ public class PDFMarkedContentExtractor extends LegacyPDFStreamEngine
             }
         }
 
-        if( showCharacter )
+        if (showCharacter && !this.currentMarkedContents.isEmpty())
         {
-            List<TextPosition> textList = new ArrayList<>();
-
-            /* In the wild, some PDF encoded documents put diacritics (accents on
-             * top of characters) into a separate Tj element.  When displaying them
-             * graphically, the two chunks get overlaid.  With text output though,
-             * we need to do the overlay. This code recombines the diacritic with
-             * its associated character if the two are consecutive.
-             */ 
-            if(textList.isEmpty())
-            {
-                textList.add(text);
-            }
-            else
-            {
-                /* test if we overlap the previous entry.  
-                 * Note that we are making an assumption that we need to only look back
-                 * one TextPosition to find what we are overlapping.  
-                 * This may not always be true. */
-                TextPosition previousTextPosition = textList.get(textList.size()-1);
-                if(text.isDiacritic() && previousTextPosition.contains(text))
-                {
-                    previousTextPosition.mergeDiacritic(text);
-                }
-                /* If the previous TextPosition was the diacritic, merge it into this
-                 * one and remove it from the list. */
-                else if(previousTextPosition.isDiacritic() && text.contains(previousTextPosition))
-                {
-                    text.mergeDiacritic(previousTextPosition);
-                    textList.remove(textList.size()-1);
-                    textList.add(text);
-                }
-                else
-                {
-                    textList.add(text);
-                }
-            }
-            if (!this.currentMarkedContents.isEmpty())
-            {
-                this.currentMarkedContents.peek().addText(text);
-            }
+            this.currentMarkedContents.peek().addText(text);
         }
     }
 
