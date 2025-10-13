3df9680a2dbac0827e0aac343b17a20f88b03745
Tilman Hausherr <tilman@apache.org>
2025-10-13 17:59:04 +0000

    PDFBOX-6083: Sonar fix
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1929127 13f79535-47bb-0310-9956-ffa450edef68

--- io/src/test/java/org/apache/pdfbox/io/RandomAccessReadBufferedFileTest.java
+++ io/src/test/java/org/apache/pdfbox/io/RandomAccessReadBufferedFileTest.java
@@ -270,7 +270,7 @@ class RandomAccessReadBufferedFileTest
     }
 
     @Test
-    void testReadFullyAcrossBuffers() throws IOException, URISyntaxException
+    void testReadFullyAcrossBuffers() throws IOException
     {
         int bufferLen;
         File file = new File("src/test/java/org/apache/pdfbox/io/NonSeekableRandomAccessReadInputStreamTest.java");
