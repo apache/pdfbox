9b2b9fcc415297547af27699cf1b4e437206ba2e
Tilman Hausherr <tilman@apache.org>
2025-10-21 20:01:53 +0000

    PDFBOX-6086: avoid IndexOutOfBoundsException
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1929257 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/cff/Type1CharStringParser.java
+++ fontbox/src/main/java/org/apache/fontbox/cff/Type1CharStringParser.java
@@ -135,7 +135,7 @@ public class Type1CharStringParser
             LOG.warn("CALLSUBR is ignored, operand: {}, subrs.size(): {} in glyph '{}' of font {}",
                     operand, subrs.size(), currentGlyph, fontName);
             // remove all parameters (there can be more than one)
-            while (sequence.get(sequence.size() - 1) instanceof Integer)
+            while (!sequence.isEmpty() && sequence.get(sequence.size() - 1) instanceof Integer)
             {
                 sequence.remove(sequence.size() - 1);
             }
