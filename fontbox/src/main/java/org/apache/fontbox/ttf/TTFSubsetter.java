92e3488a31c8075ea93c894cf8b0026acc228765
Tilman Hausherr <tilman@apache.org>
2025-10-22 14:08:02 +0000

    PDFBOX-6085: add exception; revert bugfix; new bugfix that counts the initial skip
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1929289 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/ttf/TTFSubsetter.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/TTFSubsetter.java
@@ -525,10 +525,6 @@ public final class TTFSubsetter
                     {
                         LOG.debug("Tried skipping " + (offset - lastOff) + " bytes but skipped only " + isResult + " bytes");
                     }
-                    if (invisibleGlyphIds.contains(gid))
-                    {
-                        continue;
-                    }
                     byte[] buf = new byte[(int) length];
                     isResult = is.read(buf);
 
@@ -634,6 +630,7 @@ public final class TTFSubsetter
                 // corresponding 'loca' table entry with length = 0
                 if (invisibleGlyphIds.contains(gid))
                 {
+                    lastOff = offset;
                     continue;
                 }
 
@@ -659,6 +656,11 @@ public final class TTFSubsetter
 
                         // glyphIndex
                         int componentGid = (buf[off] & 0xff) << 8 | buf[off + 1] & 0xff;
+                        if (!glyphIds.contains(componentGid))
+                        {
+                            // PDFBOX-6085
+                            throw new IOException("Internal error: componentGid " + componentGid + " not in glyphIds set");
+                        }
                         int newComponentGid = getNewGlyphId(componentGid);
                         buf[off]   = (byte)(newComponentGid >>> 8);
                         buf[off + 1] = (byte)newComponentGid;
