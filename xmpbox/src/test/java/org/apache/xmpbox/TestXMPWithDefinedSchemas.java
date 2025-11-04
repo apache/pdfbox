a162d29c44b78522ee0aea28557527154ea7e74d
Tilman Hausherr <tilman@apache.org>
2025-11-04 10:53:45 +0000

    PDFBOX-5660: close input, as suggested by Valery Bokov; refactor; closes #311
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1929507 13f79535-47bb-0310-9956-ffa450edef68

--- xmpbox/src/test/java/org/apache/xmpbox/TestXMPWithDefinedSchemas.java
+++ xmpbox/src/test/java/org/apache/xmpbox/TestXMPWithDefinedSchemas.java
@@ -21,10 +21,12 @@
 
 package org.apache.xmpbox;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
 
 import java.io.InputStream;
 import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.apache.xmpbox.xml.DomXmpParser;
 import org.apache.xmpbox.xml.XmpParsingException;
@@ -46,13 +48,14 @@ class TestXMPWithDefinedSchemas
 
     @ParameterizedTest
     @MethodSource("initializeParameters")
-    void main(String path) throws XmpParsingException
+    void main(String path) throws XmpParsingException, IOException
     {
-        InputStream is = this.getClass().getResourceAsStream(path);
-
-        DomXmpParser builder = new DomXmpParser();
-        XMPMetadata rxmp = builder.parse(is);
-        // ensure basic parsing was OK
-        assertTrue(rxmp.getAllSchemas().size()>0);
+        try (InputStream is = this.getClass().getResourceAsStream(path))
+        {
+            DomXmpParser builder = new DomXmpParser();
+            XMPMetadata rxmp = builder.parse(is);
+            // ensure basic parsing was OK
+            assertFalse(rxmp.getAllSchemas().isEmpty());
+        }
     }
 }
