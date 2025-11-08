2c6b5e68e69dce1948fce2ed18ca5ef63858407b
Tilman Hausherr <tilman@apache.org>
2025-11-08 09:21:00 +0000

    PDFBOX-5660: remove unused, as suggested by Valery Bokov; closes #318
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1929594 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/ttf/gsub/GsubWorkerFactory.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/gsub/GsubWorkerFactory.java
@@ -50,7 +50,7 @@ public class GsubWorkerFactory
         case GUJARATI:
             return new GsubWorkerForGujarati(cmapLookup, gsubData);
         case LATIN:
-            return new GsubWorkerForLatin(cmapLookup, gsubData);
+            return new GsubWorkerForLatin(gsubData);
         default:
             return new DefaultGsubWorker();
         }
