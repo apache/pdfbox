fb24082ae5fde69f3da4cf44ef38d8d718f15e55
Tilman Hausherr <tilman@apache.org>
2025-12-07 08:50:24 +0000

    PDFBOX-6118: avoid IllegalArgumentException if rasterDpi is 0
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1930306 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/printing/PDFPrintable.java
+++ pdfbox/src/main/java/org/apache/pdfbox/printing/PDFPrintable.java
@@ -267,7 +267,7 @@ public final class PDFPrintable implements Printable
             // rasterize to bitmap (optional)
             Graphics2D printerGraphics = null;
             BufferedImage image = null;
-            if (dpi > 0 || dpi == RASTERIZE_DPI_AUTO)
+            if (rasterDpi > 0)
             {
                 LOG.debug("dpi set to {}", rasterDpi);
                 float dpiScale = rasterDpi / 72;
