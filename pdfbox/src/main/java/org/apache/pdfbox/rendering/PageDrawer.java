be39de5125a9bc27ded6eb8ea7b2af2ff9305d98
Tilman Hausherr <tilman@apache.org>
2025-12-10 05:51:10 +0000

    PDFBOX-5660: optimize, as suggested by Valery Bokov, closes #365
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1930418 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/PageDrawer.java
@@ -1767,8 +1767,9 @@ public class PageDrawer extends PDFGraphicsStreamEngine
         {
             return;
         }
+        PDGraphicsState graphicsState = getGraphicsState();
         TransparencyGroup group =
-                new TransparencyGroup(form, false, getGraphicsState().getCurrentTransformationMatrix(), null);
+                new TransparencyGroup(form, false, graphicsState.getCurrentTransformationMatrix(), null);
         BufferedImage image = group.getImage();
         if (image == null)
         {
@@ -1776,7 +1777,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
             return;
         }
 
-        graphics.setComposite(getGraphicsState().getNonStrokingJavaComposite());
+        graphics.setComposite(graphicsState.getNonStrokingJavaComposite());
         setClip();
 
         // both the DPI xform and the CTM were already applied to the group, so all we do
@@ -1801,7 +1802,7 @@ public class PageDrawer extends PDFGraphicsStreamEngine
             graphics.translate(x * xformScalingFactorX, y * xformScalingFactorY);
         }
 
-        PDSoftMask softMask = getGraphicsState().getSoftMask();
+        PDSoftMask softMask = graphicsState.getSoftMask();
         if (softMask != null)
         {
             Paint awtPaint = new TexturePaint(image,
