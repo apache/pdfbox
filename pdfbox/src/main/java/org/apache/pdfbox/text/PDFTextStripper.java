28423e15b045503cf94bef30b349f1c6e1c52c25
Tilman Hausherr <tilman@apache.org>
2025-12-14 10:16:08 +0000

    PDFBOX-5660: fix typo, as suggested by Valery Bokov, closes #378
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1930550 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/text/PDFTextStripper.java
+++ pdfbox/src/main/java/org/apache/pdfbox/text/PDFTextStripper.java
@@ -1195,7 +1195,7 @@ public class PDFTextStripper extends LegacyPDFStreamEngine
     }
 
     /**
-     * By default the text stripper will attempt to remove text that overlapps each other. Word paints the same
+     * By default the text stripper will attempt to remove text that overlaps each other. Word paints the same
      * character several times in order to make it look bold. By setting this to false all text will be extracted, which
      * means that certain sections will be duplicated, but better performance will be noticed.
      *
