36a441398c85e8741a25fb938179e3b01c880a19
Tilman Hausherr <tilman@apache.org>
2025-12-25 08:05:09 +0000

    PDFBOX-5660: fix typo, improve javadoc
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1930841 13f79535-47bb-0310-9956-ffa450edef68

--- xmpbox/src/main/java/org/apache/xmpbox/type/PropertiesDescription.java
+++ xmpbox/src/main/java/org/apache/xmpbox/type/PropertiesDescription.java
@@ -27,12 +27,13 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Represents all properties known for a specific namespace Type and attributes associated to each properties are saved
- * If a specific type well declared is used, this class map it to a basic type
- * 
+ * Represents all properties known for a specific namespace Type and attributes associated to each
+ * properties. If a specific well declared type is used, this class maps it to a basic type.
+ *
  * @author a183132
- * 
- *         Attribute management pre-implemented in order to give clues to make an attribute management system
+ *
+ * Attribute management pre-implemented in order to give clues to make an attribute management
+ * system.
  */
 public class PropertiesDescription
 {
@@ -49,11 +50,11 @@ public class PropertiesDescription
     }
 
     /**
-     * Get All Properties Name
+     * Get all property names
      * 
      * @return a list of properties qualifiedName
      */
-    public List<String> getPropertiesName()
+    public List<String> getPropertiesNames()
     {
         return new ArrayList<>(types.keySet());
     }
