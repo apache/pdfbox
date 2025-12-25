e02c97f0a6ab2ab7c99a386089edf1637213b2da
Tilman Hausherr <tilman@apache.org>
2025-12-25 07:51:06 +0000

    PDFBOX-5660: implement toString for better debugging
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1930839 13f79535-47bb-0310-9956-ffa450edef68

--- xmpbox/src/main/java/org/apache/xmpbox/type/TypeMapping.java
+++ xmpbox/src/main/java/org/apache/xmpbox/type/TypeMapping.java
@@ -416,6 +416,12 @@ public final class TypeMapping
             {
                 return card;
             }
+
+            @Override
+            public String toString()
+            {
+                return "{type: " + type + ", card: " + card + '}';
+            }
         };
     }
 }
