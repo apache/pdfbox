3247cd26a87cac005a9ab613758c7816aedbdaf3
Tilman Hausherr <tilman@apache.org>
2025-12-27 14:10:33 +0000

    PDFBOX-6131: use jdk7
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1930888 13f79535-47bb-0310-9956-ffa450edef68

--- xmpbox/src/test/java/org/apache/xmpbox/xml/DomXmpParserTest.java
+++ xmpbox/src/test/java/org/apache/xmpbox/xml/DomXmpParserTest.java
@@ -1454,10 +1454,11 @@ class DomXmpParserTest
         // Contains "Seq Text" instead of "seq Text" and "Bag Text" instead of "bag Text"
         // from file RMR6DEEUWZO6IM3A7WKRPX33SZMBTTQZ
         // Fairfax County Office of Community Revitalization
-        InputStream is = DomXmpParser.class.getResourceAsStream("/org/apache/xmpbox/xml/PDFBOX-6131-RMR6DEEUWZO6IM3A7WKRPX33SZMBTTQZ.xml");
-        DomXmpParser xmpParser = new DomXmpParser();
-        XMPMetadata xmp = xmpParser.parse(is);
-        assertEquals(1, xmp.getPDFAIdentificationSchema().getPart());
-        is.close();
+        try (InputStream is = DomXmpParser.class.getResourceAsStream("/org/apache/xmpbox/xml/PDFBOX-6131-RMR6DEEUWZO6IM3A7WKRPX33SZMBTTQZ.xml"))
+        {
+            DomXmpParser xmpParser = new DomXmpParser();
+            XMPMetadata xmp = xmpParser.parse(is);
+            assertEquals(1, xmp.getPDFAIdentificationSchema().getPart());
+        }
     }
 }
