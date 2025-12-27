1f02e14bcae0c2f0e048f8392716fe53863818a5
Tilman Hausherr <tilman@apache.org>
2025-12-27 16:22:29 +0000

    PDFBOX-6133: introduce new parameter in getSpecifiedPropertyType for later changes, rename some variables for clarity - no changes in functionality in this commit
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1930896 13f79535-47bb-0310-9956-ffa450edef68

--- xmpbox/src/main/java/org/apache/xmpbox/type/TypeMapping.java
+++ xmpbox/src/main/java/org/apache/xmpbox/type/TypeMapping.java
@@ -274,15 +274,22 @@ public final class TypeMapping
         return isDefinedSchema(namespace) || isStructuredTypeNamespace(namespace) || isDefinedTypeNamespace(namespace);
     }
 
+    @Deprecated
+    public PropertyType getSpecifiedPropertyType(QName qName) throws BadFieldValueException
+    {
+        return getSpecifiedPropertyType(qName, null);
+    }
+
     /**
      * Give type of specified property in specified schema (given by its namespaceURI)
-     * 
-     * @param qName
-     *            the property Qualified Name
+     *
+     * @param qName the property Qualified Name
+     * @param parentTypeName the type name of the parent, or null if not known. This is intended to
+     * help when the field name is in several types, e.g. "Values" in exif.
      * @return Property type declared for namespace specified, null if unknown
-     * @throws org.apache.xmpbox.type.BadFieldValueException if the name was not found.
+     * @throws org.apache.xmpbox.type.BadFieldValueException if the name of a type was not found.
      */
-    public PropertyType getSpecifiedPropertyType(QName qName) throws BadFieldValueException
+    public PropertyType getSpecifiedPropertyType(QName qName, String parentTypeName) throws BadFieldValueException
     {
         XMPSchemaFactory factory = getSchemaFactory(qName.getNamespaceURI());
         if (factory != null)
