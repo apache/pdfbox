88be41572a67dade122a75baec7356be544b928c
Tilman Hausherr <tilman@apache.org>
2025-12-29 19:35:01 +0000

    PDFBOX-5660: remove dead code, add comment
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1930970 13f79535-47bb-0310-9956-ffa450edef68

--- xmpbox/src/main/java/org/apache/xmpbox/xml/XmpSerializer.java
+++ xmpbox/src/main/java/org/apache/xmpbox/xml/XmpSerializer.java
@@ -217,11 +217,10 @@ public class XmpSerializer
                     serializeFields(doc, econtainer, innerFields,resourceNS, null, true);
                 }
             }
-            else
-            {
-                // XXX finish serialization classes
-                System.err.println(">> TODO >> " + field.getClass());
-            }
+            // else doesn't happen:
+            // AbstractField is only extended by AbstractSimpleProperty and AbstractComplexProperty
+            // AbstractComplexProperty is only extended by AbstractStructuredType and ArrayProperty
+            // And all these are handled here.
         }
 
     }
