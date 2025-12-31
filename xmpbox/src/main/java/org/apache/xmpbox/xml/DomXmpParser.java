19ff679697e6115b239e2e821aabcc87f5ae5aec
Tilman Hausherr <tilman@apache.org>
2025-12-31 19:26:26 +0000

    PDFBOX-5660: remove unneeded null check
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1931017 13f79535-47bb-0310-9956-ffa450edef68

--- xmpbox/src/main/java/org/apache/xmpbox/xml/DomXmpParser.java
+++ xmpbox/src/main/java/org/apache/xmpbox/xml/DomXmpParser.java
@@ -383,7 +383,7 @@ public class DomXmpParser
                             + "' is a structured or array type, but attributes are simple types");
                 }
                 // PDFBOX-6125: Default to text or skip
-                if (attr.getValue() == null || attr.getValue().isEmpty())
+                if (attr.getValue().isEmpty())
                 {
                     schema.removeAttribute(attr.getLocalName());
                     return;
@@ -1188,7 +1188,7 @@ public class DomXmpParser
                                 + "' is a structured or array type, but attributes are simple types");
                     }
                     // PDFBOX-6125: Default to text or skip
-                    if (attr.getValue() == null || attr.getValue().isEmpty())
+                    if (attr.getValue().isEmpty())
                     {
                         continue;
                     }
