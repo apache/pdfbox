7ee6d390278fd0b06668ec65ede14810c6075ec9
Tilman Hausherr <tilman@apache.org>
2026-01-09 15:27:57 +0000

    PDFBOX-6143: avoid NPE
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1931195 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDAcroForm.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDAcroForm.java
@@ -28,6 +28,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 import org.apache.commons.logging.Log;
@@ -482,7 +483,7 @@ public final class PDAcroForm implements COSObjectable
         // get the field from the field tree
         for (PDField field : getFieldTree())
         {
-            if (field.getFullyQualifiedName().equals(fullyQualifiedName))
+            if (Objects.equals(field.getFullyQualifiedName(), fullyQualifiedName))
             {
                 return field;
             }
