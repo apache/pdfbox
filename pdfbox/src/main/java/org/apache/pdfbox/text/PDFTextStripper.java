f105675daafbe53cd43cdcc397490a16ea8df95d
Tilman Hausherr <tilman@apache.org>
2026-01-14 12:30:17 +0000

    PDFBOX-6145: revert last change because it breaks tika with pages that have no contents but have annotations
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1931312 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/text/PDFTextStripper.java
+++ pdfbox/src/main/java/org/apache/pdfbox/text/PDFTextStripper.java
@@ -299,7 +299,10 @@ public class PDFTextStripper extends LegacyPDFStreamEngine
 
         for (PDPage page : pages)
         {
-            processPage(page);
+            if (page.hasContents())
+            {
+                processPage(page);
+            }
             currentPageNo++;
         }
     }
@@ -341,10 +344,6 @@ public class PDFTextStripper extends LegacyPDFStreamEngine
                 && (startBookmarkPageNumber == -1 || currentPageNo >= startBookmarkPageNumber)
                 && (endBookmarkPageNumber == -1 || currentPageNo <= endBookmarkPageNumber))
         {
-            if (!page.hasContents())
-            {
-                return;
-            }
             startPage(page);
 
             int numberOfArticleSections = 1;
