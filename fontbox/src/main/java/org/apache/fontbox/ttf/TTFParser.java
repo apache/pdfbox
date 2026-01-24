d093900520d21cf5ace4833031da4f992d7079dc
Tilman Hausherr <tilman@apache.org>
2026-01-24 13:43:33 +0000

    PDFBOX-5660: improve comments, as suggested by Valery Bokov; closes #405
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1931505 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/ttf/TTFParser.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/TTFParser.java
@@ -99,9 +99,9 @@ public class TTFParser
     }
 
     /**
-     * Parse a RandomAccessRead and return a TrueType font.
+     * Parse a RandomAccessRead and return the table headers.
      *
-     * @param randomAccessRead The RandomAccessREad to be read from. It will be closed before returning.
+     * @param randomAccessRead The randomAccessRead to be read from. It will be closed before returning.
      * @return TrueType font headers.
      * @throws IOException If there is an error parsing the TrueType font.
      */
@@ -248,7 +248,7 @@ public class TTFParser
      * 
      * This method can be optimized further by skipping unused portions inside each individual table parser
      *
-     * @param font the TrueTypeFont instance holding the parsed data.
+     * @param raf the TrueType data stream.
      * @throws IOException If there is an error parsing the TrueType font.
      */
     FontHeaders parseTableHeaders(TTFDataStream raf) throws IOException
