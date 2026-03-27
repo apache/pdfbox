84c014f843e7a0c25e0d93f303e9f94e3bb575e0
Tilman Hausherr <tilman@apache.org>
2026-03-27 14:19:34 +0000

    PDFBOX-5660: sonar fix
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1932594 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/CreateVisibleSignature2.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/CreateVisibleSignature2.java
@@ -163,7 +163,7 @@ public class CreateVisibleSignature2 extends CreateSignatureBase
 
         // creating output document and prepare the IO streams.
 
-        SignatureOptions signatureOptions = new SignatureOptions();
+        SignatureOptions signatureOptions = null;
         try (FileOutputStream fos = new FileOutputStream(signedFile);
                 PDDocument doc = Loader.loadPDF(inputFile))
         {
@@ -297,13 +297,15 @@ public class CreateVisibleSignature2 extends CreateSignatureBase
                 doc.saveIncremental(fos);
             }
         }
-        
-        // Do not close signatureOptions before saving, because some COSStream objects within
-        // are transferred to the signed document.
-        // Do not allow signatureOptions get out of scope before saving, because then the COSDocument
-        // in signature options might by closed by gc, which would close COSStream objects prematurely.
-        // See https://issues.apache.org/jira/browse/PDFBOX-3743
-        IOUtils.closeQuietly(signatureOptions);
+        finally
+        {        
+            // Do not close signatureOptions before saving, because some COSStream objects within
+            // are transferred to the signed document.
+            // Do not allow signatureOptions get out of scope before saving, because then the COSDocument
+            // in signature options might by closed by gc, which would close COSStream objects prematurely.
+            // See https://issues.apache.org/jira/browse/PDFBOX-3743
+            IOUtils.closeQuietly(signatureOptions);
+        }
     }
 
     private PDRectangle createSignatureRectangle(PDDocument doc, Rectangle2D humanRect)
