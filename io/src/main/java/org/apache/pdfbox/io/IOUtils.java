06e2344f0da676f9d6f5088122903f560216aa44
Maruan Sahyoun <msahyoun@apache.org>
2026-04-02 10:12:58 +0000

    PDFBOX-6185: HeapByteBuffers don't need unmapping
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1932735 13f79535-47bb-0310-9956-ffa450edef68

--- io/src/main/java/org/apache/pdfbox/io/IOUtils.java
+++ io/src/main/java/org/apache/pdfbox/io/IOUtils.java
@@ -193,7 +193,9 @@ public final class IOUtils
     {
         try
         {
-            if (buf != null)
+            // HeapByteBuffers don't need to be unmapped, and unmapping only works for direct buffers,
+            //  so we can skip it in that case.
+            if (buf != null && buf.isDirect())
             {
                 UNMAPPER.ifPresent(u -> u.accept(buf));
             }
@@ -286,6 +288,7 @@ public final class IOUtils
         return (ByteBuffer buffer) -> {
             if (!buffer.isDirect())
             {
+                // defensive check, should not happen as we only call this method with direct buffers
                 throw new IllegalArgumentException("unmapping only works with direct buffers");
             }
             if (!unmappableBufferClass.isInstance(buffer))
