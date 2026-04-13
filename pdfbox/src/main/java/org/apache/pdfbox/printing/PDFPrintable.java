7e64cc548da91a4b7ee83c2f047409a6925738f8
Tilman Hausherr <tilman@apache.org>
2026-04-13 11:44:08 +0000

    PDFBOX-5660: avoid graphics leak, as suggested by Valery Bokov; closes #440
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1933018 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/printing/PDFPrintable.java
+++ pdfbox/src/main/java/org/apache/pdfbox/printing/PDFPrintable.java
@@ -205,9 +205,14 @@ public final class PDFPrintable implements Printable
         {
             return NO_SUCH_PAGE;
         }
+
+        Graphics2D printerGraphics = null;
+        Graphics2D graphics2D = null;
+
         try
         {
-            Graphics2D graphics2D = (Graphics2D)graphics;
+            printerGraphics = (Graphics2D) graphics;
+            graphics2D = printerGraphics;
 
             // capture the DPI that will be used for rasterizing the image
             // if rasterizing is specified
@@ -270,7 +275,6 @@ public final class PDFPrintable implements Printable
             }
 
             // rasterize to bitmap (optional)
-            Graphics2D printerGraphics = null;
             BufferedImage image = null;
             if (rasterDpi > 0)
             {
@@ -283,7 +287,6 @@ public final class PDFPrintable implements Printable
                                           (int)(imageableHeight * dpiScale / scale),
                                           BufferedImage.TYPE_INT_ARGB);
 
-                printerGraphics = graphics2D;
                 graphics2D = image.createGraphics();
 
                 // rescale
@@ -310,12 +313,11 @@ public final class PDFPrintable implements Printable
             }
 
             // draw rasterized bitmap (optional)
-            if (printerGraphics != null)
+            if (graphics2D != printerGraphics)
             {
                 printerGraphics.setBackground(Color.WHITE);
                 printerGraphics.clearRect(0, 0, image.getWidth(), image.getHeight());
                 printerGraphics.drawImage(image, 0, 0, null);
-                graphics2D.dispose();
             }
 
             return PAGE_EXISTS;
@@ -323,6 +325,13 @@ public final class PDFPrintable implements Printable
         catch (IOException e)
         {
             throw new PrinterIOException(e);
+        }
+        finally
+        {
+            if (graphics2D != null && graphics2D != printerGraphics)
+            {
+                graphics2D.dispose();
+            }
         }
     }
 
