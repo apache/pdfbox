b9f3049983750b68afd61b05c1dec4b2cf4834e1
Tilman Hausherr <tilman@apache.org>
2026-04-16 14:27:03 +0000

    PDFBOX-5660: simplify code, as suggested by copilot
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1933112 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/printing/PDFPageable.java
+++ pdfbox/src/main/java/org/apache/pdfbox/printing/PDFPageable.java
@@ -188,12 +188,11 @@ public final class PDFPageable extends Book
         //
         // As a workaround, we normalise all Page(s) to be portrait, then flag them as landscape in
         // the PageFormat.
-        Paper paper;
+        Paper paper = new Paper();
         boolean isLandscape;
         if (mediaBox.getWidth() > mediaBox.getHeight())
         {
             // rotate
-            paper = new Paper();
             paper.setSize(mediaBox.getHeight(), mediaBox.getWidth());
             paper.setImageableArea(cropBox.getLowerLeftY(), cropBox.getLowerLeftX(),
                     cropBox.getHeight(), cropBox.getWidth());
@@ -201,7 +200,6 @@ public final class PDFPageable extends Book
         }
         else
         {
-            paper = new Paper();
             paper.setSize(mediaBox.getWidth(), mediaBox.getHeight());
             paper.setImageableArea(cropBox.getLowerLeftX(), cropBox.getLowerLeftY(),
                     cropBox.getWidth(), cropBox.getHeight());
