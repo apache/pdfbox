23e2b65bb01a8e5f1628382b97fbe4a8f087efb6
Tilman Hausherr <tilman@apache.org>
2026-05-09 15:36:50 +0000

    PDFBOX-5660: split file to have one class per file, to avoid warning about auxiliary class
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1933995 13f79535-47bb-0310-9956-ffa450edef68

--- debugger/src/main/java/org/apache/pdfbox/debugger/streampane/tooltip/ToolTipController.java
+++ debugger/src/main/java/org/apache/pdfbox/debugger/streampane/tooltip/ToolTipController.java
@@ -27,11 +27,6 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.pdfbox.contentstream.operator.OperatorName;
 import org.apache.pdfbox.pdmodel.PDResources;
 
-interface ToolTip
-{
-    String getToolTipText();
-}
-
 /**
  * @author Khyrul Bashar
  * A class that provides the tooltip for an operator.
