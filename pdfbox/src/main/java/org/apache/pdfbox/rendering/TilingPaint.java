1e5bee9b58340e7d9061cd16535b8cb7c7f98de3
Tilman Hausherr <tilman@apache.org>
2026-05-24 12:48:25 +0000

    PDFBOX-5660: dispose graphics in finally block, as suggested by Valery Bokov; closes #455
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1934553 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/TilingPaint.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/TilingPaint.java
@@ -172,8 +172,14 @@ class TilingPaint implements Paint
         newPatternMatrix.translate(-bBox.getLowerLeftX(), -bBox.getLowerLeftY());
 
         // render using PageDrawer
-        drawer.drawTilingPattern(graphics, pattern, colorSpace, color, newPatternMatrix);
-        graphics.dispose();
+        try
+        {
+            drawer.drawTilingPattern(graphics, pattern, colorSpace, color, newPatternMatrix);
+        }
+        finally
+        {
+            graphics.dispose();
+        }
 
         return image;
     }
