1c4fdf55974fc400c2eec685e934b278c36b013e
Tilman Hausherr <tilman@apache.org>
2026-06-06 20:20:08 +0000

    PDFBOX-5660: simplify
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1935084 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/test/java/org/apache/pdfbox/multipdf/PDFMergerUtilityTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/multipdf/PDFMergerUtilityTest.java
@@ -1494,7 +1494,7 @@ class PDFMergerUtilityTest
                 checkWithNumberTree(dstDoc);
                 checkForPageOrphans(dstDoc);
             }
-            splitResult.stream().forEach(IOUtils::closeQuietly);
+            splitResult.forEach(IOUtils::closeQuietly);
         }
     }
 
