6e101ab35fe2e3835f5fdb679e1514ef933fa1d6
Tilman Hausherr <tilman@apache.org>
2026-06-07 18:48:39 +0000

    PDFBOX-5660: Sonar fix
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1935118 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/StandardSecurityHandler.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/encryption/StandardSecurityHandler.java
@@ -1183,7 +1183,7 @@ public final class StandardSecurityHandler extends SecurityHandler<StandardProte
                 byte[] eFirst = new byte[16];
                 System.arraycopy(e, 0, eFirst, 0, 16);
                 BigInteger bi = new BigInteger(1, eFirst);
-                BigInteger remainder = bi.mod(new BigInteger("3"));
+                BigInteger remainder = bi.mod(BigInteger.valueOf(3));
                 String nextHash = HASHES_2B[remainder.intValue()];
                 
                 md = MessageDigest.getInstance(nextHash);
