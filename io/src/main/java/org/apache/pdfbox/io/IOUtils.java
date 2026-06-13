9930bfec360ad418a99598db9189eec530fb286f
Tilman Hausherr <tilman@apache.org>
2026-06-13 10:43:43 +0000

    PDFBOX-5660: fix comment, as suggested by Valery Bokov; closes #471
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1935226 13f79535-47bb-0310-9956-ffa450edef68

--- io/src/main/java/org/apache/pdfbox/io/IOUtils.java
+++ io/src/main/java/org/apache/pdfbox/io/IOUtils.java
@@ -502,7 +502,7 @@ public final class IOUtils
      * @throws IOException if an I/O error occurs while setting POSIX permissions or accessing the file
      * @throws SecurityException if a security manager is installed and denies access to the file
      * @see Files#setPosixFilePermissions(Path, Set)
-     * @see Files#getFileAttributeView(Path, Class)
+     * @see Files#getFileAttributeView(Path, Class, LinkOption...)
      */
     private static void applyOwnerOnlyPermissions(Path path, boolean isDirectory) throws IOException
     {
