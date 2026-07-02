9e8bb7592a77fdfdcfd50be49c053459896563fd
Tilman Hausherr <tilman@apache.org>
2026-07-02 03:43:15 +0000

    PDFBOX-6214: catch UnsatisfiedLinkError as suggested by Wanling Fu
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1935788 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDDocument.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/PDDocument.java
@@ -111,6 +111,11 @@ public class PDDocument implements Closeable
         {
             LOG.debug("voodoo error", ex);
         }
+        catch (UnsatisfiedLinkError ex)
+        {
+            // PDFBOX-6214: awt missing with Graal
+            LOG.debug("UnsatisfiedLinkError", ex);
+        }
 
         try
         {
