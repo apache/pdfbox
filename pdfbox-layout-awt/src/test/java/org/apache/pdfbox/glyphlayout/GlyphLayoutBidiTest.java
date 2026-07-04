fe9e032112da3802b60406963bc0137a098d58ef
Tilman Hausherr <tilman@apache.org>
2026-07-04 13:31:20 +0000

    PDFBOX-4951: add tests, by Volker Kunert; move resources to test for now
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1935881 13f79535-47bb-0310-9956-ffa450edef68

---
+++ pdfbox-layout-awt/src/test/java/org/apache/pdfbox/glyphlayout/GlyphLayoutBidiTest.java
@@ -0,0 +1,117 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pdfbox.glyphlayout;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.FontFormatException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
+
+/**
+ * Examples for bidirectional text with GlyphLayoutProcessorAwt
+ *
+ * @author Volker Kunert
+ */
+public class GlyphLayoutBidiTest
+{
+    public static final String TEXT1 = "نحن الآن في شهر رمضان 1447 هجري";
+    public static final String TEXT2 = "Guten Tag ";
+    public static final String TEXT3 = "السلام عليكم";
+    public static final String TEXT4 = " Good afternoon";
+
+    /*
+     * show one line
+     */
+    private float showLine(PDPageContentStream cs, PDType0Font font, float fontSize,
+            float x, float y, String text) throws IOException
+    {
+        return showLine(cs, new PDType0Font[]{font}, fontSize, x, y, new String[]{text});
+    }
+
+    /*
+     * show one line
+     */
+    private float showLine(PDPageContentStream cs, PDType0Font[] fonts, float fontSize,
+            float x, float y, String[] texts) throws IOException
+    {
+        cs.beginText();
+        cs.newLineAtOffset(x, y);
+
+        if (fonts.length != texts.length)
+        {
+            throw new IllegalArgumentException("Size of fonts and texts is different");
+        }
+        for (int i = 0; i < texts.length; i++)
+        {
+            cs.setFont(fonts[i], fontSize);
+            cs.showText(texts[i]);
+        }
+        cs.endText();
+
+        float height = fonts[0].getBoundingBox().getHeight();
+        y -= height / 1000f * fontSize;
+        return y;
+    }
+
+    @Test
+    void testGlyphLayoutBidi() throws IOException, FontFormatException
+    {
+        GlyphLayoutProcessorAwt glyphLayoutProcessorAwt = new GlyphLayoutProcessorAwt();
+
+        String outputFilename = "target/GlyphLayoutBidi.pdf";
+        String arabicPath = "/ttf/NotoSansArabic-Regular.ttf";
+        String lgcPath = "/ttf/DejaVuSans.ttf";
+
+        float fontSize = 12.0f;
+
+        try (PDDocument doc = new PDDocument())
+        {
+            PDType0Font arabicFont = createPdType0Font(glyphLayoutProcessorAwt, doc, arabicPath);
+            PDType0Font lgcFont = createPdType0Font(glyphLayoutProcessorAwt, doc, lgcPath);
+
+            PDPage page = new PDPage();
+            doc.addPage(page);
+            try (PDPageContentStream cs = new PDPageContentStream(doc, page))
+            {
+                cs.setGlyphLayoutProcessor(glyphLayoutProcessorAwt);
+                
+                float x = page.getBBox().getLowerLeftX() + fontSize;
+                float y = page.getBBox().getUpperRightY() - fontSize;
+                
+                y = showLine(cs, arabicFont, fontSize, x, y, TEXT1);
+                showLine(cs, new PDType0Font[]{ lgcFont, arabicFont, lgcFont }, fontSize, x, y, new String[]{ TEXT2, TEXT3, TEXT4 });
+            }
+            doc.save(outputFilename);
+        }
+    }
+
+    /*
+     * Create the PDType0Font font
+     */
+    private PDType0Font createPdType0Font(GlyphLayoutProcessorAwt glyphLayoutProcessorAwt, PDDocument doc,
+            String fontPath) throws IOException, FontFormatException
+    {
+        InputStream fontStream = this.getClass().getResourceAsStream(fontPath);
+        return glyphLayoutProcessorAwt.loadFont(doc, fontStream);
+    }
+}
---
+++ pdfbox-layout-awt/src/test/java/org/apache/pdfbox/glyphlayout/GlyphLayoutLigaturesAndKerningTest.java
@@ -0,0 +1,190 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.pdfbox.glyphlayout;
+
+/**
+ * Examples for ligatures and kerning
+ * See <a href="https://issues.apache.org/jira/browse/PDFBOX-4951">PDFBOX-4951</a>
+ *
+ * The default processing of GlyphLayoutProcessor is with ligatures and kerning disabled.
+ * You can enable ligatures and kerning using FontOptions, see below.
+ *
+ * @author Volker Kunert
+ */
+
+import java.awt.FontFormatException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
+
+public class GlyphLayoutLigaturesAndKerningTest
+{
+    static final String FIRACODE_STRING = "!= == === >= <=";
+    static final String DEJAVU_STRING =  "AVATAR, effective, affiliation, float, film, affluent";
+    static final String BENGALI_STRING =  "আমি কোন পথে ক্ষীরের লক্ষ্মী ষন্ড পুতুল রুপো গঙ্গা ঋষি";
+    
+    /**
+     * Check that missing glyph is caught like in main pdfbox.
+     *
+     * @throws IOException
+     * @throws FontFormatException 
+     */
+    @Test
+    void testMissingGlyph() throws IOException, FontFormatException
+    {
+        GlyphLayoutProcessorAwt glyphLayoutProcessor = new GlyphLayoutProcessorAwt();
+
+        String lohitBengaliPath = "/ttf/Lohit-Bengali.ttf";
+
+        try (PDDocument doc = new PDDocument())
+        {
+            PDType0Font lohitBengaliFont = createPdType0Font(glyphLayoutProcessor, doc, lohitBengaliPath);
+
+            PDPage page = new PDPage();
+            doc.addPage(page);
+            try (PDPageContentStream cs = new PDPageContentStream(doc, page))
+            {
+                cs.setGlyphLayoutProcessor(glyphLayoutProcessor);
+                
+                IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> 
+                        showComposites(cs, lohitBengaliFont, 1, 0, 0, "123ABC"));
+                assertEquals("Missing glyph in font 'Lohit Bengali' for the character 'A', codePoint: 65 (U+0041).", ex.getMessage());
+            }
+        }
+    }
+  
+    @Test
+    void testLigaturesAndKerning() throws IOException, FontFormatException
+    {
+        GlyphLayoutProcessorAwt glyphLayoutProcessor = new GlyphLayoutProcessorAwt();
+
+        String outputFilename = "target/GlyphLayoutLigaturesAndKerning.pdf";
+        String firaPath = "/ttf/FiraCode-Regular.ttf";
+        String dejavuPath = "/ttf/DejaVuSans.ttf"; // ligatures not in Liberation nor in Arimo
+        String lohitBengaliPath = "/ttf/Lohit-Bengali.ttf";
+
+        float fontSize = 12.0f;
+
+        try (PDDocument doc = new PDDocument())
+        {
+            PDType0Font firaFont = createPdType0Font(glyphLayoutProcessor, doc, firaPath);
+            PDType0Font firaLigFont = createPdType0Font(glyphLayoutProcessor, doc, firaPath,
+                    new GlyphLayoutFontLoaderAwt.FontOptions().setLigaturesOn());
+            
+            PDType0Font dejavuFont = createPdType0Font(glyphLayoutProcessor, doc, dejavuPath);
+            
+            PDType0Font dejavuLigFont = createPdType0Font(glyphLayoutProcessor, doc, dejavuPath,
+                    new GlyphLayoutFontLoaderAwt.FontOptions().setLigaturesOn());
+            
+            PDType0Font dejavuKernFont = createPdType0Font(glyphLayoutProcessor, doc, dejavuPath,
+                    new GlyphLayoutFontLoaderAwt.FontOptions().setKerningOn());
+            
+            PDType0Font dejavuLigKernFont = createPdType0Font(glyphLayoutProcessor, doc, dejavuPath,
+                    new GlyphLayoutFontLoaderAwt.FontOptions().setLigaturesOn().setKerningOn());
+            
+            PDType0Font lohitBengaliFont = createPdType0Font(glyphLayoutProcessor, doc, lohitBengaliPath,
+                    new GlyphLayoutFontLoaderAwt.FontOptions());
+            
+            PDPage page = new PDPage();
+            doc.addPage(page);
+            try (PDPageContentStream cs = new PDPageContentStream(doc, page))
+            {
+                cs.setGlyphLayoutProcessor(glyphLayoutProcessor);
+                
+                float x = page.getBBox().getLowerLeftX() + fontSize;
+                float y = page.getBBox().getUpperRightY() - fontSize;
+                y = showComposites(cs, firaFont, fontSize, x, y, FIRACODE_STRING);
+                y = showComposites(cs, firaLigFont, fontSize, x, y, FIRACODE_STRING + " (Ligatures)");
+                y = showComposites(cs, dejavuFont, fontSize, x, y, DEJAVU_STRING);
+                y = showComposites(cs, dejavuLigFont, fontSize, x, y, DEJAVU_STRING + " (Ligatures)");
+                y = showComposites(cs, dejavuKernFont, fontSize, x, y, DEJAVU_STRING + " (Kerning)");
+                y = showComposites(cs, dejavuLigKernFont, fontSize, x, y, DEJAVU_STRING + " (Ligatures and kerning)");
+                showComposites(cs, lohitBengaliFont, fontSize, x, y, BENGALI_STRING + " (ভারত)");
+            }
+            doc.save(outputFilename);
+        }
+    }
+
+    /*
+     * Create the PDType0Font font
+     */
+    private PDType0Font createPdType0Font(GlyphLayoutProcessorAwt glyphLayoutProcessor, PDDocument pdDocument,
+            String fontPath) throws IOException, FontFormatException
+    {
+        InputStream fontStream = this.getClass().
+                getResourceAsStream(fontPath);
+        PDType0Font font = glyphLayoutProcessor.loadFont(pdDocument, fontStream);
+        return font;
+    }
+
+    /*
+     * Create the PDType0Font font with font options
+     */
+    private PDType0Font createPdType0Font(GlyphLayoutProcessorAwt glyphLayoutProcessor, PDDocument pdDocument,
+            String fontPath, GlyphLayoutFontLoaderAwt.FontOptions fontOptions) throws IOException, FontFormatException
+    {
+        InputStream fontStream = this.getClass().
+                getResourceAsStream(fontPath);
+        PDType0Font font = glyphLayoutProcessor.loadFont(pdDocument, fontStream, fontOptions);
+        return font;
+    }
+
+    /*
+     * break the text into lines and show them
+     */
+    public static float showComposites(PDPageContentStream cs, PDType0Font font, float fontSize,
+            float x, float y, String s) throws IOException
+    {
+
+        s = s.replaceAll("\t", "    ");
+        String[] lines = s.split("[\n]");
+
+        float height = font.getBoundingBox().getHeight();
+
+        for (String line : lines)
+        {
+            if (!line.isEmpty())
+            {
+                showCompositesLine(cs, font, fontSize, x, y, line);
+                y -= height / 1000f * fontSize;
+            }
+        }
+        return y;
+    }
+
+    /*
+     * show one line
+     */
+    public static void showCompositesLine(PDPageContentStream cs, PDType0Font font, float fontSize,
+            float x, float y, String line) throws IOException
+    {
+        cs.beginText();
+        cs.setFont(font, fontSize);
+        cs.newLineAtOffset(x, y);
+        cs.showText(line);
+        cs.endText();
+    }
+}
