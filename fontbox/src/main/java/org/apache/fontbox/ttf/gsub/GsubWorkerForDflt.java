2e7207ba9c1736b63922b0cc80563134ed90402d
Tilman Hausherr <tilman@apache.org>
2026-07-20 13:36:46 +0000

    PDFBOX-6220: cache splitter, as suggested by Olivier Bruchez
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1936379 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/main/java/org/apache/fontbox/ttf/gsub/GsubWorkerForDflt.java
+++ fontbox/src/main/java/org/apache/fontbox/ttf/gsub/GsubWorkerForDflt.java
@@ -22,6 +22,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.WeakHashMap;
 
 import org.apache.fontbox.ttf.model.GsubData;
@@ -100,7 +101,8 @@ public class GsubWorkerForDflt implements GsubWorker
     private List<Integer> applyGsubFeature(ScriptFeature scriptFeature,
             List<Integer> originalGlyphs)
     {
-        if (scriptFeature.getAllGlyphIdsForSubstitution().isEmpty())
+        Set<List<Integer>> allGlyphIdsForSubstitution = scriptFeature.getAllGlyphIdsForSubstitution();
+        if (allGlyphIdsForSubstitution.isEmpty())
         {
             LOG.debug("getAllGlyphIdsForSubstitution() for {} is empty", scriptFeature.getName());
             return originalGlyphs;
