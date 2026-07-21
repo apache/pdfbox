e8866e2e2659174587beea1701b569a541e5a58a
Tilman Hausherr <tilman@apache.org>
2026-07-21 19:06:12 +0000

    PDFBOX-5660: remove redundant SuppressWarning as suggested by Valery Bokov; use assume; closes #482
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1936435 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/digitalsignature/visible/PDVisibleSigBuilder.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/digitalsignature/visible/PDVisibleSigBuilder.java
@@ -132,7 +132,6 @@ public class PDVisibleSigBuilder implements PDFTemplateBuilder
     public void createAcroFormDictionary(PDAcroForm acroForm, PDSignatureField signatureField)
             throws IOException
     {
-        @SuppressWarnings("unchecked")
         List<PDField> acroFormFields = acroForm.getFields();
         COSDictionary acroFormDict = acroForm.getCOSObject();
         acroForm.setSignaturesExist(true);
