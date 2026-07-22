b827a10eb64b177b96faef5619ad354c906b3d25
Tilman Hausherr <tilman@apache.org>
2026-07-22 18:13:01 +0000

    PDFBOX-5660: Sonar fix (avoid NPE)
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/2.0@1936495 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDDeviceN.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/color/PDDeviceN.java
@@ -24,6 +24,7 @@ import java.awt.image.DataBuffer;
 import java.awt.image.Raster;
 import java.awt.image.WritableRaster;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -493,8 +494,15 @@ public class PDDeviceN extends PDSpecialColorSpace
      */
     public final List<String> getColorantNames()
     {
-        COSArray names = (COSArray)array.getObject(COLORANT_NAMES);
-        return COSArrayList.convertCOSNameCOSArrayToList(names);
+        COSBase base = array.getObject(COLORANT_NAMES);
+        if (base instanceof COSArray)
+        {
+            return COSArrayList.convertCOSNameCOSArrayToList((COSArray) base);
+        }
+        else
+        {
+            return Collections.emptyList();
+        }
     }
 
     /**
