cc19f8b934f9469c1e2d7f69f57b76bfc50e979e
Tilman Hausherr <tilman@apache.org>
2026-08-12 07:59:05 +0000

    PDFBOX-6235: fix NPE with CMYK images, set decode, add test, as suggested by Seungmin Lee
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1937070 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/image/JPEGFactory.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/graphics/image/JPEGFactory.java
@@ -311,10 +311,24 @@ public final class JPEGFactory
         // create XObject
         byte[] encoded = encodeImageToJPEGStream(awtColorImage, quality, dpi);
         ByteArrayInputStream encodedByteStream = new ByteArrayInputStream(encoded);
+        PDColorSpace colorSpace = getColorSpaceFromAWT(awtColorImage);
 
         PDImageXObject pdImage = new PDImageXObject(document, encodedByteStream, COSName.DCT_DECODE,
                 awtColorImage.getWidth(), awtColorImage.getHeight(), 8,
-                getColorSpaceFromAWT(awtColorImage));
+                colorSpace);
+        if (colorSpace instanceof PDDeviceCMYK)
+        {
+            COSArray decode = new COSArray();
+            decode.add(COSInteger.ONE);
+            decode.add(COSInteger.ZERO);
+            decode.add(COSInteger.ONE);
+            decode.add(COSInteger.ZERO);
+            decode.add(COSInteger.ONE);
+            decode.add(COSInteger.ZERO);
+            decode.add(COSInteger.ONE);
+            decode.add(COSInteger.ZERO);
+            pdImage.setDecode(decode);
+        }
 
         // extract alpha channel (if any)
         BufferedImage awtAlphaImage = getAlphaImage(image);
@@ -372,10 +386,16 @@ public final class JPEGFactory
             IIOMetadata data = imageWriter.getDefaultImageMetadata(imageTypeSpecifier, jpegParam);
             Element tree = (Element) data.getAsTree("javax_imageio_jpeg_image_1.0");
             Element jfif = (Element) tree.getElementsByTagName("app0JFIF").item(0);
-            String dpiString = Integer.toString(dpi);
-            jfif.setAttribute("Xdensity", dpiString);
-            jfif.setAttribute("Ydensity", dpiString);
-            jfif.setAttribute("resUnits", "1"); // 1 = dots/inch
+            // PDFBOX-6235: JFIF APP0 is only defined for 1- and 3-component JPEGs. A 4-component
+            // (CMYK) image gets an Adobe APP14 marker instead, so there is no app0JFIF
+            // node and the DPI cannot be expressed in the stream at all.
+            if (jfif != null)
+            {
+                String dpiString = Integer.toString(dpi);
+                jfif.setAttribute("Xdensity", dpiString);
+                jfif.setAttribute("Ydensity", dpiString);
+                jfif.setAttribute("resUnits", "1"); // 1 = dots/inch
+            }
 
             // write
             imageWriter.write(data, new IIOImage(image, null, null), jpegParam);
