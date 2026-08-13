1187c45f9dcee38ed5ac12bc15df04913b348875
Tilman Hausherr <tilman@apache.org>
2026-08-13 09:14:46 +0000

    PDFBOX-5660: improve output
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1937092 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/pdfbox/examples/signature/ShowSignature.java
+++ examples/src/main/java/org/apache/pdfbox/examples/signature/ShowSignature.java
@@ -549,7 +549,7 @@ public final class ShowSignature
         if (signerInformation.verify(new JcaSimpleSignerInfoVerifierBuilder().
                 setProvider(SecurityProvider.getProvider()).build(certFromSignedData)))
         {
-            System.out.println("Signature verified");
+            System.out.println(certFromSignedData.getSigAlgName() + " signature verified");
         }
         else
         {
@@ -558,7 +558,7 @@ public final class ShowSignature
 
         if (CertificateVerifier.isSelfSigned(certFromSignedData))
         {
-            System.err.println("Certificate is self-signed, LOL!");
+            System.err.println("Certificate for '" + certFromSignedData.getSubjectX500Principal().getName() + "' is self-signed, LOL!");
         }
         else
         {
