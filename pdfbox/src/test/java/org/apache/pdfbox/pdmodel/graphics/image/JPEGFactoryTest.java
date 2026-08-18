7c36593273af66b89ecc3e0f52b71513d6b4b108
Tilman Hausherr <tilman@apache.org>
2026-08-18 04:30:05 +0000

    PDFBOX-6235: fix equals, by Valery Bokov; closes #496
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1937191 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/test/java/org/apache/pdfbox/pdmodel/graphics/image/JPEGFactoryTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/graphics/image/JPEGFactoryTest.java
@@ -423,7 +423,7 @@ class JPEGFactoryTest
                 count += 3;
                 Color expectedRGB = new Color(expected.getRGB(x, y));
                 Color actualRGB = new Color(actual.getRGB(x, y));
-                if (expectedRGB == actualRGB)
+                if (expectedRGB.equals(actualRGB))
                 {
                     continue;
                 }
