1ba96c7131d449d9c6d63fdc631aac4205ffbaed
Tilman Hausherr <tilman@apache.org>
2026-08-23 11:01:29 +0000

    PDFBOX-6239: refactor
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1937345 13f79535-47bb-0310-9956-ffa450edef68

--- debugger/src/main/java/org/apache/pdfbox/debugger/PDFDebugger.java
+++ debugger/src/main/java/org/apache/pdfbox/debugger/PDFDebugger.java
@@ -927,12 +927,8 @@ public class PDFDebugger extends JFrame implements Callable<Integer>, HyperlinkL
             ArrayEntry entry = (ArrayEntry) selectedNode;
             if (entry.getValue() instanceof COSStream && parentNode instanceof MapEntry)
             {
-                MapEntry mapEntry = (MapEntry) parentNode;
-                if ((mapEntry.getKey().equals(COSName.CERT) ||
-                     mapEntry.getKey().equals(COSName.CERTS)))
-                {
-                    return true;
-                }
+                COSName key = ((MapEntry) parentNode).getKey();
+                return COSName.CERT.equals(key) || COSName.CERTS.equals(key);
             }
         }
         return false;
@@ -944,18 +940,13 @@ public class PDFDebugger extends JFrame implements Callable<Integer>, HyperlinkL
         if (selectedNode instanceof MapEntry)
         {
             MapEntry entry = (MapEntry) selectedNode;
-            if (entry.getKey().equals(COSName.CERT) && parentNode instanceof MapEntry)
+            if (COSName.CERT.equals(entry.getKey()) && parentNode instanceof MapEntry)
             {
                 MapEntry mapEntry = (MapEntry) parentNode;
-                COSDictionary sigDict;
                 if (mapEntry.getValue() instanceof COSDictionary)
                 {
-                    sigDict = (COSDictionary) mapEntry.getValue();
-                    COSName type = sigDict.getCOSName(COSName.TYPE);
-                    if (type != null && type.equals(COSName.SIG))
-                    {
-                        return true;
-                    }
+                    COSDictionary sigDict = (COSDictionary) mapEntry.getValue();
+                    return COSName.SIG.equals(sigDict.getCOSName(COSName.TYPE));
                 }
             }
         }
