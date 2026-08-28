a8a92c27b1e3500ec0b920aa07aedf1da7c1edec
Tilman Hausherr <tilman@apache.org>
2026-08-28 17:38:05 +0000

    PDFBOX-6145: remove content check so that not all pages get checked
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1937581 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/text/PDFTextStripper.java
+++ pdfbox/src/main/java/org/apache/pdfbox/text/PDFTextStripper.java
@@ -298,10 +298,7 @@ public class PDFTextStripper extends LegacyPDFStreamEngine
 
         for (PDPage page : pages)
         {
-            if (page.hasContents())
-            {
-                processPage(page);
-            }
+            processPage(page);
             currentPageNo++;
         }
     }
