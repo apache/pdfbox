633d1bacf487ef371bf124d7037eeb6115360467
Tilman Hausherr <tilman@apache.org>
2026-08-29 11:08:50 +0000

    PDFBOX-5660: improve test coverage
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1937608 13f79535-47bb-0310-9956-ffa450edef68

--- fontbox/src/test/java/org/apache/fontbox/ttf/TestTTFParser.java
+++ fontbox/src/test/java/org/apache/fontbox/ttf/TestTTFParser.java
@@ -160,6 +160,9 @@ class TestTTFParser
         assertTrue(ttf.hasGlyph("A"));
         assertFalse(ttf.hasGlyph("blubb"));
         assertEquals("LiberationSans", ttf.toString());
+        assertTrue(ttf.isEnableGsub());
+        ttf.setEnableGsub(false);
+        assertFalse(ttf.isEnableGsub());
     }
     
     @Test
