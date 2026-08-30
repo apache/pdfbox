d40f4f3b0e59b8d48856b97ae401e9a44a8131c4
Tilman Hausherr <tilman@apache.org>
2026-08-30 19:16:51 +0000

    PDFBOX-5660: simplify / optimize, as suggested by Valery Bokov; closes #510
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/branches/3.0@1937670 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType1FontEmbedder.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/font/PDType1FontEmbedder.java
@@ -28,7 +28,6 @@ import org.apache.fontbox.util.BoundingBox;
 import org.apache.pdfbox.cos.COSArray;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
-import org.apache.pdfbox.io.IOUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.common.PDStream;
@@ -60,9 +59,8 @@ class PDType1FontEmbedder
         dict.setItem(COSName.SUBTYPE, COSName.TYPE1);
 
         // read the pfb
-        byte[] pfbBytes = IOUtils.toByteArray(pfbStream);
-        PfbParser pfbParser = new PfbParser(pfbBytes);
-        type1 = Type1Font.createWithPFB(pfbBytes);
+        PfbParser pfbParser = new PfbParser(pfbStream);
+        type1 = Type1Font.createWithSegments(pfbParser.getSegment1(), pfbParser.getSegment2());
         
         if (encoding == null)
         {
