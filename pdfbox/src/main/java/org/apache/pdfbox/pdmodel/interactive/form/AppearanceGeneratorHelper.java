582567c5f087d10656ae3653558508f819142766
Tilman Hausherr <tilman@apache.org>
2026-08-30 12:39:35 +0000

    PDFBOX-5660: simplify / optimize, as suggested by Valery Bokov; closes #503
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1937653 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/AppearanceGeneratorHelper.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/AppearanceGeneratorHelper.java
@@ -438,7 +438,7 @@ class AppearanceGeneratorHelper
                 }
             }
             
-            writeToStream(output.toByteArray(), appearanceStream);
+            writeToStream(output, appearanceStream);
 
         }
     }
@@ -487,7 +487,7 @@ class AppearanceGeneratorHelper
                 // append contents after EMC
                 writer.writeTokens(tokens.subList(emcIndex, tokens.size()));
             }
-            writeToStream(output.toByteArray(), appearanceStream);
+            writeToStream(output, appearanceStream);
         }
     }
     
@@ -891,11 +891,11 @@ class AppearanceGeneratorHelper
      *
      * @throws IOException If there is an error writing to the stream
      */
-    private void writeToStream(byte[] data, PDAppearanceStream appearanceStream) throws IOException
+    private void writeToStream(ByteArrayOutputStream baos, PDAppearanceStream appearanceStream) throws IOException
     {
-        try (OutputStream out = appearanceStream.getCOSObject().createOutputStream())
+        try (OutputStream os = appearanceStream.getCOSObject().createOutputStream())
         {
-            out.write(data);
+            baos.writeTo(os);
         }
     }
 
