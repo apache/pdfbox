caaf99dbd89a1b0f787fb8202c838f2d497b7393
Tilman Hausherr <tilman@apache.org>
2026-09-10 19:33:34 +0000

    PDFBOX-6259: hasBlendMode() now recurses into the resources of form XObjects + test, by Daniel Persson and Claude Fable 5.1
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1938085 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/rendering/PDFRenderer.java
+++ pdfbox/src/main/java/org/apache/pdfbox/rendering/PDFRenderer.java
@@ -24,13 +24,20 @@ import java.awt.GraphicsDevice;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageTree;
 import org.apache.pdfbox.pdmodel.PDResources;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
 import org.apache.pdfbox.pdmodel.graphics.blend.BlendMode;
+import org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject;
 import org.apache.pdfbox.pdmodel.graphics.optionalcontent.PDOptionalContentGroup;
 import org.apache.pdfbox.pdmodel.graphics.optionalcontent.PDOptionalContentProperties;
 import org.apache.pdfbox.pdmodel.graphics.state.PDExtendedGraphicsState;
@@ -44,6 +51,8 @@ import org.apache.pdfbox.pdmodel.interactive.annotation.AnnotationFilter;
  */
 public class PDFRenderer
 {
+    private static final Logger LOG = LogManager.getLogger(PDFRenderer.class);
+
     protected final PDDocument document;
     // TODO keep rendering state such as caches here
     
@@ -556,9 +565,25 @@ public class PDFRenderer
 
     private boolean hasBlendMode(PDPage page)
     {
-        // check the current resources for blend modes
-        PDResources resources = page.getResources();
-        if (resources == null)
+        // check the page resources and the resources of nested form XObjects
+        return hasBlendMode(page.getResources(), new HashSet<>());
+    }
+
+    /**
+     * Checks whether the given resources, or the resources of any form XObject reachable from
+     * them, contain an extended graphics state with a blend mode other than Normal.
+     * Blend modes are often set inside form XObjects (e.g. artwork placed into a layout), so
+     * checking only the page level resources is not enough: a blend mode like ColorBurn or
+     * Multiply applied to an opaque white backdrop would make the content disappear.
+     *
+     * @param resources the resources to check, may be null.
+     * @param visited the resource dictionaries that have already been checked, to avoid endless
+     * recursion for self-referencing forms.
+     * @return true if a blend mode other than Normal was found.
+     */
+    private boolean hasBlendMode(PDResources resources, Set<COSDictionary> visited)
+    {
+        if (resources == null || !visited.add(resources.getCOSObject()))
         {
             return false;
         }
@@ -574,6 +599,30 @@ public class PDFRenderer
                 {
                     return true;
                 }
+            }
+        }
+        for (COSName name : resources.getXObjectNames())
+        {
+            if (resources.isImageXObject(name))
+            {
+                // avoid creating image XObjects, they can't have resources
+                continue;
+            }
+            PDXObject xObject;
+            try
+            {
+                xObject = resources.getXObject(name);
+            }
+            catch (IOException ex)
+            {
+                // broken XObject, will be reported again when the page is drawn
+                LOG.debug("Can't check XObject {} for blend modes", name, ex);
+                continue;
+            }
+            if (xObject instanceof PDFormXObject
+                    && hasBlendMode(((PDFormXObject) xObject).getResources(), visited))
+            {
+                return true;
             }
         }
         return false;
