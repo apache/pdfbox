19237f92d69b3a394d53def28b071fddf3da4dd2
Tilman Hausherr <tilman@apache.org>
2026-09-15 12:00:07 +0000

    PDFBOX-6265: protect against extreme decodeParms values (rest)
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1938245 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/filter/Predictor.java
+++ pdfbox/src/main/java/org/apache/pdfbox/filter/Predictor.java
@@ -291,6 +291,23 @@ public final class Predictor
             }
             // PDFBOX-6265: prevent OOM with extreme values
             long maxRowLength = 10_000_000L;
+            String sysProp = System.getProperty(Filter.SYSPROP_PREDICTOR_MAX_ROW_LENGTH);
+            if (sysProp != null)
+            {
+                try
+                {
+                    long parsed = Long.parseLong(sysProp);
+                    if (parsed > 0)
+                    {
+                        maxRowLength = parsed;
+                    }
+                    // else ignore zero/negative values
+                }
+                catch (NumberFormatException e)
+                {
+                    // ignore invalid value, keep default
+                }
+            }
             if (rowLength > maxRowLength)
             {
                 String msg =
