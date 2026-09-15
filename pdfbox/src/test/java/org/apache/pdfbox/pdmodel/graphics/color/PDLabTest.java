2f9ce2d21bffebf24793f79fa6b40a54bc2ac366
Tilman Hausherr <tilman@apache.org>
2026-09-15 06:43:25 +0000

    PDFBOX-6264: improve clamp test; add toRGB() test by ChatGPT
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1938227 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/test/java/org/apache/pdfbox/pdmodel/graphics/color/PDLabTest.java
+++ pdfbox/src/test/java/org/apache/pdfbox/pdmodel/graphics/color/PDLabTest.java
@@ -15,6 +15,8 @@
  */
 package org.apache.pdfbox.pdmodel.graphics.color;
 
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -97,6 +99,12 @@ class PDLabTest
     void testClamp()
     {
         PDLab lab = new PDLab();
+        assertEquals(-100, lab.getARange().getMin(), 0f);
+        assertEquals(100, lab.getARange().getMax(), 0f);
+        assertEquals(-100, lab.getBRange().getMin(), 0f);
+        assertEquals(100, lab.getBRange().getMax(), 0f);
+        assertEquals("[0.0, 100.0, -100.0, 100.0, -100.0, 100.0]", 
+                Arrays.toString(lab.getDefaultDecode(1)));
         PDRange aRange = new PDRange();
         aRange.setMin(-160);
         aRange.setMax(160);
@@ -120,5 +128,31 @@ class PDLabTest
         assertEquals("[0.0, 0.0, 0.0]", Arrays.toString(lab3));
         assertEquals("[100.0, 160.0, 160.0]", Arrays.toString(lab4));
         assertEquals("[100.0, 160.0, 160.0]", Arrays.toString(lab5));
+    }
+
+    @Test
+    void testToRGB() throws IOException
+    {
+        // Author: ChatGPT
+        PDLab lab = new PDLab();
+
+        // PDF Lab values: L* is 0..100, a* and b* are -100..100.
+        float[] black = lab.toRGB(new float[] { 0, 0, 0 });
+        float[] white = lab.toRGB(new float[] { 100, 0, 0 });
+        float[] red = lab.toRGB(new float[] { 53, 80, 67 });
+
+        // Black and white should be at the ends of the RGB range.
+        assertArrayEquals(new float[] { 0, 0, 0 }, black, 0.01f);
+        assertArrayEquals(new float[] { 1, 1, 1 }, white, 0.02f);
+
+        // Red should have a higher red component than green and blue.
+        assertTrue(red[0] > red[1]);
+        assertTrue(red[0] > red[2]);
+
+        // All RGB components must be in the valid range.
+        for (float component : red)
+        {
+            assertTrue(component >= 0 && component <= 1);
+        }
     }
 }
