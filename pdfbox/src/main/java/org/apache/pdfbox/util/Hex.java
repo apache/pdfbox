d7a6aa6f294e63fa275e1cb7bf2af799dc61f05a
Andreas Lehmkühler <lehmi@apache.org>
2026-09-26 11:06:57 +0000

    PDFBOX-5660: sonar fix, add test
    git-svn-id: https://svn.apache.org/repos/asf/pdfbox/trunk@1938532 13f79535-47bb-0310-9956-ffa450edef68

--- pdfbox/src/main/java/org/apache/pdfbox/util/Hex.java
+++ pdfbox/src/main/java/org/apache/pdfbox/util/Hex.java
@@ -134,8 +134,8 @@ public final class Hex
         // Note that the internal representation of string in Java is already UTF-16. Therefore
         // we do not need to use an encoder to convert the string to its byte representation.
         char[] hex = new char[text.length()*4];
-
-        for (int stringIdx = 0, charIdx = 0; stringIdx < text.length(); stringIdx++)
+        int charIdx = 0;
+        for (int stringIdx = 0; stringIdx < text.length(); stringIdx++)
         {
             char c = text.charAt(stringIdx);
             hex[charIdx++] = HEX_CHARS[(c >> 12) & 0x0F];
